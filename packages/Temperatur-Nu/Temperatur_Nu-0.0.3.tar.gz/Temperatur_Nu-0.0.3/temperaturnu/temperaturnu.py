44e9646e00cf6bc17a3eb8987d6e1fefc971290b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 15:03:40 +0000

    Add package Temperatur-Nu/0.0.3/Temperatur_Nu-0.0.3.tar.gz

---
+++ packages/Temperatur-Nu/Temperatur_Nu-0.0.3.tar.gz/Temperatur_Nu-0.0.3/temperaturnu/temperaturnu.py
@@ -0,0 +1,22 @@
+import requests
+
+class TemperaturNu:
+
+    async def get_temp(self, station):
+        url = "http://api.temperatur.nu/tnu_1.17.php?p=" + station + "&cli=popeensPyPi"
+        async with requests.get(url) as resp:
+            data = await resp.json()
+            if len(data) > 0:
+                return data['stations'][0]['temp']
+            else:
+                return None
+
+    async def set_temp(self, token, temp):
+        
+        url="http://www.temperatur.nu/rapportera.php?hash=" + token + "&t=" + str(temp)
+        async with requests.get(url) as resp:
+            body = await resp.content
+            if str("ok!") in str(body):
+                return True
+            else:
+                return False
