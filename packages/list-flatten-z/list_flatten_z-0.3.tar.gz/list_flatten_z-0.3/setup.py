2e8ac79310737115a526933d5346ff6b31ec94e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 15:03:18 +0000

    Add package list-flatten-z/0.3/list_flatten_z-0.3.tar.gz

---
+++ packages/list-flatten-z/list_flatten_z-0.3.tar.gz/list_flatten_z-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+import os
+setup(
+    name='list_flatten_z',
+    version='0.3',
+    packages=find_packages(),
+    author='Neu Basis',
+    author_email='streamingandflow@outlook.com',
+    description='A package for flattening nested lists',
+    url='https://github.com/haoyidaguo/list_flatten_z',
+)
