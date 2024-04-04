3b009456c1e5726e0ee317c60d04783e18c5dd7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 15:03:15 +0000

    Add package qapp-common/0.0.7/qapp-common-0.0.7.tar.gz

---
+++ packages/qapp-common/qapp-common-0.0.7.tar.gz/qapp-common-0.0.7/qapp_common/util/circuit_utils.py
@@ -0,0 +1,26 @@
+"""
+    QApp Platform Project circuit_utils.py Copyright © CITYNOW Co. Ltd. All rights reserved.
+"""
+
+
+class CircuitUtils:
+
+    @staticmethod
+    def get_depth(circuit):
+        """
+
+        @param circuit:
+        @return:
+        """
+
+        raise NotImplemented('[CircuitUtils] get_depth() method must be implemented')
+
+    @staticmethod
+    def get_qubit_amount(circuit):
+        """
+
+        @param circuit:
+        @return:
+        """
+
+        raise NotImplemented('[CircuitUtils] get_qubit_amount() method must be implemented')
