db9643ea99658da6898b713d2cfd6cf5ab59f533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:03:32 +0000

    Add package machinable/4.10.2/machinable-4.10.2-py3-none-any.whl

---
+++ packages/machinable/machinable-4.10.2-py3-none-any.whl/machinable/cli.py
@@ -0,0 +1,159 @@
+from typing import TYPE_CHECKING, Dict, List, Optional, Tuple
+
+import sys
+
+from machinable.project import Project
+from omegaconf import OmegaConf
+
+if TYPE_CHECKING:
+    from machinable.types import ElementType, VersionType
+
+
+def parse(args: List) -> tuple:
+    kwargs = []
+    methods = []
+    elements = []
+    dotlist = []
+    version = []
+
+    def _push(_elements, _dotlist, _version):
+        if len(dotlist) > 0:
+            _version.append(
+                OmegaConf.to_container(OmegaConf.from_dotlist(_dotlist))
+            )
+
+        if len(_version) > 0:
+            if len(_elements) > 0:
+                _elements[-1].extend(_version)
+            else:
+                _elements.append(_version)
+
+    for arg in args:
+        if arg.startswith("~"):
+            # version
+            if len(dotlist) > 0:
+                # parse preceding dotlist
+                version.append(
+                    OmegaConf.to_container(OmegaConf.from_dotlist(dotlist))
+                )
+                dotlist = []
+            version.append(arg)
+        elif arg.startswith("**kwargs="):
+            kwargs.append(
+                OmegaConf.to_container(OmegaConf.from_dotlist([arg[2:]]))[
+                    "kwargs"
+                ]
+            )
+        elif arg.startswith("--"):
+            # method
+            methods.append((len(elements), arg[2:]))
+        elif "=" in arg:
+            # dotlist
+            dotlist.append(arg)
+        else:
+            # module
+            _push(elements, dotlist, version)
+            dotlist = []
+            version = []
+            # auto-complete `.project` -> `interface.project`
+            if arg.startswith("."):
+                arg = "interface" + arg
+            elements.append([arg])
+            if len(elements) - 1 > len(kwargs):
+                kwargs.append({})
+            if len(elements) - 1 != len(kwargs):
+                raise ValueError(f"Multiple **kwargs for {arg}")
+
+    _push(elements, dotlist, version)
+    if len(elements) > len(kwargs):
+        kwargs.append({})
+    if len(elements) != len(kwargs):
+        raise ValueError(f"Multiple **kwargs for last interface")
+    return elements, kwargs, methods
+
+
+def from_cli(args: Optional[List] = None) -> "VersionType":
+    if args is None:
+        args = sys.argv[1:]
+
+    elements, _, _ = parse(args)
+
+    return sum(elements, [])
+
+
+def main(args: Optional[List] = None):
+    import machinable
+
+    if args is None:
+        args = Project.get().provider().on_parse_cli()
+        if isinstance(args, int):
+            # user implemented CLI, forward exit code
+            return args
+
+    if len(args) == 0:
+        print("\nhelp")
+        print("\nversion")
+        print("\nget")
+        return 0
+
+    action, args = args[0], args[1:]
+
+    if action == "help":
+        h = "get"
+        if len(args) > 0:
+            h = args[0]
+
+        if h == "get":
+            print("\nmachinable get [element_module...] [version...] --method")
+            print("\nExample:")
+            print(
+                "\tmachinable get my_component ~ver arg=1 nested.arg=2 --launch\n"
+            )
+            return 0
+        elif h == "version":
+            print("\nmachinable version")
+            return 0
+        else:
+            print("Unrecognized option")
+            return 128
+
+    if action == "version":
+        version = machinable.get_version()
+        print(version)
+        return 0
+
+    if action.startswith("get"):
+        get = machinable.get
+        if action != "get":
+            get = getattr(get, action.split(".")[-1])
+
+        elements, kwargs, methods = parse(args)
+        contexts = []
+        component = None
+        for i, (module, *version) in enumerate(elements):
+            element = get(module, version, **kwargs[i])
+            if i == len(elements) - 1:
+                component = element
+            else:
+                contexts.append(element.__enter__())
+
+        if component is None:
+            raise ValueError("You have to provide at least one interface")
+
+        for i, method in methods:
+            # check if cli_{method} exists before falling back on {method}
+            target = getattr(
+                component, f"cli_{method}", getattr(component, method)
+            )
+            if callable(target):
+                target()
+            else:
+                print(target)
+
+        for context in reversed(contexts):
+            context.__exit__()
+
+        return 0
+
+    print("Invalid argument")
+    return 128
---
+++ packages/machinable/machinable-4.10.2-py3-none-any.whl/machinable/query.py
@@ -0,0 +1,104 @@
+from machinable.collection import InterfaceCollection
+from machinable.element import extend, normversion
+from machinable.interface import Interface
+from machinable.types import Optional, Union, VersionType
+
+
+class Query:
+    def by_id(self, uuid: str) -> Optional[Interface]:
+        return Interface.find_by_id(uuid)
+
+    def from_directory(self, directory: str) -> Interface:
+        return Interface.from_directory(directory)
+
+    def __call__(
+        self,
+        module: Union[str, Interface, None] = None,
+        version: VersionType = None,
+        **kwargs,
+    ) -> Interface:
+        module, version = extend(module, version)
+        return Interface.get(module, version, **kwargs)
+
+    # modifiers
+
+    def all(
+        self,
+        module: Union[None, str, Interface] = None,
+        version: VersionType = None,
+        **kwargs,
+    ) -> "InterfaceCollection":
+        module, version = extend(module, version)
+        return Interface.find(module, version, **kwargs)
+
+    def new(
+        self,
+        module: Union[None, str, Interface] = None,
+        version: VersionType = None,
+        **kwargs,
+    ) -> Interface:
+        module, version = extend(module, version)
+        return Interface.make(module, version, **kwargs)
+
+    def or_none(
+        self,
+        module: Union[None, str, Interface] = None,
+        version: VersionType = None,
+        **kwargs,
+    ) -> Optional[Interface]:
+        module, version = extend(module, version)
+        existing = Interface.find(module, version, **kwargs)
+        if existing:
+            return existing[-1]
+
+        return None
+
+    def or_fail(
+        self,
+        module: Union[None, str, Interface] = None,
+        version: VersionType = None,
+        **kwargs,
+    ) -> Interface:
+        module, version = extend(module, version)
+        existing = Interface.find(module, version, **kwargs)
+        if existing:
+            return existing[-1]
+
+        raise ValueError(
+            f"Could not find {module}{normversion(version)} ({kwargs})"
+        )
+
+    def cached_or_fail(
+        self,
+        module: Union[None, str, Interface] = None,
+        version: VersionType = None,
+        **kwargs,
+    ) -> Interface:
+        module, version = extend(module, version)
+        existing = Interface.find(module, version, **kwargs)
+        if existing:
+            for candidate in existing:
+                if not hasattr(candidate, "cached"):
+                    continue
+                if candidate.cached():
+                    return candidate
+
+        raise ValueError(
+            f"Could not find {module}{normversion(version)} ({kwargs})"
+        )
+
+    def prefer_cached(
+        self,
+        module: Union[None, str, Interface] = None,
+        version: VersionType = None,
+        **kwargs,
+    ) -> Optional[Interface]:
+        module, version = extend(module, version)
+        existing = Interface.find(module, version, **kwargs)
+        if existing:
+            for i in range(len(existing) - 1):
+                if existing[i].cached():
+                    return existing[i]
+            return existing[-1]
+
+        return Interface.make(module, version, **kwargs)
