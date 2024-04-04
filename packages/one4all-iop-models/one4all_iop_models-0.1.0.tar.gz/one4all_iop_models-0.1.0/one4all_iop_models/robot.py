3c92d9cdf93c902de573f648c0ca768d2f7f7c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 15:03:19 +0000

    Add package one4all-iop-models/0.1.0/one4all_iop_models-0.1.0.tar.gz

---
+++ packages/one4all-iop-models/one4all_iop_models-0.1.0.tar.gz/one4all_iop_models-0.1.0/one4all_iop_models/robot.py
@@ -0,0 +1,89 @@
+import pandas as pd
+from pydantic import BaseModel, parse_obj_as, Field
+from typing import List, Union
+from datetime import datetime
+
+
+class PositionUpdates (BaseModel):
+    #timestamp: datetime
+    PoseWithCovariance_PositionX: float
+    PoseWithCovariance_PositionY: float
+    PoseWithCovariance_PositionZ: float
+    PoseWithCovariance_OrientationX: float
+    PoseWithCovariance_OrientationY: float
+    PoseWithCovariance_OrientationZ: float
+    PoseWithCovariance_OrientationW: float
+    Odom_Pose_PositionX: float
+    Odom_Pose_PositionY: float
+    Odom_Pose_PositionZ: float
+    Odom_Pose_OrientationX: float
+    Odom_Pose_OrientationY: float
+    Odom_Pose_OrientationZ: float
+    Odom_Pose_OrientationW: float
+    Odom_Twist_LinearX: float
+    Odom_Twist_LinearY: float
+    Odom_Twist_LinearZ: float
+    Odom_Twist_AngularX: float
+    Odom_Twist_AngularY: float
+    Odom_Twist_AngularZ: float
+    Path_PositionX: List[float]
+    Path_PositionY: List[float]
+    Path_PositionZ: List[float]
+    Path_OrientationX: List[float]
+    Path_OrientationY: List[float]
+    Path_OrientationZ: List[float]
+    Path_OrientationW: List[float]
+
+    @classmethod
+    def from_dataframe(cls, df: pd.DataFrame):
+        parsed_data = df.to_dict(orient='records')
+        return parse_obj_as(List[PositionUpdates], parsed_data)
+    
+
+class RobotActionBase(BaseModel):
+    name: str
+
+class AWCombo(RobotActionBase):
+    x: float = Field(ge=-5, le=5)
+    y: float = Field(ge=-5, le=5)
+    angle: float = Field(ge=0, le=360)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = "awcombo"
+
+class Joint(RobotActionBase):
+    j1: float = Field(ge=-1, le=1)
+    j2: float = Field(ge=-1, le=1)
+    j3: float = Field(ge=-1, le=1)
+    j4: float = Field(ge=-1, le=1)
+    j5: float = Field(ge=-1, le=1)
+    j6: float = Field(ge=-1, le=1)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = "joint"
+
+class Carte(RobotActionBase):
+    x: float = Field(ge=-1, le=1)
+    y: float = Field(ge=-1, le=1)
+    z: float = Field(ge=-1, le=1)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = "carte"
+
+class Increm(RobotActionBase):
+    axis: str = Field(choices=["x","y","z"])
+    delta: float
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = "increm"
+
+RobotActionsTypes = Union[Increm,AWCombo,Carte]
+
+class SubTask(BaseModel):
+    actions: List[RobotActionsTypes]
+
+
