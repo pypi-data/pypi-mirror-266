921a5fe0bb34f7fc98622077afc20f301b16df1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 15:04:09 +0000

    Add package commons_lib/1.1.3/commons_lib-1.1.3-py3-none-any.whl

---
+++ packages/commons_lib/commons_lib-1.1.3-py3-none-any.whl/commons_lib/models/entities/accounts/account_category_entity.py
@@ -0,0 +1,16 @@
+from sqlalchemy import VARCHAR, Column
+from sqlalchemy.orm import relationship
+from sqlalchemy import BIGINT, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+
+from commons_lib.models.entities.base_entity import BaseEntity
+
+
+class AccountCategoryEntity(BaseEntity):
+
+    __tablename__ = "accounts_type"
+
+    title: str = Column(VARCHAR(256), nullable=False)
+    slug: str = Column(VARCHAR(512), nullable=True)
+    accounts = relationship(argument='AccountEntity', back_populates="account_type")
+    organization_uuid = Column(UUID(as_uuid=True), ForeignKey("company.pk_uuid"))
---
+++ packages/commons_lib/commons_lib-1.1.3-py3-none-any.whl/commons_lib/models/entities/gift/vended_winning_entity.py
@@ -0,0 +1,20 @@
+from sqlalchemy import VARCHAR, Integer
+from sqlalchemy import Column, BIGINT, ForeignKey
+from sqlalchemy.orm import relationship, Mapped
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+from commons_lib.models.entities.base_entity import BaseEntity
+
+
+class VendedWinningEntity(BaseEntity):
+    __tablename__ = "vended_winnings"
+
+    pk_uuid = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    title = Column(VARCHAR(255), nullable=False)
+    image = Column(VARCHAR(512), nullable=True)
+    description = Column(VARCHAR(1000))
+    vendor_uuid = Column(BIGINT, ForeignKey("winning_vendors.pk_uuid"))
+    # winning: Mapped['WinningEntity'] = relationship("WinningEntity", back_populates="vended_winning")
+
+
