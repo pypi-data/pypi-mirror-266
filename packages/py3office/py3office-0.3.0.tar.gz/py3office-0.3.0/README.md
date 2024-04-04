2b9333f20865ca031cb64126603ad4fdfa654577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:03:17 +0000

    Add package py3office/0.3.0/py3office-0.3.0.tar.gz

---
+++ packages/py3office/py3office-0.3.0.tar.gz/py3office-0.3.0/README.md
@@ -0,0 +1,30 @@
+<img src="./logo.jpeg" />
+
+# [py3office](https://github.com/zxl20070701/py3office)
+⏰ 提供python3版本的自动化办公常用处理方法
+
+<p>
+    <a href="https://github.com/zxl20070701/py3office" target='_blank'>
+        <img alt="GitHub repo stars" src="https://img.shields.io/github/stars/zxl20070701/py3office?style=social">
+    </a>
+</p>
+
+## 使用
+
+首先需要进行安装：
+
+```
+pip3 install py3office
+```
+
+然后就可以使用了，下面列出所有可用内容：
+
+- [handler 基本辅助相关](./docs/handler.md)
+- [PDF文件相关操作](./docs/pdf.md)
+- [OCR相关操作](./docs/ocr.md)
+
+## 版权
+
+MIT License
+
+Copyright (c) [zxl20070701](https://zxl20070701.github.io/notebook/home.html) 走一步，再走一步
---
+++ packages/py3office/py3office-0.3.0.tar.gz/py3office-0.3.0/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+readmeFile = open("README.md", "r", encoding="utf-8")
+long_description = readmeFile.read().replace(
+    "./docs/", "https://github.com/zxl20070701/py3office/blob/master/docs/"
+)
+readmeFile.close()
+
+# https://setuptools.pypa.io/en/latest/setuptools.html
+
+setup(
+    name="py3office",
+    version="0.3.0",
+    description="提供python3版本的自动化办公常用处理方法",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="zxl20070701",
+    author_email="1904314465@qq.com",
+    maintainer="zxl20070701",
+    maintainer_email="1904314465@qq.com",
+    package_dir={"": "src"},
+    packages=["py3office"],
+    url="https://github.com/zxl20070701/py3office",
+    license="MIT",
+    keywords=["pdf", "office", "python3"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "PyMuPDF>=1.23.16",
+        "pdf2docx>=0.5.8",
+        "cnocr>=2.3.0.1",
+        "onnxruntime>=1.17.1",
+    ],
+)
