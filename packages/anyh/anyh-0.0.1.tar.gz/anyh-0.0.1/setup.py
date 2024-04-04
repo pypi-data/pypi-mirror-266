20493e2c252102857003e1f41802998c2a617da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 15:04:37 +0000

    Add package anyh/0.0.1/anyh-0.0.1.tar.gz

---
+++ packages/anyh/anyh-0.0.1.tar.gz/anyh-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="ShareClass",
+    version="1.0",
+    author="Beall",
+    author_email="beallhuang@163.com",
+    description="",
+    long_description="ShareClass",
+    long_description_content_type="ShareClass",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
