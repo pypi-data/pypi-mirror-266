4f3eef6890c01461070e8e959af6e4da16fdf126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:03:04 +0000

    Add package autopipeline/0.1.280/autopipeline-0.1.280-py3-none-any.whl

---
+++ packages/autopipeline/autopipeline-0.1.280-py3-none-any.whl/autopipeline/data/__init__.py
@@ -0,0 +1,78 @@
+import pkg_resources
+import csv
+import pandas as pd
+
+def get_persuasion_effect_data():
+    data_file_path = pkg_resources.resource_filename('autopipeline', 'data/persuasive-17.csv')
+    df = pd.read_csv(data_file_path)
+    df = df[['sentence']]
+    return df
+
+def get_toxic_data():
+    data_file_path = pkg_resources.resource_filename('autopipeline', 'data/toxic.csv')
+    df = pd.read_csv(data_file_path)
+    df = df[['original_sentence']]
+    return df
+
+def get_dog_whistle_data():
+    data_file_path = pkg_resources.resource_filename('autopipeline', 'data/dogwhistle.tsv')
+    df = pd.read_csv(data_file_path, on_bad_lines='skip', delimiter='\t')
+    df = df[['Linguistic Context']]
+    return df
+
+def get_ner_data():
+    data_file_path = pkg_resources.resource_filename('autopipeline', 'data/ner.csv')
+    df = pd.read_csv(data_file_path)
+    return df
+
+def get_pos_data():
+    data_file_path = pkg_resources.resource_filename('autopipeline', 'data/ner.csv')
+    df = pd.read_csv(data_file_path)
+    return df
+
+def get_case_data():
+    data_file_path = pkg_resources.resource_filename('autopipeline', 'data/ner.csv')
+    df = pd.read_csv(data_file_path)
+    return df
+
+def get_pdf_doc():
+    data_file_path = pkg_resources.resource_filename('autopipeline', 'data/legal-doc.csv')
+    df = pd.read_csv(data_file_path)
+    return df
+
+class QUIET_ML:
+    def __init__(self):
+        # Initialize if needed
+        pass
+    
+    def queries(self, qid):
+        return
+    def query_text(self, qid):
+        return
+
+    def query_data(self, qid):
+        data_file_path = pkg_resources.resource_filename('autopipeline', f'data/{qid}/data.csv')
+        df = pd.read_csv(data_file_path)
+        return df
+    
+    def query_answer(self, qid):
+        # data_file_path = pkg_resources.resource_filename('autopipeline', f'data/{qid}/answer.csv')
+        # df = pd.read_csv(data_file_path)
+        package_name = 'autopipeline'
+        directory = f'data/{qid}'
+        dir_path = pkg_resources.files(package_name).joinpath(directory)
+        all_files = [f for f in dir_path.iterdir() if f.name.startswith('answer') and f.name.endswith('.csv')]
+        return all_files
+
+    def query(self, qid):
+        # Function to load query, data, and answer altogether
+        query = self.load_query(qid)
+        data = self.load_data(qid)
+        answer = self.load_answer(qid)
+        return {
+            "query": query,
+            "data": data,
+            "answer": answer
+        }
+
+
