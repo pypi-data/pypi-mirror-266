abb9f733987b7c1a6e8faece6d5327358442c9cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:02:57 +0000

    Add package skdir/0.1.4/skdir-0.1.4.tar.gz

---
+++ packages/skdir/skdir-0.1.4.tar.gz/skdir-0.1.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.1.4'
+DESCRIPTION = 'skdir'
+LONG_DESCRIPTION = 'A package to perform 5th sem AIML lab'
+
+# Setting up
+setup(
+    name="skdir",
+    version=VERSION,
+    author="Sk",
+    author_email="karnamsharath3@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy>=1.21.0',
+        'scikit-learn>=0.24.0',
+        'matplotlib>=3.4.0',],
+    keywords=['arithmetic', 'math', 'mathematics', 'python tutorial', 'avi upadhyay'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
