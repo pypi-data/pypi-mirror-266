661e91f67ba0cc8124c7a30f576a3d3e19f997c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 15:03:40 +0000

    Add package CoolAMQP/1.3.0/CoolAMQP-1.3.0.tar.gz

---
+++ packages/CoolAMQP/CoolAMQP-1.3.0.tar.gz/CoolAMQP-1.3.0/CHANGELOG.md
@@ -0,0 +1,7 @@
+The changelog is kept as [release notes](https://github.com/smok-serwis/coolamqp/releases/)
+on GitHub. This file serves to only note what changes
+have been made so far, between releases.
+
+# v1.3.0
+
+* updated to use latest RabbitMQ AMQP 0-9-1 definition from the RabbitMQ's website
---
+++ packages/CoolAMQP/CoolAMQP-1.3.0.tar.gz/CoolAMQP-1.3.0/tests/test_exceptions.py
@@ -0,0 +1,24 @@
+# coding=UTF-8
+from __future__ import print_function, absolute_import, division
+
+import unittest
+
+from coolamqp.exceptions import AMQPError
+
+
+class TestExcs(unittest.TestCase):
+    def test_exist(self):
+        e = AMQPError(100, u'wtf', 0, 0)
+
+        self.assertTrue(u'100' in str(e))
+        self.assertTrue(u'wtf' in str(e))
+        self.assertTrue(repr(e).startswith(u'AMQPError'))
+
+    def test_parses_memoryview_correctly(self):
+        e = AMQPError(100, memoryview(u'wtf'.encode('utf8')), 0, 0)
+        self.assertIn('wtf', str(e))
+        self.assertIn('wtf', repr(e))
+
+        e = AMQPError(100, u'wtf'.encode('utf8'), 0, 0)
+        self.assertIn('wtf', str(e))
+        self.assertIn('wtf', repr(e))
