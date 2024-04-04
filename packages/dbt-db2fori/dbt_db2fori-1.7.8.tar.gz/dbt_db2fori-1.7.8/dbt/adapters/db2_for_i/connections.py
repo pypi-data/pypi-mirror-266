f89c9409c958408b79f545b4e6b63053e6b7d0c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:03:23 +0000

    Add package dbt-db2fori/1.7.8/dbt_db2fori-1.7.8.tar.gz

---
+++ packages/dbt-db2fori/dbt_db2fori-1.7.8.tar.gz/dbt_db2fori-1.7.8/dbt/adapters/db2_for_i/connections.py
@@ -0,0 +1,186 @@
+from contextlib import contextmanager
+import pyodbc
+import os
+import time
+from typing import Optional, Tuple, Any, Union
+import agate
+
+import dbt.exceptions
+from dbt.adapters.base import Credentials
+from dbt.adapters.sql import SQLConnectionManager
+from dbt.adapters.db2_for_i import __version__
+from dbt.contracts.connection import AdapterResponse
+
+from dataclasses import dataclass
+from dbt.events import AdapterLogger
+logger = AdapterLogger('Db2Fori')
+
+
+@dataclass
+class DB2ForICredentials(Credentials):
+
+    driver: str
+    system: str
+    database: str
+
+    UID: Optional[str] = None
+    PWD: Optional[str] = None
+    NAM: Optional[int] = 0
+
+
+    _ALIASES = {
+        "user": "UID",
+        "username": "UID",
+        "pass": "PWD",
+        "password": "PWD",
+        "naming": "NAM",
+        "library": "schema"
+    }
+
+    @property
+    def type(self) -> str:
+        return 'db2_for_i'
+
+    
+    @property
+    def unique_field(self) -> str:
+        return self.UID
+
+
+    def _connection_keys(self) -> Tuple[str, ...]:
+        return ('driver', 'database', 'NAM', 'UID', 'schema')
+
+    
+class DB2ForIConnectionManager(SQLConnectionManager):
+    TYPE = 'db2_for_i'
+
+    @contextmanager
+    def exception_handler(self, sql: str):
+        try:
+            yield
+        except pyodbc.DatabaseError as e:
+            self.release()
+            logger.debug(f'IBM Db2 error: {str(e)}')
+            logger.debug(f'Error running SQL: {str(sql)}')
+            raise dbt.exceptions.DbtDatabaseError(str(e))
+        except Exception as e:
+            self.release()
+            logger.debug(f"Error running SQL: {sql}")
+            logger.debug("Rolling back transaction.")
+            raise dbt.exceptions.DbtRuntimeError(str(e))
+
+
+    @classmethod
+    def open(cls, connection):
+        if connection.state == "open":
+            logger.debug("Connection is already open, skipping open.")
+            return connection
+
+        credentials = connection.credentials
+
+        try:
+            con_str = []
+            con_str.append(f"DRIVER={{{credentials.driver}}}")
+            con_str.append(f"SYSTEM={credentials.system}")
+            con_str.append(f"DATABASE={credentials.database}")
+            con_str.append(f"NAM={credentials.NAM}")
+            con_str.append(f"UID={credentials.UID}")
+            con_str.append(f"PWD={credentials.PWD}")
+
+            con_str_concat = ';'.join(con_str)
+
+            index = []
+            for i, elem in enumerate(con_str):
+                if 'pwd=' in elem.lower():
+                    index.append(i)
+
+            if len(index) !=0 :
+                con_str[index[0]]="PWD=****"
+
+            con_str_display = ';'.join(con_str)
+
+            logger.debug(f"Using connection string: {con_str_display}")
+
+            handle = pyodbc.connect(
+                con_str_concat,
+                autocommit=True
+            )
+
+            connection.state = "open"
+            connection.handle = handle
+            logger.debug(f"Connected to db: {credentials.database}")
+
+        except pyodbc.Error as e:
+            logger.debug(f"Could not connect to db: {e}")
+
+            connection.handle = None
+            connection.state = "fail"
+
+            raise dbt.exceptions.FailedToConnectError(str(e))
+
+        return connection
+
+
+    def cancel(self, connection):
+        connection_name = connection.name
+        logger.debug("Cancel query")
+        try:
+            connection.handle.close()
+        except Exception as e:
+            logger.error("Error closing connection for cancel request")
+            raise Exception(str(e))
+
+    def add_begin_query(self):
+        pass
+
+    def add_commit_query(self):
+        pass
+
+    def add_query(self, sql: str, auto_begin: bool = True, bindings: Optional[Any] = None, abridge_sql_log: bool = False):
+        connection = self.get_thread_connection()
+
+        if auto_begin and connection.transaction_open is False:
+            self.begin()
+
+        logger.debug('Using {} connection "{}".'.format(self.TYPE, connection.name))
+
+        with self.exception_handler(sql):
+            if abridge_sql_log:
+                logger.debug("On {}: {}....".format(connection.name, sql[0:512]))
+            else:
+                logger.debug("On {}: {}".format(connection.name, sql))
+            pre = time.time()
+
+            cursor = connection.handle.cursor()
+
+            # pyodbc does not handle a None type binding!
+            if bindings is None:
+                cursor.execute(sql)
+            else:
+                cursor.execute(sql, bindings)
+
+            logger.debug(
+                "SQL status: {} in {:0.2f} seconds".format(
+                    self.get_response(cursor), (time.time() - pre)
+                )
+            )
+            
+            return connection, cursor
+
+    
+    @classmethod
+    def get_credentials(cls, credentials):
+        return credentials
+
+
+    @classmethod
+    def get_response(cls, cursor) -> AdapterResponse:
+        message = 'OK'
+        rows = cursor.rowcount
+
+        return AdapterResponse(
+            _message=message,
+            rows_affected=rows
+        )
+
+
---
+++ packages/dbt-db2fori/dbt_db2fori-1.7.8.tar.gz/dbt_db2fori-1.7.8/dbt/include/db2_for_i/macros/materializations/incremental/incremental.sql
@@ -0,0 +1,127 @@
+{% macro db2_for_i__validate_get_incremental_strategy(config) %}
+    {# -- Find and validate the incremental strategy  #}
+    {% set strategy = config.get("incremental_strategy", default="merge") %}
+
+    {% set invalid_strategy_msg -%}
+        Invalid incremental strategy provided: {{ strategy }}
+        Expected one of: 'merge', 'delete_insert', 'insert_overwrite'
+    {%- endset %}
+    {% if strategy not in ['merge', 'delete_insert', 'insert_overwrite'] %}
+        {% do exceptions.raise_compiler_error(invalid_strategy_msg) %}
+    {% endif %}
+
+    {% do return(strategy) %}
+{% endmacro %}
+
+
+
+
+{% macro db2_for_i__get_incremental_sql(strategy, strategy_arg_dict) %}
+    {% if strategy == 'merge' %}
+        {% do return(get_merge_sql(**strategy_arg_dict)) %}
+    {% elif strategy == 'delete_insert' %}
+        {% do return(get_delete_insert_merge_sql(**strategy_arg_dict)) %}
+    {% elif strategy == 'insert_overwrite' %}
+        {% do return(get_insert_overwrite_merge_sql(**strategy_arg_dict)) %}
+    {% else %}
+        {% do exceptions.raise_compiler_error('invalid strategy: ' ~ strategy) %}
+    {% endif %}
+{% endmacro %}
+
+
+
+
+{% materialization incremental, adapter='db2_for_i' -%}
+
+    -- relations
+    {%- set existing_relation = load_cached_relation(this) -%}
+    {%- set target_relation = this.incorporate(type='table') -%}
+    {%- set tmp_relation = make_temp_relation(target_relation) -%}
+    {%- set intermediate_relation = make_intermediate_relation(target_relation) -%}
+    {%- set backup_relation_type = 'table' if existing_relation is none else existing_relation.type -%}
+    {%- set backup_relation = make_backup_relation(target_relation, backup_relation_type) -%}
+
+    -- configs
+    {%- set unique_key = config.get('unique_key') -%}
+    {%- set full_refresh_mode = (should_full_refresh() or existing_relation.is_view) -%}
+    {%- set on_schema_change = incremental_validate_on_schema_change(config.get('on_schema_change'), default='ignore') -%}
+
+    -- validate incremental strategy
+    {%- set incremental_strategy = db2_for_i__validate_get_incremental_strategy(config) -%}
+
+    -- the temp_ and backup_ relations should not already exist in the database; get_relation will 
+    -- return None in that case. Otherwise, we get a relation that we can drop later, before we try 
+    -- to use this name for the current operation. This has to happen before BEGIN, in a separate transaction.
+    {%- set preexisting_intermediate_relation = load_cached_relation(intermediate_relation) -%}
+    {%- set preexisting_backup_relation = load_cached_relation(backup_relation) -%}
+
+    -- grab current tables grant configs for comparison later on
+    {%- set grant_config = config.get('grants') -%}
+    {{ drop_relation_if_exists(preexisting_intermediate_relation) }}
+    {{ drop_relation_if_exists(preexisting_backup_relation) }}
+
+    {{ run_hooks(pre_hooks, inside_transaction=False) }}
+
+    -- `BEGIN` happens here:
+    {{ run_hooks(pre_hooks, inside_transaction=True) }}
+
+    {% set to_drop = [] %}
+    {% if existing_relation is none %}
+        {% set build_sql = get_create_table_as_sql(False, target_relation, sql) %}
+    {% elif full_refresh_mode %}
+        {#-- Make sure the backup doesn't exist so we don't encounter issues with the rename below #}
+        {% set build_sql = get_create_table_as_sql(False, intermediate_relation, sql) %}
+        {% set need_swap = true %}
+    {% else %}
+        {% do adapter.drop_relation(temp_relation) %}
+        {% do run_query(get_create_table_as_sql(True, temp_relation, sql)) %}
+        {% do to_drop.append(temp_relation) %}
+        {% do adapter.expand_target_column_types(
+                from_relation=temp_relation,
+                to_relation=target_relation
+            ) 
+        %}
+        {% set dest_columns = process_schema_changes(on_schema_change, temp_relation, existing_relation) %}
+        {% if not dest_columns %}
+            {% set dest_columns = adapter.get_columns_in_relation(existing_relation) %}
+        {% endif %}
+        {% set incremental_predicates = config.get('incremental_predicates', none) %}
+        {% set strategy_arg_dict = ({'target': target_relation, 'source': temp_relation, 'unique_key': unique_key, 'dest_columns': dest_columns, 'predicates': incremental_predicates}) %}
+        {% set build_sql = db2_for_i__get_incremental_sql(incremental_strategy, strategy_arg_dict) %}
+        
+    {% endif %}
+    
+    {% call statement('main') %}
+        {{ build_sql }}
+    {% endcall %}
+
+    {% if need_swap %}
+        {% do adapter.rename_relation(target_relation, backup_relation) %}
+        {% do adapter.rename_relation(intermediate_relation, target_relation) %}
+        {% do to_drop.append(backup_relation) %}
+    {% endif %}
+
+    {% set should_revoke = should_revoke(existing_relation, full_refresh_mode) %}
+    {% do apply_grants(target_relation, grant_config, should_revoke=should_revoke) %}
+
+    {% do persist_docs(target_relation, model) %}
+
+    {% if existing_relation is none or existing_relation.is_view or should_full_refresh() %}
+        {% do create_indexes(target_relation) %}
+    {% endif %}
+
+    {{ run_hooks(post_hooks, inside_transaction=True) }}
+
+    -- `COMMIT` happens here
+    {% do adapter.commit() %}
+
+    {% for rel in to_drop %}
+        {{ log("Dropping relation " ~ rel) }}
+        {% do adapter.drop_relation(rel) %}
+    {% endfor %}
+
+    {{ run_hooks(post_hooks, inside_transaction=False) }}
+
+    {{ return({'relations': [target_relation]}) }}
+
+{%- endmaterialization %}
