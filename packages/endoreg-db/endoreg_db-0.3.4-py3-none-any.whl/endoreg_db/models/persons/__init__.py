89637d339d84f51c1fba2bb2a06c06508c36ab76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 15:04:18 +0000

    Add package endoreg-db/0.3.4/endoreg_db-0.3.4-py3-none-any.whl

---
+++ packages/endoreg-db/endoreg_db-0.3.4-py3-none-any.whl/endoreg_db/models/persons/__init__.py
@@ -0,0 +1,6 @@
+from .person import Person
+from .patient import Patient, PatientForm
+from .examiner import Examiner, ExaminerSerializer
+from .portal_user_information import PortalUserInfo, Profession
+from .first_name import FirstName
+from .last_name import LastName
