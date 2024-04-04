c74ffb3f2a19f2dfc13f6eeff5658fa1ce5b0fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 15:04:20 +0000

    Add package custom-cytomine-client/0.0.8/custom_cytomine_client-0.0.8.tar.gz

---
+++ packages/custom-cytomine-client/custom_cytomine_client-0.0.8.tar.gz/custom_cytomine_client-0.0.8/custom_cytomine_client/interface/converter.py
@@ -0,0 +1,22 @@
+from abc import ABCMeta, abstractmethod
+from typing import List
+
+from object.converter import CytomineProjectInfo, CytomineTermInfo
+
+
+class CustomCytomineClientInterface(metaclass=ABCMeta):
+    @abstractmethod
+    def save_annotation_from_xml(self, xml_path: str, project_name: str) -> bool:
+        pass
+
+    @abstractmethod
+    def create_term_from_xml_file(self, xml_file_path: str, ontology_name: str) -> bool:
+        pass
+
+    @abstractmethod
+    def find_project_info_with_name(self, name: str) -> CytomineProjectInfo:
+        pass
+
+    @abstractmethod
+    def find_terms_info_with_project_name(self, project_name: str) -> List[CytomineTermInfo]:
+        pass
