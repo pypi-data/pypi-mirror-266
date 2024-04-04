348c9c0c57fc28a2cd03eeaf86ee812be97abb50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 15:04:38 +0000

    Add package undertext/0.3.0/undertext-0.3.0.tar.gz

---
+++ packages/undertext/undertext-0.3.0.tar.gz/undertext-0.3.0/src/undertext/writers/subrip.py
@@ -0,0 +1,28 @@
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+import io
+import typing as t
+from os import PathLike
+from ..structures import Caption
+from ..util import format_ts_comma as format_ts
+
+
+def write_subrip(captions: t.Iterable[Caption], filename: t.Union[str, PathLike, t.TextIO]) -> None:
+    stream = io.StringIO()
+
+    for i, caption in enumerate(captions):
+        if i != 0:
+            stream.write("\r\n")
+
+        stream.write(f"{i+1}\r\n")
+        stream.write(f"{format_ts(caption.start)} --> {format_ts(caption.end)}\r\n")
+        for line in caption.lines:
+            stream.write(f"{line}\r\n")
+
+    if isinstance(filename, (str, PathLike)):
+        with open(filename, 'w') as file:
+            file.write(stream.getvalue())
+    else:
+        filename.write(stream.getvalue())
