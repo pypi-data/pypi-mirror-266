8268ddaba418870fdc735e60aefe5f96d76778d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:03:30 +0000

    Add package ip2vulns/1.0.6/ip2vulns-1.0.6-py3-none-any.whl

---
+++ packages/ip2vulns/ip2vulns-1.0.6-py3-none-any.whl/ip2vulns/ip2vulns.py
@@ -0,0 +1,23 @@
+from . import version
+from .Services import InternetDBService
+
+from .Utils import PipeUtils
+from .Utils import ArgUtils
+
+
+def main():
+    args = ArgUtils.init_argparse().parse_args()  # init argparse
+
+    if PipeUtils.has_pipe_data():  # read from pipe, enable internetdb by default
+        args.input = PipeUtils.read_from_pipe()
+    elif not any(vars(args).values()):  # check if argument is provided, if not, print help
+        args = ArgUtils.init_argparse().parse_args(["-h"])
+
+    if args.input:  # type(input) => list
+        InternetDBService.start(args.input, args.out, args.cvss, args.disable_stdout)
+    elif args.version:
+        print(version.__version__)
+
+
+if __name__ == "__main__":
+    main()
