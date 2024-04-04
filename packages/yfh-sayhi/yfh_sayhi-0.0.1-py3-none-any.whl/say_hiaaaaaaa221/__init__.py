8f3adf07375f5d6cc0c858d8ead4c03fabcabd9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:03:26 +0000

    Add package yfh-sayhi/0.0.1/yfh_sayhi-0.0.1-py3-none-any.whl

---
+++ packages/yfh-sayhi/yfh_sayhi-0.0.1-py3-none-any.whl/say_hiaaaaaaa221/__init__.py
@@ -0,0 +1,21 @@
+import xlwings as xw
+import pandas as pd
+import os
+import re
+import time
+
+
+class Tool:
+    def __init__(self):
+        pass
+
+    def run(self):
+
+
+def main():
+    tool = Tool()
+    tool.run()
+
+
+if __name__ == '__main__':
+    main()
