21907a2de6288fbc8964683707f786607c7ca2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:03:23 +0000

    Add package dbt-db2fori/1.7.8/dbt_db2fori-1.7.8-py3-none-any.whl

---
+++ packages/dbt-db2fori/dbt_db2fori-1.7.8-py3-none-any.whl/dbt/adapters/db2_for_i/relation.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass, field
+from dbt.adapters.base.relation import BaseRelation, Policy
+
+@dataclass
+class DB2ForIQuotePolicy(Policy):
+    database: bool = False
+    schema: bool = False
+    identifier: bool = False
+
+
+@dataclass
+class DB2ForIIncludePolicy(Policy):
+    database: bool = False
+    schema: bool = True
+    identifier: bool = True
+
+
+@dataclass(frozen=True, eq=False, repr=False)
+class DB2ForIRelation(BaseRelation):
+    quote_policy: DB2ForIQuotePolicy = field(default_factory = lambda: DB2ForIQuotePolicy())
+    include_policy: DB2ForIIncludePolicy = field(default_factory = lambda: DB2ForIIncludePolicy())
+
+    @staticmethod
+    def add_ephemeral_prefix(name: str):
+        return f'DBT_CTE__{name}'
