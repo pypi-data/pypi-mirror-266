51d0553303d03e0c6435f559e3d7915204015841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 15:03:21 +0000

    Add package openverse-api-client/0.0.1a1/openverse_api_client-0.0.1a1-py3-none-any.whl

---
+++ packages/openverse-api-client/openverse_api_client-0.0.1a1-py3-none-any.whl/openverse_api_client/__init__.py
@@ -0,0 +1,2 @@
+from openverse_api_client.async_client import *  # noqa: F403
+from openverse_api_client.sync_client import *  # noqa: F403
---
+++ packages/openverse-api-client/openverse_api_client-0.0.1a1-py3-none-any.whl/openverse_api_client/endpoints/__init__.py
@@ -0,0 +1,3 @@
+from openverse_api_client.endpoints.auth import *  # noqa: F403
+from openverse_api_client.endpoints.base import *  # noqa: F403
+from openverse_api_client.endpoints.media import *  # noqa: F403
