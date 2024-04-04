77a05e815f9ca366738e12179a260bc69f1cd371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:03:07 +0000

    Add package fovus/1.1.35/fovus-1.1.35-py3-none-any.whl

---
+++ packages/fovus/fovus-1.1.35-py3-none-any.whl/fovus/constants/util_constants.py
@@ -0,0 +1,8 @@
+from http import HTTPStatus
+
+USER_ERROR_PREFIX = "4"
+SERVER_ERROR_PREFIX = "5"
+
+SUCCESS_STATUS_CODES = [HTTPStatus.OK.value, HTTPStatus.CREATED.value, HTTPStatus.ACCEPTED.value]
+
+UTF8 = "utf-8"
