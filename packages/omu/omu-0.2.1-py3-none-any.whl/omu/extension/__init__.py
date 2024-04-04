e11eb090de087949da88995985314f7b26fe343b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:03:23 +0000

    Add package omu/0.2.1/omu-0.2.1-py3-none-any.whl

---
+++ packages/omu/omu-0.2.1-py3-none-any.whl/omu/extension/__init__.py
@@ -0,0 +1,9 @@
+from .extension import Extension, ExtensionType
+from .extension_manager import ExtensionManager
+
+__all__ = [
+    "Extension",
+    "ExtensionType",
+    "ExtensionManager",
+    "ExtensionManager",
+]
---
+++ packages/omu/omu-0.2.1-py3-none-any.whl/omu/extension/registry/__init__.py
@@ -0,0 +1,6 @@
+from .registry_extension import REGISTRY_EXTENSION_TYPE, RegistryExtension
+
+__all__ = [
+    "RegistryExtension",
+    "REGISTRY_EXTENSION_TYPE",
+]
