1b9bb96363ffded219a9d8f69cd0efa37bc7fcc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:03:12 +0000

    Add package psy-transect/0.1.0/psy_transect-0.1.0-py3-none-any.whl

---
+++ packages/psy-transect/psy_transect-0.1.0-py3-none-any.whl/psy_transect/__init__.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: 2021-2024 Helmholtz-Zentrum hereon GmbH
+#
+# SPDX-License-Identifier: LGPL-3.0-only
+
+"""psy-transect
+
+Psyplot plugin for visualizing data along a transect
+"""
+
+from __future__ import annotations
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
+
+__author__ = "Philipp S. Sommer"
+__copyright__ = "2021-2024 Helmholtz-Zentrum hereon GmbH"
+__credits__ = [
+    "Philipp S. Sommer",
+]
+__license__ = "LGPL-3.0-only"
+
+__maintainer__ = "Philipp S. Sommer"
+__email__ = "philipp.sommer@hereon.de"
+
+__status__ = "Pre-Alpha"
