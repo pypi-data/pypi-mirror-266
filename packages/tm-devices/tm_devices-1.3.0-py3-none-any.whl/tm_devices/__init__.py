63a1d1cd3c41de3b45c3d8816a4cdcdec05e2b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:03:30 +0000

    Add package tm-devices/1.3.0/tm_devices-1.3.0-py3-none-any.whl

---
+++ packages/tm-devices/tm_devices-1.3.0-py3-none-any.whl/tm_devices/__init__.py
@@ -0,0 +1,33 @@
+"""Test & Measurement Device Management.
+
+Provides access to commonly imported items from the tm_devices package.
+
+Examples:
+    >>> from tm_devices import DeviceManager
+    >>> with DeviceManager() as dm:  # doctest: +SKIP
+    ...     print(dm.is_open)
+    2023-09-21 01:50:16.031 - Opening DeviceManager
+    True
+    2023-09-21 01:50:16.031 - DeviceManager Closed
+"""
+
+from importlib.metadata import version
+
+from tm_devices.device_manager import DeviceManager, print_available_visa_devices
+from tm_devices.helpers.constants_and_dataclasses import (
+    PACKAGE_NAME,
+    PYVISA_PY_BACKEND,
+    SYSTEM_DEFAULT_VISA_BACKEND,
+)
+from tm_devices.helpers.enums import SupportedModels
+
+# Read version from installed package.
+__version__ = version(PACKAGE_NAME)
+
+__all__ = [
+    "DeviceManager",
+    "print_available_visa_devices",
+    "PYVISA_PY_BACKEND",
+    "SupportedModels",
+    "SYSTEM_DEFAULT_VISA_BACKEND",
+]
