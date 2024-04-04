80b289c00b092eb36a76571b3bf0ed6249f0049e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:03:05 +0000

    Add package rattail-wave/0.1.1/rattail-wave-0.1.1.tar.gz

---
+++ packages/rattail-wave/rattail-wave-0.1.1.tar.gz/rattail-wave-0.1.1/rattail_wave/commands.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8; -*-
+################################################################################
+#
+#  Rattail -- Retail Software Framework
+#  Copyright © 2010-2022 Lance Edgar
+#
+#  This file is part of Rattail.
+#
+#  Rattail is free software: you can redistribute it and/or modify it under the
+#  terms of the GNU General Public License as published by the Free Software
+#  Foundation, either version 3 of the License, or (at your option) any later
+#  version.
+#
+#  Rattail is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+#  details.
+#
+#  You should have received a copy of the GNU General Public License along with
+#  Rattail.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+"""
+rattail-wave commands
+"""
+
+from rattail import commands
+
+
+class ImportWave(commands.ImportSubcommand):
+    """
+    Import data to Rattail, from Wave API
+    """
+    name = 'import-wave'
+    description = __doc__.strip()
+    handler_key = 'to_rattail.from_wave.import'
