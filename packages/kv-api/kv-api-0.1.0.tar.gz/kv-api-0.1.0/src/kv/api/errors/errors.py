507eb8dcb402cd71bbffc6be5f1fdbdb3fecb5f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 04:03:10 +0000

    Add package kv-api/0.1.0/kv-api-0.1.0.tar.gz

---
+++ packages/kv-api/kv-api-0.1.0.tar.gz/kv-api-0.1.0/src/kv/api/errors/errors.py
@@ -0,0 +1,24 @@
+from typing import Literal, Any
+from dataclasses import dataclass
+
+@dataclass(eq=False)
+class InexistentItem(BaseException):
+  key: str
+  detail: Any | None = None
+  reason: Literal['inexistent-item'] = 'inexistent-item'
+
+@dataclass(eq=False)
+class ExistentItem(BaseException):
+  key: str
+  detail: Any | None = None
+  reason: Literal['existent-item'] = 'existent-item'
+
+@dataclass(eq=False)
+class DBError(BaseException):
+  detail: Any = None
+  reason: Literal['db-error'] = 'db-error'
+
+@dataclass(eq=False)
+class InvalidData(BaseException):
+  detail: Any = None
+  reason: Literal['invalid-data'] = 'invalid-data'
