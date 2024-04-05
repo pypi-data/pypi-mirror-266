c897395729b8a69ec0bb6bcbf391ad024a55d427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 15:05:40 +0000

    Add package ceasylog/1.2.2/ceasylog-1.2.2.tar.gz

---
+++ packages/ceasylog/ceasylog-1.2.2.tar.gz/ceasylog-1.2.2/ceasylog/__init__.py
@@ -0,0 +1,7 @@
+from ceasylog.Logger import Logger
+from ceasylog.LoggerConfiger import LoggerConfiger
+import ceasylog.LoggerLevel as LoggerLevel
+from ceasylog.LoggerNetworkConfiger import LoggerNetworkConfiger
+from ceasylog import LoggerStatic as logger
+
+VERSION = "1.2.2"
