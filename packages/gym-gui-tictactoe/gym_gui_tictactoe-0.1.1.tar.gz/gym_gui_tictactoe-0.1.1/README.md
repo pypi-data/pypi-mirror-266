289f064fb020f98f608989dbdf1331aa787df3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 04:02:54 +0000

    Add package gym-gui-tictactoe/0.1.1/gym_gui_tictactoe-0.1.1.tar.gz

---
+++ packages/gym-gui-tictactoe/gym_gui_tictactoe-0.1.1.tar.gz/gym_gui_tictactoe-0.1.1/README.md
@@ -0,0 +1,8 @@
+## Installation
+```
+pip install gym-gui-tictactoe
+```
+
+## How to Use
+```
+gym.make('tictactoe-gui-v0')
---
+++ packages/gym-gui-tictactoe/gym_gui_tictactoe-0.1.1.tar.gz/gym_gui_tictactoe-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+      name='gym_gui_tictactoe',
+      version='0.1.1',
+      description='An OpenAI gym environment for playing Tic Tac Toe',
+      url='https://github.com/fauzisho/T3_Env',
+      author='AI Trafic Project',
+      license='MIT License',
+      install_requires=['gym','pygame']
+)
