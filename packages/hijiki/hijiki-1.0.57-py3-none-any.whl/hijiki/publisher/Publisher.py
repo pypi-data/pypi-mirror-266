25ff2df9489e839ed80709188f268ddf0d3fc985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 05:03:58 +0000

    Add package hijiki/1.0.57/hijiki-1.0.57-py3-none-any.whl

---
+++ packages/hijiki/hijiki-1.0.57-py3-none-any.whl/hijiki/publisher/Publisher.py
@@ -0,0 +1,16 @@
+from kombu import producers, Exchange
+
+from hijiki.broker.hijiki_broker import HijikiBroker
+
+
+class Publisher():
+
+    def __init__(self, host, username, password, port, cluster_hosts: str = None):
+        self.client = HijikiBroker('client', host, username, password, port, cluster_hosts)
+
+    def publish_message(self, event_name: str, data: str):
+        payload = {"value": data}
+        connection = self.client.get_celery_broker().broker_connection()
+        with producers[connection].acquire(block=True) as producer:
+            task_exchange = Exchange(event_name, type='topic')
+            producer.publish(payload,  exchange=task_exchange, declare=[task_exchange], routing_key='x')
