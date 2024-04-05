12ec438ef1144b2b4aa10ad6df998c6076108f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 03:04:49 +0000

    Add package deepeval/0.21.20/deepeval-0.21.20.tar.gz

---
+++ packages/deepeval/deepeval-0.21.20.tar.gz/deepeval-0.21.20/deepeval/test_run/api.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List
+
+
+class MetricMetadata(BaseModel):
+    metric: str
+    threshold: float
+    success: bool
+    score: Optional[float] = None
+    reason: Optional[str] = None
+    strict_mode: Optional[bool] = Field(False, alias="strictMode")
+    evaluation_model: Optional[str] = Field(None, alias="evaluationModel")
+    error: Optional[str] = None
+
+
+class APITestCase(BaseModel):
+    name: str
+    input: str
+    actual_output: str = Field(..., alias="actualOutput")
+    expected_output: Optional[str] = Field(None, alias="expectedOutput")
+    success: bool
+    metrics_metadata: List[MetricMetadata] = Field(..., alias="metricsMetadata")
+    run_duration: float = Field(..., alias="runDuration")
+    latency: Optional[float] = Field(None)
+    cost: Optional[float] = Field(None)
+    traceStack: Optional[dict] = Field(None)
+    context: Optional[list] = Field(None)
+    retrieval_context: Optional[list] = Field(None, alias="retrievalContext")
+    id: Optional[str] = None
+
+
+class TestRunHttpResponse(BaseModel):
+    testRunId: str
+    projectId: str
+    link: str
