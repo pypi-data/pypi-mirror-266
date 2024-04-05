d0391540ebd9ecf4aabed0f2f0422d8ceb889785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 15:05:07 +0000

    Add package mov-cli-files/1.0.2/mov-cli-files-1.0.2.tar.gz

---
+++ packages/mov-cli-files/mov-cli-files-1.0.2.tar.gz/mov-cli-files-1.0.2/mov_cli_files/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from mov_cli.plugins import PluginHookData
+
+from .files import FilesScraper
+
+plugin: PluginHookData = {
+    "version": 1, 
+    "scrapers": {
+        "DEFAULT": FilesScraper, 
+        "boilerplate": FilesScraper,
+    }
+}
+
+__version__ = "1.0.2"
