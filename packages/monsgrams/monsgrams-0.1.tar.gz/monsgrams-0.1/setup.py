b264613f0250db80db8bbf5c3d9cc904d20b633c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 05:03:40 +0000

    Add package monsgrams/0.1/monsgrams-0.1.tar.gz

---
+++ packages/monsgrams/monsgrams-0.1.tar.gz/monsgrams-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='monsgrams',
+    version='0.1',
+    description='A simple Python library for calculating the area of geometric shapes.',
+    author='grubx64',
+    packages=find_packages(),
+)
