569f9bf59a7f3e8ef4fb41720ab62c2f8eb5db22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 14:04:10 +0000

    Add package processing-pypelines/0.0.55/processing-pypelines-0.0.55.tar.gz

---
+++ packages/processing-pypelines/processing-pypelines-0.0.55.tar.gz/processing-pypelines-0.0.55/src/pypelines/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.0.55"
+
+from . import loggs
+from .pipes import *
+from .pipelines import *
+from .steps import *
+from .disk import *
+from .sessions import *
+
+loggs.enable_logging()
+
+# from .versions import *
