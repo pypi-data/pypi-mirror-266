deef9348432dea9058307a0221cd0027a3f94b48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 06:03:19 +0000

    Add package langchain-provider/0.1.0/langchain_provider-0.1.0-py3-none-any.whl

---
+++ packages/langchain-provider/langchain_provider-0.1.0-py3-none-any.whl/langchain_provider/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © Spyder Project Contributors
+# Licensed under the terms of the MIT License
+
+"""Langchain client requests and constants."""
---
+++ packages/langchain-provider/langchain_provider-0.1.0-py3-none-any.whl/langchain_provider/widgets/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © Spyder Project Contributors
+# Licensed under the terms of the MIT License
+
+"""Langchain related widgets."""
+
+from .status import LangchainStatusWidget
+
+__all__ = [
+    "LangchainStatusWidget",
+]
