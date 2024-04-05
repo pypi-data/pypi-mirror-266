6024c58edc9a6af19f371659e17d692adae98f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 03:03:15 +0000

    Add package bump440/0.1.0.dev0/bump440-0.1.0.dev0-py3-none-any.whl

---
+++ packages/bump440/bump440-0.1.0.dev0-py3-none-any.whl/bump440/__init__.py
@@ -0,0 +1,6 @@
+# src/bump440/__init__.py
+"""
+This is the __init__.py file for the bump440 module.
+"""
+
+__version__ = "0.1.0.dev0"
---
+++ packages/bump440/bump440-0.1.0.dev0-py3-none-any.whl/bump440/main.py
@@ -0,0 +1,13 @@
+# src/bump440/main.py
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def hello(name: str):
+    print(f"Hello {name}")
+
+
+if __name__ == "__main__":
+    app()
