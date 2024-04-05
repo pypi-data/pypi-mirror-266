9bff2250c9c515f0b86078dc60a6025b6151dbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 05:04:08 +0000

    Add package r2r/0.1.29/r2r-0.1.29-py3-none-any.whl

---
+++ packages/r2r/r2r-0.1.29-py3-none-any.whl/r2r/core/providers/eval.py
@@ -0,0 +1,26 @@
+import random
+from abc import ABC, abstractmethod
+from typing import Optional
+
+
+class EvalProvider(ABC):
+    providers = ["deepeval", "parea"]
+
+    def __init__(self, provider: str, sampling_fraction: float = 1.0):
+        if provider not in self.providers:
+            raise ValueError(f"Provider {provider} not supported.")
+        self.provider = provider
+        self.sampling_fraction = sampling_fraction
+
+    def evaluate(
+        self, query: str, context: str, completion: str
+    ) -> Optional[dict]:
+        if random.random() < self.sampling_fraction:
+            return self.evaluate(query, context, completion)
+        return None
+
+    @abstractmethod
+    def _evaluate(
+        self, query: str, context: str, completion: str
+    ) -> dict[str, dict[str, str]]:
+        pass
