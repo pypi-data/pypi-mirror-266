b02edf9daebaef65273b1a386c5c9cd106056742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 04:02:59 +0000

    Add package uwuify/1.3.1/uwuify-1.3.1-py3-none-any.whl

---
+++ packages/uwuify/uwuify-1.3.1-py3-none-any.whl/uwuify/__init__.py
@@ -0,0 +1,9 @@
+# TODO: 2.0 remove this so I don't have to maintain it
+__version__ = "1.3.0"
+
+from .core import UwuifyFlag, uwu
+
+SMILEY = UwuifyFlag.SMILEY
+YU = UwuifyFlag.YU
+STUTTER = UwuifyFlag.STUTTER
+NOUWU = UwuifyFlag.NOUWU
