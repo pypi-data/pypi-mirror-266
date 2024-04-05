eae6db1465ff8dc79454959bf3d8a2e83642ef0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 15:03:43 +0000

    Add package dalpha-ai/0.3.0/dalpha_ai-0.3.0.tar.gz

---
+++ packages/dalpha-ai/dalpha_ai-0.3.0.tar.gz/dalpha_ai-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open("requirements.txt", "r") as req:
+        content = req.read()
+        requirements = content.split("\n")
+    return requirements
+
+
+setup(
+    name="dalpha_ai",
+    version="0.3.0",
+    packages=find_packages(),
+    install_requires=read_requirements(),
+)
