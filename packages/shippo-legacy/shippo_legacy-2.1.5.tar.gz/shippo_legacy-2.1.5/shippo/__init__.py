b445cf5e32ff06933e006fc4f816c35e4031248d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 14:03:44 +0000

    Add package shippo-legacy/2.1.5/shippo_legacy-2.1.5.tar.gz

---
+++ packages/shippo-legacy/shippo_legacy-2.1.5.tar.gz/shippo_legacy-2.1.5/shippo/__init__.py
@@ -0,0 +1,20 @@
+from shippo.config import config
+
+from shippo.resource import (
+    Address,
+    Batch,
+    CarrierAccount,
+    CustomsDeclaration,
+    CustomsItem,
+    LineItem,
+    Manifest,
+    Order,
+    Parcel,
+    Pickup,
+    Rate,
+    Refund,
+    Shipment,
+    Track,
+    Transaction,
+    Webhook,
+)
