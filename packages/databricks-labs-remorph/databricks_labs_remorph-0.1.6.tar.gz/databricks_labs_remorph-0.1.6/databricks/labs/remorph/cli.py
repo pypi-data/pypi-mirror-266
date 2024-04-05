98cb83d6b24473aadcfd38c99b1892e59a667473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 03:04:31 +0000

    Add package databricks-labs-remorph/0.1.6/databricks_labs_remorph-0.1.6.tar.gz

---
+++ packages/databricks-labs-remorph/databricks_labs_remorph-0.1.6.tar.gz/databricks_labs_remorph-0.1.6/databricks/labs/remorph/cli.py
@@ -0,0 +1,86 @@
+import json
+import os
+
+from databricks.labs.blueprint.cli import App
+from databricks.labs.blueprint.entrypoint import get_logger
+from databricks.labs.blueprint.installation import Installation
+from databricks.sdk import WorkspaceClient
+
+from databricks.labs.remorph.config import MorphConfig
+from databricks.labs.remorph.reconcile.execute import recon
+from databricks.labs.remorph.transpiler.execute import morph
+
+remorph = App(__file__)
+logger = get_logger(__file__)
+
+
+def raise_validation_exception(msg: str) -> Exception:
+    raise ValueError(msg)
+
+
+@remorph.command
+def transpile(
+    w: WorkspaceClient,
+    source: str,
+    input_sql: str,
+    output_folder: str,
+    skip_validation: str,
+    catalog_name: str,
+    schema_name: str,
+):
+    """transpiles source dialect to databricks dialect"""
+    logger.info(f"user: {w.current_user.me()}")
+    installation = Installation.current(w, 'remorph')
+    default_config = installation.load(MorphConfig)
+
+    # TODO refactor cli based on the default config
+
+    if source.lower() not in {"snowflake", "tsql"}:
+        raise_validation_exception(
+            f"Error: Invalid value for '--source': '{source}' is not one of 'snowflake', 'tsql'. "
+        )
+    if not os.path.exists(input_sql) or input_sql in {None, ""}:
+        raise_validation_exception(f"Error: Invalid value for '--input_sql': Path '{input_sql}' does not exist.")
+    if output_folder == "":
+        output_folder = default_config.output_folder if default_config.output_folder else None
+    if skip_validation.lower() not in {"true", "false"}:
+        raise_validation_exception(
+            f"Error: Invalid value for '--skip_validation': '{skip_validation}' is not one of 'true', 'false'. "
+        )
+
+    sdk_config = default_config.sdk_config if default_config.sdk_config else None
+    config = MorphConfig(
+        source=source.lower(),
+        input_sql=input_sql,
+        output_folder=output_folder,
+        skip_validation=skip_validation.lower() == "true",  # convert to bool
+        catalog_name=catalog_name,
+        schema_name=schema_name,
+        sdk_config=sdk_config,
+    )
+
+    status = morph(w, config)
+
+    print(json.dumps(status))
+
+
+@remorph.command
+def reconcile(w: WorkspaceClient, recon_conf: str, conn_profile: str, source: str, report: str):
+    """reconciles source to databricks datasets"""
+    logger.info(f"user: {w.current_user.me()}")
+    if not os.path.exists(recon_conf) or recon_conf in {None, ""}:
+        raise_validation_exception(f"Error: Invalid value for '--recon_conf': Path '{recon_conf}' does not exist.")
+    if not os.path.exists(conn_profile) or conn_profile in {None, ""}:
+        raise_validation_exception(f"Error: Invalid value for '--conn_profile': Path '{conn_profile}' does not exist.")
+    if source.lower() not in "snowflake":
+        raise_validation_exception(f"Error: Invalid value for '--source': '{source}' is not one of 'snowflake'. ")
+    if report.lower() not in {"data", "schema", "all"}:
+        raise_validation_exception(
+            f"Error: Invalid value for '--report': '{report}' is not one of 'data', 'schema', 'all' "
+        )
+
+    recon(recon_conf, conn_profile, source, report)
+
+
+if __name__ == "__main__":
+    remorph()
---
+++ packages/databricks-labs-remorph/databricks_labs_remorph-0.1.6.tar.gz/databricks_labs_remorph-0.1.6/databricks/labs/remorph/transpiler/execute.py
@@ -0,0 +1,181 @@
+import logging
+import os
+from pathlib import Path
+
+from databricks.sdk import WorkspaceClient
+
+from databricks.labs.remorph.config import MorphConfig
+from databricks.labs.remorph.helpers import db_sql
+from databricks.labs.remorph.helpers.execution_time import timeit
+from databricks.labs.remorph.helpers.file_utils import (
+    dir_walk,
+    is_sql_file,
+    make_dir,
+    remove_bom,
+)
+from databricks.labs.remorph.helpers.morph_status import MorphStatus, ValidationError
+from databricks.labs.remorph.helpers.validation import Validator
+from databricks.labs.remorph.snow import dialect_utils
+from databricks.labs.remorph.snow.sql_transpiler import SQLTranspiler
+
+# pylint: disable=unspecified-encoding
+
+logger = logging.getLogger(__name__)
+
+
+def process_file(config: MorphConfig, validator: Validator, input_file: str | Path, output_file: str | Path):
+    source = config.source
+    parse_error_list = []
+    validate_error_list = []
+    no_of_sqls = 0
+
+    input_file = Path(input_file)
+    output_file = Path(output_file)
+
+    with input_file.open("r") as f:
+        sql = remove_bom(f.read())
+
+    lca_error = dialect_utils.check_for_unsupported_lca(source, sql, str(input_file))
+    if lca_error:
+        validate_error_list.append(lca_error)
+    transpiler = SQLTranspiler(source, parse_error_list)  # [TODO] move the object creation outside the loop
+    transpiled_sql = transpiler.transpile(sql, str(input_file))
+
+    with output_file.open("w") as w:
+        for output in transpiled_sql:
+            if output:
+                no_of_sqls = no_of_sqls + 1
+                if config.skip_validation:
+                    w.write(output)
+                    w.write("\n;\n")
+                else:
+                    output_string, exception = validator.validate_format_result(config, output)
+                    w.write(output_string)
+                    if exception is not None:
+                        validate_error_list.append(ValidationError(str(input_file), exception))
+            else:
+                warning_message = (
+                    f"Skipped a query from file {input_file!s}. "
+                    f"Check for unsupported operations related to STREAM, TASK, SESSION etc."
+                )
+                logger.warning(warning_message)
+
+    return no_of_sqls, parse_error_list, validate_error_list
+
+
+def process_directory(config: MorphConfig, validator: Validator, root: str | Path, base_root: str, files: list[str]):
+    output_folder = config.output_folder
+    parse_error_list = []
+    validate_error_list = []
+    counter = 0
+
+    root = Path(root)
+
+    for file in files:
+        logger.debug(f"Processing file :{file}")
+        if is_sql_file(file):
+            if output_folder in {None, "None"}:
+                output_folder_base = root / "transpiled"
+            else:
+                output_folder_base = f'{output_folder.rstrip("/")}/{base_root}'
+
+            output_file_name = Path(output_folder_base) / Path(file).name
+            make_dir(output_folder_base)
+
+            no_of_sqls, parse_error, validation_error = process_file(config, validator, file, output_file_name)
+            counter = counter + no_of_sqls
+            parse_error_list.extend(parse_error)
+            validate_error_list.extend(validation_error)
+        else:
+            # Only SQL files are processed with extension .sql or .ddl
+            pass
+
+    return counter, parse_error_list, validate_error_list
+
+
+def process_recursive_dirs(config: MorphConfig, validator: Validator):
+    input_sql = Path(config.input_sql)
+    parse_error_list = []
+    validate_error_list = []
+
+    file_list = []
+    counter = 0
+    for root, _, files in dir_walk(input_sql):
+        base_root = str(root).replace(str(input_sql), "")
+        folder = str(input_sql.resolve().joinpath(base_root))
+        msg = f"Processing for sqls under this folder: {folder}"
+        logger.info(msg)
+        file_list.extend(files)
+        no_of_sqls, parse_error, validation_error = process_directory(config, validator, root, base_root, files)
+        counter = counter + no_of_sqls
+        parse_error_list.extend(parse_error)
+        validate_error_list.extend(validation_error)
+
+    error_log = parse_error_list + validate_error_list
+
+    return MorphStatus(file_list, counter, len(parse_error_list), len(validate_error_list), error_log)
+
+
+@timeit
+def morph(workspace_client: WorkspaceClient, config: MorphConfig):
+    """
+    Transpiles the SQL queries from one dialect to another.
+
+    :param config: The configuration for the morph operation.
+    :param workspace_client: The WorkspaceClient object.
+    """
+    input_sql = Path(config.input_sql)
+    skip_validation = config.skip_validation
+    status = []
+    result = MorphStatus([], 0, 0, 0, [])
+    validator = None
+    if not config.skip_validation:
+        validator = Validator(db_sql.get_sql_backend(workspace_client, config))
+
+    if input_sql.is_file():
+        if is_sql_file(input_sql):
+            msg = f"Processing for sqls under this file: {input_sql}"
+            logger.info(msg)
+            if config.output_folder in {None, "None"}:
+                output_folder = input_sql.parent / "transpiled"
+            else:
+                output_folder = Path(config.output_folder.rstrip("/"))
+
+            make_dir(output_folder)
+            output_file = output_folder / input_sql.name
+            no_of_sqls, parse_error, validation_error = process_file(config, validator, input_sql, output_file)
+            error_log = parse_error + validation_error
+            result = MorphStatus([str(input_sql)], no_of_sqls, len(parse_error), len(validation_error), error_log)
+        else:
+            msg = f"{input_sql} is not a SQL file."
+            logger.warning(msg)
+    elif input_sql.is_dir():
+        result = process_recursive_dirs(config, validator)
+    else:
+        msg = f"{input_sql} does not exist."
+        logger.error(msg)
+        raise FileNotFoundError(msg)
+
+    parse_error_count = result.parse_error_count
+    validate_error_count = result.validate_error_count
+
+    error_list_count = parse_error_count + validate_error_count
+    if not skip_validation:
+        logger.info(f"No of Sql Failed while Validating: {validate_error_count}")
+
+    error_log_file = "None"
+    if error_list_count > 0:
+        error_log_file = Path.cwd() / f"err_{os.getpid()}.lst"
+        with error_log_file.open("a") as e:
+            e.writelines(f"{err}\n" for err in result.error_log_list)
+
+    status.append(
+        {
+            "total_files_processed": len(result.file_list),
+            "total_queries_processed": result.no_of_queries,
+            "no_of_sql_failed_while_parsing": parse_error_count,
+            "no_of_sql_failed_while_validating": validate_error_count,
+            "error_log_file": str(error_log_file),
+        }
+    )
+    return status
