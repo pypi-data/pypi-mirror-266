937ada0026f08a7f535806ff3de0d82854ba48b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 04:03:30 +0000

    Add package xad/1.5.2/xad-1.5.2-cp39-cp39-win_amd64.whl

---
+++ packages/xad/xad-1.5.2-cp39-cp39-win_amd64.whl/xad/_xad/__init__.pyi
@@ -0,0 +1,6 @@
+from __future__ import annotations
+from . import adj_1st
+from . import exceptions
+from . import fwd_1st
+from . import math
+__all__ = ['adj_1st', 'exceptions', 'fwd_1st', 'math']
