2ada11988d5c5684d8987df7e55aa0732f5f9457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 03:03:18 +0000

    Add package logs-py/2.8/logs_py-2.8-py3-none-any.whl

---
+++ packages/logs-py/logs_py-2.8-py3-none-any.whl/LOGS/Entities/BridgeRequestParameter.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import List, Optional
+
+from LOGS.Entities.BridgeType import BridgeType
+from LOGS.Entities.IRelatedEntityRequest import IRelatedEntityRequest
+from LOGS.Entity.EntityRequestParameter import DefaultOrder, EntityRequestParameter
+from LOGS.Interfaces.INamedEntity import INamedEntityRequest
+
+
+@dataclass
+class BridgeRequestParameter(
+    EntityRequestParameter[DefaultOrder], INamedEntityRequest, IRelatedEntityRequest
+):
+    hostnames: Optional[List[str]] = None
+    usernames: Optional[List[str]] = None
+    ipAddresses: Optional[List[str]] = None
+    dataSourceIds: Optional[List[int]] = None
+    types: Optional[List[BridgeType]] = None
+    isConfigured: Optional[bool] = None
