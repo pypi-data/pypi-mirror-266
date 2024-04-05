ee46e0ff80ea5002e30e7336c5266a640857993b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 05:03:23 +0000

    Add package tljh-repo2docker/2.0.0a0/tljh_repo2docker-2.0.0a0-py2.py3-none-any.whl

---
+++ packages/tljh-repo2docker/tljh_repo2docker-2.0.0a0-py2.py3-none-any.whl/tljh_repo2docker/servers.py
@@ -0,0 +1,34 @@
+from inspect import isawaitable
+
+from tornado import web
+
+from .base import BaseHandler
+from .docker import list_images
+
+
+class ServersHandler(BaseHandler):
+    """
+    Handler to show the list of servers available
+    """
+
+    @web.authenticated
+    async def get(self):
+        images = await list_images()
+        user_data = await self.fetch_user()
+
+        server_data = user_data.all_spawners()
+        named_server_limit = 0
+        result = self.render_template(
+            "servers.html",
+            images=images,
+            allow_named_servers=True,
+            named_server_limit_per_user=named_server_limit,
+            server_data=server_data,
+            default_server_data={},
+            user_is_admin=True,
+        )
+
+        if isawaitable(result):
+            self.write(await result)
+        else:
+            self.write(result)
