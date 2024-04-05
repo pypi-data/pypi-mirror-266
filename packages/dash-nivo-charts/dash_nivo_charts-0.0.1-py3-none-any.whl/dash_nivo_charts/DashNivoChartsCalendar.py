44baa89eaae3ac553f0a3932932ddd6d862b6bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 15:05:01 +0000

    Add package dash-nivo-charts/0.0.1/dash_nivo_charts-0.0.1-py3-none-any.whl

---
+++ packages/dash-nivo-charts/dash_nivo_charts-0.0.1-py3-none-any.whl/dash_nivo_charts/DashNivoChartsCalendar.py
@@ -0,0 +1,36 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class DashNivoChartsCalendar(Component):
+    """A DashNivoChartsCalendar component.
+ExampleComponent is an example component.
+It takes a property, `label`, and
+displays it.
+It renders an input with the property `value`
+which is editable by the user.
+
+Keyword arguments:
+
+- id (string; optional):
+    The ID used to identify this component in Dash callbacks.
+
+- eventData (dict; optional):
+    The value displayed in the input."""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'dash_nivo_charts'
+    _type = 'DashNivoChartsCalendar'
+    @_explicitize_args
+    def __init__(self, id=Component.UNDEFINED, eventData=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'eventData']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'eventData']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args}
+
+        super(DashNivoChartsCalendar, self).__init__(**args)
