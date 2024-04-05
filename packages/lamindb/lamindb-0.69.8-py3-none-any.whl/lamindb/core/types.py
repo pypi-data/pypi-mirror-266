befd47a8279f89103e760e8bad9816311356c7c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 05:03:49 +0000

    Add package lamindb/0.69.8/lamindb-0.69.8-py3-none-any.whl

---
+++ packages/lamindb/lamindb-0.69.8-py3-none-any.whl/lamindb/core/types.py
@@ -0,0 +1,18 @@
+"""Types.
+
+.. autosummary::
+   :toctree: .
+
+   UPathStr
+   DataLike
+   StrField
+   ListLike
+   TransformType
+"""
+from lamindb_setup.core.types import UPathStr
+from lnschema_core.types import (
+    DataLike,
+    ListLike,
+    StrField,
+    TransformType,
+)
---
+++ packages/lamindb/lamindb-0.69.8-py3-none-any.whl/lamindb/setup/core/__init__.py
@@ -0,0 +1,15 @@
+"""Core setup library.
+
+.. autosummary::
+   :toctree:
+
+   UserSettings
+   InstanceSettings
+   StorageSettings
+
+"""
+from lamindb_setup.core import (  # pragma: no cover
+    InstanceSettings,
+    StorageSettings,
+    UserSettings,
+)
