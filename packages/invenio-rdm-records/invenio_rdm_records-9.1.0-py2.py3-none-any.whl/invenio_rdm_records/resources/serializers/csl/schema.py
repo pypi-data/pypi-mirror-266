d46bc507df57604b7a9841fca0e48a55b5bf56e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 03:04:07 +0000

    Add package invenio-rdm-records/9.1.0/invenio_rdm_records-9.1.0-py2.py3-none-any.whl

---
+++ packages/invenio-rdm-records/invenio_rdm_records-9.1.0-py2.py3-none-any.whl/invenio_rdm_records/resources/serializers/csl/schema.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""CSL based Schema for Invenio RDM Records."""
+
+from babel_edtf import parse_edtf
+from edtf.parser.edtf_exceptions import EDTFParseException
+from edtf.parser.parser_classes import Date, Interval
+from flask_resources.serializers import BaseSerializerSchema
+from invenio_access.permissions import system_identity
+from invenio_records_resources.proxies import current_service_registry
+from invenio_vocabularies.proxies import current_service as vocabulary_service
+from marshmallow import Schema, fields, missing, pre_dump
+from marshmallow_utils.fields import SanitizedUnicode, StrippedHTML
+
+from ..utils import get_preferred_identifier
+
+
+class CSLCreatorSchema(Schema):
+    """Creator/contributor common schema."""
+
+    family = fields.Str(attribute="person_or_org.family_name")
+    given = fields.Str(attribute="person_or_org.given_name")
+
+    @pre_dump
+    def update_names(self, data, **kwargs):
+        """Organizational creators do not have family/given name."""
+        # family is required by CSL
+        if not data.get("person_or_org").get("family_name"):
+            name = data["person_or_org"]["name"]
+            data["person_or_org"]["family_name"] = name
+
+        return data
+
+
+def add_if_not_none(year, month, day):
+    """Adds year, month a day to a list if each are not None."""
+    _list = []
+    _list.append(year) if year else None
+    _list.append(month) if month else None
+    _list.append(day) if day else None
+    return _list
+
+
+class CSLJSONSchema(BaseSerializerSchema):
+    """CSL Marshmallow Schema."""
+
+    id_ = SanitizedUnicode(data_key="id", attribute="id")
+    type_ = fields.Method("get_type", data_key="type")
+    title = SanitizedUnicode(attribute="metadata.title")
+    abstract = StrippedHTML(attribute="metadata.description")
+    author = fields.List(fields.Nested(CSLCreatorSchema), attribute="metadata.creators")
+    issued = fields.Method("get_issued")
+    language = fields.Method("get_language")
+    version = SanitizedUnicode(attribute="metadata.version")
+    note = fields.Method("get_note")
+    doi = fields.Str(attribute="pids.doi.identifier", data_key="DOI")
+    isbn = fields.Method("get_isbn", data_key="ISBN")
+    issn = fields.Method("get_issn", data_key="ISSN")
+    publisher = SanitizedUnicode(attribute="metadata.publisher")
+
+    def _read_resource_type(self, id_):
+        """Retrieve resource type record using service."""
+        rec = vocabulary_service.read(system_identity, ("resourcetypes", id_))
+        return rec._record
+
+    def get_type(self, obj):
+        """Get resource type."""
+        resource_type = obj["metadata"].get(
+            "resource_type", {"id": "publication-article"}
+        )
+
+        resource_type_record = self._read_resource_type(resource_type["id"])
+        props = resource_type_record["props"]
+        return props.get("csl", "article")  # article is CSL "Other"
+
+    def get_issued(self, obj):
+        """Get issued dates."""
+        try:
+            parsed = parse_edtf(obj["metadata"].get("publication_date"))
+        except EDTFParseException:
+            return missing
+
+        if isinstance(parsed, Date):
+            parts = add_if_not_none(parsed.year, parsed.month, parsed.day)
+            return {"date-parts": [parts]}
+        elif isinstance(parsed, Interval):
+            d1 = parsed.lower
+            d2 = parsed.upper
+            return {
+                "date-parts": [
+                    add_if_not_none(d1.year, d1.month, d1.day),
+                    add_if_not_none(d2.year, d2.month, d2.day),
+                ]
+            }
+        else:
+            return missing
+
+    def get_language(self, obj):
+        """Get language."""
+        metadata = obj["metadata"]
+        languages = metadata.get("languages")
+
+        return languages[0]["id"] if languages else missing
+
+    def get_isbn(self, obj):
+        """Get ISBN."""
+        identifiers = obj["metadata"].get("identifiers", [])
+        for identifier in identifiers:
+            if identifier["scheme"] == "ISBN":
+                return identifier["identifier"]
+
+        return missing
+
+    def get_issn(self, obj):
+        """Get ISSN."""
+        identifiers = obj["metadata"].get("identifiers", [])
+        for identifier in identifiers:
+            if identifier["scheme"] == "ISSN":
+                return identifier["identifier"]
+
+        return missing
+
+    def get_note(self, obj):
+        """Get note from funders."""
+        funding = obj["metadata"].get("funding")
+        if funding:
+            funder = funding[0]["funder"]
+            id_ = funder.get("id")
+            if id_:
+                funder_service = current_service_registry.get("funders")
+                funder = funder_service.read(system_identity, id_).to_dict()
+
+            note = f"Funding by {funder['name']}"
+            identifiers = funder.get("identifiers", [])
+            identifier = get_preferred_identifier(
+                priority=("ror", "grid", "doi", "isni", "gnd"), identifiers=identifiers
+            )
+
+            if identifier:
+                note += (
+                    f" {identifier['scheme'].upper()} " f"{identifier['identifier']}."
+                )
+            return note
+
+        return missing
---
+++ packages/invenio-rdm-records/invenio_rdm_records-9.1.0-py2.py3-none-any.whl/tests/resources/serializers/test_dcat_serializer.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023-2024 CERN.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Resources serializers tests."""
+
+from invenio_rdm_records.resources.serializers import DCATSerializer
+
+
+def test_dcat_serializer(running_app, enhanced_full_record):
+    enhanced_full_record["links"] = dict(self_html="https://self-link.com")
+    expected_data = [
+        "<?xml version='1.0' encoding='utf-8'?>",
+        '<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:adms="http://www.w3.org/ns/adms#" xmlns:bibo="http://purl.org/ontology/bibo/" xmlns:citedcat="https://w3id.org/citedcat-ap/" xmlns:dct="http://purl.org/dc/terms/" xmlns:dctype="http://purl.org/dc/dcmitype/" xmlns:dcat="http://www.w3.org/ns/dcat#" xmlns:foaf="http://xmlns.com/foaf/0.1/" xmlns:gsp="http://www.opengis.net/ont/geosparql#" xmlns:locn="http://www.w3.org/ns/locn#" xmlns:org="http://www.w3.org/ns/org#" xmlns:owl="http://www.w3.org/2002/07/owl#" xmlns:prov="http://www.w3.org/ns/prov#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#" xmlns:skos="http://www.w3.org/2004/02/skos/core#" xmlns:vcard="http://www.w3.org/2006/vcard/ns#" xmlns:wdrs="http://www.w3.org/2007/05/powder-s#">',  # noqa
+        '  <rdf:Description rdf:about="https://doi.org/10.1234/inveniordm.1234">',  # noqa
+        '    <rdf:type rdf:resource="http://www.w3.org/ns/dcat#Dataset"/>',  # noqa
+        '    <dct:type rdf:resource="http://purl.org/dc/dcmitype/Image"/>',  # noqa
+        '    <dct:identifier rdf:datatype="http://www.w3.org/2001/XMLSchema#anyURI">https://doi.org/10.1234/inveniordm.1234</dct:identifier>',  # noqa
+        '    <foaf:page rdf:resource="https://doi.org/10.1234/inveniordm.1234"/>',  # noqa
+        "    <dct:creator>",
+        '      <rdf:Description rdf:about="https://orcid.org/0000-0001-8135-3489">',  # noqa
+        '        <rdf:type rdf:resource="http://xmlns.com/foaf/0.1/Person"/>',  # noqa
+        "        <foaf:name>Nielsen, Lars Holm</foaf:name>",
+        "        <foaf:givenName>Lars Holm</foaf:givenName>",
+        "        <foaf:familyName>Nielsen</foaf:familyName>",
+        "        <org:memberOf>",
+        "          <foaf:Organization>",
+        "            <foaf:name>free-text</foaf:name>",
+        "          </foaf:Organization>",
+        "        </org:memberOf>",
+        "        <org:memberOf>",
+        '          <foaf:Organization rdf:about="https://ror.org/https://ror.org/01ggx4157">',  # noqa
+        '            <dct:identifier rdf:datatype="http://www.w3.org/2001/XMLSchema#anyURI">https://ror.org/01ggx4157</dct:identifier>',  # noqa
+        "            <foaf:name>CERN</foaf:name>",
+        "          </foaf:Organization>",
+        "        </org:memberOf>",
+        "      </rdf:Description>",
+        "    </dct:creator>",
+        "    <dct:creator>",
+        "      <rdf:Description>",
+        '        <rdf:type rdf:resource="http://xmlns.com/foaf/0.1/Person"/>',  # noqa
+        "        <foaf:name>Tom, Blabin</foaf:name>",
+        "        <foaf:givenName>Blabin</foaf:givenName>",
+        "        <foaf:familyName>Tom</foaf:familyName>",
+        "      </rdf:Description>",
+        "    </dct:creator>",
+        "    <dct:title>InvenioRDM</dct:title>",
+        "    <dct:publisher>",
+        "      <foaf:Agent>",
+        "        <foaf:name>InvenioRDM</foaf:name>",
+        "      </foaf:Agent>",
+        "    </dct:publisher>",
+        '    <dct:issued rdf:datatype="http://www.w3.org/2001/XMLSchema#gYear">2018</dct:issued>',  # noqa
+        "    <dcat:keyword>custom</dcat:keyword>",
+        "    <citedcat:isFundedBy>",
+        "      <foaf:Project>",
+        '        <dct:identifier rdf:datatype="http://www.w3.org/2001/XMLSchema#string">111023</dct:identifier>',  # noqa
+        "        <dct:title>Launching of the research program on meaning processing</dct:title>",
+        '        <citedcat:isAwardedBy rdf:resource="https://ror.org/00k4n6c32"/>',  # noqa
+        "      </foaf:Project>",
+        "    </citedcat:isFundedBy>",
+        '    <citedcat:funder rdf:resource="https://ror.org/00k4n6c32"/>',  # noqa
+        "    <citedcat:dataManager>",
+        '      <rdf:Description rdf:about="https://orcid.org/0000-0001-8135-3489">',  # noqa
+        '        <rdf:type rdf:resource="http://xmlns.com/foaf/0.1/Person"/>',  # noqa
+        "        <foaf:name>Nielsen, Lars Holm</foaf:name>",
+        "        <foaf:givenName>Lars Holm</foaf:givenName>",
+        "        <foaf:familyName>Nielsen</foaf:familyName>",
+        "        <org:memberOf>",
+        "          <foaf:Organization>",
+        "            <foaf:name>TU Wien</foaf:name>",
+        "          </foaf:Organization>",
+        "        </org:memberOf>",
+        "        <org:memberOf>",
+        '          <foaf:Organization rdf:about="https://ror.org/https://ror.org/01ggx4157">',  # noqa
+        '            <dct:identifier rdf:datatype="http://www.w3.org/2001/XMLSchema#anyURI">https://ror.org/01ggx4157</dct:identifier>',  # noqa
+        "            <foaf:name>CERN</foaf:name>",
+        "          </foaf:Organization>",
+        "        </org:memberOf>",
+        "      </rdf:Description>",
+        "    </citedcat:dataManager>",
+        "    <dct:contributor>",
+        "      <rdf:Description>",
+        '        <rdf:type rdf:resource="http://xmlns.com/foaf/0.1/Person"/>',  # noqa
+        "        <foaf:name>Dirk, Dirkin</foaf:name>",
+        "        <foaf:givenName>Dirkin</foaf:givenName>",
+        "        <foaf:familyName>Dirk</foaf:familyName>",
+        "      </rdf:Description>",
+        "    </dct:contributor>",
+        "    <prov:wasGeneratedBy>",
+        "      <foaf:Project>",
+        '        <rdf:type rdf:resource="http://www.w3.org/ns/prov#Activity"/>',
+        "        <citedcat:projectManager>",
+        "          <rdf:Description>",
+        '            <rdf:type rdf:resource="http://xmlns.com/foaf/0.1/Person"/>',
+        "            <foaf:name>Dirk, Dirkin</foaf:name>",
+        "            <foaf:givenName>Dirkin</foaf:givenName>",
+        "            <foaf:familyName>Dirk</foaf:familyName>",
+        "          </rdf:Description>",
+        "        </citedcat:projectManager>",
+        "      </foaf:Project>",
+        "    </prov:wasGeneratedBy>",
+        '    <dct:issued rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2018/2020-09</dct:issued>',  # noqa
+        '    <dct:date rdf:datatype="http://www.w3.org/2001/XMLSchema#date">1939/1945</dct:date>',  # noqa
+        '    <dct:language rdf:resource="http://publications.europa.eu/resource/authority/language/DAN"/>',  # noqa
+        '    <owl:sameAs rdf:resource="https://self-link.com"/>',  # noqa
+        "    <adms:identifier>",
+        "      <adms:Identifier>",
+        '        <skos:notation rdf:datatype="http://www.w3.org/2001/XMLSchema#anyURI">https://self-link.com</skos:notation>',  # noqa
+        "        <adms:schemeAgency>URL</adms:schemeAgency>",
+        "      </adms:Identifier>",
+        "    </adms:identifier>",
+        "    <adms:identifier>",
+        "      <adms:Identifier>",
+        "        <skos:notation>oai:invenio-rdm.com:vs40t-1br10</skos:notation>",
+        "        <adms:schemeAgency>oai</adms:schemeAgency>",
+        "      </adms:Identifier>",
+        "    </adms:identifier>",
+        '    <owl:sameAs rdf:resource="http://adsabs.harvard.edu/abs/1924MNRAS..84..308E"/>',  # noqa
+        "    <adms:identifier>",
+        "      <adms:Identifier>",
+        '        <skos:notation rdf:datatype="http://www.w3.org/2001/XMLSchema#anyURI">http://adsabs.harvard.edu/abs/1924MNRAS..84..308E</skos:notation>',  # noqa
+        "        <adms:schemeAgency>bibcode</adms:schemeAgency>",
+        "      </adms:Identifier>",
+        "    </adms:identifier>",
+        "    <dct:isReferencedBy>",
+        '      <rdf:Description rdf:about="https://doi.org/10.1234/foo.bar">',  # noqa
+        "        <dct:identifier>https://doi.org/10.1234/foo.bar</dct:identifier>",
+        '        <rdf:type rdf:resource="http://www.w3.org/ns/dcat#Dataset"/>',  # noqa
+        '        <dct:type rdf:resource="http://purl.org/dc/dcmitype/Dataset"/>',  # noqa
+        "      </rdf:Description>",
+        "    </dct:isReferencedBy>",
+        "    <dct:isVersionOf>",
+        '      <rdf:Description rdf:about="https://doi.org/10.1234/inveniordm.1234.parent">',  # noqa
+        "        <dct:identifier>https://doi.org/10.1234/inveniordm.1234.parent</dct:identifier>",  # noqa
+        "      </rdf:Description>",
+        "    </dct:isVersionOf>",
+        "    <owl:versionInfo>v1.0</owl:versionInfo>",
+        "    <dct:description>A description with HTML tags</dct:description>",
+        "    <dct:provenance>",
+        "      <dct:ProvenanceStatement>",
+        "        <rdfs:label>Bla bla bla</rdfs:label>",
+        "      </dct:ProvenanceStatement>",
+        "    </dct:provenance>",
+        "    <dct:spatial>",
+        "      <dct:Location>",
+        '        <rdf:type rdf:resource="http://www.w3.org/2004/02/skos/core#Concept"/>',  # noqa
+        "        <skos:prefLabel>test location place</skos:prefLabel>",
+        "        <locn:geographicName>test location place</locn:geographicName>",
+        '        <dcat:centroid rdf:datatype="http://www.opengis.net/ont/geosparql#wktLiteral"><![CDATA[POINT(-32.94682 -60.63932)]]></dcat:centroid>',  # noqa
+        '        <dcat:centroid rdf:datatype="http://www.opengis.net/ont/geosparql#gmlLiteral"><![CDATA[<gml:Point srsName="http://www.opengis.net/def/crs/OGC/1.3/CRS84"><gml:pos srsDimension="2">-32.94682 -60.63932</gml:pos></gml:Point>]]></dcat:centroid>',  # noqa
+        '        <dcat:centroid rdf:datatype="http://www.opengis.net/ont/geosparql#geoJSONLiteral"><![CDATA[{"type":"Point","coordinates":[-32.94682,-60.63932]}]]></dcat:centroid>',  # noqa
+        "      </dct:Location>",
+        "    </dct:spatial>",
+        "    <dcat:distribution>",
+        "      <dcat:Distribution>",
+        "        <dct:extent>",
+        "          <dct:SizeOrDuration>",
+        "            <rdfs:label>11 pages</rdfs:label>",
+        "          </dct:SizeOrDuration>",
+        "        </dct:extent>",
+        '        <dcat:mediaType rdf:resource="https://www.iana.org/assignments/media-types/application/pdf"/>',  # noqa
+        "        <dct:rights>",
+        '          <dct:RightsStatement rdf:about="https://customlicense.org/licenses/by/4.0/">',  # noqa
+        "            <rdfs:label>A custom license</rdfs:label>",
+        "          </dct:RightsStatement>",
+        "        </dct:rights>",
+        '        <dct:license rdf:resource="https://creativecommons.org/licenses/by/4.0/legalcode"/>',  # noqa
+        '        <dcat:accessURL rdf:resource="https://doi.org/10.1234/inveniordm.1234"/>',  # noqa
+        "      </dcat:Distribution>",
+        "    </dcat:distribution>",
+        "    <dcat:distribution>",
+        "      <dcat:Distribution>",
+        '        <dcat:downloadURL rdf:resource="https://127.0.0.1:5000/records/w502q-xzh22/files/big-dataset.zip"/>',  # noqa
+        "        <dcat:mediaType>application/zip</dcat:mediaType>",  # noqa
+        "        <dcat:byteSize>1114324524355</dcat:byteSize>",  # noqa
+        '        <dcat:accessURL rdf:resource="https://doi.org/10.1234/inveniordm.1234"/>',  # noqa
+        "      </dcat:Distribution>",
+        "    </dcat:distribution>",
+        "  </rdf:Description>",
+        "</rdf:RDF>",
+        "",  # this is because of the split
+    ]
+
+    serializer = DCATSerializer()
+    serialized_record = serializer.serialize_object(enhanced_full_record)
+    assert serialized_record == "\n".join(expected_data)
