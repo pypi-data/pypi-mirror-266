99f174efd7508b093844f8a895ba512372fe7934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 06:03:33 +0000

    Add package streamlit-navigation-bar/3.0.3/streamlit-navigation-bar-3.0.3.tar.gz

---
+++ packages/streamlit-navigation-bar/streamlit-navigation-bar-3.0.3.tar.gz/streamlit-navigation-bar-3.0.3/setup.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+from setuptools import setup
+
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="streamlit-navigation-bar",
+    version="3.0.3",
+    author="Gabriel Tem Pass",
+    author_email="redo_hint_0x@icloud.com",
+    description="A component that allows you to place a navigation bar in your Streamlit app.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gabrieltempass/streamlit-navigation-bar",
+    packages=[
+        "streamlit_navigation_bar",
+    ],
+    include_package_data=True,
+    package_data={
+        "streamlit_navigation_bar": ["templates/*.css"],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "streamlit >= 1.29.0, != 1.32.2",  # Navbar doesn't work with 1.32.2
+        "st-theme >= 1.2.2",
+    ],
+)
