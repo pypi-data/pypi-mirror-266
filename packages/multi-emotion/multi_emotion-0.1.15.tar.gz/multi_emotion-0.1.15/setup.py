81e2afd1dde286b7bdad9fdcd7f94b573b5ed966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 05:03:39 +0000

    Add package multi-emotion/0.1.15/multi_emotion-0.1.15.tar.gz

---
+++ packages/multi-emotion/multi_emotion-0.1.15.tar.gz/multi_emotion-0.1.15/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="multi_emotion",
+    version='0.1.15',
+
+    description="detect multiple emotions in a sentence including [anger, anticipation, disgust, fear, joy, love, optimism, pessimism, sadness,\
+                    surprise, trust]",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Jinfen Li",
+    author_email="jli284@syr.edu",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        'Programming Language :: Python :: 3.9'
+    ],
+    url="https://github.com/JinfenLi/multi_emotion_recognition",
+    license="MIT",
+    install_requires=["lightning>=2",
+                      "emotlib",
+                      "numpy", "pandas", "rich", "torchmetrics>=1"],
+    packages=find_packages(),
+
+
+)
