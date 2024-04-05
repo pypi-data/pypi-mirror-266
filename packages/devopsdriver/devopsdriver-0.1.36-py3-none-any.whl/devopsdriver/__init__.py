76c33a34b4fdadc6871eee81e3e21130c8757c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 14:03:58 +0000

    Add package devopsdriver/0.1.36/devopsdriver-0.1.36-py3-none-any.whl

---
+++ packages/devopsdriver/devopsdriver-0.1.36-py3-none-any.whl/devopsdriver/__init__.py
@@ -0,0 +1,9 @@
+""" DevOps tools """
+
+from .azdo import Azure
+from .settings import Settings
+from .sendmail import send_email
+
+__version__ = "0.1.36"
+__author__ = "Marc Page"
+__credits__ = ""
