bb01a4948a4d9f79a4840e33763c9ce5924aa7bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 04:03:34 +0000

    Add package spiakid-drs/0.20/spiakid_drs-0.20-py3-none-any.whl

---
+++ packages/spiakid-drs/spiakid_drs-0.20-py3-none-any.whl/spiakid_DRS/inter.py
@@ -0,0 +1,32 @@
+from SpectralRes import Detect as D
+import os
+import argparse
+from pathlib import Path
+import pathlib
+
+"""
+Link between Terminal and pipeline
+"""
+def parse():
+    # read in command line arguments
+    parser = argparse.ArgumentParser(description='MKID Pipeline CLI')
+    parser.add_argument('--init', action='store_true', help='launch the interface')
+    parser.add_argument('--outp', help='output destination', dest='out_cfg', default=None)
+    parser.add_argument('--inp', help='data location', dest='in_cfg' , default=None)
+    parser.add_argument('--format', help='format of the plot', default='.jpg', dest='form')
+    parser.add_argument('--dir', help='create input and output folder', action='store_true')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse()
+
+    if args.init:
+        try:
+            path = str(sorted(pathlib.Path('/').glob('**/Home.py'))[0]) #Docker version
+        except:
+            path = str(sorted(pathlib.Path('').glob('**/Home.py'))[0])  #Terminal version
+
+        os.system("streamlit run "+path)
+    
+  
