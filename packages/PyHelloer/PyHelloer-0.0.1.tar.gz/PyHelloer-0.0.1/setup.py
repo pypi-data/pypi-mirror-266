73cb5f9168afde4fb1d0270ec5ed2a52d7b04056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 06:03:36 +0000

    Add package PyHelloer/0.0.1/PyHelloer-0.0.1.tar.gz

---
+++ packages/PyHelloer/PyHelloer-0.0.1.tar.gz/PyHelloer-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PyHelloer',
+    version='0.0.1',
+    description='Description',
+    url='https://pypi.org/manage/unverified-account/#account-emails',
+    author=' Zorro18',
+    author_email='danielrose6688@gmail.com',
+    packages=['PyHelloer'],
+    install_requires=[],  # Здесь перечисли зависимости, если они есть
+)
