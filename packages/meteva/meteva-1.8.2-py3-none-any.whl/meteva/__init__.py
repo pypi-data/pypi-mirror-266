dbcd102040d485dab29530e1fd21df53a54e44c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 05:04:01 +0000

    Add package meteva/1.8.2/meteva-1.8.2-py3-none-any.whl

---
+++ packages/meteva/meteva-1.8.2-py3-none-any.whl/meteva/__init__.py
@@ -0,0 +1,10 @@
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
+__author__ = "The R & D Center for Weather Forecasting Technology in NMC, CMA"
+__version__ = '1.8.2'
+
+from . import base
+from . import method
+from . import perspact
+from . import product
+
