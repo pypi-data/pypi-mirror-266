75ee19168b60d7d7b80087a073e91416a98abcd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 05:03:33 +0000

    Add package virtool-cli/2.0.0a21/virtool_cli-2.0.0a21-py3-none-any.whl

---
+++ packages/virtool-cli/virtool_cli-2.0.0a21-py3-none-any.whl/virtool_cli/ncbi/model.py
@@ -0,0 +1,105 @@
+import re
+from enum import StrEnum
+
+from pydantic import BaseModel, Field, AliasChoices, field_validator, computed_field
+
+GATC = re.compile("[gatc]+")
+
+
+class NCBIDatabase(StrEnum):
+    NUCCORE = "nuccore"
+    TAXONOMY = "taxonomy"
+
+
+class NCBIRank(StrEnum):
+    SPECIES = "species"
+    ISOLATE = "isolate"
+
+
+class NCBISource(BaseModel):
+    taxid: int = Field(validation_alias="db_xref")
+    organism: str
+    mol_type: str
+    isolate: str = ""
+    host: str = ""
+    segment: str = ""
+    strain: str = ""
+    clone: str = ""
+
+    @field_validator("taxid", mode="before")
+    @classmethod
+    def db_xref_to_taxid(cls, raw: str) -> int:
+        return int(raw.split(":")[1])
+
+
+class NCBIGenbank(BaseModel):
+    accession: str = Field(validation_alias="GBSeq_primary-accession")
+    definition: str = Field(validation_alias="GBSeq_definition")
+    sequence: str = Field(validation_alias="GBSeq_sequence")
+    source: NCBISource = Field(validation_alias="GBSeq_feature-table")
+    comment: str = Field("", validation_alias="GBSeq_comment")
+
+    @field_validator("sequence", mode="before")
+    @classmethod
+    def validate_sequence(cls, raw: str) -> str:
+        if GATC.match(raw):
+            return raw
+
+        raise ValueError("Invalid sequence code")
+
+    @field_validator("sequence", mode="after")
+    @classmethod
+    def to_uppercase(cls, raw: str) -> str:
+        return raw.upper()
+
+    @field_validator("source", mode="before")
+    @classmethod
+    def create_source(cls, raw: list) -> NCBISource:
+        for feature in raw:
+            if feature["GBFeature_key"] == "source":
+                return NCBISource(
+                    **{
+                        qual["GBQualifier_name"]: qual.get("GBQualifier_value", "")
+                        for qual in feature["GBFeature_quals"]
+                    }
+                )
+
+        raise ValueError("Feature table contains no ``source`` table.")
+
+
+class NCBILineage(BaseModel):
+    id: int = Field(validation_alias=AliasChoices("id", "TaxId"))
+    name: str = Field(validation_alias=AliasChoices("name", "ScientificName"))
+    rank: str = Field(validation_alias=AliasChoices("rank", "Rank"))
+
+
+class NCBITaxonomy(BaseModel):
+    id: int = Field(validation_alias=AliasChoices("id", "TaxId"))
+    name: str = Field(
+        validation_alias=AliasChoices("name", "ScientificName"),
+    )
+    lineage: list[NCBILineage] = Field(
+        validation_alias=AliasChoices("lineage", "LineageEx"),
+    )
+    rank: NCBIRank = Field(validation_alias=AliasChoices("rank", "Rank"))
+
+    @computed_field
+    def species(self) -> NCBILineage:
+        if self.rank is NCBIRank.SPECIES:
+            return NCBILineage(id=self.id, name=self.name, rank=self.rank)
+
+        for item in self.lineage:
+            if item.rank == "species":
+                return item
+
+        raise ValueError("No species level taxon found in lineage")
+
+    @field_validator("lineage", mode="before")
+    @classmethod
+    def create_lineage_list(cls, raw: list[dict]):
+        return [NCBILineage(**level_data) for level_data in raw]
+
+    @field_validator("rank", mode="before")
+    @classmethod
+    def validate_rank(cls, raw: str):
+        return NCBIRank(raw)
