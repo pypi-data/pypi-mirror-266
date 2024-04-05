3d1157776c747fa9bf8f9b935c97a9262fb3b17d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 05:03:24 +0000

    Add package f2/0.0.1.5/f2-0.0.1.5.tar.gz

---
+++ packages/f2/f2-0.0.1.5.tar.gz/f2-0.0.1.5/f2/apps/douyin/crawler.py
@@ -0,0 +1,269 @@
+# path: f2/apps/douyin/crawler.py
+
+import f2
+
+from f2.log.logger import logger
+from f2.i18n.translator import _
+from f2.utils.conf_manager import ConfigManager
+from f2.crawlers.base_crawler import BaseCrawler
+from f2.apps.douyin.api import DouyinAPIEndpoints as dyendpoint
+from f2.apps.douyin.model import (
+    UserProfile,
+    UserPost,
+    UserLike,
+    UserCollection,
+    UserCollects,
+    UserCollectsVideo,
+    UserMusicCollection,
+    PostDetail,
+    UserMix,
+    UserLive,
+    UserLive2,
+    FollowUserLive,
+    LoginGetQr,
+    LoginCheckQr,
+    UserFollowing,
+    UserFollower,
+)
+from f2.apps.douyin.utils import XBogusManager
+
+
+class DouyinCrawler(BaseCrawler):
+    def __init__(
+        self,
+        kwargs: dict = ...,
+    ):
+        f2_manager = ConfigManager(f2.F2_CONFIG_FILE_PATH)
+        f2_conf = f2_manager.get_config("f2").get("douyin")
+        proxies_conf = kwargs.get("proxies", {"http": None, "https": None})
+        proxies = {
+            "http://": proxies_conf.get("http", None),
+            "https://": proxies_conf.get("https", None),
+        }
+
+        self.headers = {
+            "User-Agent": f2_conf["headers"]["User-Agent"],
+            "Referer": f2_conf["headers"]["Referer"],
+            "Cookie": kwargs["cookie"],
+        }
+
+        super().__init__(proxies=proxies, crawler_headers=self.headers)
+
+    async def fetch_user_profile(self, params: UserProfile):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.USER_DETAIL,
+            params.dict(),
+        )
+        logger.debug(_("用户信息接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_user_post(self, params: UserPost):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.USER_POST,
+            params.dict(),
+        )
+        logger.debug(_("主页作品接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_user_like(self, params: UserLike):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.USER_FAVORITE_A,
+            params.dict(),
+        )
+        logger.debug(_("主页喜欢作品接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_user_collection(self, params: UserCollection):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.USER_COLLECTION,
+            params.dict(),
+        )
+        logger.debug(_("主页收藏作品接口地址：{0}").format(endpoint))
+        return await self._fetch_post_json(endpoint, params.dict())
+
+    async def fetch_user_collects(self, params: UserCollects):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.USER_COLLECTS,
+            params.dict(),
+        )
+        logger.debug(_("收藏夹接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_user_collects_video(self, params: UserCollectsVideo):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.USER_COLLECTS_VIDEO,
+            params.dict(),
+        )
+        logger.debug(_("收藏夹作品接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_user_music_collection(self, params: UserMusicCollection):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.USER_MUSIC_COLLECTION,
+            params.dict(),
+        )
+        logger.debug(_("音乐收藏接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_user_mix(self, params: UserMix):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.MIX_AWEME,
+            params.dict(),
+        )
+        logger.debug(_("合集作品接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_post_detail(self, params: PostDetail):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.POST_DETAIL,
+            params.dict(),
+        )
+        logger.debug(_("作品详情接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_post_comment(self, params: PostDetail):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.POST_COMMENT,
+            params.dict(),
+        )
+        logger.debug(_("作品评论接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_post_feed(self, params: PostDetail):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.TAB_FEED,
+            params.dict(),
+        )
+        logger.debug(_("首页推荐作品接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_follow_feed(self, params: PostDetail):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.FOLLOW_FEED,
+            params.dict(),
+        )
+        logger.debug(_("关注作品接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_friend_feed(self, params: PostDetail):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.FRIEND_FEED,
+            params.dict(),
+        )
+        logger.debug(_("朋友作品接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_post_related(self, params: PostDetail):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.POST_RELATED,
+            params.dict(),
+        )
+        logger.debug(_("相关推荐作品接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_live(self, params: UserLive):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.LIVE_INFO,
+            params.dict(),
+        )
+        logger.debug(_("直播接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_live_room_id(self, params: UserLive2):
+        original_headers = self.aclient.headers.copy()
+        try:
+            # 避免invalid session
+            self.aclient.headers.update({"Cookie": ""})
+            endpoint = XBogusManager.model_2_endpoint(
+                self.headers.get("User-Agent"),
+                dyendpoint.LIVE_INFO_ROOM_ID,
+                params.dict(),
+            )
+            logger.debug(_("直播接口地址（room_id）：{0}").format(endpoint))
+            return await self._fetch_get_json(endpoint)
+        finally:
+            self.aclient.headers = original_headers
+
+    async def fetch_follow_live(self, params: FollowUserLive):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.FOLLOW_USER_LIVE,
+            params.dict(),
+        )
+        logger.debug(_("关注用户直播接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_locate_post(self, params: UserPost):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.LOCATE_POST,
+            params.dict(),
+        )
+        logger.debug(_("定位上一次作品接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_login_qrcode(self, parms: LoginGetQr):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.SSO_LOGIN_GET_QR,
+            parms.dict(),
+        )
+        logger.debug(_("SSO获取二维码接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_check_qrcode(self, parms: LoginCheckQr):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.SSO_LOGIN_CHECK_QR,
+            parms.dict(),
+        )
+        logger.debug(_("SSO检查扫码状态接口地址：{0}").format(endpoint))
+        return await self._fetch_response(endpoint)
+
+    async def fetch_check_login(self, parms: LoginCheckQr):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.SSO_LOGIN_CHECK_LOGIN,
+            parms.dict(),
+        )
+        logger.debug(_("SSO检查登录状态接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_user_following(self, params: UserFollowing):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.USER_FOLLOWING,
+            params.dict(),
+        )
+        logger.debug(_("用户关注列表接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def fetch_user_follower(self, params: UserFollower):
+        endpoint = XBogusManager.model_2_endpoint(
+            self.headers.get("User-Agent"),
+            dyendpoint.USER_FOLLOWER,
+            params.dict(),
+        )
+        logger.debug(_("用户粉丝列表接口地址：{0}").format(endpoint))
+        return await self._fetch_get_json(endpoint)
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.close()
---
+++ packages/f2/f2-0.0.1.5.tar.gz/f2-0.0.1.5/f2/exceptions/file_exceptions.py
@@ -0,0 +1,48 @@
+# path: f2/exceptions/file_exceptions.py
+
+from f2.cli.cli_console import RichConsoleManager
+
+exception_console = RichConsoleManager().exception_console
+
+
+class FileError(Exception):
+    """基本的文件错误异常类，其他文件异常都会继承这个类"""
+
+    def __init__(self, message, filepath=None):
+        exception_console.print(
+            "请前往QA文档 https://johnserf-seed.github.io/f2/question-answer/qa.html 查看相关帮助"
+        )
+        self.filepath = filepath
+        super().__init__(message)
+
+    def __str__(self):
+        """返回错误信息和文件路径（如果有的话）"""
+        return f"{super().__str__()} Filepath: {self.filepath}" if self.filepath else ""
+
+
+class FileNotFound(FileError):
+    """文件不存在错误"""
+
+    def __init__(self, message=None, filepath=None):
+        super().__init__(message, filepath)
+
+
+class FilePermissionError(FileError):
+    """文件权限错误"""
+
+    def __init__(self, message, filepath=None):
+        super().__init__(message, filepath)
+
+
+class FileReadError(FileError):
+    """文件读取错误"""
+
+    def __init__(self, message, filepath=None):
+        super().__init__(message, filepath)
+
+
+class FileWriteError(FileError):
+    """文件写入错误"""
+
+    def __init__(self, message, filepath=None):
+        super().__init__(message, filepath)
