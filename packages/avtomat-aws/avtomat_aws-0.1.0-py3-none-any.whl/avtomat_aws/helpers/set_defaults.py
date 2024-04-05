25a65103e011e08417ee3657bbc408d71f88c401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 06:03:37 +0000

    Add package avtomat-aws/0.1.0/avtomat_aws-0.1.0-py3-none-any.whl

---
+++ packages/avtomat-aws/avtomat_aws-0.1.0-py3-none-any.whl/avtomat_aws/helpers/set_defaults.py
@@ -0,0 +1,10 @@
+def set_defaults(kwargs, default_values):
+    """Set default values for input parameters."""
+
+    if default_values is None:
+        return kwargs
+
+    for key, default in default_values.items():
+        kwargs[key] = default if kwargs.get(key) is None else kwargs[key]
+
+    return kwargs
