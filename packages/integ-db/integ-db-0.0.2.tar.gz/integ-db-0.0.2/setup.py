d1c222553a944eb235ceefc7936e59c177b99542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 15:04:14 +0000

    Add package integ-db/0.0.2/integ-db-0.0.2.tar.gz

---
+++ packages/integ-db/integ-db-0.0.2.tar.gz/integ-db-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="integ-db",
+    version="0.0.2",
+    author="byeongin.jeong",
+    author_email="jbi0214@gmail.com",
+    description="Python Integrated Database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Byeongin-Jeong/integdb",
+    project_urls={
+        "Bug Tracker": "https://github.com/Byeongin-Jeong/integdb/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src/integrate"},
+    py_modules=['integdb'],
+    packages=find_packages(where="integrate"),
+    python_requires=">=3.6",
+    keywords=['mysql', 'mssql', 'mariadb', 'python db', 'python database', 'integrate database', 'sqlalchemy'],
+    install_requires=['pymysql', 'mariadb', 'pymssql', 'pymysql-pool', 'SQLAlchemy', 'pandas'],
+)
---
+++ packages/integ-db/integ-db-0.0.2.tar.gz/integ-db-0.0.2/src/integrate/module/__init__.py
@@ -0,0 +1,4 @@
+from module.mysql import MySQL
+from module.mssql import MSSQL
+from module.mariadb import MariaDB
+from module.sqlalchemy import SQLAlchemy
