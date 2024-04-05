8614a02f0d335e7576aa1cf4c7d17ddf7e878e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 15:04:41 +0000

    Add package advcalc/0.0.5/advcalc-0.0.5.tar.gz

---
+++ packages/advcalc/advcalc-0.0.5.tar.gz/advcalc-0.0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5' 
+DESCRIPTION = 'Advanced Python Calculator'
+LONG_DESCRIPTION = 'An advanced Python calculator'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="advcalc", 
+        version=VERSION,
+        author="Zack",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        keywords=['python', 'calculator'],
+)
