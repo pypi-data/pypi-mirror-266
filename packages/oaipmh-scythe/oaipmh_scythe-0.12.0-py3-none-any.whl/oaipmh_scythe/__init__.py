881a801b661bc25d7a26a626bdbd1cb299d7ee3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 03:03:42 +0000

    Add package oaipmh-scythe/0.12.0/oaipmh_scythe-0.12.0-py3-none-any.whl

---
+++ packages/oaipmh-scythe/oaipmh_scythe-0.12.0-py3-none-any.whl/oaipmh_scythe/__init__.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2015 Mathias Loesch
+# SPDX-FileCopyrightText: 2023 Heinz-Alexander Fütterer
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""oaipmh-scythe: A Scythe for harvesting OAI-PMH repositories."""
+
+from oaipmh_scythe.client import Scythe
+from oaipmh_scythe.response import OAIResponse
+
+__all__ = [
+    "Scythe",
+    "OAIResponse",
+]
