5aff837b0aa44120e6fda5d10399edef93e37201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 15:04:07 +0000

    Add package RAG-metrics/0.0.1.25/RAG_metrics-0.0.1.25.tar.gz

---
+++ packages/RAG-metrics/RAG_metrics-0.0.1.25.tar.gz/RAG_metrics-0.0.1.25/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# Function to read the list of requirements from 'requirements.txt'
+def read_requirements():
+    with open('requirements.txt', 'r') as req:
+        return [line for line in req.read().splitlines() if "file:" not in line]
+    
+
+setup(
+    name='RAG_metrics',
+    version='0.0.1.25', # all versions prior to launch will go into 0.0.1.--
+    packages=find_packages(),
+    license='MIT',
+    description='This library is to search the best parameters across different steps of the RAG process.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Ankit',
+    author_email='a.baliyan008@gmail.com',
+    url='https://github.com/hidevscommunity/gen-ai-library/tree/main/Ankit',
+    install_requires=read_requirements(),
+)
+
+
+# python setup.py sdist bdist_wheel
