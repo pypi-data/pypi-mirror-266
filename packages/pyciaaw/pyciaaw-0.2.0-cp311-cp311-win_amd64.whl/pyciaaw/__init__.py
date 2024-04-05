9bbed6e5a454df5bc87c3da3db00ec62fd047307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 07:03:26 +0000

    Add package pyciaaw/0.2.0/pyciaaw-0.2.0-cp311-cp311-win_amd64.whl

---
+++ packages/pyciaaw/pyciaaw-0.2.0-cp311-cp311-win_amd64.whl/pyciaaw/__init__.py
@@ -0,0 +1,8 @@
+"""Standard Atomic Weights and Isotopes.
+
+Standard Atomic Weights: the lattest values for are from 2021.
+
+"""
+from .version import __version__
+from . import saw_latest as saw
+from . import saw_2021
