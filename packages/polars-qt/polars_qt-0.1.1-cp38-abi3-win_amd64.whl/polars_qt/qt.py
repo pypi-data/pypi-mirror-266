c97f8bd37b818debde5dca95da800d445b64d9b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 04:03:11 +0000

    Add package polars-qt/0.1.1/polars_qt-0.1.1-cp38-abi3-win_amd64.whl

---
+++ packages/polars-qt/polars_qt-0.1.1-cp38-abi3-win_amd64.whl/polars_qt/qt.py
@@ -0,0 +1,13 @@
+import polars as pl
+from .funcs import *
+
+@pl.api.register_expr_namespace("qt")
+class ExprQuantExtend:
+    def __init__(self, expr: pl.Expr):
+        self.expr = expr
+
+    def rolling_rank(self, window, min_periods=None, pct=False, rev=False) -> pl.Expr:
+        return rolling_rank(self.expr, window=window, min_periods=min_periods, pct=pct, rev=rev)
+    
+    def if_then(self, flag, then):
+        return if_then(flag, then, self.expr)
