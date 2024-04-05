abf156c2a373ea4450c3df3c829ef954ff6d6441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 03:05:46 +0000

    Add package windowsregistry/0.1.0/windowsregistry-0.1.0.tar.gz

---
+++ packages/windowsregistry/windowsregistry-0.1.0.tar.gz/windowsregistry-0.1.0/windowsregistry/__init__.py
@@ -0,0 +1,19 @@
+from . import models
+from .core import RegistryPath, open_subkey
+from .errors import WindowsRegistryError
+
+HKCR = HKEY_CLASSES_ROOT = open_subkey("HKCR")
+HKCU = HKEY_CURRENT_USER = open_subkey("HKCU")
+HKLM = HKEY_LOCAL_MACHINE = open_subkey("HKLM")
+HKU = HKEY_USERS = open_subkey("HKU")
+
+__all__ = [
+    "models",
+    "RegistryPath",
+    "open_subkey",
+    "WindowsRegistryError",
+    "HKCR",
+    "HKCU",
+    "HKLM",
+    "HKU"
+]
