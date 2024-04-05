7d191de3c347bdb34628f550fdee36792d60d152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 04:03:12 +0000

    Add package praklib/0.0.7/praklib-0.0.7.tar.gz

---
+++ packages/praklib/praklib-0.0.7.tar.gz/praklib-0.0.7/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='praklib',
+    version='0.0.7',
+    author='Brystof_Klazek',
+    author_email='blazekr2@cvut.cz',
+    description='Moje mala knihovna na praktika',
+    url='',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
