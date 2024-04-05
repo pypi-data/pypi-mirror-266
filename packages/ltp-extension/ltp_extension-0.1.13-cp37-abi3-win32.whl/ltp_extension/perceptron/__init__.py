5fad083957eb42c7b6d9cc6d38f4c4330e7534d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 06:03:09 +0000

    Add package ltp-extension/0.1.13/ltp_extension-0.1.13-cp37-abi3-win32.whl

---
+++ packages/ltp-extension/ltp_extension-0.1.13-cp37-abi3-win32.whl/ltp_extension/perceptron/__init__.py
@@ -0,0 +1,14 @@
+# Generated content DO NOT EDIT
+from .. import perceptron
+
+Algorithm = perceptron.Algorithm
+CWSModel = perceptron.CWSModel
+CWSTrainer = perceptron.CWSTrainer
+CharacterType = perceptron.CharacterType
+Model = perceptron.Model
+ModelType = perceptron.ModelType
+NERModel = perceptron.NERModel
+NERTrainer = perceptron.NERTrainer
+POSModel = perceptron.POSModel
+POSTrainer = perceptron.POSTrainer
+Trainer = perceptron.Trainer
