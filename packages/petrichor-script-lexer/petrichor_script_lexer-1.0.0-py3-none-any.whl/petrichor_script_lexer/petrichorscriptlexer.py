5eb0c0faceb1cb8cfd1e858fc1d6a76432c5a955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 15:04:07 +0000

    Add package petrichor-script-lexer/1.0.0/petrichor_script_lexer-1.0.0-py3-none-any.whl

---
+++ packages/petrichor-script-lexer/petrichor_script_lexer-1.0.0-py3-none-any.whl/petrichor_script_lexer/petrichorscriptlexer.py
@@ -0,0 +1,26 @@
+from pygments.lexer import RegexLexer, bygroups
+from pygments.token import *
+
+
+__all__ = ('PetrichorScriptLexer')
+
+
+class PetrichorScriptLexer(RegexLexer):
+	name = 'PetrichorScript'
+	aliases = ['petrichor']
+	filenames = ['*.petrichor','*.ptcr']
+
+	tokens = {
+		'root': [
+			(r'\s+', Text), # whitespace
+			(r'\\.', String.Escape), # escaped character
+			(r'//.*\n', Comment.Single),
+			(r'[\{\},]', Operator),
+			(r'::', Operator),
+			(r'([a-z][a-z0-9\-]*)(\s*)(:(?!:))', bygroups(Keyword.Reserved, Text, Operator)),
+			(r'\[[a-z\-]+\]', Name.Variable),
+			(r'".*"', String),
+			(r'[0-9]+', Number),
+			(r'.', Text),
+		]
+	}
