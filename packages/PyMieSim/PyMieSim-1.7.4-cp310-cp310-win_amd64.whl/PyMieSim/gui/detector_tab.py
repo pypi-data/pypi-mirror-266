a114d893b749f8006f9359fa8a866ede1b857d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 06:04:55 +0000

    Add package PyMieSim/1.7.4/PyMieSim-1.7.4-cp310-cp310-win_amd64.whl

---
+++ packages/PyMieSim/PyMieSim-1.7.4-cp310-cp310-win_amd64.whl/PyMieSim/gui/detector_tab.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from typing import NoReturn
+import tkinter
+from PyMieSim.experiment.detector import Photodiode, LPMode
+from PyMieSim.gui.base_tab import BaseTab
+from PyMieSim.gui.utils import Widget, WidgetCollection
+
+
+class DetectorTab(BaseTab):
+    """
+    A GUI tab for configuring the detector parameters for simulations in PyMieSim.
+
+    Allows for the setup of detector characteristics such as numerical aperture (NA),
+    angular offsets (gamma and phi), and polarization filter angle, facilitating
+    the detailed configuration of the simulation's detector component.
+
+    Attributes:
+        variables (WidgetCollection): A collection of widgets for detector configuration.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes the DetectorTab with UI components for detector configuration.
+
+        Parameters:
+            *args: Variable length argument list for BaseTab.
+            **kwargs: Arbitrary keyword arguments for BaseTab.
+        """
+        self.type_button = tkinter.StringVar(value='Photodiode')
+
+        super().__init__(*args, **kwargs)
+
+        self._setup_combobox()
+
+    def _setup_combobox(self):
+        """
+        Sets up the combobox for selecting the scatterer type.
+        """
+        combobox = tkinter.ttk.Combobox(
+            self.frame,
+            textvariable=self.type_button,
+            values=['Photodiode', 'LPMode'],
+            state="readonly"
+        )
+
+        combobox.pack(side=tkinter.TOP)
+        combobox.bind("<<ComboboxSelected>>", self.on_type_change)
+
+    def on_type_change(self, event=None) -> NoReturn:
+        """
+        Handles the scatterer type change event, updates the UI widgets and scatterer configuration accordingly.
+        """
+        self.widget_collection.clear_widgets()
+
+        self.setup()
+
+    def get_selected_type(self) -> str:
+        """
+        Retrieves the currently selected scatterer type.
+
+        Returns:
+            The selected scatterer type as a string.
+        """
+        return self.type_button.get()
+
+    def setup(self) -> NoReturn:
+        """
+        Configures the GUI elements for the Detector tab.
+
+        Sets up labels, entry fields, and other UI components based on the detector
+        configuration options, enabling user interaction for configuring the detector.
+        """
+        match self.get_selected_type().lower():
+            case 'photodiode':
+                self._setup_photodiode_widgets()
+                self._setup_photodiode_component()
+            case 'lpmode':
+                self._setup_lpmode_widgets()
+                self._setup_lpmode_component()
+            case _:
+                raise ValueError('Detector type not valid')
+
+    def _setup_photodiode_widgets(self):
+        self.widget_collection = WidgetCollection(
+            Widget(default_value='0.2, 0.3, 0.4', label='Numerical aperture (NA)', component_label='NA'),
+            Widget(default_value='0', label='Gamma [degree]', component_label='gamma_offset'),
+            Widget(default_value='0:360:200', label='Phi [degree]', component_label='phi_offset'),
+            Widget(default_value='None', label='Polarization filter [degree]', component_label='polarization_filter')
+        )
+
+        self.widget_collection.setup_widgets(frame=self.frame)
+
+    def _setup_lpmode_widgets(self):
+        self.widget_collection = WidgetCollection(
+            Widget(default_value='0.', label='Polarization filter [degree]', component_label='polarization_filter'),
+            Widget(default_value='0', label='Gamma [degree]', component_label='gamma_offset'),
+            Widget(default_value='180:-180:200', label='Phi [degree]', component_label='phi_offset'),
+            Widget(default_value='0.2, 0.3, 0.4', label='Numerical aperture (NA)', component_label='NA'),
+            Widget(default_value='LP01', label='Mode field', component_label='mode_number', to_float=False),
+        )
+
+        self.widget_collection.setup_widgets(frame=self.frame)
+
+    def _setup_photodiode_component(self):
+        self.component = Photodiode(**self.widget_collection.to_component_dict(), sampling=500)
+
+        self.mapping = {
+            'NA': self.component.NA,
+            'gamma': self.component.gamma_offset,
+            'phi': self.component.phi_offset,
+            'polarization_filter': self.component.polarization_filter
+        }
+
+    def _setup_lpmode_component(self):
+        self.component = LPMode(**self.widget_collection.to_component_dict(), sampling=500)
+
+        self.mapping = {
+            'NA': self.component.NA,
+            'gamma': self.component.gamma_offset,
+            'phi': self.component.phi_offset,
+            'polarization_filter': self.component.polarization_filter
+        }
+
+    def setup_component(self) -> NoReturn:
+        """
+        Initializes the Photodiode detector component based on user input.
+
+        This method reads input values from the UI widgets and uses them to configure
+        a Photodiode detector component for the simulation, including setting the sampling
+        parameter explicitly.
+        """
+        self.update_user_input()
+
+        selected_type = self.get_selected_type()
+
+        match selected_type.lower():
+            case 'photodiode':
+                self._setup_photodiode_component()
+            case 'lpmode':
+                self._setup_lpmode_component()
+
+# -
---
+++ packages/PyMieSim/PyMieSim-1.7.4-cp310-cp310-win_amd64.whl/PyMieSim/single/detector.py
@@ -0,0 +1,276 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from PyMieSim import single
+    from PyMieSim.single.scatterer import Sphere, CoreShell, Cylinder
+
+
+import numpy
+import logging
+from dataclasses import dataclass, field
+from PyMieSim import load_single_lp_mode
+
+from PyMieSim.single.representations import Footprint
+from PyMieSim.binary.Fibonacci import FibonacciMesh as CPPFibonacciMesh  # has to be imported as extension
+from PyMieSim.binary.DetectorInterface import BindedDetector
+from PyMieSim.tools.special_functions import NA_to_angle
+
+from MPSPlots.render3D import SceneList as SceneList3D
+
+
+class GenericDetector():
+    """
+    Base class for different types of detectors with methods for setup, rotation,
+    calculating light coupling, and generating footprint and 3D plots.
+    """
+
+    def __init__(self, **kwargs):
+        self.initialize_attributes(**kwargs)
+        self.initialize()
+
+    def initialize_attributes(self, **kwargs):
+        """
+        Initialize detector attributes from keyword arguments.
+        """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def initialize(self):
+        """
+        Perform initial setup by setting max angle based on NA, converting
+        polarization_filter to float, and binding C++ backend.
+        """
+        self.max_angle = NA_to_angle(NA=self.NA)
+        self.polarization_filter = numpy.float64(self.polarization_filter)
+        self.set_cpp_binding()
+
+    def set_cpp_binding(self):
+        """
+        Establish binding with the C++ backend for the detector.
+        """
+        point_coupling = (self.coupling_mode.lower() == 'point')
+
+        self.cpp_binding = BindedDetector(
+            scalar_field=self.unstructured_farfield,
+            NA=self.NA,
+            phi_offset=numpy.deg2rad(self.phi_offset),
+            gamma_offset=numpy.deg2rad(self.gamma_offset),
+            polarization_filter=numpy.deg2rad(self.polarization_filter),
+            coherent=self.coherent,
+            rotation_angle=0,
+            point_coupling=point_coupling
+        )
+
+    def rotate_around_axis(self, rotation_angle: float):
+        """
+        Rotate the detector mesh around its principal axis by a given angle.
+
+        Args:
+            rotation_angle (float): Rotation angle in degrees.
+        """
+        self.cpp_binding.mesh.rotate_around_axis(rotation_angle)
+
+    def coupling(self, scatterer: Sphere | CoreShell | Cylinder) -> float:
+        r"""
+        Calculate the light coupling between the detector and a scatterer.
+
+        .. math::
+            |\iint_{\Omega}  \Phi_{det} \,\, \Psi_{scat}^* \,  d \Omega|^2
+
+        | Where:
+        |   :math:`\Phi_{det}` is the capturing field of the detector and
+        |   :math:`\Psi_{scat}` is the scattered field.
+
+        Args:
+            scatterer (Sphere|CoreShell|Cylinder): The scatterer object.
+
+        Returns:
+            float: The coupling in watts.
+        """
+        return getattr(self.cpp_binding, "Coupling" + type(scatterer).__name__)(scatterer.Bind)
+
+    def get_footprint(self, scatterer: Sphere | CoreShell | Cylinder) -> Footprint:
+        r"""
+        Generate the footprint of the scattered light coupling with the detector.
+
+        .. math::
+            \big| \mathscr{F}^{-1} \big\{ \tilde{ \psi } (\xi, \nu),\
+                   \tilde{ \phi}_{l,m}(\xi, \nu)  \big\}
+                   (\delta_x, \delta_y) \big|^2
+
+        | Where:
+        |   :math:`\Phi_{det}` is the capturing field of the detector and
+        |   :math:`\Psi_{scat}` is the scattered field.
+
+        Args:
+            scatterer (Sphere|CoreShell|Cylinder): The scatterer object.
+
+        Returns:
+            Footprint: The scatterer footprint with this detector.
+        """
+        return Footprint(scatterer=scatterer, detector=self)
+
+    def plot(self) -> SceneList3D:
+        """
+        Plot the real and imaginary parts of the scattered fields.
+
+        Returns:
+            SceneList3D: The 3D plotting scene containing the field plots.
+        """
+        coordinate = numpy.row_stack((
+            self.cpp_binding.mesh.x,
+            self.cpp_binding.mesh.y,
+            self.cpp_binding.mesh.z
+        ))
+
+        figure = SceneList3D()
+
+        for scalar_type in ['real', 'imag']:
+            scalar = getattr(self.unstructured_farfield, scalar_type)
+
+            ax = figure.append_ax()
+            artist = ax.add_unstructured_mesh(
+                coordinates=coordinate,
+                scalar_coloring=scalar,
+                symmetric_map=True,
+                symmetric_colormap=True
+            )
+
+            ax.add_unit_sphere(opacity=0.3)
+            ax.add_unit_axis(show_label=False)
+            ax.add_colorbar(artist=artist, title=f'field [{scalar_type}]')
+
+        return figure
+
+
+@dataclass
+class Photodiode(GenericDetector):
+    """
+    Detector type class representing a photodiode, light coupling mechanism is non-coherent and thus
+    independent of the phase of the impinging scattered light field.
+    """
+    NA: float
+    """ Numerical aperture of imaging system. """
+    gamma_offset: float
+    """ Angle [Degree] offset of detector in the direction perpendicular to polarization. """
+    phi_offset: float
+    """ Angle [Degree] offset of detector in the direction parallel to polarization. """
+    sampling: int = 200
+    """ Sampling of the farfield distribution """
+    polarization_filter: float = None
+    """ Angle [Degree] of polarization filter in front of detector. """
+    coherent: bool = field(default=False, init=False)
+    """ Indicate if the coupling mechanism is coherent or not """
+    coupling_mode: str = field(default='point', init=False)
+    """ indicate if the coupling mechanism is point-wise or mean-wise. Value is either point or mean. """
+
+    def __post_init__(self):
+        self.unstructured_farfield = numpy.ones(self.sampling, dtype=complex)
+
+        super().initialize()
+
+    def get_structured_scalarfield(self, sampling: int = 500) -> numpy.ndarray:
+        return numpy.ones([sampling, sampling])
+
+
+@dataclass
+class IntegratingSphere(GenericDetector):
+    """
+    Detector type class representing a photodiode, light coupling mechanism is non-coherent and thus
+    independent of the phase of the impinging scattered light field.
+    """
+    sampling: int = 200
+    """ Sampling of the farfield distribution """
+    polarization_filter: float = None
+    """ Angle [Degree] of polarization filter in front of detector. """
+    NA: float = field(default=2, init=False)
+    """ Numerical aperture of imaging system. """
+    gamma_offset: float = field(default=0, init=False)
+    """ Angle [Degree] offset of detector in the direction perpendicular to polarization. """
+    phi_offset: float = field(default=0, init=False)
+    """ Angle [Degree] offset of detector in the direction parallel to polarization. """
+    coherent: bool = field(default=False, init=False)
+    """ Indicate if the coupling mechanism is coherent or not """
+    coupling_mode: str = field(default='point', init=False)
+    """ indicate if the coupling mechanism is point-wise or mean-wise. Value is either point or mean. """
+
+    def __post_init__(self):
+        self.unstructured_farfield = numpy.ones(self.sampling, dtype=complex)
+
+        super().initialize()
+
+    def get_structured_scalarfield(self, sampling: int = 500) -> numpy.ndarray:
+        return numpy.ones([sampling, sampling])
+
+
+@dataclass
+class LPMode(GenericDetector):
+    """
+    Detector type class representing a fiber LP mode, light coupling mechanism is coherent
+    and thus, dependent of the phase of the impinging scattered light field.
+    """
+    mode_number: str
+    """ String representing the LP mode to be initialized (e.g. 'LP01', 'LP11' etc)"""
+    NA: float
+    """ Numerical aperture of imaging system. """
+    gamma_offset: float
+    """ Angle [Degree] offset of detector in the direction perpendicular to polarization. """
+    phi_offset: float
+    """ Angle [Degree] offset of detector in the direction parallel to polarization. """
+    sampling: int = 200
+    """ Sampling of the farfield distribution """
+    polarization_filter: float = None
+    """ Angle [Degree] of polarization filter in front of detector. """
+    coupling_mode: str = 'Point'
+    """ indicate if the coupling mechanism is point-wise or mean-wise. Value is either point or mean. """
+    coherent: bool = field(default=True, init=False)
+    """ Indicate if the coupling mechanism is coherent or not. """
+
+    def __post_init__(self):
+        if self.NA > 0.3 or self.NA < 0:
+            logging.warning(f"High values of NA: {self.NA} do not comply with the paraxial approximation. Value under 0.3 are prefered.")
+
+        self.interpret_mode_name()
+
+        self.unstructured_farfield = load_single_lp_mode(
+            mode_number=self.mode_number,
+            structure_type='unstructured',
+            sampling=self.sampling
+        )
+
+        super().initialize()
+
+        if self.rotation_angle != 0:
+            self.rotate_around_axis(self.rotation_angle)
+
+    def interpret_mode_name(self) -> None:
+        """
+        Intepret the mode_number parameter to check if there is a rotation associated
+
+        :returns:   { description_of_the_return_value }
+        :rtype:     None
+        """
+        assert isinstance(self.mode_number, str), "Mode number must be a string. Example 'LP01' or 'LP11:90 (rotation of 90 degree)'"
+        if ':' not in self.mode_number:
+            self.mode_number += ':0'
+
+        split = self.mode_number.split(':')
+
+        self.mode_number, rotation_angle = split
+
+        self.rotation_angle = float(rotation_angle)
+
+    def get_structured_scalarfield(self, sampling: int = 500) -> numpy.ndarray:
+        far_field = load_single_lp_mode(
+            mode_number=self.mode_number,
+            structure_type='structured',
+            sampling=sampling,
+        )
+
+        return far_field
+
+
+# -
