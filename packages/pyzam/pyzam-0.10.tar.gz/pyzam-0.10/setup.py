7e2d5e5b868e6b5b916cfb899865e33fc4c03a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 15:04:22 +0000

    Add package pyzam/0.10/pyzam-0.10.tar.gz

---
+++ packages/pyzam/pyzam-0.10.tar.gz/pyzam-0.10/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyzam",
+    version="0.10",
+    entry_points={"console_scripts": ["pyzam = pyzam.__main__:main"]},
+    author="lukafilipxvic",
+    description="A CLI music recognition tool for audio and mixtapes using Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lukafilipxvic/Pyzam",
+    install_requires=[
+        "asyncio",
+        "rich",
+        "soundfile",
+        "shazamio",
+        "SoundCard",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.9",
+)
