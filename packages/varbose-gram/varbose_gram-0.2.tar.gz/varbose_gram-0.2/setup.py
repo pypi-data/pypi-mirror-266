00002028d669a9aeb95e780892ac3e5a574c03fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 04:03:35 +0000

    Add package varbose-gram/0.2/varbose_gram-0.2.tar.gz

---
+++ packages/varbose-gram/varbose_gram-0.2.tar.gz/varbose_gram-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='varbose_gram',
+    version='0.2',
+    packages=find_packages(),
+    author='grubx64',
+    author_email='nn.neisven@gmail.com',
+    description='package for creating telegram bot',
+    url='https://github.com/tech-voyager/zero_gram',
+)
