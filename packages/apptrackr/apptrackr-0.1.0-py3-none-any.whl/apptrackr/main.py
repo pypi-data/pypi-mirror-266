f04d5a8d8fde2eece25a1d1fa191d89caacf67d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 16:03:30 +0000

    Add package apptrackr/0.1.0/apptrackr-0.1.0-py3-none-any.whl

---
+++ packages/apptrackr/apptrackr-0.1.0-py3-none-any.whl/apptrackr/main.py
@@ -0,0 +1,158 @@
+from typing import Optional
+
+import typer
+from .model import Applications
+from typing_extensions import Annotated
+
+tracker = typer.Typer()
+
+
+@tracker.command()
+def count(
+    active: Annotated[
+        bool,
+        typer.Option(
+            help="Get only active application",
+        ),
+    ] = False
+) -> None:
+    """Get count of application"""
+    if active:
+        count = Applications.select().where(Applications.status == "active").count()
+    else:
+        count = Applications.select().count()
+
+    typer.echo(
+        typer.style(f"Application Count: {count}", fg=typer.colors.BRIGHT_WHITE),
+    )
+
+
+@tracker.command()
+def add_application(
+    name: Annotated[str, typer.Argument(help="Job application name")],
+    status: Annotated[str, typer.Argument(help="Job status")],
+    apply_link: Annotated[str, typer.Argument(help="Application link")],
+    location: Annotated[
+        Optional[str], typer.Argument(help="Optional Job location")
+    ] = None,
+    date: Annotated[
+        Optional[str], typer.Argument(help="Optional Application date")
+    ] = None,
+) -> None:
+    if status not in ("active", "rejected", "accepted"):
+        typer.echo("Invalid status argument!")
+        raise typer.Exit(-1)
+
+    application_details = {"name": name, "status": status, "apply_link": apply_link}
+
+    if location:
+        application_details["location"] = location
+
+    if date:
+        application_details["date"] = date
+
+    application = Applications(**application_details)
+    application.save()
+
+
+@tracker.command()
+def fetch_applications(
+    name: Annotated[
+        Optional[str],
+        typer.Option("--name", "-n", help="Filter by application name"),
+    ] = None,
+    status: Annotated[
+        Optional[str],
+        typer.Option("--status", "-s", help="Filter by application status"),
+    ] = None,
+    location: Annotated[
+        Optional[str],
+        typer.Option("--location", "-l", help="Filter by application location"),
+    ] = None,
+    date: Annotated[
+        Optional[str],
+        typer.Option("--date", "-d", help="Filter by application date"),
+    ] = None,
+    apply_link: Annotated[
+        Optional[str],
+        typer.Option("--apply-link", "-a", help="Filter by application apply link"),
+    ] = None,
+    page_number: Annotated[
+        Optional[int], typer.Option("--page", "-p", help="Page number")
+    ] = None,
+):
+    filters = []
+    columns = {
+        "name": name,
+        "status": status,
+        "location": location,
+        "date": date,
+        "apply_link": apply_link,
+    }
+    per_page = 10
+
+    for k, v in columns.items():
+        if v:
+            filters.append(getattr(Applications, k) == v)
+
+    applications = (
+        Applications.select().where(*filters).paginate(page_number or 1, per_page)
+        if filters
+        else Applications.select().paginate(page_number or 1, per_page)
+    )
+    for application in applications:
+        print(
+            application.application_id,
+            application.name,
+            application.status,
+            application.location,
+            application.date,
+            application.apply_link,
+        )
+
+
+@tracker.command()
+def delete_application(
+    name: Optional[str] = typer.Option(
+        None, "--name", "-n", help="Name of the application"
+    ),
+    application_id: Optional[str] = typer.Option(
+        None, "--id", "-i", help="ID of the application"
+    ),
+) -> None:
+    deleted_records: int = 0
+    if not name and not application_id:
+        typer.secho(
+            "Provide either application Id or application Name to delete.",
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(-1)
+
+    if application_id:
+        typer.confirm(f"Deleting application: {application_id}", abort=True)
+        deleted_records = Applications.delete_by_id(application_id)
+
+    if name:
+        deleting_options = Applications.select().where(Applications.name == name)
+        if deleting_options:
+            typer.secho("Type the application Id to delete the record...")
+            for deleting_option in deleting_options:
+                print(
+                    deleting_option.application_id,
+                    deleting_option.name,
+                    deleting_option.status,
+                    deleting_option.location,
+                    deleting_option.date,
+                    deleting_option.apply_link,
+                )
+            selected_id = typer.prompt(
+                "Select one of the above ids to delete", type=int
+            )
+            typer.confirm(f"Deleting application: {selected_id}", abort=True)
+            deleted_records = Applications.delete_by_id(selected_id)
+
+    typer.secho(f"Deleted records: {deleted_records}", fg=typer.colors.BRIGHT_BLACK)
+
+
+if __name__ == "__main__":
+    tracker()
---
+++ packages/apptrackr/apptrackr-0.1.0-py3-none-any.whl/apptrackr/model/model.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from peewee import AutoField, CharField, DateTimeField, Model, SqliteDatabase
+
+database = SqliteDatabase("applications.db")
+
+
+class Applications(Model):
+    application_id = AutoField()
+    name = CharField(max_length=100)
+    status = CharField()
+    location = CharField(default="uk")
+    date = DateTimeField(formats="%d/%m/%y %H:%M:%S.%f", default=datetime.now)
+    apply_link = CharField(max_length=150)
+
+    class Meta:
+        database = database
+
+
+if not Applications.table_exists():
+    print("Creating Application Table...")
+    Applications.create_table()
