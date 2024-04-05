989892d910aa0fb3a2d925b750c664e2469f3573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 06:03:10 +0000

    Add package rdf-shacl-generator/1.0.8/rdf_shacl_generator-1.0.8.tar.gz

---
+++ packages/rdf-shacl-generator/rdf_shacl_generator-1.0.8.tar.gz/rdf_shacl_generator-1.0.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('README.md') as f:
+    description = f.read()
+
+setup(
+    name='rdf_shacl_generator',
+    version='1.0.8',
+    description = 'Synthetic RDF graph generator based on SHACL constraints.',
+    long_description = description,
+    long_description_content_type = 'text/markdown',
+    packages=find_packages(),
+    package_data={'rdf_shacl_generator': ['datasets/*.csv']},
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'genrdf = rdf_shacl_generator.script:main',
+        ],
+    },
+)
