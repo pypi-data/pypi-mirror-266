e64a4ea4a1faf9648f8546d2ce90414f9c39cc9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 14:03:33 +0000

    Add package freegenius/0.0.65/freegenius-0.0.65-py3-none-any.whl

---
+++ packages/freegenius/freegenius-0.0.65-py3-none-any.whl/freegenius/plugins/send tweet.py
@@ -0,0 +1,39 @@
+"""
+FreeGenius AI Plugin - send tweet
+
+send a tweet to twitter
+
+[FUNCTION_CALL]
+"""
+
+from freegenius import config
+from freegenius.utils.shared_utils import SharedUtil
+import urllib.parse
+
+def send_tweet(function_args):
+    message = function_args.get("message") # required
+    config.stopSpinning()
+    if message:
+        SharedUtil.openURL(f"""https://twitter.com/intent/tweet?text={urllib.parse.quote(message)}""")
+    return ""
+
+functionSignature = {
+    "examples": [
+        "send a tweet",
+        "post to twitter",
+    ],
+    "name": "send_tweet",
+    "description": f'''Send a tweet to twitter''',
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "message": {
+                "type": "string",
+                "description": "The message that is to be sent to twitter",
+            },
+        },
+        "required": ["message"],
+    },
+}
+
+config.addFunctionCall(signature=functionSignature, method=send_tweet)
