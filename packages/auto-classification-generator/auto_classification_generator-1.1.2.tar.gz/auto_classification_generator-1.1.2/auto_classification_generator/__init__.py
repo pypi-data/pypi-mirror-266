0804c81d354f91cb018b9d94f6d96ae1f54a94be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 03:03:20 +0000

    Add package auto-classification-generator/1.1.2/auto_classification_generator-1.1.2.tar.gz

---
+++ packages/auto-classification-generator/auto_classification_generator-1.1.2.tar.gz/auto_classification_generator-1.1.2/auto_classification_generator/__init__.py
@@ -0,0 +1,14 @@
+"""
+auto_classification_generator package definitions
+
+Author: Christopher Prince
+license: Apache License 2.0"
+"""
+
+from .common import *
+from .classification_generator import ClassificationGenerator
+from .cli import parse_args,run_cli
+from .version import __version__
+__author__ = "Christopher Prince (c.pj.prince@gmail.com)"
+__version__ = __version__
+__license__ = "Apache License Version 2.0"
