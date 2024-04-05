e3437983d9ecd8d803add0a4e7b1d4d0dc148cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 15:04:27 +0000

    Add package NUGigSkillNER/2.0.4/NUGigSkillNER-2.0.4.tar.gz

---
+++ packages/NUGigSkillNER/NUGigSkillNER-2.0.4.tar.gz/NUGigSkillNER-2.0.4/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+dependencies = [
+    "numpy",
+    "pandas",
+    "nltk",
+    "spacy",
+    "jellyfish",
+    "scipy"
+]
+
+setuptools.setup(
+    name="NUGigSkillNER",
+    version="2.0.4",
+    author="Ethan Mandel",
+    author_email="emandel2630@gmail.com",
+    description="An NLP module to automatically Extract skills and certifications from unstructured job postings, texts, and applicant's resumes",
+    url="https://github.com/emandel2630/NUGigSkillNER",
+    keywords=["skillNer", 'python', 'NLP', "NER",
+              "skills-extraction", "job-description"],
+    packages=setuptools.find_packages(),
+    install_requires=dependencies,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent",
+    ],
+)
