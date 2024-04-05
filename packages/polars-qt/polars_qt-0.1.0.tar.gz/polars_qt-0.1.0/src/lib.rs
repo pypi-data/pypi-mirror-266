c6ea2c827301b4bafb4022c3c8c0cf0b79b263f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 03:05:27 +0000

    Add package polars-qt/0.1.0/polars_qt-0.1.0.tar.gz

---
+++ packages/polars-qt/polars_qt-0.1.0.tar.gz/polars_qt-0.1.0/src/lib.rs
@@ -0,0 +1,10 @@
+mod rolling_rank;
+
+use pyo3::types::PyModule;
+use pyo3::{pymodule, PyResult, Python};
+
+#[pymodule]
+fn polars_qt(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+    Ok(())
+}
