92a77e8e162e8f766f9c88dae6baab833732cc46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 04:03:09 +0000

    Add package varbose-gram/0.1/varbose_gram-0.1.tar.gz

---
+++ packages/varbose-gram/varbose_gram-0.1.tar.gz/varbose_gram-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='varbose_gram',
+    version='0.1',
+    packages=find_packages(),
+    author='grubx64',
+    author_email='nn.neisven@gmail.com',
+    description='test',
+    url='https://github.com/tech-voyager/zero_gram',
+)
