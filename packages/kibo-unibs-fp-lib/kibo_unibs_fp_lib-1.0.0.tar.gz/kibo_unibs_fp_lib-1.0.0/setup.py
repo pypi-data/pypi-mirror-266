f87c096ab4de5a5876c9732b5e14e586161e372f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 16:03:24 +0000

    Add package kibo-unibs-fp-lib/1.0.0/kibo_unibs_fp_lib-1.0.0.tar.gz

---
+++ packages/kibo-unibs-fp-lib/kibo_unibs_fp_lib-1.0.0.tar.gz/kibo_unibs_fp_lib-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kibo_unibs_fp_lib",
+    version="1.0.0",
+    packages=find_packages(),
+    description="Libreria UniBSFpLib modificata e convertita in Python da me",
+    author="Alessandro Muscio",
+    author_email="a.muscio001@unibs.studenti.it",
+    url="https://github.com/AlessandroMuscio/kibo_unibs_fp_lib",
+)
