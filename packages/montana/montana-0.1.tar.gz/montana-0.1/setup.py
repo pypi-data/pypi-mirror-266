0d681b2f6cae4475c0cd96bca2dd287dc6ed180c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 05:04:09 +0000

    Add package montana/0.1/montana-0.1.tar.gz

---
+++ packages/montana/montana-0.1.tar.gz/montana-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='montana',
+    version='0.1',
+    packages=find_packages(),
+    author='grubx64',
+    author_email='nn.neisven@gmail.com',
+    description='package for creating telegram bot',
+    url='https://github.com/tech-voyager/zero_gram',
+)
