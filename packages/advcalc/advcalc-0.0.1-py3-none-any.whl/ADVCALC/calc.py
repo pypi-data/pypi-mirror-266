d3edfd988b3f03601186d96294e850a81b8f06e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 15:04:21 +0000

    Add package advcalc/0.0.1/advcalc-0.0.1-py3-none-any.whl

---
+++ packages/advcalc/advcalc-0.0.1-py3-none-any.whl/ADVCALC/calc.py
@@ -0,0 +1,46 @@
+import math as m
+def advcalc(equation):
+    print(eval(equation))
+    
+def add(num1, num2):
+    sum = float(num1) + float(num2)
+    print(sum)
+
+def sub(num1, num2):
+    sum = float(num1) - float(num2)
+    print(sum)
+    
+def div(num1, num2):
+    sum = float(num1) / float(num2)
+    print(sum)
+    
+def mul(num1, num2):
+    sum = float(num1) * float(num2)
+    print(sum)
+
+def sqrt(num):
+    sum = float(m.sqrt(num))
+    print(sum)
+
+def square(num):
+    sum = float(num * num)
+    print(sum)
+    
+def round_without_decimal_place(num):
+    sum = float(round(num))
+    print(sum)
+
+def round_with_decimal_place(num,decimal):
+    sum = float(round(num,decimal))
+    print(sum)
+
+def cube(num):
+    sum = float(num**3)
+    print(sum)
+    
+def cbrt(num):
+    sum = float(num) ** (1. / 3)
+    print(sum)
+
+def pow(num1, num2):
+    sum = float(num1) ** float(num2)
