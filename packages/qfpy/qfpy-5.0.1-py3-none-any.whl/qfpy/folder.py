3451de9a88f41ff7cd74259c3637d8918fbf70cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 05:03:50 +0000

    Add package qfpy/5.0.1/qfpy-5.0.1-py3-none-any.whl

---
+++ packages/qfpy/qfpy-5.0.1-py3-none-any.whl/qfpy/folder.py
@@ -0,0 +1,16 @@
+from collections import Counter
+from pathlib import Path
+
+
+def count_suffix(folder: str) -> Counter:
+    """
+    统计文件夹中所有文件后缀
+    :param file_path: 文件路径
+    :return: 字典
+    """
+    suffix_counter = Counter()
+    for file in Path(folder).rglob("*"):
+        if file.is_dir():
+            continue
+        suffix_counter[file.suffix.lower()] += 1
+    return suffix_counter
