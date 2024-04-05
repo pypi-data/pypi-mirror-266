72edcaef671472ea9bdbe1a86287c993ff4c07b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 04:03:43 +0000

    Add package praktickaKnihovna/0.0.5/praktickaKnihovna-0.0.5.tar.gz

---
+++ packages/praktickaKnihovna/praktickaKnihovna-0.0.5.tar.gz/praktickaKnihovna-0.0.5/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='praktickaKnihovna',
+    version='0.0.5',
+    author='Brystof_Klazek',
+    author_email='blazekr2@cvut.cz',
+    description='Moje mala knihovna na praktika',
+    url='',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
