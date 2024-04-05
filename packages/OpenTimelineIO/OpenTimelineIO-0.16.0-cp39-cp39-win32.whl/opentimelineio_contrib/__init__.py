ec2288759332ea63c57233d49a2cecfb09432789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 05:04:03 +0000

    Add package OpenTimelineIO/0.16.0/OpenTimelineIO-0.16.0-cp39-cp39-win32.whl

---
+++ packages/OpenTimelineIO/OpenTimelineIO-0.16.0-cp39-cp39-win32.whl/opentimelineio_contrib/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the OpenTimelineIO project
+
+"""Unsupported contrib code for OpenTimelineIO."""
+
+# flake8: noqa
+
+from . import (
+    adapters
+)
+
+__version__ = "0.16.0"
+__author__ = "Contributors to the OpenTimelineIO project"
+__author_email__ = "otio-discussion@lists.aswf.io"
+__license__ = "Apache 2.0 License"
