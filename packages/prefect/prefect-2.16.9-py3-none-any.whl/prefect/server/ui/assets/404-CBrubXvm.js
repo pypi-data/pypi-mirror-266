7dd9183f4f8f7673eff8c525a4e1c58141ae63c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 06:04:12 +0000

    Add package prefect/2.16.9/prefect-2.16.9-py3-none-any.whl

---
+++ packages/prefect/prefect-2.16.9-py3-none-any.whl/prefect/server/ui/assets/404-CBrubXvm.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,u as a,o as t,cm as r}from"./index-BK3LTUv3.js";import{u as s}from"./usePageTitle-D12lhMNb.js";const _=e({__name:"404",setup(n){return s("Page Not Found"),(c,u)=>(t(),o(a(r)))}});export{_ as default};
+//# sourceMappingURL=404-CBrubXvm.js.map
---
+++ packages/prefect/prefect-2.16.9-py3-none-any.whl/prefect/server/ui/assets/usePageTitle-D12lhMNb.js
@@ -0,0 +1,2 @@
+import{e as o,ar as u,u as c}from"./index-BK3LTUv3.js";function i(...t){const r=[...t,"Prefect Server"],a=o(()=>r.map(e=>c(e)).filter(e=>e!==null).join(" • "));u(()=>document.title=a.value)}export{i as u};
+//# sourceMappingURL=usePageTitle-D12lhMNb.js.map
