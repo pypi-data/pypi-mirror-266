8eff2c0825eb1b6ab2c198ca2dd5e983376d4758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 06:03:18 +0000

    Add package metaflow-torchrun/0.0.6/metaflow-torchrun-0.0.6.tar.gz

---
+++ packages/metaflow-torchrun/metaflow-torchrun-0.0.6.tar.gz/metaflow-torchrun-0.0.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_namespace_packages
+
+version = "0.0.6"
+
+setup(
+    name="metaflow-torchrun",
+    version=version,
+    description="An EXPERIMENTAL torchrun decorator for Metaflow",
+    author="Eddie Mattia",
+    author_email="eddie@outerbounds.com",
+    packages=find_namespace_packages(include=["metaflow_extensions.*"]),
+    py_modules=[
+        "metaflow_extensions",
+    ],
+    install_requires=[],
+)
