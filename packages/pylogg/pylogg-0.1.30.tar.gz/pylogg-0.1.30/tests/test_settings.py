65b89affd43610966b7b96330c9c3197cb099a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-05 05:03:23 +0000

    Add package pylogg/0.1.30/pylogg-0.1.30.tar.gz

---
+++ packages/pylogg/pylogg-0.1.30.tar.gz/pylogg-0.1.30/tests/test_settings.py
@@ -0,0 +1,114 @@
+from pylogg.settings import NamedTuple, PLSettings
+
+
+def test_load_settings(assets, tmp_path):
+    asset_file = assets / "settings.yaml"
+    test_output = tmp_path / "settings.yaml"
+
+    class Test(NamedTuple):
+        row1: float = 100.0
+        row2: str   = 'Package'
+        row3: str   = 'Settings'
+
+    class Person(NamedTuple):
+        name : str = 'John'
+        age : int = 3
+
+    class Settings(NamedTuple):
+        YAML = None
+        TestSettings : Test
+        PersonSettings : Person
+
+        @classmethod
+        def load(c, yaml_file = None, first_arg = False) -> 'Settings':
+            c.YAML = PLSettings('pytest', yaml_file, first_arg=first_arg)
+            return c.YAML.populate(c)
+
+        def create(self, newfile = None):
+            self.YAML.save(self, yaml_file=newfile)
+
+
+    sett = Settings.load(asset_file)
+    sett = sett._replace(TestSettings = sett.TestSettings._replace(row1 = 90.0))
+    sett.create(test_output)
+
+
+def test_yaml_write(assets, tmp_path):
+    asset_file = assets / "settings.yaml"
+    test_output = tmp_path / "settings.yaml"
+
+    class Test(NamedTuple):
+        row1: float = 23.6
+        row2: str   = 'Hello'
+        row3: str   = 'World'
+
+    class Settings(NamedTuple):
+        YAML = None
+        TestSettings : Test
+
+        @classmethod
+        def load(c, yaml_file = None, first_arg = False) -> 'Settings':
+            c.YAML = PLSettings('pytest', yaml_file, first_arg=first_arg)
+            return c.YAML.populate(c)
+
+        def save(self, newfile = None):
+            self.YAML.save(self, yaml_file=newfile)
+
+    settings = Settings.load(asset_file)
+    test = settings.TestSettings
+
+    assert test.row1 == 23.6
+    assert type(test.row1) == float
+    assert test.row2 == 'Hello'
+
+    settings.save(newfile=test_output)
+    assert str(test.row1) in test_output.read_text()
+    assert test.row2 in test_output.read_text()
+    assert test.row3 in test_output.read_text()
+
+
+def test_args_subs():
+    import sys
+    sys.argv += ['--name', 'world', '--debug', '--num', '22']
+
+    class Test(NamedTuple):
+        greeting: str   = 'Hello $name'
+        number : int    = '$num'
+        debug : bool    = '$debug'
+
+    class Settings(NamedTuple):
+        YAML = None
+        TestSettings : Test
+
+        @classmethod
+        def load(c, yaml_file = None, first_arg = False) -> 'Settings':
+            c.YAML = PLSettings('pytest', yaml_file, first_arg=first_arg)
+            return c.YAML.populate(c)
+
+        def save(self, newfile = None):
+            self.YAML.save(self, yamlfile=newfile)
+
+    settings = Settings.load(None)
+    test = settings.TestSettings
+
+    assert test.greeting == 'Hello world'
+    assert test.number == 22
+    assert test.debug == True
+
+    print(test)
+
+
+def test_postitional_args():
+    import sys
+
+    yaml = PLSettings('pytest')
+    print(yaml._pos_args)
+
+    sys.argv += ['--name', 'world', 'settings2.yaml', '--debug', '--num', '22']
+    yaml = PLSettings('pytest')
+    print(yaml._pos_args)
+
+    sys.argv += ['settings2.yaml']
+    yaml = PLSettings('pytest')
+    assert yaml._pos_args == ['settings2.yaml', 'settings2.yaml']
+    print(yaml._pos_args)
