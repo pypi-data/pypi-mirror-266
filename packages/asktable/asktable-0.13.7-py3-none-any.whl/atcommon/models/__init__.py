6ea8daa23593fceeadc484d5be2e67396a4b0719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 15:04:42 +0000

    Add package asktable/0.13.7/asktable-0.13.7-py3-none-any.whl

---
+++ packages/asktable/asktable-0.13.7-py3-none-any.whl/atcommon/models/__init__.py
@@ -0,0 +1,18 @@
+from atcommon.models.datasource import DataSourceCore
+from atcommon.models.meta import MetaData, DataField, DataTable, DataSchema
+from atcommon.models.chat import ChatCore, MessageCore, RunCore
+from atcommon.models.bi import StrucQuery, QueryResult, BIAnswer
+
+__all__ = [
+    'MetaData',
+    'DataField',
+    'DataTable',
+    'DataSchema',
+    'DataSourceCore',
+    'ChatCore',
+    'MessageCore',
+    'RunCore',
+    'StrucQuery',
+    'QueryResult',
+    'BIAnswer',
+]
