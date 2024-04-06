e8a20aad3148f75a269de020b6e2162a2b523ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 05:05:56 +0000

    Add package mov-cli-youtube/1.1.1/mov_cli_youtube-1.1.1-py3-none-any.whl

---
+++ packages/mov-cli-youtube/mov_cli_youtube-1.1.1-py3-none-any.whl/mov_cli_youtube/youtube.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import List, Optional, Dict, Generator, Any
+
+    from mov_cli import Config
+    from mov_cli.http_client import HTTPClient
+
+import os
+import sys
+from pytube import YouTube, Search
+
+from mov_cli import utils
+from mov_cli.scraper import Scraper
+from mov_cli import Series, Movie, Metadata, MetadataType
+
+__all__ = ("YouTubeScraper",)
+
+class YouTubeScraper(Scraper):
+    def __init__(self, config: Config, http_client: HTTPClient) -> None:
+        super().__init__(config, http_client)
+
+    def search(self, query: str, limit: int = None) -> Generator[Metadata, Any, None]:
+        search_query = Search(query)
+        search_results: List[YouTube] = search_query.results
+
+        max_videos = 20 if limit is None else limit
+
+        # suppress pytube's stupid errors from getting to the console and ruining fzf output.
+        sys.stderr = open(os.devnull, "w")
+
+        for index, video in enumerate(search_results):
+            if (index + 1) == len(search_results) and not (index + 1) >= max_videos:
+                search_query.get_next_results()
+
+            yield Metadata(
+                id = video.watch_url, 
+                title = f"{video.title} ~ {video.author}", 
+                type = MetadataType.MOVIE, 
+                year = str(video.publish_date.year)
+            )
+
+        # restore the console.
+        sys.stderr = sys.__stderr__
+
+    def scrape(self, metadata: Metadata, episode: Optional[utils.EpisodeSelector] = None, **kwargs: Dict[str, bool]) -> Series | Movie:
+        audio_only: bool = kwargs.get("audio", False)
+
+        if episode is None:
+            episode = utils.EpisodeSelector()
+
+        watch_url = metadata.id
+        video = YouTube(watch_url)
+
+        if audio_only:
+            url = video.streams.get_audio_only().url
+        else:
+            if self.config.resolution is not None:
+                url = video.streams.get_by_resolution(f"{self.config.resolution}p").url
+                
+                if url is None:
+                    url = video.streams.get_highest_resolution().url
+            else:
+                url = video.streams.get_highest_resolution().url
+
+        return Movie(
+            url = url, 
+            title = metadata.title, 
+            referrer = "", 
+            year = metadata.year, 
+            subtitles = None
+        )
+
+    def scrape_episodes(self, metadata: Metadata, **kwargs) -> Dict[None, int]:
+        # Returning None as search does not return any metadata of type series.
+        return {None: 1}
