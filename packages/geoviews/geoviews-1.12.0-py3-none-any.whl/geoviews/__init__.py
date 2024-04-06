1918e262ba4cf35a177ea9c59c24e2c60102fa5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 03:04:20 +0000

    Add package geoviews/1.12.0/geoviews-1.12.0-py3-none-any.whl

---
+++ packages/geoviews/geoviews-1.12.0-py3-none-any.whl/geoviews/__init__.py
@@ -0,0 +1,68 @@
+import param
+
+from holoviews import (extension, help, opts, output, renderer, Store, # noqa (API import)
+                       Cycle, Palette, Overlay, Layout, NdOverlay, NdLayout,
+                       HoloMap, DynamicMap, GridSpace, Dimension, dim)
+
+from holoviews import render, save # noqa (API import)
+
+from .element import ( # noqa (API import)
+    _Element, Feature, Tiles, WMTS, LineContours, FilledContours,
+    Text, Image, ImageStack, Points, Path, Polygons, Shape, Dataset, RGB,
+    Contours, Graph, TriMesh, Nodes, EdgePaths, QuadMesh, VectorField,
+    HexTiles, Labels, Rectangles, Segments, WindBarbs
+)
+from .util import from_xarray # noqa (API import)
+from ._warnings import GeoviewsDeprecationWarning, GeoviewsUserWarning  # noqa: F401
+from . import data                                  # noqa (API import)
+from . import plotting                              # noqa (API import)
+from . import feature                               # noqa (API import)
+from . import tile_sources                          # noqa (API import)
+
+__version__ = str(param.version.Version(fpath=__file__, archive_commit="$Format:%h$",
+                                        reponame="geoviews"))
+
+# Ensure opts utility is initialized with GeoViews elements
+if Store._options:
+    Store.set_current_backend(Store.current_backend)
+
+# make pyct's example/data commands available if possible
+from functools import partial
+try:
+    from pyct.cmd import copy_examples as _copy, fetch_data as _fetch, examples as _examples
+    copy_examples = partial(_copy, 'geoviews')
+    fetch_data = partial(_fetch, 'geoviews')
+    examples = partial(_examples, 'geoviews')
+except ImportError:
+    def _missing_cmd(*args,**kw): return("install pyct to enable this command (e.g. `conda install -c pyviz pyct`)")
+    _copy = _fetch = _examples = _missing_cmd
+    def _err(): raise ValueError(_missing_cmd())
+    fetch_data = copy_examples = examples = _err
+del partial, _examples, _copy, _fetch
+
+
+def __getattr__(attr):
+    # Lazy loading heavy modules
+    if attr == 'annotate':
+        from .annotators import annotate
+        return annotate
+    elif attr == 'project':
+        from .operation import project
+        return project
+    elif attr == 'operation':
+        from . import operation
+        return operation
+    raise AttributeError(f"module {__name__} has no attribute {attr!r}")
+
+__all__ = [k for k in locals() if not k.startswith('_')]
+__all__ += ['annotate', 'project', 'operation', '__version__']
+
+def __dir__():
+    return __all__
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .annotators import annotate
+    from .operation import project
+    from . import operation
