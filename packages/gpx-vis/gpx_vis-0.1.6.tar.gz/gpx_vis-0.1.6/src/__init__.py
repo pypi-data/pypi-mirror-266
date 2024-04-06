82d202f60b162713f3d7a194ec136d987233b8c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 04:03:56 +0000

    Add package gpx-vis/0.1.6/gpx_vis-0.1.6.tar.gz

---
+++ packages/gpx-vis/gpx_vis-0.1.6.tar.gz/gpx_vis-0.1.6/src/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 13:03:15 2024
+
+@author: Jia Wei Teh
+"""
+
+# from .gpxvis import Track
+
+# __author__ = 'Jia Wei Teh'
+
+
