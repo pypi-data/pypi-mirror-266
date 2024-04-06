fa46a71ae33de7a2d653236015f7bf8d981c0067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 03:04:24 +0000

    Add package dti-tax-core-vsdc-api-client-swagger/1.0.0/dti_tax_core_vsdc_api_client_swagger-1.0.0-py3-none-any.whl

---
+++ packages/dti-tax-core-vsdc-api-client-swagger/dti_tax_core_vsdc_api_client_swagger-1.0.0-py3-none-any.whl/dti_tax_core_vsdc_api_client_swagger/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from dti_tax_core_vsdc_api_client_swagger.api.invoices_api import InvoicesApi
+from dti_tax_core_vsdc_api_client_swagger.api.status_api import StatusApi
