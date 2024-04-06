9bbaf34573851fb482bd0ecff79b8238d25d24d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 03:03:54 +0000

    Add package qibo/0.2.7/qibo-0.2.7.tar.gz

---
+++ packages/qibo/qibo-0.2.7.tar.gz/qibo-0.2.7/src/qibo/__init__.py
@@ -0,0 +1,33 @@
+import importlib.metadata as im
+
+__version__ = im.version(__package__)
+
+from qibo import (
+    callbacks,
+    gates,
+    hamiltonians,
+    models,
+    optimizers,
+    parallel,
+    parameter,
+    result,
+    solvers,
+)
+from qibo.backends import (
+    get_backend,
+    get_device,
+    get_precision,
+    get_threads,
+    matrices,
+    set_backend,
+    set_device,
+    set_precision,
+    set_threads,
+)
+from qibo.config import (
+    get_batch_size,
+    get_metropolis_threshold,
+    set_batch_size,
+    set_metropolis_threshold,
+)
+from qibo.models.circuit import Circuit
