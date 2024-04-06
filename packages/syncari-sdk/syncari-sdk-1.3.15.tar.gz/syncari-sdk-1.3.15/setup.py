05d8c6337fa180ea5113b8c992a55dbb70f35344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 04:04:14 +0000

    Add package syncari-sdk/1.3.15/syncari-sdk-1.3.15.tar.gz

---
+++ packages/syncari-sdk/syncari-sdk-1.3.15.tar.gz/syncari-sdk-1.3.15/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import find_packages, setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='syncari-sdk',
+    version='1.3.15',
+    description='Syncari Synapse Development Kit',
+    author='Syncari',
+    author_email='dev@syncari.com',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='TBD',
+    packages=find_packages(exclude=('unittests')),
+    install_requires=[
+        'pydantic~=1.6',
+        'requests',
+        'urllib3==1.26.0',
+        'backoff'
+    ],
+    python_requires='>=3.7.0',
+)
