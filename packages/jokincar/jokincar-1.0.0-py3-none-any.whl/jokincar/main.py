c254fc8bf6b4d372167057d22e23a980e54a8ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 05:04:15 +0000

    Add package jokincar/1.0.0/jokincar-1.0.0-py3-none-any.whl

---
+++ packages/jokincar/jokincar-1.0.0-py3-none-any.whl/jokincar/main.py
@@ -0,0 +1,7 @@
+def hello():
+    print("hello from car")
+    
+    #pypi-AgEIcHlwaS5vcmcCJDM5OWJhY2I1LWVjMWEtNGJlYy04OTY0LWU0OTJjMzQ0MmQ0NwACKlszLCJhZjc4ZjkwMi0yODBiLTRiMmMtYjliYi1mNzQ3MzI3Y2I3NDciXQAABiCOLtqb6FSHv4umPeGcm7rXdHMO3tg7FqyzKYp2srM-wQ
+
+
+#python -m twine upload --repository-url https://upload.pypi.org/legacy/ --username __token__ --password AgEIcHlwaS5vcmcCJDM5OWJhY2I1LWVjMWEtNGJlYy04OTY0LWU0OTJjMzQ0MmQ0NwACKlszLCJhZjc4ZjkwMi0yODBiLTRiMmMtYjliYi1mNzQ3MzI3Y2I3NDciXQAABiCOLtqb6FSHv4umPeGcm7rXdHMO3tg7FqyzKYp2srM-wQ dist/*
