8d9dfa887a7a082ee5345e4c3827ced96c499b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 03:04:33 +0000

    Add package pyrtz2/1.1.14/pyrtz2-1.1.14.tar.gz

---
+++ packages/pyrtz2/pyrtz2-1.1.14.tar.gz/pyrtz2-1.1.14/README.md
@@ -0,0 +1,12 @@
+# pyrtz2
+
+Analysis of AFM force curves in Python.
+
+Developed at Georgia Institute of Technology
+
+# Installation
+pyrtz2 is on PyPI. Install using pip (Python version >= 3.10.0 is required)
+
+```
+pip install pyrtz2
+```
---
+++ packages/pyrtz2/pyrtz2-1.1.14.tar.gz/pyrtz2-1.1.14/pyrtz2/app.py
@@ -0,0 +1,15 @@
+from dash import Dash
+from dash_bootstrap_components.themes import BOOTSTRAP
+
+from .src.components.layout import create_layout
+
+
+def run() -> None:
+    app = Dash(__name__, external_stylesheets=[BOOTSTRAP])
+    app.title = "AFM Curve Analysis"
+    app.layout = create_layout(app)
+    app.run(debug=True)
+
+
+if __name__ == "__main__":
+    run()
