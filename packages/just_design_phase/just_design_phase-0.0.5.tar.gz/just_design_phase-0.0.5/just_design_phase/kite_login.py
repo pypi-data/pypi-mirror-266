4bf340c7467c63f03a6e56a640a1da5a8984fef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 15:04:39 +0000

    Add package just_design_phase/0.0.5/just_design_phase-0.0.5.tar.gz

---
+++ packages/just_design_phase/just_design_phase-0.0.5.tar.gz/just_design_phase-0.0.5/just_design_phase/kite_login.py
@@ -0,0 +1,19 @@
+import os
+
+from kiteconnect import KiteConnect
+
+PROJECT_ROOT = os.path.abspath("")
+
+print(PROJECT_ROOT)
+
+KITE_API_KEY = os.getenv("KITE_API_KEY")
+KITE_API_SECRET = os.getenv("KITE_API_SECRET")
+
+
+def login():
+    kite = KiteConnect(api_key=KITE_API_KEY)
+    with open(PROJECT_ROOT + "/sample.txt", "w") as file:
+        file.write("Hello World")
+
+
+kite = login()
