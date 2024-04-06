5532081de52f6910e38bb1771acbfe17a1dc0dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 06:03:49 +0000

    Add package alluxiofs/1.0.2/alluxiofs-1.0.2.tar.gz

---
+++ packages/alluxiofs/alluxiofs-1.0.2.tar.gz/alluxiofs-1.0.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="alluxiofs",
+    version="1.0.2",
+    description="Alluxio Fsspec provides Alluxio filesystem spec implementation.",
+    url="https://github.com/fsspec/alluxiofs",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # Alluxio fs dependencies
+        "fsspec",
+        # Alluxio client dependencies
+        "aiohttp",
+        "decorator",
+        "humanfriendly",
+        "requests",
+        "etcd3",
+        "mmh3",
+        "sortedcontainers",
+        "protobuf>=3.20.0,<3.21.0",
+    ],
+    extras_require={
+        "tests": [
+            "pytest",
+            "pytest-aiohttp",
+            "ray",
+        ]
+    },
+    python_requires=">=3.8",
+    maintainer="Lu Qiu",
+    maintainer_email="luqiujob@gmail.com",
+    entry_points={
+        "fsspec.specs": [
+            "alluxio=alluxiofs.AlluxioFileSystem",
+        ],
+    },
+)
