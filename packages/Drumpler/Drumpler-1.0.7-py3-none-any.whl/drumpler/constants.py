11a0c56fa76145be8e9f570a97aaa2162285f11f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 06:04:08 +0000

    Add package Drumpler/1.0.7/Drumpler-1.0.7-py3-none-any.whl

---
+++ packages/Drumpler/Drumpler-1.0.7-py3-none-any.whl/drumpler/constants.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+AUTHORIZATION_KEY = os.getenv('AUTHORIZATION_KEY')
+
+# Load environment variables
+DRUMPLER_HOST = os.getenv('DRUMPLER_HOST', "http://127.0.0.1")
+DRUMPLER_PORT = int(os.getenv('DRUMPLER_PORT', "5000"))
+DRUMPLER_URL = f"{DRUMPLER_HOST}:{DRUMPLER_PORT}"  # Adjust the URL/port as necessary
+DRUMPLER_DEBUG = bool(os.getenv('DRUMPLER_DEBUG', "True"))
+MAMMOTH_WORKERS = 1
+
+DB_NAME = os.getenv('DB_NAME')
+DB_HOST = os.getenv('DB_HOST')
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASS')
+DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
