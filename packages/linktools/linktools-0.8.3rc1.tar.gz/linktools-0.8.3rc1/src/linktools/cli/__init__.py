0b5d3ee16eaa28e49189d6a7e4103936b53b1ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 03:04:25 +0000

    Add package linktools/0.8.3rc1/linktools-0.8.3rc1.tar.gz

---
+++ packages/linktools/linktools-0.8.3rc1.tar.gz/linktools-0.8.3rc1/src/linktools/cli/__init__.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@author  : Hu Ji
+@file    : __init__.py.py 
+@time    : 2023/01/14
+@site    :  
+@software: PyCharm 
+
+              ,----------------,              ,---------,
+         ,-----------------------,          ,"        ,"|
+       ,"                      ,"|        ,"        ,"  |
+      +-----------------------+  |      ,"        ,"    |
+      |  .-----------------.  |  |     +---------+      |
+      |  |                 |  |  |     | -==----'|      |
+      |  | $ sudo rm -rf / |  |  |     |         |      |
+      |  |                 |  |  |/----|`---=    |      |
+      |  |                 |  |  |   ,/|==== ooo |      ;
+      |  |                 |  |  |  // |(((( [33]|    ,"
+      |  `-----------------'  |," .;'| |((((     |  ,"
+      +-----------------------+  ;;  | |         |,"
+         /_)______________(_/  //'   | +---------+
+    ___________________________/___  `,
+   /  oooooooooooooooo  .o.  oooo /,   \,"-----------
+  / ==ooooooooooooooo==.o.  ooo= //   ,`\--{)B     ,"
+ /_==__==========__==_ooo__ooo=_/'   /___________,"
+"""
+
+from . import argparse
+
+from .command import \
+    BaseCommand, BaseCommandGroup, CommandError, \
+    SubCommand, SubCommandGroup, SubCommandWrapper, \
+    subcommand, subcommand_argument, SubCommandError
+
+from .device import \
+    DeviceCommandMixin, AndroidCommandMixin, IOSCommandMixin, \
+    AndroidCommand, IOSCommand
