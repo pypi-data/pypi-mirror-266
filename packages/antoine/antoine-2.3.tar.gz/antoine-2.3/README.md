d602c4d5fedd3b4c056afd2b455f76cbc855c811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 03:04:13 +0000

    Add package antoine/2.3/antoine-2.3.tar.gz

---
+++ packages/antoine/antoine-2.3.tar.gz/antoine-2.3/README.md
@@ -0,0 +1,16 @@
+# Antoine
+
+Antoine is just one pip install away !
+You can install Antoine directly using pip.
+
+## Hire Antoine with a quick pip install
+
+```bash
+## Installing
+pip install antoine
+
+## Upgrading
+pip install antoine --upgrade
+
+## Running in terminal
+$ hire_antoine
---
+++ packages/antoine/antoine-2.3.tar.gz/antoine-2.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='antoine',
+    version='2.3',
+    packages=['antoine'],  # 'antoine' is the name of the package
+    author='Antoine Bertin',
+    author_email='monolok35@gmail.com',
+    description='Hiring Antoine is so much fun!',
+    url='https://github.com/monolok/antoine/',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'hire_antoine=antoine.app:main',  # 'antoine' is the name of the package and 'app.py' contains a function 'main'
+        ],
+    },
+    include_package_data=True,
+    install_requires=[]
+)
