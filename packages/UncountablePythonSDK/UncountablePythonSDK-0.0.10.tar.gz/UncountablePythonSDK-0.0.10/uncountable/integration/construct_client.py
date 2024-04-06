783d19469e69f7ab99288c27cf2a2bfc1f1d9e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 05:03:59 +0000

    Add package UncountablePythonSDK/0.0.10/UncountablePythonSDK-0.0.10.tar.gz

---
+++ packages/UncountablePythonSDK/UncountablePythonSDK-0.0.10.tar.gz/UncountablePythonSDK-0.0.10/uncountable/integration/construct_client.py
@@ -0,0 +1,30 @@
+import os
+from typing import assert_never
+
+from uncountable.core.client import AuthDetailsApiKey, Client
+from uncountable.integration.types import (
+    AuthRetrievalEnv,
+    ProfileMetadata,
+)
+
+
+def construct_uncountable_client(
+    profile_meta: ProfileMetadata
+) -> Client:
+    match profile_meta.auth_retrieval:
+        case AuthRetrievalEnv():
+            api_id = os.getenv(f"UNC_PROFILE_{profile_meta.name}_API_ID")
+            api_secret_key = os.getenv(
+                f"UNC_PROFILE_{profile_meta.name}_API_SECRET_KEY"
+            )
+
+            assert api_id is not None
+            assert api_secret_key is not None
+
+            return Client(
+                base_url=profile_meta.base_url,
+                auth_details=AuthDetailsApiKey(
+                    api_id=api_id, api_secret_key=api_secret_key
+                ),
+            )
+    assert_never(profile_meta.auth_retrieval)
