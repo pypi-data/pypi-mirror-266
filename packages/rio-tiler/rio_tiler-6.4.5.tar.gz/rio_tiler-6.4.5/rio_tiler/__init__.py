cfe84d14f82521fbc292f3df7ccbdc9d827fea18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 04:03:36 +0000

    Add package rio-tiler/6.4.5/rio_tiler-6.4.5.tar.gz

---
+++ packages/rio-tiler/rio_tiler-6.4.5.tar.gz/rio_tiler-6.4.5/rio_tiler/__init__.py
@@ -0,0 +1,16 @@
+"""rio-tiler."""
+
+__version__ = "6.4.5"
+
+from . import (  # noqa
+    colormap,
+    constants,
+    errors,
+    expression,
+    io,
+    mosaic,
+    profiles,
+    reader,
+    tasks,
+    utils,
+)
