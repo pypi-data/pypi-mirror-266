106ff4ecdd9fbd950fa4e1b12909c3e03a837140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 06:04:08 +0000

    Add package StathamQuotes/0.0.3/StathamQuotes-0.0.3.tar.gz

---
+++ packages/StathamQuotes/StathamQuotes-0.0.3.tar.gz/StathamQuotes-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+version = "0.0.3"
+
+setup(
+    name='StathamQuotes',
+    version=version,
+    description='A useful module',
+    author='gbu1at',
+    author_email='bulatchess@mail.ru',
+    packages=['StathamQuotes'],  # same as name
+    # install_requires=[''],  # external packages as dependencies
+)
