8f77fef1efe0f6306c2f78957532aaa3cac13411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 04:03:31 +0000

    Add package learning3d/0.0.2/learning3d-0.0.2-py3-none-any.whl

---
+++ packages/learning3d/learning3d-0.0.2-py3-none-any.whl/learning3d/__init__.py
@@ -0,0 +1,2 @@
+from .models import PointNet, create_pointconv, DGCNN, PPFNet, Pooling, Classifier, Segmentation
+from .models import DCP, PRNet, iPCRNet, PointNetLK, RPMNet, PCN, DeepGMR, MaskNet, MaskNet2
