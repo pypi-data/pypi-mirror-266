87b2e9910619e1ec7adefac6aee2483ce85d272d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 05:05:52 +0000

    Add package i-bip-wallet/1.0.0rc4/i_bip_wallet-1.0.0rc4.tar.gz

---
+++ packages/i-bip-wallet/i_bip_wallet-1.0.0rc4.tar.gz/i_bip_wallet-1.0.0rc4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['i_bip_wallet']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ecdsa>=0.18.0,<0.19.0', 'pycryptodomex>=3.20.0,<4.0.0']
+
+setup_kwargs = {
+    'name': 'i-bip-wallet',
+    'version': '1.0.0rc4',
+    'description': '',
+    'long_description': '',
+    'author': 'DesKaOne',
+    'author_email': 'DesKaOne@users.noreply.github.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
