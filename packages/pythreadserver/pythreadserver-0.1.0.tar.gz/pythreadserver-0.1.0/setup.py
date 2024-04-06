f1e06f94cb0fa4615a431e81cb1915517aa42c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 06:04:12 +0000

    Add package pythreadserver/0.1.0/pythreadserver-0.1.0.tar.gz

---
+++ packages/pythreadserver/pythreadserver-0.1.0.tar.gz/pythreadserver-0.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.0'
+DESCRIPTION = 'Simple multi-threaded server package'
+LONG_DESCRIPTION = ''
+
+# Setting up
+setup(
+    name="pythreadserver",
+    version=VERSION,
+    author="rain-1107",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['threading', 'socket'],
+    keywords=['python', 'server', 'network', 'multi-threaded', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
