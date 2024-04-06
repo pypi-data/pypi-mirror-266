e5f317c08c45eb5e46de73c5e1f742a89873b475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 05:04:23 +0000

    Add package omdeventhandler/0.5.0/omdeventhandler-0.5.0.tar.gz

---
+++ packages/omdeventhandler/omdeventhandler-0.5.0.tar.gz/omdeventhandler-0.5.0/tests/pythonpath/local/lib/python/notificationforwarder/eventhandler_report/formatter.py
@@ -0,0 +1,17 @@
+import time
+import os
+from notificationforwarder.baseclass import NotificationFormatter, FormattedEvent
+
+class EventhandlerReportFormatter(NotificationFormatter):
+
+    def format_event(self, event):
+        json_payload = {
+            'timestamp': time.time(),
+        }
+        json_payload['description'] = event.eventopts['description']
+        if 'signature' in event.eventopts:
+            json_payload['signature'] = event.eventopts['signature']
+        event.payload = json_payload
+        # ! event.eventopts["eventhandler_success"] is a string !
+        # all eventopts are cast to strings, so "True" or "False"
+        event.summary = "eventhandler for {}/{} {} (signature={})".format(event.eventopts["HOSTNAME"], event.eventopts["SERVICEDESC"], "succeeded" if event.eventopts["eventhandler_success"] == "True" else "failed", event.eventopts["signature"])
