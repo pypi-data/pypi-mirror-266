940ebe23056a57bb1218db874249ff3424152437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 14:04:17 +0000

    Add package ABSQL/0.5.2/ABSQL-0.5.2.tar.gz

---
+++ packages/ABSQL/ABSQL-0.5.2.tar.gz/ABSQL-0.5.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ABSQL",
+    version="0.5.2",
+    author="Chris Cardillo",
+    author_email="cfcardillo23@gmail.com",
+    description="A rendering engine for templated SQL",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pipeline-tools/ABSQL",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "colorama",
+        "flatdict",
+        "Jinja2",
+        "pendulum",
+        "PyYaml",
+        "SQLAlchemy",
+        "sql-metadata",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
