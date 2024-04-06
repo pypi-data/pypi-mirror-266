0bd2d4e3ddeb8b81ac8965544bc1d301f3d20be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 03:04:23 +0000

    Add package BicefalasPDFApp/1.0/BicefalasPDFApp-1.0-py3-none-any.whl

---
+++ packages/BicefalasPDFApp/BicefalasPDFApp-1.0-py3-none-any.whl/pdfApp/pdf2image.py
@@ -0,0 +1,8 @@
+""" This module provides functions to convert a PDF to text.
+
+Here you can type more detailed explanation.
+"""
+
+def convert():
+    """PDF to image conversor"""
+    print("pdf2image")
