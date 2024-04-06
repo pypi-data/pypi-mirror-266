c0e218da4aa5f0bc237c3cf8d6e89f190327e96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 04:04:25 +0000

    Add package xarg-python/1.4.1/xarg-python-1.4.1.tar.gz

---
+++ packages/xarg-python/xarg-python-1.4.1.tar.gz/xarg-python-1.4.1/xarg/__init__.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+
+from .actuator import Namespace
+from .actuator import commands
+from .actuator import add_command
+from .actuator import end_command
+from .actuator import pre_command
+from .actuator import run_command
+
+from .parser import argp
+
+from .colorful import color
+from .colorful import Style
+from .colorful import Fore
+from .colorful import Back
+from .safefile import safile
+from .scanner import scanner
+
+from .util import chdir
+from .util import singleton
