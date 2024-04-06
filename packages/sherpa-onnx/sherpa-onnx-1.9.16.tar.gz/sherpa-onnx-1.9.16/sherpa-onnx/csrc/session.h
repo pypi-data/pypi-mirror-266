02465ad4875f66d5dc4df6e6b087b5e9fe2c4ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 04:04:10 +0000

    Add package sherpa-onnx/1.9.16/sherpa-onnx-1.9.16.tar.gz

---
+++ packages/sherpa-onnx/sherpa-onnx-1.9.16.tar.gz/sherpa-onnx-1.9.16/sherpa-onnx/csrc/session.h
@@ -0,0 +1,40 @@
+// sherpa-onnx/csrc/session.h
+//
+// Copyright (c)  2023  Xiaomi Corporation
+
+#ifndef SHERPA_ONNX_CSRC_SESSION_H_
+#define SHERPA_ONNX_CSRC_SESSION_H_
+
+#include "onnxruntime_cxx_api.h"  // NOLINT
+#include "sherpa-onnx/csrc/offline-lm-config.h"
+#include "sherpa-onnx/csrc/offline-model-config.h"
+#include "sherpa-onnx/csrc/offline-tts-model-config.h"
+#include "sherpa-onnx/csrc/online-lm-config.h"
+#include "sherpa-onnx/csrc/online-model-config.h"
+#include "sherpa-onnx/csrc/speaker-embedding-extractor.h"
+#include "sherpa-onnx/csrc/spoken-language-identification.h"
+#include "sherpa-onnx/csrc/vad-model-config.h"
+
+namespace sherpa_onnx {
+
+Ort::SessionOptions GetSessionOptions(const OnlineModelConfig &config);
+
+Ort::SessionOptions GetSessionOptions(const OfflineModelConfig &config);
+
+Ort::SessionOptions GetSessionOptions(const OfflineLMConfig &config);
+
+Ort::SessionOptions GetSessionOptions(const OnlineLMConfig &config);
+
+Ort::SessionOptions GetSessionOptions(const VadModelConfig &config);
+
+Ort::SessionOptions GetSessionOptions(const OfflineTtsModelConfig &config);
+
+Ort::SessionOptions GetSessionOptions(
+    const SpeakerEmbeddingExtractorConfig &config);
+
+Ort::SessionOptions GetSessionOptions(
+    const SpokenLanguageIdentificationConfig &config);
+
+}  // namespace sherpa_onnx
+
+#endif  // SHERPA_ONNX_CSRC_SESSION_H_
