93bb91a959324d9caa1ea172d77b910e65923f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 04:03:34 +0000

    Add package lemon-tcg/0.1.1/lemon_tcg-0.1.1-py3-none-any.whl

---
+++ packages/lemon-tcg/lemon_tcg-0.1.1-py3-none-any.whl/lemon_tcg/game.py
@@ -0,0 +1,25 @@
+from lemon_tcg.entities.game_state import GameState
+from lemon_tcg.entities.player import Player
+from lemon_tcg.libraries.game_state_library import GameStateLibrary
+
+GAME_STATES = GameStateLibrary.get_instance()
+
+class Game():
+    def __init__(self, state: GameState) -> None:
+        self.state = state
+
+    @staticmethod
+    def load_from_game_id(game_id: str) -> 'Game':
+        game_state = GAME_STATES.get_by_id(id=game_id)
+        if not isinstance(game_state, GameState):
+            raise RuntimeError(f"Invalid game id '{game_id}'.")
+        return Game(state=game_state)
+    
+    @staticmethod
+    def new(players: list[Player]) -> 'Game':
+        state = GameState.create_new(players=players)
+        state.save_state()
+        return Game(state=state)
+    
+    def save(self) -> None:
+        self.state.save_state()
