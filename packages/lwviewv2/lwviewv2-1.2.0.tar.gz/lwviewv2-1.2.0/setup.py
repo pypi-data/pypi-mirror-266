1f4dc1e5b7e049f7a627fa32023f17cde7b863fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 03:04:28 +0000

    Add package lwviewv2/1.2.0/lwviewv2-1.2.0.tar.gz

---
+++ packages/lwviewv2/lwviewv2-1.2.0.tar.gz/lwviewv2-1.2.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+setup(name='lwviewv2',
+        version='1.2.0',
+        description='Viewer of images in python.',
+        long_description="See Readme.md on github for more details.",
+        url='https://github.com/ReubenDo/lightweight-viewer',
+        author='Sandy Wells',
+        author_email='sw@bwh.harvard.edu',
+        python_requires='>=3.9',
+        license='MIT',
+        packages=find_packages(),
+        install_requires=[
+            'pynrrd',
+            'scipy',
+            'pillow',
+            'tk',
+            'numpy',
+            'nibabel',
+        ],
+        zip_safe=False,
+        classifiers=[
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python',
+            'Topic :: Scientific/Engineering',
+            'Operating System :: Unix',
+            'Operating System :: MacOS'
+        ],
+    )
