a4750eb25dd0d4da27eff3cc89d1bf8c4230754d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 14:03:54 +0000

    Add package Information-Retrieval/3.0/Information_Retrieval-3.0-py3-none-any.whl

---
+++ packages/Information-Retrieval/Information_Retrieval-3.0-py3-none-any.whl/Information_Retrieval/__init__.py
@@ -0,0 +1,39 @@
+from .main import Stop_Word_Removal_Code
+from .main import Incident_Matrix_Creation_Code
+from .main import flip_bits
+from .main import bitwise_and
+from .main import bitwise_or
+
+from .main import K_Gram_Code
+
+from .main import Crawler
+
+from .main import Edit_Distance_Code
+
+from .main import Cosine_Similarity
+
+from .main import Cosine_Similarity_2
+from .main import cosine_similarity
+from .main import preprocess_text
+from .main import compute_similarity
+
+from .main import Soundex_Code
+
+from .main import Page_Rank_Code
+
+from .main import Porter_Stemmer_Code
+
+from .main import Collaborative_Filtering
+from .main import Collab_Cosine
+from .main import Collab_Nearest
+
+
+from .main import Linear_Regression
+from .main import Linear_Regression_Solve
+from .main import Polynomial_Regression
+from .main import Logistic_Regression
+
+from .main import Decision_Tree_1
+from .main import Decision_Tree_2
+
+from .main import Chart
