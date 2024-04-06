1b4974123856374f57426a25e6ce849477628be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 15:03:43 +0000

    Add package RelaDB/1.0.0/RelaDB-1.0.0.tar.gz

---
+++ packages/RelaDB/RelaDB-1.0.0.tar.gz/RelaDB-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RelaDB',
+    version='1.0.0',
+    author='Mulham Alamry',
+    author_email='mulhamreacts@gmail.com',
+    description='A fast and lightweight relational db',
+    long_description='A set of peformant python classes similiar to a relation db with sqlite compatibility',
+    long_description_content_type='text/markdown',
+    url='https://github.com/D4r3d3vil/RelaDB',
+    packages=find_packages(),
+    python_requires='>=3.0',
+)
