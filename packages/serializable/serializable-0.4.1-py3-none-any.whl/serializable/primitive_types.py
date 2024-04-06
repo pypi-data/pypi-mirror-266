d668314145bfca743678dcfd2e65e63dcfae9970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 05:04:35 +0000

    Add package serializable/0.4.1/serializable-0.4.1-py3-none-any.whl

---
+++ packages/serializable/serializable-0.4.1-py3-none-any.whl/serializable/primitive_types.py
@@ -0,0 +1,33 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+A type is considered "primitive" if it is a non-collection type which can
+be serialized and deserialized successfully.
+"""
+from functools import wraps
+
+NoneType = type(None)
+
+PRIMITIVE_TYPES = (bool, float, NoneType, str, int)
+
+def return_primitive(fn):
+    """
+    Decorator which wraps a single argument function to ignore any
+    arguments of primitive type (simply returning them unmodified).
+    """
+    @wraps(fn)
+    def wrapped_fn(x):
+        if isinstance(x, PRIMITIVE_TYPES):
+            return x
+        return fn(x)
+    return wrapped_fn
