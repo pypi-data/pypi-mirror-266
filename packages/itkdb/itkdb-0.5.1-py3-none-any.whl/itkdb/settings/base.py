84d4c685c8f4d2a380644d7da7aa2c744b1ac394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 15:04:10 +0000

    Add package itkdb/0.5.1/itkdb-0.5.1-py3-none-any.whl

---
+++ packages/itkdb/itkdb-0.5.1-py3-none-any.whl/itkdb/settings/base.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+SIMPLE_SETTINGS = {"OVERRIDE_BY_ENV": True}
+ITKDB_ACCESS_CODE1 = ""
+ITKDB_ACCESS_CODE2 = ""
+ITKDB_ACCESS_SCOPE = "openid https://itkpd-test.unicorncollege.cz"
+ITKDB_ACCESS_AUDIENCE = "https://itkpd-test.unicorncollege.cz"
+ITKDB_AUTH_URL = "https://uuidentity.plus4u.net/uu-oidc-maing02/bb977a99f4cc4c37a2afce3fd599d0a7/oidc/"
+ITKDB_SITE_URL = "https://itkpd-test.unicorncollege.cz/"
+ITKDB_CASSETTE_LIBRARY_DIR = "tests/integration/cassettes"
+ITKDB_LEEWAY = 2
