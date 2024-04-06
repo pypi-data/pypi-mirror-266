215ff0c550e708e524e3a57111889f42d3273eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 15:04:51 +0000

    Add package lungmask/0.2.20/lungmask-0.2.20.tar.gz

---
+++ packages/lungmask/lungmask-0.2.20.tar.gz/lungmask-0.2.20/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lungmask",
+    version="0.2.20",
+    author="Johannes Hofmanninger",
+    author_email="j.hofmanninger@gmail.com",
+    description="Package for automated lung segmentation in CT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JoHof/lungmask",
+    packages=setuptools.find_packages(),
+    entry_points={"console_scripts": ["lungmask = lungmask.__main__:main"]},
+    install_requires=[
+        "pydicom",
+        "numpy",
+        "torch",
+        "scipy",
+        "SimpleITK",
+        "tqdm",
+        "scikit-image",
+        "fill_voids",
+        "more-itertools",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
