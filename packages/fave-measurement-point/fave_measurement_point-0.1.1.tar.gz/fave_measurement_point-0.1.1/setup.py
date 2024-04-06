7df8f4ff60dbc15ba237ae773585147d146cbf85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 04:04:07 +0000

    Add package fave-measurement-point/0.1.1/fave_measurement_point-0.1.1.tar.gz

---
+++ packages/fave-measurement-point/fave_measurement_point-0.1.1.tar.gz/fave_measurement_point-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['fave_measurement_point']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['nptyping>=2.5.0,<3.0.0', 'numpy>=1.26.4,<2.0.0']
+
+setup_kwargs = {
+    'name': 'fave-measurement-point',
+    'version': '0.1.1',
+    'description': 'A library for defining and evaluating formant measurement point heuristics.',
+    'long_description': '# fave-measurement-point\nA library for defining and evaluating formant measurement point heuristics.\n',
+    'author': 'JoFrhwld',
+    'author_email': 'JoFrhwld@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
