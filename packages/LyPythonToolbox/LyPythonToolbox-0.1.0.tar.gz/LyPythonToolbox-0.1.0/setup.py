914c50723dc22875b8eb158a20873b75a1daf9d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 03:04:19 +0000

    Add package LyPythonToolbox/0.1.0/LyPythonToolbox-0.1.0.tar.gz

---
+++ packages/LyPythonToolbox/LyPythonToolbox-0.1.0.tar.gz/LyPythonToolbox-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                          = "LyPythonToolbox",                              # 包名
+    version                       = "0.1.0",                                        # 版本号
+    author                        = "Yue Lin",                                      # 作者名字
+    author_email                  = "linyue3h1@gmail.com",                          # 作者邮箱
+    description                   = "Ly's personal python toolbox.",                # 简短描述
+    long_description              = open("README.md").read(),                       # 长描述，通常是README文件
+    long_description_content_type = "text/markdown",                                # 长描述内容的格式，这里为Markdown
+    url                           = "https://github.com/YueLin301/LyPythonToolbox", # 项目的URL，通常是GitHub的URL
+    packages                      = find_packages(),                                # 告诉Python要包含哪些目录中的文件
+    install_requires              = [
+        # 'torch>=1.7.1',  # 表示安装torch时，版本需要1.7.1或更高
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",  # 3.x
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",  # suitable for any OS.
+    ],
+)
