d16fc576b775bcb6ba2c1966ba3807fd79e65d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 05:05:21 +0000

    Add package sparkautomapper.fhir/2.0.1/sparkautomapper.fhir-2.0.1.tar.gz

---
+++ packages/sparkautomapper.fhir/sparkautomapper.fhir-2.0.1.tar.gz/sparkautomapper.fhir-2.0.1/setup.py
@@ -0,0 +1,76 @@
+# noinspection Mypy
+from typing import Any
+
+from setuptools import setup, find_packages
+from os import path, getcwd
+
+# from https://packaging.python.org/tutorials/packaging-projects/
+
+# noinspection SpellCheckingInspection
+package_name = "sparkautomapper.fhir"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+try:
+    with open(path.join(getcwd(), "VERSION")) as version_file:
+        version = version_file.read().strip()
+except IOError:
+    raise
+
+
+def fix_setuptools() -> None:
+    """Work around bugs in setuptools.
+
+    Some versions of setuptools are broken and raise SandboxViolation for normal
+    operations in a virtualenv. We therefore disable the sandbox to avoid these
+    issues.
+    """
+    try:
+        from setuptools.sandbox import DirectorySandbox
+
+        # noinspection PyUnusedLocal
+        def violation(operation: Any, *args: Any, **_: Any) -> None:
+            print("SandboxViolation: %s" % (args,))
+
+        DirectorySandbox._violation = violation
+    except ImportError:
+        pass
+
+
+# Fix bugs in setuptools.
+fix_setuptools()
+
+# classifiers list is here: https://pypi.org/classifiers/
+
+# create the package setup
+setup(
+    name=package_name,
+    version=version,
+    author="Imran Qureshi",
+    author_email="imranq2@hotmail.com",
+    description="FHIR extensions for SparkAutoMapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/imranq2/SparkAutoMapper.FHIR",
+    packages=find_packages(),
+    install_requires=[
+        "logger",
+        "pyspark==3.3.0",
+        "sparkautomapper>=2.0.1",
+        "sparkfhirschemas>=1.0.12",
+        "deprecated",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6,<4",
+    dependency_links=[],
+    include_package_data=True,
+    zip_safe=False,
+    package_data={package_name: ["py.typed"]},
+    data_files=[],
+)
