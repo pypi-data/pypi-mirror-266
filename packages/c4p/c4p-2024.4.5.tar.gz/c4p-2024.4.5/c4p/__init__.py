2627d3a3e3ffaf3581bb7e8482bb6d19c023e64f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 15:04:30 +0000

    Add package c4p/2024.4.5/c4p-2024.4.5.tar.gz

---
+++ packages/c4p/c4p-2024.4.5.tar.gz/c4p-2024.4.5/c4p/__init__.py
@@ -0,0 +1,16 @@
+from .case import PaleoSetup
+from .case import CESMCase
+from .pp import Archive, PPCase
+from .visual import set_style
+from .rotation import Rotation
+
+set_style(style='journal', font_scale=1.2)
+
+# get the version
+from importlib.metadata import version
+__version__ = version('c4p')
+
+
+# mute future warnings from pkgs like pandas
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
