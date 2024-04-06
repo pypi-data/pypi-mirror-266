bc4372d1260f159e2ca280ad90fa5010c5283f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 14:04:14 +0000

    Add package onediffx/0.13.0.dev202404060123/onediffx-0.13.0.dev202404060123.tar.gz

---
+++ packages/onediffx/onediffx-0.13.0.dev202404060123.tar.gz/onediffx-0.13.0.dev202404060123/onediffx/__init__.py
@@ -0,0 +1,7 @@
+from onediff.infer_compiler.oneflow_compiler_config import (
+    oneflow_compiler_config as compiler_config,
+)
+from .compilers.diffusion_pipeline_compiler import compile_pipe, save_pipe, load_pipe
+
+__all__ = ["compile_pipe", "compiler_config", "save_pipe", "load_pipe"]
+__version__ = "0.13.0.dev202404060123"
