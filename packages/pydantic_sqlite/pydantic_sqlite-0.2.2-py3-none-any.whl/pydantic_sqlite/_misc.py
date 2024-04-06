b2b31b21651a570a2f7500ae545068f9c61c91a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 06:04:06 +0000

    Add package pydantic_sqlite/0.2.2/pydantic_sqlite-0.2.2-py3-none-any.whl

---
+++ packages/pydantic_sqlite/pydantic_sqlite-0.2.2-py3-none-any.whl/pydantic_sqlite/_misc.py
@@ -0,0 +1,19 @@
+import os
+
+
+def uniquify(path):
+    filename, extension = os.path.splitext(path)
+    counter = 1
+
+    while os.path.exists(path):
+        path = filename + "_(" + str(counter) + ")" + extension
+        counter += 1
+
+    return path
+
+
+def iterable_in_type_repr(type_repr):
+    if 'List' in type_repr:
+        return True
+    else:
+        return False
