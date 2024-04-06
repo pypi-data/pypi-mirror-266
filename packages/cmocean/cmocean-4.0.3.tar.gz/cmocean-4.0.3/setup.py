fe688713c8a42f4da8ae383e7bbc7c00c141fe06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 14:03:36 +0000

    Add package cmocean/4.0.3/cmocean-4.0.3.tar.gz

---
+++ packages/cmocean/cmocean-4.0.3.tar.gz/cmocean-4.0.3/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+"""
+setup.py for cmocean
+
+"""
+import sys
+from setuptools import setup # to support "develop" mode
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.verbose = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+extras_require={
+    'plots':  ["colorspacious", "viscm"],
+}
+# # in case I add more later
+# extras_require['complete'] = sorted(set(sum(extras_require.values(), [])))
+
+setup(
+    name = "cmocean",
+    version = "v4.0.3",
+    author = "Kristen Thyng",
+    author_email = "kthyng@gmail.com",
+    url = 'https://github.com/matplotlib/cmocean',
+    # download_url = 'https://github.com/matplotlib/cmocean/tarball/2.0',
+    description = ("Colormaps for Oceanography"),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+                 "Development Status :: 3 - Alpha",
+                 ],
+    package_data={
+        'cmocean': ['rgb/*.txt', 'rgb/inverted/*.txt'],
+    },
+    packages = ["cmocean"],
+    ext_package='cmocean',
+    scripts = [],
+    keywords = ['colormaps', 'oceanography', 'plotting', 'visualization'],
+    setup_requires=['setuptools'],
+    install_requires=['matplotlib', 'numpy', 'packaging'],
+    tests_require=['pytest'],
+    python_requires=">=3.8",
+    extras_require=extras_require
+    )
