50d71057d3493e660ae573b56cf76a012637c653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 04:04:03 +0000

    Add package sherpa-onnx/1.9.16/sherpa_onnx-1.9.16-cp38-cp38-win32.whl

---
+++ packages/sherpa-onnx/sherpa_onnx-1.9.16-cp38-cp38-win32.whl/sherpa_onnx/__init__.py
@@ -0,0 +1,28 @@
+from _sherpa_onnx import (
+    Alsa,
+    CircularBuffer,
+    Display,
+    OfflineStream,
+    OfflineTts,
+    OfflineTtsConfig,
+    OfflineTtsModelConfig,
+    OfflineTtsVitsModelConfig,
+    OnlineStream,
+    SileroVadModelConfig,
+    SpeakerEmbeddingExtractor,
+    SpeakerEmbeddingExtractorConfig,
+    SpeakerEmbeddingManager,
+    SpeechSegment,
+    SpokenLanguageIdentification,
+    SpokenLanguageIdentificationConfig,
+    SpokenLanguageIdentificationWhisperConfig,
+    VadModel,
+    VadModelConfig,
+    VoiceActivityDetector,
+)
+
+from .keyword_spotter import KeywordSpotter
+from .offline_recognizer import OfflineRecognizer
+from .online_recognizer import OnlineRecognizer
+from .utils import text2token
+__version__ = '1.9.16'
