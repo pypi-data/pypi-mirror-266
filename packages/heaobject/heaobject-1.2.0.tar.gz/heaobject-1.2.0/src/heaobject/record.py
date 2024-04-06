02b4a0004398c8b575dac414e7ade4d82e3eb25f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 05:05:56 +0000

    Add package heaobject/1.2.0/heaobject-1.2.0.tar.gz

---
+++ packages/heaobject/heaobject-1.2.0.tar.gz/heaobject-1.2.0/src/heaobject/record.py
@@ -0,0 +1,7 @@
+from heaobject import root
+
+
+class Record(root.AbstractDesktopObject):
+    def __init__(self):
+        super().__init__()
+
---
+++ packages/heaobject/heaobject-1.2.0.tar.gz/heaobject-1.2.0/src/heaobject/settings.py
@@ -0,0 +1,20 @@
+from heaobject.root import View
+from heaobject.data import DataObject
+
+
+class SettingsObject(DataObject, View):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__user = None
+
+    @property
+    def mime_type(self) -> str:
+        return 'application/x.settingsobject'
+
+    @property
+    def user(self) -> str | None:
+        return self.__user
+
+    @user.setter
+    def user(self, user: str | None):
+        self.__user = str(user) if user is not None else None
