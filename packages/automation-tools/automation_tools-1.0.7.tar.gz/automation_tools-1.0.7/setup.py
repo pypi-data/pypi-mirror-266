0a34e6dd4587f3f445c1da1e551085aaf7946657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 06:03:41 +0000

    Add package automation-tools/1.0.7/automation_tools-1.0.7.tar.gz

---
+++ packages/automation-tools/automation_tools-1.0.7.tar.gz/automation_tools-1.0.7/setup.py
@@ -0,0 +1,22 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='automation_tools',
+    version='1.0.7',
+    packages=find_packages(),
+    install_requires=[
+        'openpyxl',
+    ],
+    author='Omar Lydale Morrison',
+    author_email='omar.morrison@sportyventures.com',
+    description='A suite of automation tools for various tasks.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/sportyomar/automation_tools',
+    license='MIT',
+)
+
+
+
+
