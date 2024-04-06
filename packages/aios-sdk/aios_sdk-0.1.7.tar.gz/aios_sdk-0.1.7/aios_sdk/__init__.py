692b8e8e4592a592630a35ea1f11458d35b850c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 14:04:09 +0000

    Add package aios-sdk/0.1.7/aios_sdk-0.1.7.tar.gz

---
+++ packages/aios-sdk/aios_sdk-0.1.7.tar.gz/aios_sdk-0.1.7/aios_sdk/__init__.py
@@ -0,0 +1,25 @@
+from typing import List
+from grpc import Channel
+import grpc
+from .rpc import models_pb2_grpc, models_pb2
+from .models import Model
+
+
+class Client():
+    channel: Channel
+    def __init__(self, channel: str):
+        self.channel = grpc.insecure_channel(channel)
+
+    def get_models(self) -> List[Model]:
+        stub = models_pb2_grpc.ModelsServiceStub(self.channel)
+        request = models_pb2.ListRequest()
+        list: models_pb2.ListResponse = stub.List(request)
+
+        
+        return [Model(id=model.id, model_size=model.model_size, tokenizer_size=model.tokenizer_size) for model in list.models]
+        
+        
+        
+        
+        
+        
