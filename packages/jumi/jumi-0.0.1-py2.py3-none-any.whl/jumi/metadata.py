af495eb2d577d3f5b3ba5fd0b15a657932fc7f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 14:04:02 +0000

    Add package jumi/0.0.1/jumi-0.0.1-py2.py3-none-any.whl

---
+++ packages/jumi/jumi-0.0.1-py2.py3-none-any.whl/jumi/metadata.py
@@ -0,0 +1,8 @@
+# --------------------------------------------------------------------------------------------------
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2024 Jayesh Badwaik <j.badwaik@fz-juelich.de>
+# --------------------------------------------------------------------------------------------------
+import semver
+
+__version__ = "0.0.1"
+semver = semver.VersionInfo.parse(__version__)
