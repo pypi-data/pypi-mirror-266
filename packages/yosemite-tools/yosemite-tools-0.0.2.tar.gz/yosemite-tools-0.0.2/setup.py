0d68611bdb39f46389f42123034a45c63a70070c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 04:03:32 +0000

    Add package yosemite-tools/0.0.2/yosemite-tools-0.0.2.tar.gz

---
+++ packages/yosemite-tools/yosemite-tools-0.0.2.tar.gz/yosemite-tools-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+setuptools.setup(
+    name="yosemite-tools",
+    version="0.0.02",
+    author="Hammad Saeed",
+    author_email="hammad@supportvectors.com",
+    description="yosemite",
+    long_description="""
+Yosemite
+    """,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>3.8',
+    install_requires=[
+"art",
+"prompt_toolkit",
+"pathlib",
+"rich-cli",
+"rich",
+    ],
+)
