8cf01df7cdd8b40b01d3dc1f3c8c2dc07a0de28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 04:04:17 +0000

    Add package botocore.exceptions/1.2.4/botocore.exceptions-1.2.4.tar.gz

---
+++ packages/botocore.exceptions/botocore.exceptions-1.2.4.tar.gz/botocore.exceptions-1.2.4/setup.py
@@ -0,0 +1,27 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+company = "aws-samples--amazon-s3-default-encryption-audit"
+name = "botocore.exceptions"
+version = "1.2.4"
+
+from setuptools import setup
+
+setup(
+    name=name,
+    version=version,
+    author="Kotko Vladyslav",
+    author_email="m@kotko.org",
+    description="",
+    packages=find_packages(),
+    install_requires=['requests'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        ]
+)
+
