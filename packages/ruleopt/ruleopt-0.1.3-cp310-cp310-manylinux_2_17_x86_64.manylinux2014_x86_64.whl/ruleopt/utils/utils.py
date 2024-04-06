0c9f67afad4a8f5e6f3b1c8dfb16debcbc88fda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 15:04:43 +0000

    Add package ruleopt/0.1.3/ruleopt-0.1.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ruleopt/ruleopt-0.1.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ruleopt/utils/utils.py
@@ -0,0 +1,34 @@
+from typing import Tuple
+from sklearn.utils import check_array
+import numpy as np
+import importlib.util
+    
+def check_inputs(x, y = None) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Validates and preprocesses input data for the model.
+
+    Parameters
+    ----------
+    x : array-like of shape (n_samples, n_features)
+        The training input samples. Internally, it will be converted to dtype=np.float32.
+    y : array-like of shape (n_samples,) or (n_samples, n_outputs)
+        The target values (class labels) as integers
+
+    Returns
+    -------
+    Tuple[np.ndarray, np.ndarray]
+        The validated and preprocessed input matrix `x` and target vector `y`.
+    """
+    x = check_array(np.asarray(x, dtype=np.float32), force_all_finite="allow-nan")
+    if y is not None:
+        y = check_array(np.asarray(y, dtype=np.intp), ensure_2d=False)
+        return x, y
+    return x
+
+
+def check_module_available(module_name):
+    """
+    Checks module is installed.
+    """
+    spec = importlib.util.find_spec(module_name)
+    return spec is not None
