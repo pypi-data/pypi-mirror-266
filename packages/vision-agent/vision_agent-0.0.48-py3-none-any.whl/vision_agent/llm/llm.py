21ac100f9bde92a4914a4f1ce4f1c02d84ae86db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 14:03:45 +0000

    Add package vision-agent/0.0.48/vision_agent-0.0.48-py3-none-any.whl

---
+++ packages/vision-agent/vision_agent-0.0.48-py3-none-any.whl/vision_agent/llm/llm.py
@@ -0,0 +1,122 @@
+import json
+from abc import ABC, abstractmethod
+from typing import Any, Callable, Dict, List, Mapping, Union, cast
+
+from openai import OpenAI
+
+from vision_agent.tools import (
+    CHOOSE_PARAMS,
+    CLIP,
+    SYSTEM_PROMPT,
+    GroundingDINO,
+    GroundingSAM,
+)
+
+
+class LLM(ABC):
+    @abstractmethod
+    def generate(self, prompt: str) -> str:
+        pass
+
+    @abstractmethod
+    def chat(self, chat: List[Dict[str, str]]) -> str:
+        pass
+
+    @abstractmethod
+    def __call__(self, input: Union[str, List[Dict[str, str]]]) -> str:
+        pass
+
+
+class OpenAILLM(LLM):
+    r"""An LLM class for any OpenAI LLM model."""
+
+    def __init__(
+        self,
+        model_name: str = "gpt-4-turbo-preview",
+        json_mode: bool = False,
+        **kwargs: Any
+    ):
+        self.model_name = model_name
+        self.client = OpenAI()
+        self.kwargs = kwargs
+        if json_mode:
+            self.kwargs["response_format"] = {"type": "json_object"}
+
+    def generate(self, prompt: str) -> str:
+        response = self.client.chat.completions.create(
+            model=self.model_name,
+            messages=[
+                {"role": "user", "content": prompt},
+            ],
+            **self.kwargs,
+        )
+
+        return cast(str, response.choices[0].message.content)
+
+    def chat(self, chat: List[Dict[str, str]]) -> str:
+        response = self.client.chat.completions.create(
+            model=self.model_name,
+            messages=chat,  # type: ignore
+            **self.kwargs,
+        )
+
+        return cast(str, response.choices[0].message.content)
+
+    def __call__(self, input: Union[str, List[Dict[str, str]]]) -> str:
+        if isinstance(input, str):
+            return self.generate(input)
+        return self.chat(input)
+
+    def generate_classifier(self, question: str) -> Callable:
+        api_doc = CLIP.description + "\n" + str(CLIP.usage)
+        prompt = CHOOSE_PARAMS.format(api_doc=api_doc, question=question)
+        response = self.client.chat.completions.create(
+            model=self.model_name,
+            response_format={"type": "json_object"},
+            messages=[
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": prompt},
+            ],
+        )
+
+        params = json.loads(cast(str, response.choices[0].message.content))[
+            "Parameters"
+        ]
+
+        return lambda x: CLIP()(**{"prompt": params["prompt"], "image": x})
+
+    def generate_detector(self, question: str) -> Callable:
+        api_doc = GroundingDINO.description + "\n" + str(GroundingDINO.usage)
+        prompt = CHOOSE_PARAMS.format(api_doc=api_doc, question=question)
+        response = self.client.chat.completions.create(
+            model=self.model_name,
+            response_format={"type": "json_object"},
+            messages=[
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": prompt},
+            ],
+        )
+
+        params: Mapping = json.loads(cast(str, response.choices[0].message.content))[
+            "Parameters"
+        ]
+
+        return lambda x: GroundingDINO()(**{"prompt": params["prompt"], "image": x})
+
+    def generate_segmentor(self, question: str) -> Callable:
+        api_doc = GroundingSAM.description + "\n" + str(GroundingSAM.usage)
+        prompt = CHOOSE_PARAMS.format(api_doc=api_doc, question=question)
+        response = self.client.chat.completions.create(
+            model=self.model_name,
+            response_format={"type": "json_object"},
+            messages=[
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": prompt},
+            ],
+        )
+
+        params: Mapping = json.loads(cast(str, response.choices[0].message.content))[
+            "Parameters"
+        ]
+
+        return lambda x: GroundingSAM()(**{"prompt": params["prompt"], "image": x})
