e6110a13fbae450db830b3b3bf5d726a447e1636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-06 04:04:25 +0000

    Add package pyspedas/1.5.7/pyspedas-1.5.7.tar.gz

---
+++ packages/pyspedas/pyspedas-1.5.7.tar.gz/pyspedas-1.5.7/pyspedas/version.py
@@ -0,0 +1,16 @@
+"""Print the version number for the current installation."""
+import logging
+
+
+def version():
+    """
+    Print the pyspedas version number.
+
+    Returns
+    -------
+    None.
+
+    """
+    from importlib.metadata import version
+    ver = version("pyspedas")
+    logging.info("pyspedas version: " + ver)
