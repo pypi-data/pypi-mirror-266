83420f7f8b6af2a136757b22070f24d9ca09f946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 03:03:40 +0000

    Add package solie/8.5.1/solie-8.5.1-py3-none-any.whl

---
+++ packages/solie/solie-8.5.1-py3-none-any.whl/solie/utility/__init__.py
@@ -0,0 +1,113 @@
+from .analyze_market import (
+    CalculationInput,
+    CalculationOutput,
+    SimulationError,
+    decide,
+    make_indicators,
+    simulate_chunk,
+)
+from .api_requester import ApiRequester, ApiRequestError
+from .api_streamer import ApiStreamer
+from .backward_compatibility import examine_data_files
+from .ball import ball_ceil, ball_floor
+from .check_internet import (
+    add_connected_functions,
+    add_disconnected_functions,
+    internet_connected,
+    is_internet_checked,
+    monitor_internet,
+)
+from .compare_versions import is_left_version_higher
+from .convert import list_to_dict, value_to_indexes
+from .download_from_binance import (
+    DownloadPreset,
+    download_aggtrade_data,
+    fill_holes_with_aggtrades,
+)
+from .log_handler import LogHandler
+from .pandas_related import combine_candle_data
+from .percent_axis_item import PercentAxisItem
+from .rw_lock import RWLock
+from .simply_format import format_numeric
+from .sort_pandas import sort_data_frame, sort_series
+from .standardize import (
+    create_strategy_code_name,
+    standardize_account_state,
+    standardize_asset_record,
+    standardize_candle_data,
+    standardize_unrealized_changes,
+)
+from .stop_flag import find_stop_flag, make_stop_flag
+from .structs import (
+    SimulationSettings,
+    SimulationSummary,
+    Strategies,
+    Strategy,
+    TransactionSettings,
+)
+from .syntax_highlighter import SyntaxHighlighter
+from .time_axis_item import TimeAxisItem
+from .timing import add_task_duration, get_current_moment, get_task_duration
+from .user_settings import (
+    DataSettings,
+    read_data_settings,
+    read_datapath,
+    save_data_settings,
+    save_datapath,
+)
+
+__all__ = [
+    "ApiRequester",
+    "ApiRequestError",
+    "ApiStreamer",
+    "ball_ceil",
+    "ball_floor",
+    "add_connected_functions",
+    "add_disconnected_functions",
+    "internet_connected",
+    "monitor_internet",
+    "combine_candle_data",
+    "is_left_version_higher",
+    "list_to_dict",
+    "value_to_indexes",
+    "decide",
+    "download_aggtrade_data",
+    "examine_data_files",
+    "fill_holes_with_aggtrades",
+    "LogHandler",
+    "make_indicators",
+    "PercentAxisItem",
+    "add_task_duration",
+    "get_task_duration",
+    "RWLock",
+    "format_numeric",
+    "sort_data_frame",
+    "sort_series",
+    "standardize_candle_data",
+    "standardize_account_state",
+    "standardize_asset_record",
+    "standardize_unrealized_changes",
+    "create_strategy_code_name",
+    "make_stop_flag",
+    "find_stop_flag",
+    "Strategy",
+    "Strategies",
+    "TransactionSettings",
+    "SimulationSettings",
+    "SimulationSummary",
+    "SyntaxHighlighter",
+    "TimeAxisItem",
+    "read_data_settings",
+    "read_datapath",
+    "save_data_settings",
+    "save_datapath",
+    "DataSettings",
+    "DownloadPreset",
+    "CalculationInput",
+    "CalculationOutput",
+    "simulate_chunk",
+    "SimulationError",
+    "is_internet_checked",
+    "monitor_internet",
+    "get_current_moment",
+]
---
+++ packages/solie/solie-8.5.1-py3-none-any.whl/solie/utility/check_internet.py
@@ -0,0 +1,72 @@
+import asyncio
+import logging
+from typing import Callable, Coroutine
+
+import aiohttp
+
+logger = logging.getLogger(__name__)
+
+is_internet_checked = asyncio.Event()
+was_connected = False
+connected_functions: list[Callable[[], Coroutine]] = []
+disconnected_functions: list[Callable[[], Coroutine]] = []
+
+
+def internet_connected():
+    if is_internet_checked.is_set():
+        return was_connected
+    else:
+        raise RuntimeError("Internet connection is not being monitored")
+
+
+async def monitor_internet():
+    global was_connected
+    while True:
+        # try to connect to DNS servers
+        attempt_ips = [
+            "1.0.0.1",  # Cloudflare
+            "1.1.1.1",  # Cloudflare
+            "8.8.4.4",  # Google
+            "8.8.8.8",  # Google
+            "9.9.9.9",  # Quad9
+            "149.112.112.112",  # Quad9
+            "208.67.222.222",  # OpenDNS
+            "208.67.220.220",  # OpenDNS
+        ]
+        is_connected = False
+        async with aiohttp.ClientSession() as session:
+            for attempt_ip in attempt_ips:
+                try:
+                    async with session.get(f"http://{attempt_ip}") as response:
+                        if response.status == 200:
+                            is_connected = True
+                            break
+                except Exception:
+                    pass
+
+        # detect changes
+        if was_connected and not is_connected:
+            for job in disconnected_functions:
+                asyncio.create_task(job())
+            logger.warning("Internet disconnected")
+        elif not was_connected and is_connected:
+            for job in connected_functions:
+                asyncio.create_task(job())
+            logger.info("Internet connected")
+
+        # remember connection state
+        was_connected = is_connected
+        is_internet_checked.set()
+
+        # wait for a while
+        await asyncio.sleep(1)
+
+
+def add_connected_functions(job: Callable[[], Coroutine]):
+    global connected_functions
+    connected_functions.append(job)
+
+
+def add_disconnected_functions(job: Callable[[], Coroutine]):
+    global disconnected_functions
+    disconnected_functions.append(job)
