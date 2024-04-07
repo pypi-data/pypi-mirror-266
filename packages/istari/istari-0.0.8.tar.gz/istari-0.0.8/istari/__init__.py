08f4c136d6c4d8d3ef44fe6924314df18da17079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 14:04:00 +0000

    Add package istari/0.0.8/istari-0.0.8.tar.gz

---
+++ packages/istari/istari-0.0.8.tar.gz/istari-0.0.8/istari/__init__.py
@@ -0,0 +1,11 @@
+import secrets
+from pathlib import Path
+
+__version__ = '0.0.8'
+
+BASE_DIR = Path(__file__).resolve().parent
+
+SECRET_KEY_INSECURE_PREFIX = 'django-insecure'
+
+def get_random_secret_key():
+    return ''.join(secrets.choice('abcdefghijklmnopqrstuvwxyz0123456789!@#%^&*(-_=+)') for i in range(50))
