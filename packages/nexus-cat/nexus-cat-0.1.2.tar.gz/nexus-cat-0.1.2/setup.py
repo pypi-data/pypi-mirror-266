4c36a1193b2e63c98335c807baf2856cf040f17b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 04:05:01 +0000

    Add package nexus-cat/0.1.2/nexus-cat-0.1.2.tar.gz

---
+++ packages/nexus-cat/nexus-cat-0.1.2.tar.gz/nexus-cat-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nexus-cat',
+    version='0.1.2',
+    description='Nexus is a Cluster Analysing Toolkit package for atomic systems.',
+    author='Julien Perradin',
+    author_email='julien.perradin@umontpellier.fr',
+    url='https://github.com/JulienPerradin/nexus',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'tqdm',
+        'natsort'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
