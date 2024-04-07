2975330ae5c6048f5969793ab73d0e0b27b2a3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 04:04:57 +0000

    Add package Toisto/0.18.0/Toisto-0.18.0-py3-none-any.whl

---
+++ packages/Toisto/Toisto-0.18.0-py3-none-any.whl/toisto/ui/dictionary.py
@@ -0,0 +1,28 @@
+"""Link words to an online dictionary."""
+
+from string import punctuation
+from typing import Final
+
+DICTIONARY_URL: Final = "https://en.wiktionary.org/wiki"
+
+
+def linkify_and_enumerate(*texts: str, sep: str = ", ") -> str:
+    """Return a linkified and enumerated version of the texts."""
+    return sep.join(f'"{linkify(text)}"' for text in texts)
+
+
+def linkify(text: str) -> str:
+    """Return a version of the text where each word is turned into a link to a dictionary."""
+    linkified_words = []
+    for word in text.split():
+        prefix, infix, postfix = split_punctuation(word)
+        linkified_words.append(f"{prefix}[link={DICTIONARY_URL}/{infix.lower()}]{infix}[/link]{postfix}")
+    return " ".join(linkified_words)
+
+
+def split_punctuation(text: str) -> tuple[str, str, str]:
+    """Return a tuple of prefixed punctuation, the text without punctuation, and postfix punctuation."""
+    stripped_text = text.strip(punctuation)
+    stripped_text_start = text.find(stripped_text)
+    stripped_text_end = stripped_text_start + len(stripped_text)
+    return text[:stripped_text_start], text[stripped_text_start:stripped_text_end], text[stripped_text_end:]
