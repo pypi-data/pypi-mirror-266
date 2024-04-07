b800930dfa0969087ae36df3b8d5dc661d9c5cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 15:03:45 +0000

    Add package crawlab-ai/0.0.3/crawlab-ai-0.0.3.tar.gz

---
+++ packages/crawlab-ai/crawlab-ai-0.0.3.tar.gz/crawlab-ai-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='crawlab-ai',
+    version='0.0.3',
+    packages=['crawlab_ai'],
+    url='https://github.com/crawlab-team/crawlab-ai-sdk',
+    license='MIT',
+    author='Marvin Zhang',
+    author_email='tikazyq@163.com',
+    description='SDK for Crawlab AI',
+    package_dir={'crawlab_ai': 'crawlab_ai'},
+    keywords=['crawlab', 'ai'],
+)
