64ec95d5d77aebc542466f14b6ac414b1e5b9720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 03:03:51 +0000

    Add package komodo-sdk/0.0.78/komodo_sdk-0.0.78-py3-none-any.whl

---
+++ packages/komodo-sdk/komodo_sdk-0.0.78-py3-none-any.whl/komodo/framework/komodo_features.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class Komodo:
+    company = "Komodo AI"
+
+
+class KomodoApplianceType(Enum):
+    enterprise = "enterprise"
+    retail = "retail"
+
+
+class KomodoFeatures(Enum):
+    chat = "chat"
+    chatdoc = "chatdoc"
+    reportbuilder = "reportbuilder"
+
+
+if __name__ == '__main__':
+    print(KomodoApplianceType.enterprise.name)
+    features = [e.value for e in KomodoFeatures]
+    print(", ".join(features))
