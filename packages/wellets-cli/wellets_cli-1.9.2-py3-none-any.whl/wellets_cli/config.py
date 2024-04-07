c341a5ba17ad2de3301abb88c2939b4c9c7e5531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 03:03:59 +0000

    Add package wellets-cli/1.9.2/wellets_cli-1.9.2-py3-none-any.whl

---
+++ packages/wellets-cli/wellets_cli-1.9.2-py3-none-any.whl/wellets_cli/config.py
@@ -0,0 +1,15 @@
+from dynaconf import Dynaconf, Validator
+
+settings = Dynaconf(
+    envvar_prefix="WELLETS",
+    settings_files=["settings.toml", ".secrets.toml"],
+    environments=True,
+    env_switcher="WELLETS_ENV",
+    validators=[
+        Validator("api_url", required=True),
+    ],
+    env="production",
+)
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load these files in the order.
