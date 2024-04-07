6de708b201d3ca3e55697151f857a6a9b795366d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 03:03:46 +0000

    Add package pyeventmanager/0.5.2/pyeventmanager-0.5.2-py3-none-any.whl

---
+++ packages/pyeventmanager/pyeventmanager-0.5.2-py3-none-any.whl/event_manager/listeners/base.py
@@ -0,0 +1,37 @@
+import inspect
+from collections.abc import Callable
+from concurrent.futures import Future
+from typing import Protocol
+
+from event_manager.fork_types import ForkType
+
+
+class BaseListener(Protocol):
+    """
+    An abstract class that represents a listener. It should not be used directly, but through its concrete subclasses.
+    """
+
+    event: str
+    fork_type: ForkType
+    func: Callable
+
+    def __call__(self, *args, **kwargs) -> Future:
+        raise NotImplementedError()
+
+
+def _wrapper(_func: Callable, _future: Future, *args, **kwargs):
+    """
+    Wrapper function to run the function and store the result in the future.
+
+    Args:
+        _func (Callable): Function to run.
+        _future (Future): Future to store the result in.
+    """
+    if _future.set_running_or_notify_cancel():
+        try:
+            if inspect.getfullargspec(_func).args or inspect.getfullargspec(_func).kwonlyargs:
+                _future.set_result(_func(*args, **kwargs))
+            else:
+                _future.set_result(_func())
+        except Exception as e:
+            _future.set_exception(e)
