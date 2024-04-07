44e76a1de71a0a5bcbfccbc4ab66ecea6504087e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 04:04:59 +0000

    Add package xknxproject/3.7.1/xknxproject-3.7.1.tar.gz

---
+++ packages/xknxproject/xknxproject-3.7.1.tar.gz/xknxproject-3.7.1/test/test_util.py
@@ -0,0 +1,51 @@
+"""Test utilities."""
+
+import pytest
+
+from xknxproject.util import get_dpt_type, parse_dpt_types
+
+
+@pytest.mark.parametrize(
+    ("dpt_string", "expected"),
+    [
+        ("DPT-1", {"main": 1, "sub": None}),
+        ("DPT-1 DPST-1-1", {"main": 1, "sub": None}),
+        ("DPT-7 DPST-7-1", {"main": 7, "sub": None}),
+        ("DPST-5-1", {"main": 5, "sub": 1}),
+        ("DPT-1 DPT-5", {"main": 1, "sub": None}),
+        ("DPT-14 DPST-14-1", {"main": 14, "sub": None}),
+        ("DPST-6-10", {"main": 6, "sub": 10}),
+        ("Wrong", None),
+        ("DPT-Wrong", None),
+        ("DPST-1-Wrong", None),
+        ("DPST-5", None),
+        ([], None),
+        (None, None),
+    ],
+)
+def test_get_dpt_type(dpt_string, expected):
+    """Test parsing single DPT from ETS project."""
+    assert get_dpt_type(dpt_string) == expected
+
+
+@pytest.mark.parametrize(
+    ("dpt_string", "expected"),
+    [
+        ("DPT-1", [{"main": 1, "sub": None}]),
+        ("DPT-1 DPST-1-1", [{"main": 1, "sub": None}, {"main": 1, "sub": 1}]),
+        ("DPT-7 DPST-7-1", [{"main": 7, "sub": None}, {"main": 7, "sub": 1}]),
+        ("DPST-5-1", [{"main": 5, "sub": 1}]),
+        ("DPT-1 DPT-5", [{"main": 1, "sub": None}, {"main": 5, "sub": None}]),
+        ("DPT-14 DPST-14-1", [{"main": 14, "sub": None}, {"main": 14, "sub": 1}]),
+        ("DPST-6-10", [{"main": 6, "sub": 10}]),
+        ("Wrong", []),
+        ("DPT-Wrong", []),
+        ("DPST-1-Wrong", []),
+        ("DPST-5", []),
+        ([], []),
+        (None, []),
+    ],
+)
+def test_parse_dpt_types(dpt_string, expected):
+    """Test parsing list of DPT from ETS project."""
+    assert parse_dpt_types(dpt_string) == expected
---
+++ packages/xknxproject/xknxproject-3.7.1.tar.gz/xknxproject-3.7.1/xknxproject/exceptions/__init__.py
@@ -0,0 +1,15 @@
+"""Package for exception handling."""
+
+from .exceptions import (
+    InvalidPasswordException,
+    ProjectNotFoundException,
+    UnexpectedFileContent,
+    XknxProjectException,
+)
+
+__all__ = [
+    "XknxProjectException",
+    "InvalidPasswordException",
+    "ProjectNotFoundException",
+    "UnexpectedFileContent",
+]
