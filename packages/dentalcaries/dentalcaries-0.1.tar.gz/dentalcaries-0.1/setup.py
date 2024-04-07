50737e2b6956a185e4935679383b19febeb1d006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 15:03:32 +0000

    Add package dentalcaries/0.1/dentalcaries-0.1.tar.gz

---
+++ packages/dentalcaries/dentalcaries-0.1.tar.gz/dentalcaries-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dentalcaries',
+    version='0.1',
+    description='A brief description of your project',
+    author='Ahsan Ali',
+    author_email='ahsanalime3@email.com',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    # Add other metadata as needed
+)
