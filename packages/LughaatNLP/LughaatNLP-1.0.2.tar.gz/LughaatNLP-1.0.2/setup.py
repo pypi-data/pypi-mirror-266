309e3f240dd5f5ccc7de4fdb9ebdf8e05437f75b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 03:03:52 +0000

    Add package LughaatNLP/1.0.2/LughaatNLP-1.0.2.tar.gz

---
+++ packages/LughaatNLP/LughaatNLP-1.0.2.tar.gz/LughaatNLP-1.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of README file
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename), 'r', encoding='utf-8') as f:
+        return f.read()
+
+setup(
+    name='LughaatNLP',
+    version='1.0.2',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'LughaatNLP': ['Urdu_Lemma.json', 'stopwords.json', 'tokenizer.json']},
+    entry_points={
+        'console_scripts': [
+            'lughaatnlp = LughaatNLP.LughaatNLP:main'
+        ]
+    },
+    install_requires=[
+        'python-Levenshtein'
+    ],
+    author='Muhammad Noman',
+    author_email='muhammadnomanshafiq76@gmail.com',
+    description='A Python package for natural language preprocessing tasks in Urdu language.',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    license_files=['LICENSE'],
+    keywords='nlp urdu LughaatNLP urdunlp UrduNLP urduhack stanza natural-language-processing text-processing language-processing preprocessing stemming lemmatization tokenization stopwords',
+    url='https://github.com/MuhammadNoman76/LughaatNLP'
+)
