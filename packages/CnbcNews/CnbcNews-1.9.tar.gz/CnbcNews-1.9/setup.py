77b4af6038801982e45f350d9c5a10d7f3cc7700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 03:04:08 +0000

    Add package CnbcNews/1.9/CnbcNews-1.9.tar.gz

---
+++ packages/CnbcNews/CnbcNews-1.9.tar.gz/CnbcNews-1.9/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='CnbcNews',
+    version='1.9',
+    packages=find_packages(),
+    install_requires=[
+        'beautifulsoup4',
+        'pandas'
+    ],
+    entry_points={
+        "console_scripts": [
+            "CnbcNews-getArticles = CnbcNews.main:main",
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
