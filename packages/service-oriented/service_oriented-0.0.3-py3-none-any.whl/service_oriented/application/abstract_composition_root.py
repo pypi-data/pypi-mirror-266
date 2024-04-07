fbade1f6d1f6d4d2cacbce0b89ff20e0cd81da6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 04:04:55 +0000

    Add package service-oriented/0.0.3/service_oriented-0.0.3-py3-none-any.whl

---
+++ packages/service-oriented/service_oriented-0.0.3-py3-none-any.whl/service_oriented/application/abstract_composition_root.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from contextlib import contextmanager
+from typing import Generator, Generic, TypeVar
+
+from service_oriented.application.config import BaseConfig
+
+
+SomeConfig = TypeVar("SomeConfig", bound=BaseConfig)
+SomeContainer = TypeVar("SomeContainer")
+
+
+class AbstractCompositionRoot(Generic[SomeConfig, SomeContainer], ABC):
+    def __init__(self, config: SomeConfig) -> None:
+        self.config = config
+
+    def run(self) -> None:
+        with self.container() as container:
+            self.run_with_container(container=container)
+
+    @contextmanager
+    @abstractmethod
+    def container(self) -> Generator[SomeContainer, None, None]:
+        pass
+
+    @abstractmethod
+    def run_with_container(self, container: SomeContainer) -> None:
+        pass
