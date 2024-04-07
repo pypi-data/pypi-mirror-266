dfb2bbec7afeaa03d23aec3e551128f6ed184831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 14:04:02 +0000

    Add package istari/0.0.9/istari-0.0.9.tar.gz

---
+++ packages/istari/istari-0.0.9.tar.gz/istari-0.0.9/istari/templates/plugins/base.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+class BasePlugin:
+    help = None
+
+    template = None
+
+    def get_help(self):
+        assert self.help is not None, (
+            f'{self.__class__.__name__} must define `.help` or implement `.get_help()`'
+        )
+        return self.help
+
+    def get_template(self):
+        assert self.template is not None, (
+            f'{self.__class__.__name__} must define `.template` or implement `.get_template()`'
+        )
+        return self.template
+
+    def process(self, target_dir: Path):
+        raise NotImplementedError(f'{self.__class__.__name__} must implement `.process()`')
