2b48bbb0f8f32c8cdb15b63306463a735b352a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 04:04:44 +0000

    Add package dlio-profiler-py/0.0.5/dlio_profiler_py-0.0.5-cp39-cp39-manylinux_2_34_x86_64.whl

---
+++ packages/dlio-profiler-py/dlio_profiler_py-0.0.5-cp39-cp39-manylinux_2_34_x86_64.whl/dlio_profiler/lib/cmake/cpp-logger/cpp-logger-targets-relwithdebinfo.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "RelWithDebInfo".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "cpp-logger" for configuration "RelWithDebInfo"
+set_property(TARGET cpp-logger APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(cpp-logger PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "/home/runner/work/dlio-profiler/dlio-profiler/build/lib.linux-x86_64-3.9/dlio_profiler/lib/libcpp-logger.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "libcpp-logger.so"
+  )
+
+list(APPEND _cmake_import_check_targets cpp-logger )
+list(APPEND _cmake_import_check_files_for_cpp-logger "/home/runner/work/dlio-profiler/dlio-profiler/build/lib.linux-x86_64-3.9/dlio_profiler/lib/libcpp-logger.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
