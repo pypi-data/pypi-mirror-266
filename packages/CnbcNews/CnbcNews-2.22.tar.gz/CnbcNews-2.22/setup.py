4d59018eb8da94578105488b8602eee2f9bc8702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 04:04:42 +0000

    Add package CnbcNews/2.22/CnbcNews-2.22.tar.gz

---
+++ packages/CnbcNews/CnbcNews-2.22.tar.gz/CnbcNews-2.22/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='CnbcNews',
+    version='2.22',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'pandas',
+        'json'
+    ],
+    entry_points={
+        "console_scripts": [
+            "CnbcNews-getArticles = CnbcNews.main:main",
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
