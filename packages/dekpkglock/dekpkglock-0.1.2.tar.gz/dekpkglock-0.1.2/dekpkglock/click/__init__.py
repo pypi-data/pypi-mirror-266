1020b53fa269838000d53ca22e86cc05c9563254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 03:03:59 +0000

    Add package dekpkglock/0.1.2/dekpkglock-0.1.2.tar.gz

---
+++ packages/dekpkglock/dekpkglock-0.1.2.tar.gz/dekpkglock-0.1.2/dekpkglock/click/__init__.py
@@ -0,0 +1,18 @@
+import typer
+from dektools.typer import command_version
+from ..core import lock_path, patch_lock
+
+app = typer.Typer(add_completion=False)
+
+command_version(app, __name__)
+
+
+@app.command()
+def lock(name, path):
+    if not lock_path(name, path):
+        raise typer.Exit(1)
+
+
+@app.command()
+def patch(name, path, index=0):
+    patch_lock(name, path, int(index))
