e1c01bbc2bb6dcce334f57f9d92ea89bf19813bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 04:04:36 +0000

    Add package SMsfan/1.2.0/SMsfan-1.2.0.tar.gz

---
+++ packages/SMsfan/SMsfan-1.2.0.tar.gz/SMsfan-1.2.0/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='SMsfan',
+    packages=setuptools.find_packages(),
+    version='1.2.0',
+    license='MIT',
+    description='Library to control Multi-IO Automation Card',
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
+    author='Sequent Microsystems',
+    author_email='olcitu@gmail.com',
+    url='https://sequentmicrosystems.com',
+    keywords=['industrial', 'raspberry', 'power', '4-20mA', '0-10V', 'optoisolated'],
+    install_requires=[
+        "smbus",
+        "RPi.GPIO",
+        ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ],
+)
