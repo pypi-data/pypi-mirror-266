7394cb9709de8d76db3b0a2146b1a99f9a0aefa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 04:04:48 +0000

    Add package discord-check/1.0.2/discord_check-1.0.2.tar.gz

---
+++ packages/discord-check/discord_check-1.0.2.tar.gz/discord_check-1.0.2/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+requirements = []
+setuptools.setup(
+    name='discord_check',
+    version='1.0.2',
+    install_requires=requirements,
+    packages=setuptools.find_packages(),
+    long_description=open('README').read(),
+    long_description_content_type='text/markdown',
+    project_urls={
+        'GitHub': 'https://github.com/desotana/discord_check/',
+    },
+)
