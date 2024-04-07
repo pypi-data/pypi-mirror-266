9cab92776d6be6736dab5e656a5aa3114386ede2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 03:03:51 +0000

    Add package PyGz1/0.3.1/PyGz1-0.3.1.tar.gz

---
+++ packages/PyGz1/PyGz1-0.3.1.tar.gz/PyGz1-0.3.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='PyGz1',
+    version='0.3.1',
+    description='A Python library for Internet interaction.',
+    author='MrGengar',
+    author_email='rydikminecraft@gmail.com',
+    packages=['pygz1'],
+    install_requires=['requests'],
+)
