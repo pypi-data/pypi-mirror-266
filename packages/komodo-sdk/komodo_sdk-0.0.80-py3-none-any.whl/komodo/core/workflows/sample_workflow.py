831bf3ab14e60a2ba04400d3f243eaab31f3b0ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 03:04:06 +0000

    Add package komodo-sdk/0.0.80/komodo_sdk-0.0.80-py3-none-any.whl

---
+++ packages/komodo-sdk/komodo_sdk-0.0.80-py3-none-any.whl/komodo/core/workflows/sample_workflow.py
@@ -0,0 +1,23 @@
+from komodo.core.agents.sample_agent import SampleAgent
+from komodo.framework.komodo_workflow import KomodoWorkflow
+from komodo.models.framework.workflow_runner import WorkflowRunner
+
+
+class SampleWorkflow(KomodoWorkflow):
+    def __init__(self, path):
+        super().__init__(shortcode="sample_workflow", name="Sample Workflow",
+                         purpose="Sample workflow to invoke sample agent.")
+
+        agent = SampleAgent(path)
+        self.add_node(agent=agent, prompt="Call the sample agent with hello.txt file and call hello_world function.")
+
+
+if __name__ == "__main__":
+    from komodo.config import PlatformConfig
+
+    path = PlatformConfig().komodo_hello_path
+    workflow = SampleWorkflow(path)
+    print(workflow.to_dict())
+    runner = WorkflowRunner(workflow)
+    response = runner.run("Run the workflow.")
+    print(response.text)
