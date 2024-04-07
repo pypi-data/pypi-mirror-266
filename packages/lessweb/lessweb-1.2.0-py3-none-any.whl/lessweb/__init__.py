4ef847a2bf7405ca374870e29ad1241f32e4d650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 15:04:01 +0000

    Add package lessweb/1.2.0/lessweb-1.2.0-py3-none-any.whl

---
+++ packages/lessweb/lessweb-1.2.0-py3-none-any.whl/lessweb/__init__.py
@@ -0,0 +1,12 @@
+from .bridge import Bridge, \
+    get_mapping, \
+    post_mapping, \
+    put_mapping, \
+    patch_mapping, \
+    delete_mapping, \
+    rest_mapping, \
+    rest_response, \
+    rest_error, \
+    autowire, \
+    service, \
+    getdoc
