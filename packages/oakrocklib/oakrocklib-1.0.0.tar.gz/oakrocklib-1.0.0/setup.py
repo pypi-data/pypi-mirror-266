95f1148a84e4d03e1dfc2a21c4f76a41e2be2247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 03:04:01 +0000

    Add package oakrocklib/1.0.0/oakrocklib-1.0.0.tar.gz

---
+++ packages/oakrocklib/oakrocklib-1.0.0.tar.gz/oakrocklib-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'oakrocklib',
+    version = '1.0.0',
+    py_modules= ['oakrocklib'],
+    author='sean massie',
+    author_email='sean.massie1@gmail.com',
+    url='http://www.headfirstlabs.com',
+    description='a simple printer of nested lists',
+)
