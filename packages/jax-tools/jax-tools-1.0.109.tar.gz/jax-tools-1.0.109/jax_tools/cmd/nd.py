d0c3d287c4e7b1dd627ec078bd32f6c947e469d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 15:03:49 +0000

    Add package jax-tools/1.0.109/jax-tools-1.0.109.tar.gz

---
+++ packages/jax-tools/jax-tools-1.0.109.tar.gz/jax-tools-1.0.109/jax_tools/cmd/nd.py
@@ -0,0 +1,19 @@
+"""
+nd command
+"""
+import sys
+from jax_tools.network_test import check_connectivity
+
+
+def main() -> None:
+    """
+    Main function
+    Returns:
+
+    """
+    if len(sys.argv) == 3:
+        target_ip = sys.argv[1]
+        target_port = int(sys.argv[2])
+        check_connectivity(target_ip, target_port, True)
+    else:
+        print("Usage: nd <TargetIP> <TargetPort>")
