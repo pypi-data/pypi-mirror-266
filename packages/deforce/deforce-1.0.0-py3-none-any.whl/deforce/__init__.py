c81db1673de04cfdce6269b24642d40f2f2a34d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 03:04:00 +0000

    Add package deforce/1.0.0/deforce-1.0.0-py3-none-any.whl

---
+++ packages/deforce/deforce-1.0.0-py3-none-any.whl/deforce/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 15:23, 10/08/2023 ----------%
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+__version__ = "1.0.0"
+
+from deforce.toolkit.preprocessor import Data, DataTransformer
+from deforce.model.gd_cfn import CfnRegressor, CfnClassifier
+from deforce.model.dfo_cfn import DfoCfnRegressor, DfoCfnClassifier
+from deforce.model.dfo_tune_cfn import DfoTuneCfn
