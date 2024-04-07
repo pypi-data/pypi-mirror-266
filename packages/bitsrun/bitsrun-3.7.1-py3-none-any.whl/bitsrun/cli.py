e087fa232a11c59bd361b26f302ef6b7139af894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 15:04:10 +0000

    Add package bitsrun/3.7.1/bitsrun-3.7.1-py3-none-any.whl

---
+++ packages/bitsrun/bitsrun-3.7.1-py3-none-any.whl/bitsrun/cli.py
@@ -0,0 +1,149 @@
+import sys
+import warnings
+from getpass import getpass
+from json import dumps
+
+import click
+from rich import print_json
+
+from bitsrun.config import get_config_paths, read_config
+from bitsrun.user import User, get_login_status
+from bitsrun.utils import print_status_table
+
+# A hacky way to specify shared options for multiple click commands:
+# https://stackoverflow.com/questions/40182157/shared-options-and-flags-between-commands
+_options = [
+    click.option('-u', '--username', help='Your username.', required=False),
+    click.option('-p', '--password', help='Your password.', required=False),
+    click.option('-v', '--verbose', is_flag=True, help='Verbosely echo API response.'),
+]
+
+# Replace the default implementation
+warnings.showwarning = (
+    lambda message, category, filename, lineno, file=None, line=None: click.echo(
+        f"{click.style('warning:', fg='yellow')} {message}", err=True
+    )
+)
+
+
+# Decorator to add options to a click command (used w/ the hack above)
+def add_options(options):
+    def _add_options(func):
+        for option in reversed(options):
+            func = option(func)
+        return func
+
+    return _add_options
+
+
+# Declaration of the main command group starts here
+@click.group()
+@click.version_option()
+def cli():
+    pass
+
+
+@cli.command()
+def config_paths():
+    """List possible paths of the configuration file."""
+    click.echo('\n'.join(map(str, get_config_paths())))
+
+
+@cli.command()
+@click.option('--json/--no-json', default=False, help='Output in JSON format.')
+def status(json: bool):
+    """Check current network login status."""
+    login_status = get_login_status()
+
+    # Output in JSON format if `--json` is passed, then exit
+    if json:
+        print(dumps(login_status))
+        return
+
+    # Output in human-readable format
+    if login_status.get('user_name'):
+        click.echo(
+            click.style('bitsrun: ', fg='green')
+            + f"{login_status['user_name']} ({login_status['online_ip']}) is online"
+        )
+        print_status_table(login_status)
+    else:
+        click.echo(
+            click.style('bitsrun: ', fg='cyan')
+            + f"{login_status['online_ip']} is offline"
+        )
+
+
+@cli.command()
+@add_options(_options)
+def login(username, password, verbose):
+    """Log into the BIT network."""
+    do_action('login', username, password, verbose)
+
+
+@cli.command()
+@add_options(_options)
+def logout(username, password, verbose):
+    """Log out of the BIT network."""
+    do_action('logout', username, password, verbose)
+
+
+def do_action(action, username, password, verbose):
+    # Support reading password from stdin when not passed via `--password`
+    if username and not password:
+        password = getpass(prompt='Please enter your password: ')
+
+    try:
+        # Try to read username and password from args provided. If none, look for config
+        # files in possible paths. If none, fail and prompt user to provide one.
+        if username and password:
+            user = User(username, password)
+        elif conf := read_config():
+            if verbose:
+                click.echo(
+                    click.style('bitsrun: ', fg='blue')
+                    + 'Reading config from '
+                    + click.style(conf[1], fg='yellow', underline=True)
+                )
+            user = User(**conf[0])
+        else:
+            ctx = click.get_current_context()
+            ctx.fail('No username or password provided')
+            sys.exit(1)
+
+        if action == 'login':
+            resp = user.login()
+            message = f"{user.username} ({resp['online_ip']}) logged in"
+        elif action == 'logout':
+            resp = user.logout()
+            message = f"{resp['online_ip']} logged out"
+        else:
+            # Should not reach here, but just in case
+            raise ValueError(f'Unknown action `{action}`')
+
+        # Output direct result of the API response if verbose
+        if verbose:
+            click.echo(f"{click.style('bitsrun:', fg='cyan')} Response from API:")
+            print_json(data=resp)
+
+        # Handle error from API response. When field `error` is not `ok`, then the
+        # login/logout action has likely failed. Hints are provided in the `error_msg`.
+        if resp['error'] != 'ok':
+            raise Exception(
+                resp['error_msg']
+                if resp['error_msg']
+                else 'Action failed, use --verbose for more info'
+            )
+
+        # Print success message
+        click.echo(f"{click.style('bitsrun:', fg='green')} {message}")
+
+    except Exception as e:
+        # Exception is caught and printed to stderr
+        click.echo(f"{click.style('error:', fg='red')} {e}", err=True)
+        # Throw with error code 1 for scripts to pick up error state
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    cli()
---
+++ packages/bitsrun/bitsrun-3.7.1-py3-none-any.whl/bitsrun/utils.py
@@ -0,0 +1,128 @@
+import math
+from base64 import b64encode
+
+from humanize import naturaldelta, naturalsize
+from rich import box
+from rich.console import Console
+from rich.table import Table
+
+from bitsrun.models import LoginStatusRespType
+
+
+def print_status_table(login_status: LoginStatusRespType) -> None:
+    """Print the login status table to the console if logged in.
+
+    You should get something like this:
+
+    ┌──────────────┬──────────────┬──────────────┬──────────────┐
+    │ Traffic Used │ Online Time  │ User Balance │ Wallet       │
+    ├──────────────┼──────────────┼──────────────┼──────────────┤
+    │ 879.3 MiB    │ 3 hours      │ 10.00        │ 0.00         │
+    └──────────────┴──────────────┴──────────────┴──────────────┘
+    """
+
+    if not login_status.get('user_name'):
+        return
+
+    table = Table(box=box.SQUARE)
+
+    table.add_column('Traffic Used', style='magenta', width=12)
+    table.add_column('Online Time', style='yellow', width=12)
+    table.add_column('User Balance', style='green', width=12)
+    table.add_column('Wallet', style='blue', width=12)
+
+    table.add_row(
+        naturalsize(login_status.get('sum_bytes', 0), binary=True),  # type: ignore
+        naturaldelta(login_status.get('sum_seconds', 0)),  # type: ignore
+        f"{login_status.get('user_balance', 0):0.2f}",
+        f"{login_status.get('wallet_balance', 0):0.2f}",
+    )
+
+    console = Console()
+    console.print(table)
+
+
+def fkbase64(raw_s: str) -> str:
+    """Encode string with a magic base64 mask"""
+    trans = str.maketrans(
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',
+        'LVoJPiCN2R8G90yg+hmFHuacZ1OWMnrsSTXkYpUq/3dlbfKwv6xztjI7DeBE45QA',
+    )
+    ret = b64encode(bytes(ord(i) & 0xFF for i in raw_s))
+    return ret.decode().translate(trans)
+
+
+def xencode(msg, key) -> str:
+    def sencode(msg, key):
+        def ordat(msg, idx):
+            if len(msg) > idx:
+                return ord(msg[idx])
+            return 0
+
+        msg_len = len(msg)
+        pwd = []
+        for i in range(0, msg_len, 4):
+            pwd.append(
+                ordat(msg, i)
+                | ordat(msg, i + 1) << 8
+                | ordat(msg, i + 2) << 16
+                | ordat(msg, i + 3) << 24
+            )
+        if key:
+            pwd.append(msg_len)
+        return pwd
+
+    def lencode(msg, key) -> str:
+        msg_len = len(msg)
+        ll = (msg_len - 1) << 2
+        if key:
+            m = msg[msg_len - 1]
+            if m < ll - 3 or m > ll:
+                return ''
+            ll = m
+        for i in range(0, msg_len):
+            msg[i] = (
+                chr(msg[i] & 0xFF)
+                + chr(msg[i] >> 8 & 0xFF)
+                + chr(msg[i] >> 16 & 0xFF)
+                + chr(msg[i] >> 24 & 0xFF)
+            )
+        if key:
+            return ''.join(msg)[0:ll]
+        return ''.join(msg)
+
+    if msg == '':
+        return ''
+    pwd = sencode(msg, True)
+    pwdk = sencode(key, False)
+    if len(pwdk) < 4:
+        pwdk = pwdk + [0] * (4 - len(pwdk))
+    n = len(pwd) - 1
+    z = pwd[n]
+    y = pwd[0]
+    c = 0x86014019 | 0x183639A0
+    m = 0
+    e = 0
+    p = 0
+    q = math.floor(6 + 52 / (n + 1))
+    d = 0
+    while q > 0:
+        d = d + c & (0x8CE0D9BF | 0x731F2640)
+        e = d >> 2 & 3
+        p = 0
+        while p < n:
+            y = pwd[p + 1]
+            m = z >> 5 ^ y << 2
+            m = m + ((y >> 3 ^ z << 4) ^ (d ^ y))
+            m = m + (pwdk[(p & 3) ^ e] ^ z)
+            pwd[p] = pwd[p] + m & (0xEFB8D130 | 0x10472ECF)
+            z = pwd[p]
+            p = p + 1
+        y = pwd[0]
+        m = z >> 5 ^ y << 2
+        m = m + ((y >> 3 ^ z << 4) ^ (d ^ y))
+        m = m + (pwdk[(p & 3) ^ e] ^ z)
+        pwd[n] = pwd[n] + m & (0xBB390742 | 0x44C6F8BD)
+        z = pwd[n]
+        q = q - 1
+    return lencode(pwd, False)
