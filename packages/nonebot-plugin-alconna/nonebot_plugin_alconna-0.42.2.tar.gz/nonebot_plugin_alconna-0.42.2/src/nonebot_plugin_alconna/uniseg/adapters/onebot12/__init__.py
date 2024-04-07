f780cc5c45b6bd229c9f5a99856cc2024e2b1086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 14:03:49 +0000

    Add package nonebot-plugin-alconna/0.42.2/nonebot_plugin_alconna-0.42.2.tar.gz

---
+++ packages/nonebot-plugin-alconna/nonebot_plugin_alconna-0.42.2.tar.gz/nonebot_plugin_alconna-0.42.2/src/nonebot_plugin_alconna/uniseg/adapters/onebot12/__init__.py
@@ -0,0 +1,17 @@
+from nonebot_plugin_alconna.uniseg.loader import BaseLoader
+from nonebot_plugin_alconna.uniseg.constraint import SupportAdapter
+
+
+class Loader(BaseLoader):
+    def get_adapter(self) -> SupportAdapter:
+        return SupportAdapter.onebot12
+
+    def get_builder(self):
+        from .builder import Onebot12MessageBuilder
+
+        return Onebot12MessageBuilder()
+
+    def get_exporter(self):
+        from .exporter import Onebot12MessageExporter
+
+        return Onebot12MessageExporter()
