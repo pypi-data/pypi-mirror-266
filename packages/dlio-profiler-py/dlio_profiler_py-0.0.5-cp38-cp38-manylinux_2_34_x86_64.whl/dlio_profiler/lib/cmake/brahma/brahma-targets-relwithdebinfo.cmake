b286660911a22a9b22a0cccb3531bf1ca8e4b5d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 04:04:44 +0000

    Add package dlio-profiler-py/0.0.5/dlio_profiler_py-0.0.5-cp38-cp38-manylinux_2_34_x86_64.whl

---
+++ packages/dlio-profiler-py/dlio_profiler_py-0.0.5-cp38-cp38-manylinux_2_34_x86_64.whl/dlio_profiler/lib/cmake/brahma/brahma-targets-relwithdebinfo.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "RelWithDebInfo".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "brahma" for configuration "RelWithDebInfo"
+set_property(TARGET brahma APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(brahma PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "/home/runner/work/dlio-profiler/dlio-profiler/build/lib.linux-x86_64-3.8/dlio_profiler/lib/libbrahma.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "libbrahma.so"
+  )
+
+list(APPEND _cmake_import_check_targets brahma )
+list(APPEND _cmake_import_check_files_for_brahma "/home/runner/work/dlio-profiler/dlio-profiler/build/lib.linux-x86_64-3.8/dlio_profiler/lib/libbrahma.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
