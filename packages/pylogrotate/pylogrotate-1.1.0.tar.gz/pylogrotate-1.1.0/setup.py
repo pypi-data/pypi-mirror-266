0b0e07ee91f186c5a6a4d7a9c3559bf439595b48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 15:04:10 +0000

    Add package pylogrotate/1.1.0/pylogrotate-1.1.0.tar.gz

---
+++ packages/pylogrotate/pylogrotate-1.1.0.tar.gz/pylogrotate-1.1.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+install_requires = ['pyyaml', 'hdfs', 'pqueue']
+
+
+setup(
+    name='pylogrotate',
+    version='1.1.0',
+    description='Logrotate in Python',
+    author='gfreezy',
+    author_email='gfreezy@gmail.com',
+    url='https://github.com/xiachufang/pylogrotate',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=install_requires,
+    extras_require=dict(test=['pytest', 'freezegun', 'zstandard']),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    entry_points={
+        'console_scripts': ['pylogrotate=pylogrotate.main:main'],
+    },
+    keywords='logrotate',
+)
