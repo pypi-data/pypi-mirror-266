f38feba44b2a9ab8e48f11c92029a3c1537d21e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 03:03:53 +0000

    Add package notion2pandas/0.9.0/notion2pandas-0.9.0-py3-none-any.whl

---
+++ packages/notion2pandas/notion2pandas-0.9.0-py3-none-any.whl/notion2pandas/__init__.py
@@ -0,0 +1,11 @@
+"""
+Package notion2pandas.
+
+Notion Client extension to import notion Database into pandas Dataframe.
+"""
+
+from .notion2pandas import Notion2PandasClient
+
+__version__ = "0.9.0"
+__author__ = 'Andrea Rosati'
+__credits__ = 'Argonne National Laboratory'
