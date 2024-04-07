76544472a0c66795255ddc07d8c335f8b5448267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 03:03:44 +0000

    Add package jaraco.context/5.2.0/jaraco.context-5.2.0.tar.gz

---
+++ packages/jaraco.context/jaraco.context-5.2.0.tar.gz/jaraco.context-5.2.0/conftest.py
@@ -0,0 +1,32 @@
+import http.server
+import io
+import functools
+import tarfile
+import threading
+
+import portend
+import pytest
+
+
+@pytest.fixture
+def tarfile_served(tmp_path_factory):
+    """
+    Start an HTTP server serving a tarfile.
+    """
+    tmp_path = tmp_path_factory.mktemp('www')
+    fn = tmp_path / 'served.tgz'
+    tf = tarfile.open(fn, mode='w:gz')
+    info = tarfile.TarInfo('served/contents.txt')
+    tf.addfile(info, io.BytesIO('hello, contents'.encode()))
+    tf.close()
+    httpd, url = start_server(tmp_path)
+    with httpd:
+        yield url + '/served.tgz'
+
+
+def start_server(path):
+    _host, port = addr = ('', portend.find_available_local_port())
+    Handler = functools.partial(http.server.SimpleHTTPRequestHandler, directory=path)
+    httpd = http.server.HTTPServer(addr, Handler)
+    threading.Thread(target=httpd.serve_forever, daemon=True).start()
+    return httpd, f'http://localhost:{port}'
