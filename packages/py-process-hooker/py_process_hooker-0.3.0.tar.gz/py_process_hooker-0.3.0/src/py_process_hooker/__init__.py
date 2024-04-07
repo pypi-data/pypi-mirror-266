7736411002528745d230c04ecc69b34d40faab33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 14:04:01 +0000

    Add package py-process-hooker/0.3.0/py_process_hooker-0.3.0.tar.gz

---
+++ packages/py-process-hooker/py_process_hooker-0.3.0.tar.gz/py_process_hooker-0.3.0/src/py_process_hooker/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+'''
+Time    : 2023-12-12
+Author  : kanadeblisst
+FileName: py_process_hooker
+github ：https://github.com/kanadeblisst00/py_hooker
+Function:  Inject python into other processes and implement 
+           Hook and active calls, supporting x86 and x64
+'''
+import platform
+from .inject_dll import inject_python_and_monitor_dir, inject_python_and_open_console
+
+if "64" in platform.architecture()[0]:
+    from .hook64 import Hook
+else:
+    from .hook32 import Hook
+
+
+__version__ = "0.3.0"
+
+
+__all__ = [
+    "inject_python_and_monitor_dir",
+    "inject_python_and_open_console",
+    "Hook"
+]
+
+
+
