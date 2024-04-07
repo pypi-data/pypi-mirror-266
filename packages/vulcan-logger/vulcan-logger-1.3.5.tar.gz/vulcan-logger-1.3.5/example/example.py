fc37b5eb6bfc12258f6cea63c9cae99cdd2055ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 04:04:41 +0000

    Add package vulcan-logger/1.3.5/vulcan-logger-1.3.5.tar.gz

---
+++ packages/vulcan-logger/vulcan-logger-1.3.5.tar.gz/vulcan-logger-1.3.5/example/example.py
@@ -0,0 +1,47 @@
+# example/example.py
+import os
+
+from vulcan_logger.logger import Logger
+from vulcan_logger.decorator import log
+
+
+os.environ["MD_LOG_LEVEL"] = "DEBUG"  # Global log level filter
+
+# Functions defined with a log decorator. Level or Condition may be
+# passed as arguments on the decorator, or omitted.
+
+
+@log
+def baseline_function(x, y):
+    """A simple function to demonstrate baseline logging."""
+    return x / y
+
+
+@log(level="WARNING")
+def level_function(x, y):
+    """A simple function to demonstrate specific level logging."""
+    return x + y
+
+
+@log(condition=True)
+def condition_function(x, y):
+    """A simple function to demonstrate condition logging."""
+    return x + y
+
+
+# Individual log statements
+logger = Logger(__name__)
+logger.critical("Test logging on the critical level")
+logger.warning("Test logging on the warning level")
+logger.info("Test logging on the info level")
+logger.debug("Test logging on the info level")
+
+# Calling functions that have @log decorator
+result = baseline_function(10, 2)
+result = level_function(x="foo", y="bar")
+result = condition_function(3, 6)
+
+try:
+    result = baseline_function(10, 0)
+except ZeroDivisionError as e:
+    logger.error(f"Caught an exception as expected: {e}")
