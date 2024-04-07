f8d1eb49a9ef405de26b8e86dceb0f2de416ef76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-07 04:04:42 +0000

    Add package dvcx/0.72.2/dvcx-0.72.2-py3-none-any.whl

---
+++ packages/dvcx/dvcx-0.72.2-py3-none-any.whl/dvcx/query/__init__.py
@@ -0,0 +1,13 @@
+from .dataset import DatasetQuery
+from .schema import C, DatasetRow, LocalFilename, Object, Stream
+from .udf import udf
+
+__all__ = [
+    "C",
+    "DatasetQuery",
+    "LocalFilename",
+    "Object",
+    "Stream",
+    "udf",
+    "DatasetRow",
+]
