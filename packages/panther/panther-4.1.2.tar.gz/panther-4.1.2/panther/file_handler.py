4c6c5d0890a209b72d7ab57d6b6ed0daa480eebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 14:04:44 +0000

    Add package panther/4.1.2/panther-4.1.2.tar.gz

---
+++ packages/panther/panther-4.1.2.tar.gz/panther-4.1.2/panther/file_handler.py
@@ -0,0 +1,31 @@
+from functools import cached_property
+
+from panther import status
+from pydantic import BaseModel, field_validator
+
+from panther.exceptions import APIError
+
+
+class File(BaseModel):
+    file_name: str
+    content_type: str
+    file: bytes
+
+    @cached_property
+    def size(self):
+        return len(self.file)
+
+    def __repr__(self) -> str:
+        return f'{self.__repr_name__()}(file_name={self.file_name}, content_type={self.content_type})'
+
+    __str__ = __repr__
+
+
+class Image(File):
+    @field_validator('content_type')
+    @classmethod
+    def validate_content_type(cls, content_type: str) -> str:
+        if not content_type.startswith('image/'):
+            msg = f"{content_type} is not a valid image 'content_type'"
+            raise APIError(detail=msg, status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+        return content_type
