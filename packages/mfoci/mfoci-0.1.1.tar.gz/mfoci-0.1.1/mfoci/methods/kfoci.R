12eb06a482168b2f66d44a5d4794aea95f1519a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 03:04:32 +0000

    Add package mfoci/0.1.1/mfoci-0.1.1.tar.gz

---
+++ packages/mfoci/mfoci-0.1.1.tar.gz/mfoci-0.1.1/mfoci/methods/kfoci.R
@@ -0,0 +1,19 @@
+library("pacman")
+p_load(KPC, proxy, kernlab)
+
+args <- commandArgs(trailingOnly = TRUE)
+path <- args[1]
+setwd(path)
+
+num_features <- NULL
+stop <- TRUE
+x <- read.csv("x.csv")
+y <- read.csv("y.csv")
+
+result <- KFOCI(y, x,  kernlab::vanilladot(), num_features = num_features, stop = stop, numCores = 1)
+selected_cols <- colnames(x)[result]
+write.csv(selected_cols, paste0(path, "selected_cols_linear.csv"), row.names = FALSE)
+
+result <- KFOCI(y, x, kernlab::rbfdot(1), num_features = num_features, stop = stop, numCores = 1)
+selected_cols <- colnames(x)[result]
+write.csv(selected_cols, paste0(path, "selected_cols_gaussian.csv"), row.names = FALSE)
