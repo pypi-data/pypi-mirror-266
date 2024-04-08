50c3d20c276aa2c67eaebdc0480edd0942ea5f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 03:04:34 +0000

    Add package astromcad/0.1.3/astromcad-0.1.3.tar.gz

---
+++ packages/astromcad/astromcad-0.1.3.tar.gz/astromcad-0.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+with open("READMEPyPI.md", "r") as fh:
+    long_description = fh.read();
+
+VERSION = '0.1.3'
+DESCRIPTION = 'Classifier-Based Anomaly Detection for Astronomical Transients'
+LONG_DESCRIPTION = long_description
+
+# Setting up
+setup(
+    name="astromcad",
+    version=VERSION,
+    author="Rithwik Gupta",
+    author_email="<rithwikca2020@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy', 'keras==2.15.0', 'tensorflow==2.15.0', 'matplotlib', 'scikit-learn'],
+    keywords=[],
+    package_data={'astromcad': ['*.keras', '*.pickle']},
+    include_package_data=True,
+    
+)
