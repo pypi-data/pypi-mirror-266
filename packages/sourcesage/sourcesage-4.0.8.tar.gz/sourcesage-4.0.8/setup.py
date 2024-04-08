ae991c7bb22f6f8ee3a05f591d83f3be8e4180ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 03:04:30 +0000

    Add package sourcesage/4.0.8/sourcesage-4.0.8.tar.gz

---
+++ packages/sourcesage/sourcesage-4.0.8.tar.gz/sourcesage-4.0.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+# READMEファイルの内容を読み込む
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='sourcesage',
+    version='4.0.8',
+    packages=find_packages(),
+    package_data={
+        'sourcesage': ['config/*', ],
+    },
+    entry_points={
+        'console_scripts': [
+            'sourcesage=sourcesage.cli:main',
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sunwood-ai-labs/SourceSage",
+    install_requires=[
+        'loguru',
+        'GitPython',
+        'requests',
+        'art',
+    ],
+)
