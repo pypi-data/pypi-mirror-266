37eeea3784cba6384a5084d60d405cbd9aafe6bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 04:03:48 +0000

    Add package dgcloud/0.1.1/dgcloud-0.1.1.tar.gz

---
+++ packages/dgcloud/dgcloud-0.1.1.tar.gz/dgcloud-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dgcloud',
+    version='0.1.1',
+    packages=find_packages(),
+    author='Dhinagaran (Dg)',
+    author_email='dhinagaran1411@gmail.com',
+    url='https://github.com/Dhinagaran-s/dgcloud',
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'pyyaml',
+        'paramiko',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dgcloud=dgcloud.cli:main',
+        ],
+    }
+)
