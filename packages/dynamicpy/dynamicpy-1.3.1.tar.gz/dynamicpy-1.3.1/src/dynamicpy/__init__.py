f6e95a7af86e93a1854f0ef810c15b5e18abe35e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 03:04:40 +0000

    Add package dynamicpy/1.3.1/dynamicpy-1.3.1.tar.gz

---
+++ packages/dynamicpy/dynamicpy-1.3.1.tar.gz/dynamicpy-1.3.1/src/dynamicpy/__init__.py
@@ -0,0 +1,52 @@
+"""
+A python module for dynamically interacting with objects to improve expandability.
+
+https://github.com/NimajnebEC/dynamicpy
+"""
+
+
+__version__ = "1.3.1"
+__author__ = "NimajnebEC <nimajnebec@users.noreply.github.com>"
+
+from dynamicpy.dependencies import DependencyLibrary
+from dynamicpy.errors import (
+    DependencyNotFoundError,
+    DuplicateDependencyError,
+    DynamicPyError,
+    InjectDependenciesError,
+    NoForeignModulesError,
+    NoParentError,
+)
+from dynamicpy.loader import DynamicLoader
+from dynamicpy.model import Model, field
+from dynamicpy.utils import (
+    ConstructorProtocol,
+    functionify,
+    get_foreign_module,
+    get_module,
+    get_module_parent,
+    get_stack_module_up,
+    is_package,
+)
+from dynamicpy.widgets import BaseWidget
+
+__all__ = (
+    "DynamicLoader",
+    "DependencyLibrary",
+    "DynamicPyError",
+    "NoForeignModulesError",
+    "NoParentError",
+    "ConstructorProtocol",
+    "functionify",
+    "get_foreign_module",
+    "get_module_parent",
+    "get_stack_module_up",
+    "get_module",
+    "is_package",
+    "DependencyNotFoundError",
+    "DuplicateDependencyError",
+    "InjectDependenciesError",
+    "BaseWidget",
+    "Model",
+    "field",
+)
