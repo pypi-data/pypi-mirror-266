e83701d83ea06bd0d1a96c2b7c0fc4c0d85fbf3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 04:03:53 +0000

    Add package honeybadger/0.19.1/honeybadger-0.19.1-py3-none-any.whl

---
+++ packages/honeybadger/honeybadger-0.19.1-py3-none-any.whl/honeybadger/fake_connection.py
@@ -0,0 +1,10 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def send_notice(config, payload):
+    notice_id = payload.get("error", {}).get("token", None)
+    logger.info('Development mode is enabled; this error will be reported if it occurs after you deploy your app.')
+    logger.debug('The config used is {} with payload {}'.format(config, payload))
+    return notice_id
