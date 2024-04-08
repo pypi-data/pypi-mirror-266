39b100eb89a75dd56215671341c7817c665df09e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 14:04:42 +0000

    Add package allanms/0.1.0/allanms-0.1.0.tar.gz

---
+++ packages/allanms/allanms-0.1.0.tar.gz/allanms-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages 
+
+# Leer el contenido del archivo README.md 
+with open("README.md", "r", encoding="utf-8") as fh: 
+    long_description = fh.read()
+
+setup(
+    name="allanms", # Nombre
+    version="0.1.0", # Versión 
+    packages=find_packages(), # Busca para el paquete creado todos los paquetes que existen 
+    install_requires=[], 
+    author="Allan Murillo",
+    description="Una biblioteca para consultar cursos de la plataforma hack4u",
+    long_description=long_description, 
+    long_description_content_type="text/markdown", # Tipo de contenido en el que se trata (En este caso sería .md de Marckdown)
+    url="https://hack4u.io",
+)
