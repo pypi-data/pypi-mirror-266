e6fb9a56e31ae04a626510b8a17e8adf657e8c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 15:06:15 +0000

    Add package lyytext/1.12/lyytext-1.12.tar.gz

---
+++ packages/lyytext/lyytext-1.12.tar.gz/lyytext-1.12/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='lyytext',
+    version='1.12',
+    author='lyy',
+    author_email='',
+    description='userfull application for  lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+
+
+    ],
+)
