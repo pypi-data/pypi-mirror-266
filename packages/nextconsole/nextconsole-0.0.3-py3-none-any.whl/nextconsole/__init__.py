dffc36887ecfb1f657272e3c8921d8597276795e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 16:04:40 +0000

    Add package nextconsole/0.0.3/nextconsole-0.0.3-py3-none-any.whl

---
+++ packages/nextconsole/nextconsole-0.0.3-py3-none-any.whl/nextconsole/__init__.py
@@ -0,0 +1,36 @@
+__version__ = __VERISON__ = "0.0.3"
+import os
+def cmd():
+    import requests
+    import json
+    from rich.console import Console
+    from rich.markdown import Markdown
+    from rich.prompt import Prompt
+    import re
+    import sys
+    if len(sys.argv) < 2:
+        print("NextConsole 未识别出需求呢!")
+        exit()
+    prompt = sys.argv[1]
+    console = Console()
+
+    def chat(c):
+        r = requests.post("http://nc.turing.fun:8228/api/nc", 
+                      json={'chat': c}).content.decode('utf8')
+        content = json.loads(r)['data']
+        return content
+
+    console.print(f"({prompt}) 正在执行...")
+
+    content = chat(prompt)
+    cmd = "\n".join(re.findall("```shell\n([\s\S]*?)```", 
+                             content))
+
+
+
+    md = Markdown(content)
+    console.print(md)
+    sure = Prompt.ask("Exec these commands?", choices=["Yes(Default)", "No", "N", "n"], default="Yes")
+    if sure == 'Yes':
+        os.system(cmd)
+
