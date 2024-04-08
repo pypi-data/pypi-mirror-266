887488de0d34aeafd6a7f9e19d4dce6e1420ea68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 03:04:30 +0000

    Add package torch-fn/1.0.0/torch_fn-1.0.0-py3-none-any.whl

---
+++ packages/torch-fn/torch_fn-1.0.0-py3-none-any.whl/torch_fn/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: "2024-04-07 21:33:32 (ywatanabe)"
+
+from ._torch_fn import torch_fn
+
+__copyright__ = "Copyright (C) 2024 Yusuke Watanabe"
+__version__ = "1.0.0"
+__license__ = "MIT"
+__author__ = "ywatanabe1989"
+__author_email__ = "ywata1989@gmail.com"
+__url__ = "https://github.com/ywatanabe1989/torch_fn"
