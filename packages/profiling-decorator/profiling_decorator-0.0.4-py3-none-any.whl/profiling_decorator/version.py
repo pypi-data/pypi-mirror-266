9d369f1ec0f9d7749282ffa882ca22685482e1f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 03:04:51 +0000

    Add package profiling-decorator/0.0.4/profiling_decorator-0.0.4-py3-none-any.whl

---
+++ packages/profiling-decorator/profiling_decorator-0.0.4-py3-none-any.whl/profiling_decorator/version.py
@@ -0,0 +1,12 @@
+"""
+profiling_decorator
+
+History:
+
+0.0.1   First release.
+0.0.2   Workflows
+0.0.3   PyPi workflow
+0.0.4   Tag after release to PyPi with workflow
+"""
+
+__version__ = "0.0.4"
