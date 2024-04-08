4b824c960099d4413943ee2fd60db66f21633c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 15:04:02 +0000

    Add package believe/1.0.14/believe-1.0.14-py3-none-any.whl

---
+++ packages/believe/believe-1.0.14-py3-none-any.whl/believe/list_matcher.py
@@ -0,0 +1,86 @@
+from typing import Any
+from typing import List
+
+from .internal import NO_CHECK
+from .internal import BelieveBase
+from .internal import validate
+
+
+class OneOf(BelieveBase):
+    def __init__(self, *candidates: Any) -> None:
+        super().__init__(*candidates)
+        self.__candidates = candidates
+
+    def validate(self, rhs: Any, e_path: str = "") -> None:
+        for i in self.__candidates:
+            if i == rhs:
+                return
+        self.raise_validate_error(rhs, e_path=e_path, e_msg="invalid_argument")
+
+
+class AnyOrder(BelieveBase):
+    def __init__(self, list_obj: List) -> None:
+        assert isinstance(list_obj, list)
+
+        super().__init__(list_obj)
+        self.__list_obj = list_obj
+
+    def validate(self, rhs: List[Any], e_path: str = "") -> None:
+        if not isinstance(rhs, list):
+            self.raise_validate_error(rhs, e_path=e_path)
+        if len(rhs) != len(self.__list_obj):
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="different_length")
+        list_obj = self.__list_obj[:]
+        for idx, i in enumerate(rhs):
+            if not i in list_obj:
+                self.raise_validate_error(
+                    rhs, e_path=e_path, e_msg=f"item_not_found_at_index: {idx}"
+                )
+            list_obj.remove(i)
+
+
+class ListOf(BelieveBase):
+    def __init__(
+        self,
+        one_item: Any,
+        n_item: int = NO_CHECK,
+        min_item: int = NO_CHECK,
+        max_item: int = NO_CHECK,
+    ) -> None:
+        assert n_item == NO_CHECK or isinstance(n_item, int)
+        assert min_item == NO_CHECK or isinstance(min_item, int)
+        assert max_item == NO_CHECK or isinstance(max_item, int)
+
+        super().__init__(one_item, n_item=n_item, min_item=min_item, max_item=max_item)
+
+        self.__one_item = one_item
+        self.__n_item = n_item
+        self.__min_item = min_item
+        self.__max_item = max_item
+
+    def validate(self, rhs: List, e_path: str = "") -> None:
+        if not isinstance(rhs, list):
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="not_list")
+        if self.__n_item != NO_CHECK:
+            if not len(rhs) == self.__n_item:
+                self.raise_validate_error(
+                    rhs,
+                    e_path=e_path,
+                    e_msg=f"mismatch_item_count: {len(rhs)} != {self.__n_item}",
+                )
+        if self.__min_item != NO_CHECK:
+            if len(rhs) < self.__min_item:
+                self.raise_validate_error(
+                    rhs,
+                    e_path=e_path,
+                    e_msg=f"too_few_items: {len(rhs)} < {self.__min_item}",
+                )
+        if self.__max_item != NO_CHECK:
+            if len(rhs) > self.__max_item:
+                self.raise_validate_error(
+                    rhs,
+                    e_path=e_path,
+                    e_msg=f"too_many_items: {len(rhs)} > {self.__max_item}",
+                )
+        for idx, val in enumerate(rhs):
+            validate(self.__one_item, val, "%s.%s" % (e_path, str(idx)))
---
+++ packages/believe/believe-1.0.14-py3-none-any.whl/believe/str_matcher.py
@@ -0,0 +1,147 @@
+import json
+import uuid
+from typing import Dict
+from typing import List
+from typing import Union
+from urllib.parse import urlparse
+
+from .internal import NO_CHECK
+from .internal import BelieveBase
+
+
+class AnyStr(BelieveBase):
+    def __init__(self, min_len: int = NO_CHECK, max_len: int = NO_CHECK) -> None:
+        assert min_len == NO_CHECK or isinstance(min_len, int)
+        assert max_len == NO_CHECK or isinstance(max_len, int)
+
+        super().__init__(min_len=min_len, max_len=max_len)
+        self.__min_len = min_len
+        self.__max_len = max_len
+
+    def validate(self, rhs: str, e_path: str = "") -> None:
+        if not isinstance(rhs, str):
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="not_string")
+        if self.__min_len != NO_CHECK and len(rhs) < self.__min_len:
+            self.raise_validate_error(
+                rhs,
+                e_path=e_path,
+                e_msg=f"string_too_short: {len(rhs)} < {self.__min_len}",
+            )
+        if self.__max_len != NO_CHECK and len(rhs) > self.__max_len:
+            self.raise_validate_error(
+                rhs,
+                e_path=e_path,
+                e_msg=f"string_too_long: {len(rhs)} > {self.__max_len}",
+            )
+
+
+class AnyIntStr(BelieveBase):
+    def validate(self, rhs: str, e_path: str = "") -> None:
+        if not isinstance(rhs, str):
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="not_string")
+
+        try:
+            int(rhs)
+        except ValueError:
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="not_int_string")
+
+
+class AnyUUID(BelieveBase):
+    def validate(self, rhs: str, e_path: str = "") -> None:
+        if not isinstance(rhs, str) or len(rhs) != 36:
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="invalid_uuid")
+        try:
+            uuid.UUID(rhs)
+        except (TypeError, ValueError):
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="invalid_uuid")
+
+
+class AnyIPV4(BelieveBase):
+    def validate(self, rhs: str, e_path: str = "") -> None:
+        if not isinstance(rhs, str):
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="invalid_ipv4")
+        tokens = rhs.split(".")
+        if len(tokens) != 4:
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="invalid_ipv4")
+        for token in tokens:
+            try:
+                token = int(token)
+            except ValueError:
+                self.raise_validate_error(rhs, e_path=e_path, e_msg="invalid_ipv4")
+            if not 0 <= token < 256:
+                self.raise_validate_error(rhs, e_path=e_path, e_msg="invalid_ipv4")
+
+
+class AnySHA1(BelieveBase):
+    def validate(self, rhs: str, e_path: str = "") -> None:
+        if not isinstance(rhs, str):
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="invalid_sha1")
+        if len(rhs) != 40:
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="invalid_sha1")
+        try:
+            int(rhs, 16)
+        except ValueError:
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="invalid_sha1")
+
+
+class AnyJsonStr(BelieveBase):
+    def __init__(self, json_obj: Union[Dict, List]) -> None:
+        assert isinstance(json_obj, (dict, list))
+        super().__init__(json_obj)
+        self.__json_obj = json_obj
+
+    def validate(self, rhs: str, e_path: str = "") -> None:
+        if not isinstance(rhs, str):
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="not_string")
+        try:
+            obj = json.loads(rhs)
+        except:
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="not_json_string")
+        if obj != self.__json_obj:
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="mismatch_json_string")
+
+
+class AnyUrl(BelieveBase):
+    def __init__(self, url: str) -> None:
+        super().__init__(url)
+        self.__parsed_url = urlparse(url)
+
+    def _normalize_query(self, query: str) -> str:
+        return "&".join(sorted(query.split("&")))
+
+    def _default_port(self, scheme: str) -> int:
+        if scheme == "http":
+            return 80
+        elif scheme == "https":
+            return 443
+
+    def validate(self, rhs: str, e_path: str = "") -> None:
+        if not isinstance(rhs, str):
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="not_string")
+
+        o = urlparse(rhs)
+
+        if self.__parsed_url.scheme != o.scheme:
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="mismatch_scheme")
+
+        default_port = self._default_port(self.__parsed_url.scheme)
+
+        if self.__parsed_url.username != o.username:
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="mismatch_username")
+
+        if self.__parsed_url.password != o.password:
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="mismatch_password")
+
+        if self.__parsed_url.hostname != o.hostname:
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="mismatch_hostname")
+
+        if (self.__parsed_url.port or default_port) != (o.port or default_port):
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="mismatch_port")
+
+        if self.__parsed_url.path != o.path:
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="mismatch_path")
+
+        if self._normalize_query(self.__parsed_url.query) != self._normalize_query(
+            o.query
+        ):
+            self.raise_validate_error(rhs, e_path=e_path, e_msg="mismatch_query")
