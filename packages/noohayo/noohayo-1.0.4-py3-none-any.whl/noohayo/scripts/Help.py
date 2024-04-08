af0c252ff6d6430c92547156a751acb3144a165a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 15:05:24 +0000

    Add package noohayo/1.0.4/noohayo-1.0.4-py3-none-any.whl

---
+++ packages/noohayo/noohayo-1.0.4-py3-none-any.whl/noohayo/scripts/Help.py
@@ -0,0 +1,21 @@
+import argparse
+
+def Help(parser,des) :
+    logo = """                                           
+ _____ _____ _____ _____ _____ __ __ _____ 
+|   | |     |     |  |  |  _  |  |  |     |
+| | | |  |  |  |  |     |     |_   _|  |  |
+|_|___|_____|_____|__|__|__|__| |_| |_____|
+                                           """
+    print(logo)
+    print(f"\n Usage : \033[38;2;70;167;226mnoohayo\033[0m [\033[38;2;232;157;30moption\033[0m]")
+    print(f"\n {des}")
+    print(f"\n \033[38;2;232;157;30mOptions\033[0m :\n")
+    for action in parser :
+        name = action.dest
+        met = (f" {action.metavar}" if action.metavar is not None else "")
+        print(f"\t -{name[0]}{met}, --{name}{met} \t\t {action.help}")
+    print("\nGithub : https://github.com/BreakRyo/noohayo")
+    print()
+    
+
