af017dc66f669754bcabd8697a426abdae220a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 15:06:18 +0000

    Add package just_design_phase/0.0.16/just_design_phase-0.0.16-py3-none-any.whl

---
+++ packages/just_design_phase/just_design_phase-0.0.16-py3-none-any.whl/just_design_phase/__init__.py
@@ -0,0 +1,10 @@
+from just_design_phase.order_placement import (
+    cancel_order,
+    exit_orders, get_average_price_and_status,
+    get_holdings, get_order_history, get_positions, get_sl_order,
+    get_status, place_slm_order, get_fno_instrument_token, get_fno_price,
+    get_stock_instrument_token, get_stock_price,  sell_intraday_limit_order,
+    buy_intraday_limit_order, buy_intraday_normal_order,
+    modify_order_price, modify_order_quantity, modify_order_to_buy,
+    modify_order_to_sell, sell_intraday_normal_order
+)
