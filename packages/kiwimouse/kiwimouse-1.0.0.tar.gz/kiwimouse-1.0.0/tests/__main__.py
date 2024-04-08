28d43171836afe342548ff3aa7928bb46f3defaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 03:05:02 +0000

    Add package kiwimouse/1.0.0/kiwimouse-1.0.0.tar.gz

---
+++ packages/kiwimouse/kiwimouse-1.0.0.tar.gz/kiwimouse-1.0.0/tests/__main__.py
@@ -0,0 +1,26 @@
+import kiwimouse
+import time
+
+def square():
+    """
+    move mouse in square pattern
+    """
+    kiwimouse.move(0, 100)
+    time.sleep(1)
+    kiwimouse.move(-200, 0)
+    time.sleep(1)
+    kiwimouse.move(0, -100)
+    time.sleep(1)
+    kiwimouse.move(200, 0)
+
+def click_left():
+    """
+    click left mouse button
+    """
+    kiwimouse.click()
+
+
+if __name__ == '__main__':
+    square()
+    time.sleep(1)
+    click_left()
