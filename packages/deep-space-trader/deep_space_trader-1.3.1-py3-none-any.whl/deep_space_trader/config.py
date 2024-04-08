ed1566b8936620c203915453e0a4b9eae316f2f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 15:05:20 +0000

    Add package deep-space-trader/1.3.1/deep_space_trader-1.3.1-py3-none-any.whl

---
+++ packages/deep-space-trader/deep_space_trader-1.3.1-py3-none-any.whl/deep_space_trader/config.py
@@ -0,0 +1,84 @@
+import os
+import json
+
+from deep_space_trader.utils import errorDialog, scores_encode, scores_decode
+from deep_space_trader.constants import MAX_HIGH_SCORES
+
+
+FILENAME = os.path.join(os.path.expanduser('~'), '.deep_space_trader_config.json')
+
+
+SCORES_KEY = 'highscores'
+SHOWINTRO_KEY = 'show_intro'
+
+config = {
+    SHOWINTRO_KEY: True,
+    SCORES_KEY: []
+}
+
+
+def _malformed_config():
+    errorDialog(None, "Error", message="Malformed config file: %s" % FILENAME)
+
+
+def config_load():
+    if not os.path.isfile(FILENAME):
+        return
+
+    try:
+        with open(FILENAME, 'r') as fh:
+            loaded = json.load(fh)
+    except:
+        _malformed_config()
+        return
+
+    try:
+        config[SHOWINTRO_KEY] = loaded[SHOWINTRO_KEY]
+
+        if loaded[SCORES_KEY] == '':
+            config[SCORES_KEY] = []
+        else:
+            data = scores_decode(loaded[SCORES_KEY]).decode('utf-8')
+            config[SCORES_KEY] = json.loads(data)
+
+            # Handle case where config contains more than the max. high scores,
+            # since the limit on number of stored high scores was only added in version 1.3.0
+            config[SCORES_KEY] = config[SCORES_KEY][:MAX_HIGH_SCORES]
+    except:
+        _malformed_config()
+        return
+
+def config_store():
+    cfg = {}
+
+    if config[SCORES_KEY]:
+        string = json.dumps(config[SCORES_KEY])
+        encoded = scores_encode(bytes(string, encoding='utf8')).decode('utf-8')
+    else:
+        encoded = ''
+
+    cfg[SCORES_KEY] = encoded
+    cfg[SHOWINTRO_KEY] = config[SHOWINTRO_KEY]
+
+    try:
+        with open(FILENAME, 'w') as fh:
+            json.dump(cfg, fh)
+    except:
+        errorDialog(None, "Error", message="Unable to write file %s" % FILENAME)
+
+def set_show_intro(value):
+    config[SHOWINTRO_KEY] = value
+
+def get_show_intro():
+    return config[SHOWINTRO_KEY]
+
+def add_highscore(name, score):
+    config[SCORES_KEY].append([name, score])
+    config[SCORES_KEY].sort(key=lambda x: x[1])
+    config[SCORES_KEY].reverse()
+
+    if (len(config[SCORES_KEY]) > MAX_HIGH_SCORES):
+        del config[SCORES_KEY][-1]
+
+def get_highscores():
+    return config[SCORES_KEY]
