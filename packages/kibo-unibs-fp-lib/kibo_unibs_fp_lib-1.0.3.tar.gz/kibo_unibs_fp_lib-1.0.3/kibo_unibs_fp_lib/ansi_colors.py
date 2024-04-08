187737586dd92e83592beca48842e96c373d86ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 04:04:11 +0000

    Add package kibo-unibs-fp-lib/1.0.3/kibo_unibs_fp_lib-1.0.3.tar.gz

---
+++ packages/kibo-unibs-fp-lib/kibo_unibs_fp_lib-1.0.3.tar.gz/kibo_unibs_fp_lib-1.0.3/kibo_unibs_fp_lib/ansi_colors.py
@@ -0,0 +1,22 @@
+"""Used for accessing a list of colo customization for writing in the terminal"""
+
+ansi_colors = {
+    "reset": "\033[0m",
+    "black": "\033[30m",
+    "red": "\033[31m",
+    "green": "\033[32m",
+    "yellow": "\033[33m",
+    "blue": "\033[34m",
+    "purple": "\033[35m",
+    "cyan": "\033[36m",
+    "white": "\033[37m",
+    "black_background": "\033[40m",
+    "red_background": "\033[41m",
+    "green_background": "\033[42m",
+    "yellow_background": "\033[43m",
+    "blue_background": "\033[44m",
+    "purple_background": "\033[45m",
+    "cyan_background": "\033[46m",
+    "white_background": "\033[47m",
+    "clear": "\033[H\033[2J",
+}
