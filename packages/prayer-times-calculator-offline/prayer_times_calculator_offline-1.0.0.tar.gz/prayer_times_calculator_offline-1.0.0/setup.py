6c37e11b9cdbe374627b4ff7320e3a3f4e0df0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 04:05:17 +0000

    Add package prayer-times-calculator-offline/1.0.0/prayer_times_calculator_offline-1.0.0.tar.gz

---
+++ packages/prayer-times-calculator-offline/prayer_times_calculator_offline-1.0.0.tar.gz/prayer_times_calculator_offline-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='prayer_times_calculator_offline',
+      version='1.0.0',
+      description='Prayer Times Calculator - Offline',
+      long_description_content_type='text/markdown',
+      url='https://github.com/cfair/prayer-times-calculator-offline',
+      author='cpfair, Umair Chagani',
+      author_email='cpf@cpfx.ca',
+      license='MIT',
+      packages=['prayer_times_calculator_offline'],
+      zip_safe=False)
