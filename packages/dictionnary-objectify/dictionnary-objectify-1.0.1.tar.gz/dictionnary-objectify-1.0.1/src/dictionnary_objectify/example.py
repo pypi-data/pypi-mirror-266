2f003de386eb6da4b1f5c4510972c912fa987275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 16:04:33 +0000

    Add package dictionnary-objectify/1.0.1/dictionnary-objectify-1.0.1.tar.gz

---
+++ packages/dictionnary-objectify/dictionnary-objectify-1.0.1.tar.gz/dictionnary-objectify-1.0.1/src/dictionnary_objectify/example.py
@@ -0,0 +1,16 @@
+from __init__ import to_object
+
+dictionnary = {
+    "some_key": "some_value",
+    "some_other_key": {
+        "some_sub_key": "some_sub_value"
+    }
+}
+
+object = dictionnary.to_object()
+
+print(object.some_key)
+# output : "some_value"
+
+print(object.some_other_key.some_sub_key)
+# output : "some_sub_value"
