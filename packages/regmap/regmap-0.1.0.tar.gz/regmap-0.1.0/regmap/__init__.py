81713c0775a9359f89118a3a13e59e8adcee7629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 03:04:39 +0000

    Add package regmap/0.1.0/regmap-0.1.0.tar.gz

---
+++ packages/regmap/regmap-0.1.0.tar.gz/regmap-0.1.0/regmap/__init__.py
@@ -0,0 +1,10 @@
+from .bitfield import BitField
+from .interface import Interface
+from .register import Mode, Register
+
+__all__ = [
+    "BitField",
+    "Interface",
+    "Mode",
+    "Register",
+]
