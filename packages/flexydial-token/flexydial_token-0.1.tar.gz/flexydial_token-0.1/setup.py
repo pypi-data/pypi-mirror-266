53255ecd9e050b3a3ae807dcc25822c350060912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 15:05:23 +0000

    Add package flexydial-token/0.1/flexydial_token-0.1.tar.gz

---
+++ packages/flexydial-token/flexydial_token-0.1.tar.gz/flexydial_token-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flexydial_token',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['requests'],  # Add any dependencies your package needs
+    author='Saikrishna Macha',
+    author_email='saikrishna.macha@flexydial.in',
+    description='Package for the Token',
+    url='https://github.com/saikrishna7660/flexy_token',
+    license='MIT',
+)
