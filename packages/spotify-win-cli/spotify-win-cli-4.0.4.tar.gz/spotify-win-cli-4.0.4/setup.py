36726c41c0a4d1ff06e5b5df8a7a43687132ad05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 04:04:11 +0000

    Add package spotify-win-cli/4.0.4/spotify-win-cli-4.0.4.tar.gz

---
+++ packages/spotify-win-cli/spotify-win-cli-4.0.4.tar.gz/spotify-win-cli-4.0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+from os import path
+
+setup(
+    name="spotify-win-cli",
+    long_description="This is a detailed description of my package.",
+    long_description_content_type='text/plain'
+)
