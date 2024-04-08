053dfeb123f097152176cc0ba86f8381d0e11740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 14:04:26 +0000

    Add package ts-soup/0.0.10/ts-soup-0.0.10.tar.gz

---
+++ packages/ts-soup/ts-soup-0.0.10.tar.gz/ts-soup-0.0.10/ts_soup/__init__.py
@@ -0,0 +1,13 @@
+from ts_soup.app import run_sync
+from ts_soup.common import db_operator
+from ts_soup.workers import Source,RawSqlSource,MultiSource,TargetTable
+
+
+__all__ = [
+    'run_sync',
+    'db_operator',
+    'TargetTable',
+    'Source',
+    'RawSqlSource',
+    'MultiSource',
+]
