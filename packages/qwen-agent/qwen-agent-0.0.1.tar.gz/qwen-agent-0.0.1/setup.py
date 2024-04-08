03ecfed578250dc78cb149ddcd540a28d3588f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 03:04:51 +0000

    Add package qwen-agent/0.0.1/qwen-agent-0.0.1.tar.gz

---
+++ packages/qwen-agent/qwen-agent-0.0.1.tar.gz/qwen-agent-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+
+def read_requirements():
+    with open('requirements.txt') as req:
+        content = req.read()
+        requirements = content.split('\n')
+    return requirements
+
+
+setup(
+    name='qwen-agent',
+    version='0.0.1',
+    packages=find_packages(
+        exclude=['examples', 'examples.*', 'qwen_server', 'qwen_server.*']),
+    package_data={
+        'qwen_agent':
+        ['utils/qwen.tiktoken', 'tools/resource/*.ttf', 'tools/resource/*.py'],
+    },
+    install_requires=read_requirements(),
+    url='https://github.com/QwenLM/Qwen-Agent')
