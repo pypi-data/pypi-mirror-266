5dab5b882343b61b0334713c9037f39fefb3ff3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 03:04:54 +0000

    Add package profiling-decorator/0.0.6/profiling_decorator-0.0.6.tar.gz

---
+++ packages/profiling-decorator/profiling_decorator-0.0.6.tar.gz/profiling_decorator-0.0.6/src/profiling_decorator/version.py
@@ -0,0 +1,14 @@
+"""
+profiling_decorator
+
+History:
+
+0.0.1   First release.
+0.0.2   Workflows
+0.0.3   PyPi workflow
+0.0.4   Tag after release to PyPi with workflow
+0.0.5   Release to PyPi on tag pushes
+0.0.6   Update ReadME
+"""
+
+__version__ = "0.0.6"
