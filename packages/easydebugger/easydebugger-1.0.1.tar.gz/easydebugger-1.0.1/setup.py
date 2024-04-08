488937c710f601c6433257ebbe8f370c86d5c52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 04:05:15 +0000

    Add package easydebugger/1.0.1/easydebugger-1.0.1.tar.gz

---
+++ packages/easydebugger/easydebugger-1.0.1.tar.gz/easydebugger-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.1'
+DESCRIPTION = 'A clean Python debugging tool'
+LONG_DESCRIPTION = 'A clean Python debugging tool. Documentation at https://github.com/Blob2763/easydebugger'
+
+# Setting up
+setup(
+    name="easydebugger",
+    version=VERSION,
+    author="blob2763",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(include=["easydebugger"]),
+    install_requires=[],
+    keywords=['python', 'debugger']
+)
