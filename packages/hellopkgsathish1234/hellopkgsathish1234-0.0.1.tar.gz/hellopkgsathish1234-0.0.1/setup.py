75fffc6ad455fdb955647f93fec0ed188a1996f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 04:05:23 +0000

    Add package hellopkgsathish1234/0.0.1/hellopkgsathish1234-0.0.1.tar.gz

---
+++ packages/hellopkgsathish1234/hellopkgsathish1234-0.0.1.tar.gz/hellopkgsathish1234-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'this is a simple hello world pkg'
+# Setting up
+setup(
+    name="hellopkgsathish1234",
+    version=VERSION,
+    author="Sathish Kumar",
+    author_email="sathishmahi456@gmail.com",
+    packages=find_packages(),
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
