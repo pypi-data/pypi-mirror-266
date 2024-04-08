aea3d1f6cb8029855b8acca8b1ac8d7a1b6e41b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 14:04:18 +0000

    Add package waylake-utils/0.1.10/waylake_utils-0.1.10.tar.gz

---
+++ packages/waylake-utils/waylake_utils-0.1.10.tar.gz/waylake_utils-0.1.10/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='waylake_utils',
+    version='0.1.10',
+    packages=find_packages(),
+    description='Custom logging utility',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='waylake',
+    author_email='plmokn7034soo@icloud.com',
+)
