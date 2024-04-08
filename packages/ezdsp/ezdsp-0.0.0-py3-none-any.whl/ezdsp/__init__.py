d0e634fa9b55cae4390a290395e8dc6793f3b0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 14:04:31 +0000

    Add package ezdsp/0.0.0/ezdsp-0.0.0-py3-none-any.whl

---
+++ packages/ezdsp/ezdsp-0.0.0-py3-none-any.whl/ezdsp/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: "2024-04-08 12:56:47 (ywatanabe)"
+
+from ._ezdsp import hello
+
+__copyright__ = "Copyright (C) 2024 Yusuke Watanabe"
+__version__ = "0.0.0"
+__license__ = "MIT"
+__author__ = "ywatanabe1989"
+__author_email__ = "ywata1989@gmail.com"
+__url__ = "https://github.com/ywatanabe1989/ezdsp"
