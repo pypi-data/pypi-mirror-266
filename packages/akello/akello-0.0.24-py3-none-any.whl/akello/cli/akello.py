7977359d3771d84b6cf127a7fdc297044627656f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 04:03:48 +0000

    Add package akello/0.0.24/akello-0.0.24-py3-none-any.whl

---
+++ packages/akello/akello-0.0.24-py3-none-any.whl/akello/cli/akello.py
@@ -0,0 +1,23 @@
+import click
+from akello.cli.commands.build import build
+from akello.cli.commands.setup import setup
+from akello.cli.commands.start import start, mock_server
+from akello.cli.commands.analytics import analytics
+from akello.cli.commands.prompt import prompt
+
+@click.group()
+def local_commands():
+    pass
+
+local_commands.add_command(build)
+local_commands.add_command(setup)
+local_commands.add_command(mock_server)
+local_commands.add_command(start)
+local_commands.add_command(analytics)
+local_commands.add_command(prompt)
+
+def cli():
+    local_commands()
+
+if __name__ == '__main__':
+    local_commands()
