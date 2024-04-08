4701a02f0962d5cc57af0cf712cff59d9a87e648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 14:04:49 +0000

    Add package pyvncs/0.0.1/pyvncs-0.0.1.tar.gz

---
+++ packages/pyvncs/pyvncs-0.0.1.tar.gz/pyvncs-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import os
+
+HERE = os.path.dirname(__file__)
+VERSION_FILE = os.path.join(HERE, "VERSION.txt")
+
+setup(
+    name="pyvncs",
+    version_config={
+        "count_commits_from_version_file": True,
+        "template": "{tag}",
+        "dev_template": "{tag}.dev.{ccount}",
+        "dirty_template": "{tag}.dev.{ccount}",
+        "version_file": VERSION_FILE,
+    },
+    setup_requires=["setuptools-git-versioning"],
+    description="A 3rd-party package of pyvncs since the author has not published it to pypi",
+    long_description="A 3rd-party package of pyvncs since the author has not published it to pypi",
+    url="http://github.com/darkpixel/pyvncs",
+    author="Matias Fernandez",
+    author_email="matias.fernandez@gmail.com",
+    packages=["pyvncs"],
+    zip_safe=False,
+)
