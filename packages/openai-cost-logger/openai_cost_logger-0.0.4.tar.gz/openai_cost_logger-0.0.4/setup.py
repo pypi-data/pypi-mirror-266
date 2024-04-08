37d68762862c7df8862dad5405345fd6341dc506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 16:04:34 +0000

    Add package openai-cost-logger/0.0.4/openai_cost_logger-0.0.4.tar.gz

---
+++ packages/openai-cost-logger/openai_cost_logger-0.0.4.tar.gz/openai_cost_logger-0.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='openai_cost_logger',
+    version='0.0.4',
+    description='OpenAI Cost Logger',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author='Lorenzo Drudi | Mikolaj Boronski | Ivan Zakazov',
+    author_email='lorenzodrudi11@gmail.com',
+    license='MIT',
+    packages=find_packages(include=['openai_cost_logger', 'openai_cost_logger.*']),
+    requires=['openai', 'pandas', 'matplotlib'],
+    install_requires=['openai', 'pandas', 'matplotlib'],
+)
