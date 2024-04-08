b992a55847ed6ca6a3d121c1ab1dc5c68873b2a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 04:04:36 +0000

    Add package jupyter-repl-msgbridge/0.2.8/jupyter_repl_msgbridge-0.2.8.tar.gz

---
+++ packages/jupyter-repl-msgbridge/jupyter_repl_msgbridge-0.2.8.tar.gz/jupyter_repl_msgbridge-0.2.8/setup.py
@@ -0,0 +1,8 @@
+__import__("setuptools").setup(
+    name="jupyter-repl-msgbridge",
+    version='0.2.5',
+    description="Plugins for handling messages between iframe and hosting website",
+    url="https://github.com/ZDL-LLC/jupyter-repl-msgbridge",
+    author="ZDL, LLC",
+    keywords="jupyter-repl-msgbridge, jupyterlite",
+)
