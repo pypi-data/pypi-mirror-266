99d587460fd407be83346fbfbb8849f2d88f26f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 15:06:25 +0000

    Add package prodpadlm-client/0.1.0/prodpadlm_client-0.1.0.tar.gz

---
+++ packages/prodpadlm-client/prodpadlm_client-0.1.0.tar.gz/prodpadlm_client-0.1.0/setup.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+from setuptools import setup, find_packages
+
+ROOT_DIR = os.path.dirname(__file__)
+
+def read_readme() -> str:
+    """Read the README file."""
+    return (Path(__file__).parent / "README.md").read_text(encoding="UTF-8")
+
+requirements = [
+    "pydantic-settings",
+    "langchain", 
+]
+
+setup(
+    name='prodpadlm_client',
+    version='0.1.0',
+    description='Production LaunchPad for Language Models [Client] - ⚡ Ship Open Source LLMs to production faster and efficiently ⚡',
+    author='Ayo Kehinde Samuel',
+    packages=find_packages(),
+    long_description=read_readme(),
+    long_description_content_type="text/markdown",
+    license="Apache 2.0",
+    license_files="LICENSE.txt",
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires=">=3.9.0",
+    
+)
