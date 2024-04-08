f032ff556f313bc2e4281d29d5b0320650244f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 15:06:24 +0000

    Add package ign8/4.8.19/ign8-4.8.19-py2.py3-none-any.whl

---
+++ packages/ign8/ign8-4.8.19-py2.py3-none-any.whl/ign8/main.py
@@ -0,0 +1,32 @@
+from .common import prettylog
+import os
+import subprocess
+import sys
+
+def getenv(key, default):
+    if key in os.environ:
+        return os.environ[key]
+    else:
+        return default
+
+def main():
+    prettylog("INFO", )
+    return True
+
+def in_venv():
+    return sys.prefix != sys.base_prefix
+
+def init_service():
+    prettylog("INFO", "Init service")
+
+def start_service():
+    prettylog("INFO", "start service")
+
+def start_service():
+    prettylog("INFO", "stop service")
+
+def serve():
+    while True:
+        prettylog("info" , "Start loop")
+        
+    return True
