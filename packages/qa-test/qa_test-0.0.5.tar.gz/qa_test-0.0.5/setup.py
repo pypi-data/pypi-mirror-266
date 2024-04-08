57d59b46ccd4c7b258fe00ade88eb1e1ebee0f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 03:04:58 +0000

    Add package qa-test/0.0.5/qa_test-0.0.5.tar.gz

---
+++ packages/qa-test/qa_test-0.0.5.tar.gz/qa_test-0.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+
+def read_requirements():
+    with open('requirements.txt') as req:
+        content = req.read()
+        requirements = content.split('\n')
+    return requirements
+
+
+setup(
+    name='qa_test',
+    version='0.0.5',
+    packages=find_packages(exclude=['examples', 'examples.*', 'qwen_server', 'qwen_server.*']),
+    package_data={
+        'qwen_agent': ['utils/qwen.tiktoken', 'tools/resource/*.ttf', 'tools/resource/*.py'],
+    },
+    install_requires=read_requirements(),
+    url='https://github.com/QwenLM/Qwen-Agent'
+)
