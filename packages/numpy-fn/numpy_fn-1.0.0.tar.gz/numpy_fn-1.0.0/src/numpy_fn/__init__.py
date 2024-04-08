5e164b2e3f742a4a632bbd16c4e356f8646a5542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 03:04:36 +0000

    Add package numpy-fn/1.0.0/numpy_fn-1.0.0.tar.gz

---
+++ packages/numpy-fn/numpy_fn-1.0.0.tar.gz/numpy_fn-1.0.0/src/numpy_fn/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: "2024-04-07 21:39:59 (ywatanabe)"
+
+from ._numpy_fn import numpy_fn
+
+__copyright__ = "Copyright (C) 2024 Yusuke Watanabe"
+__version__ = "1.0.0"
+__license__ = "MIT"
+__author__ = "ywatanabe1989"
+__author_email__ = "ywata1989@gmail.com"
+__url__ = "https://github.com/ywatanabe1989/numpy_fn"
