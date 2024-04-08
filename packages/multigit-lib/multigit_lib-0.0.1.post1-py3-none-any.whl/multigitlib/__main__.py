8f696d13fe164f16972e3e593550669c11ba8960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 03:04:42 +0000

    Add package multigit-lib/0.0.1.post1/multigit_lib-0.0.1.post1-py3-none-any.whl

---
+++ packages/multigit-lib/multigit_lib-0.0.1.post1-py3-none-any.whl/multigitlib/__main__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# See https://packaging.python.org/en/latest/specifications/version-specifiers
+__version__ = '0.0.1-1'
+
+# Import stuff
+import sys
+
+def main():
+	print('RUNNING MULTIGIT LIB')
+
+
+if __name__ == '__main__':
+	sys.exit(
+		main()
+	)
