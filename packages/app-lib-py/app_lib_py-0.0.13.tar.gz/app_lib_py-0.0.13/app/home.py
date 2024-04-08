e68495984285c58421572032453b4cdbb03f64fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 04:03:46 +0000

    Add package app-lib-py/0.0.13/app_lib_py-0.0.13.tar.gz

---
+++ packages/app-lib-py/app_lib_py-0.0.13.tar.gz/app_lib_py-0.0.13/app/home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="app-lib-py",
+    page_icon="app/static/a_l_p.png",
+    layout="wide",
+)
+
+st.header("APP-LIB-PY")
+st.write(
+    "**A template library to demonstrate good practice for a scientific python library.**"
+)
