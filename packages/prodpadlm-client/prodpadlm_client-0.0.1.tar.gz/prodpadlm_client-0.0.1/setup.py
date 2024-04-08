7ddb56ce8449c4dfc042a48bb02e0d449617cf64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 14:04:17 +0000

    Add package prodpadlm-client/0.0.1/prodpadlm_client-0.0.1.tar.gz

---
+++ packages/prodpadlm-client/prodpadlm_client-0.0.1.tar.gz/prodpadlm_client-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='prodpadlm_client',
+    version='0.0.1',
+    description='Production Pad for Language Models Client - ⚡ Ship Open Source LLMs to production faster and efficiently ⚡',
+    author='Ayo Kehinde Samuel',
+    packages=find_packages(),
+)
