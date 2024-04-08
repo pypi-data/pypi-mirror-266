18ae627118aa3387a2ead3c625aeb539b1ee4e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 04:04:43 +0000

    Add package slinn/2.2.4/slinn-2.2.4.tar.gz

---
+++ packages/slinn/slinn-2.2.4.tar.gz/slinn-2.2.4/slinn/templates/firstrun/app.py
@@ -0,0 +1,19 @@
+from slinn import Dispatcher, Filter, HttpResponse
+import slinn
+ 
+dp = Dispatcher()
+
+def read(filename):
+    file = open(filename, 'r', encoding='utf-8')
+    data = file.read()
+    file.close()
+    return data
+
+# Write your code down here  
+@dp(Filter('/styles.css'))
+def colorscss(request):
+    return HttpResponse(read('templates_data/firstrun/styles.css'))
+                       
+@dp(Filter('.*'))
+def index(request):
+    return HttpResponse(read('templates_data/firstrun/slinn.html').replace('{% version %}', slinn.version.split()[2]), content_type='text/html')
