fb5cb9ccd0208886a8b06cb91e5a4ebdbf84232e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 03:04:41 +0000

    Add package xibabel/0.0.1b4/xibabel-0.0.1b4.tar.gz

---
+++ packages/xibabel/xibabel-0.0.1b4.tar.gz/xibabel-0.0.1b4/src/xibabel/__init__.py
@@ -0,0 +1,8 @@
+""" Xibabel neuroimaging interface
+
+Xibabel is an experimental package for working with neuroimaging data formats.
+"""
+
+from .loaders import load, save, to_nifti  # noqa:F401
+
+__version__ = "0.0.1b4"
