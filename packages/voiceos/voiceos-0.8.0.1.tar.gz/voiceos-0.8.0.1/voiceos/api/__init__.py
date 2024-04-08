54cb356fe590a9c7a39f59b92fc0d941e50bfb65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 14:03:58 +0000

    Add package voiceos/0.8.0.1/voiceos-0.8.0.1.tar.gz

---
+++ packages/voiceos/voiceos-0.8.0.1.tar.gz/voiceos-0.8.0.1/voiceos/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from voiceos.api.agents_api import AgentsApi
+from voiceos.api.calls_api import CallsApi
+from voiceos.api.phone_numbers_api import PhoneNumbersApi
+
