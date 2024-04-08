ca80530c4f32bb53e8dc9f5c52f5b9c6e59bf535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 04:05:23 +0000

    Add package FNews/5.1/FNews-5.1.tar.gz

---
+++ packages/FNews/FNews-5.1.tar.gz/FNews-5.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='FNews',
+    version='5.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'pandas',
+    ],
+    entry_points={
+        "console_scripts": [
+            "FNews-getArticles = FNews.main:main",
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
