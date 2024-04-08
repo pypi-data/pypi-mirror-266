cc99786c38600cf5e4375fb368cdce8e299164c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 14:05:11 +0000

    Add package fsinfo/0.0.7/fsinfo-0.0.7-py3-none-any.whl

---
+++ packages/fsinfo/fsinfo-0.0.7-py3-none-any.whl/fsinfo/__init__.py
@@ -0,0 +1,7 @@
+# fsinfo/__init__.py
+
+from .fileinfo import FileInfo
+from .directoryinfo import DirectoryInfo
+from .filesysteminfo import FileSystemInfo
+
+__all__ = ['FileInfo', 'DirectoryInfo', 'FileSystemInfo']
