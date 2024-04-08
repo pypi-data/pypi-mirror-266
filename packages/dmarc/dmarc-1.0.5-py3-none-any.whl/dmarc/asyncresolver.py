9051ff675a128f354b2131a8cd8f57440f2024ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 03:04:36 +0000

    Add package dmarc/1.0.5/dmarc-1.0.5-py3-none-any.whl

---
+++ packages/dmarc/dmarc-1.0.5-py3-none-any.whl/dmarc/asyncresolver.py
@@ -0,0 +1,30 @@
+"""DMARC asyncresolver
+
+Typical Usage:
+
+    >>> from dmarc.asyncresolver import resolve, RecordNotFoundError, RecordMultiFoundError, RecordResolverError
+    >>> try:
+    ...     record = await resolve('example.com')
+    ... except RecordNotFoundError:
+    ...     pass
+    ... except RecordMultiFoundError:
+    ...     raise # permerror
+    ... except RecordResolverError:
+    ...     raise # temperror
+    ... 
+"""
+
+from dns.asyncresolver import resolve as resolver, NXDOMAIN, NoAnswer
+from dns.exception import DNSException
+from .resolver import response, RecordResolverError, RecordNotFoundError, RecordNoDataError, RecordMultiFoundError
+
+async def resolve(domain):
+    try:
+        answers = await resolver('_dmarc.{0}'.format(domain), 'TXT')
+        return response(answers)
+    except NXDOMAIN as err:
+        raise RecordNotFoundError(err)
+    except NoAnswer as err:
+        raise RecordNoDataError(err)
+    except DNSException as err:
+        raise RecordResolverError(err)
