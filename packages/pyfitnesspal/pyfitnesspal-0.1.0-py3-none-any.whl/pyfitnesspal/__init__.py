b54abbb25ea81d330cb924a3cdd0abf854948899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-08 15:04:54 +0000

    Add package pyfitnesspal/0.1.0/pyfitnesspal-0.1.0-py3-none-any.whl

---
+++ packages/pyfitnesspal/pyfitnesspal-0.1.0-py3-none-any.whl/pyfitnesspal/__init__.py
@@ -0,0 +1,11 @@
+"""
+
+MyFitnessPal API Library
+
+------------------
+
+Welcome to the python myfitnesspal library!
+
+For more information, please check the github:
+    https://github.com/hexponent/pyfitnesspal
+"""
