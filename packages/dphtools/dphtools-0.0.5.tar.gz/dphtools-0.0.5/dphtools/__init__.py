46379141ea5cacb56e05bc4bedb60c12d073fe79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 06:04:25 +0000

    Add package dphtools/0.0.5/dphtools-0.0.5.tar.gz

---
+++ packages/dphtools/dphtools-0.0.5.tar.gz/dphtools-0.0.5/dphtools/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# __init__.py
+"""
+Tools for optics and image analysis.
+
+Copyright (c) 2021, David Hoffman
+"""
+
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
