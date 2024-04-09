6ab5b5f6b445ca9ab88727466f042b58ebb9e7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 14:04:07 +0000

    Add package MLEase/0.0.1/MLEase-0.0.1-py3-none-any.whl

---
+++ packages/MLEase/MLEase-0.0.1-py3-none-any.whl/mlease/__init__.py
@@ -0,0 +1,5 @@
+from .bucketizer import Bucketizer
+from .missing_value_imputer import MissingValueImputer
+from .outlier_remover import OutlierRemover
+
+__all__ = ['Bucketizer', 'MissingValueImputer', 'OutlierRemover']
