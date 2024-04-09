ee43d1b701744093adca3fcbd63f1db24a9e9761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 06:04:20 +0000

    Add package tradescope/2024.4/tradescope-2024.4-py3-none-any.whl

---
+++ packages/tradescope/tradescope-2024.4-py3-none-any.whl/tradescope/exchange/bingx.py
@@ -0,0 +1,19 @@
+""" Bingx exchange subclass """
+import logging
+from typing import Dict
+
+from tradescope.exchange import Exchange
+
+
+logger = logging.getLogger(__name__)
+
+
+class Bingx(Exchange):
+    """
+    Bingx exchange class. Contains adjustments needed for Tradescope to work
+    with this exchange.
+    """
+
+    _ts_has: Dict = {
+        "ohlcv_candle_limit": 1000,
+    }
