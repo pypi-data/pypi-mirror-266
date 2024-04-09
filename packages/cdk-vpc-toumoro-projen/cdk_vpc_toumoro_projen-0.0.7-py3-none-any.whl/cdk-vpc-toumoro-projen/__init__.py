7ccafedba0dd7c5ffa537f752eaff243af055a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 05:04:49 +0000

    Add package cdk-vpc-toumoro-projen/0.0.7/cdk_vpc_toumoro_projen-0.0.7-py3-none-any.whl

---
+++ packages/cdk-vpc-toumoro-projen/cdk_vpc_toumoro_projen-0.0.7-py3-none-any.whl/cdk-vpc-toumoro-projen/__init__.py
@@ -0,0 +1,90 @@
+'''
+# replace this
+'''
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+from ._jsii import *
+
+import aws_cdk as _aws_cdk_ceddda9d
+import constructs as _constructs_77d1e7e8
+
+
+class Vpc(
+    _aws_cdk_ceddda9d.Stack,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="cdk-vpn-toumoro-projen.Vpc",
+):
+    def __init__(
+        self,
+        scope: _constructs_77d1e7e8.Construct,
+        id: builtins.str,
+        *,
+        analytics_reporting: typing.Optional[builtins.bool] = None,
+        description: typing.Optional[builtins.str] = None,
+        env: typing.Optional[typing.Union[_aws_cdk_ceddda9d.Environment, typing.Dict[builtins.str, typing.Any]]] = None,
+        stack_name: typing.Optional[builtins.str] = None,
+        synthesizer: typing.Optional[_aws_cdk_ceddda9d.IStackSynthesizer] = None,
+        tags: typing.Optional[typing.Mapping[builtins.str, builtins.str]] = None,
+        termination_protection: typing.Optional[builtins.bool] = None,
+    ) -> None:
+        '''
+        :param scope: -
+        :param id: -
+        :param analytics_reporting: Include runtime versioning information in this Stack. Default: ``analyticsReporting`` setting of containing ``App``, or value of 'aws:cdk:version-reporting' context key
+        :param description: A description of the stack. Default: - No description.
+        :param env: The AWS environment (account/region) where this stack will be deployed. Set the ``region``/``account`` fields of ``env`` to either a concrete value to select the indicated environment (recommended for production stacks), or to the values of environment variables ``CDK_DEFAULT_REGION``/``CDK_DEFAULT_ACCOUNT`` to let the target environment depend on the AWS credentials/configuration that the CDK CLI is executed under (recommended for development stacks). If the ``Stack`` is instantiated inside a ``Stage``, any undefined ``region``/``account`` fields from ``env`` will default to the same field on the encompassing ``Stage``, if configured there. If either ``region`` or ``account`` are not set nor inherited from ``Stage``, the Stack will be considered "*environment-agnostic*"". Environment-agnostic stacks can be deployed to any environment but may not be able to take advantage of all features of the CDK. For example, they will not be able to use environmental context lookups such as ``ec2.Vpc.fromLookup`` and will not automatically translate Service Principals to the right format based on the environment's AWS partition, and other such enhancements. Default: - The environment of the containing ``Stage`` if available, otherwise create the stack will be environment-agnostic.
+        :param stack_name: Name to deploy the stack with. Default: - Derived from construct path.
+        :param synthesizer: Synthesis method to use while deploying this stack. Default: - ``DefaultStackSynthesizer`` if the ``@aws-cdk/core:newStyleStackSynthesis`` feature flag is set, ``LegacyStackSynthesizer`` otherwise.
+        :param tags: Stack tags that will be applied to all the taggable resources and the stack itself. Default: {}
+        :param termination_protection: Whether to enable termination protection for this stack. Default: false
+        '''
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__01edc1e74ec6d72160e79189832fc4939e7a91c964a660b7c80f1e7dcbd91ecc)
+            check_type(argname="argument scope", value=scope, expected_type=type_hints["scope"])
+            check_type(argname="argument id", value=id, expected_type=type_hints["id"])
+        props = _aws_cdk_ceddda9d.StackProps(
+            analytics_reporting=analytics_reporting,
+            description=description,
+            env=env,
+            stack_name=stack_name,
+            synthesizer=synthesizer,
+            tags=tags,
+            termination_protection=termination_protection,
+        )
+
+        jsii.create(self.__class__, self, [scope, id, props])
+
+
+__all__ = [
+    "Vpc",
+]
+
+publication.publish()
+
+def _typecheckingstub__01edc1e74ec6d72160e79189832fc4939e7a91c964a660b7c80f1e7dcbd91ecc(
+    scope: _constructs_77d1e7e8.Construct,
+    id: builtins.str,
+    *,
+    analytics_reporting: typing.Optional[builtins.bool] = None,
+    description: typing.Optional[builtins.str] = None,
+    env: typing.Optional[typing.Union[_aws_cdk_ceddda9d.Environment, typing.Dict[builtins.str, typing.Any]]] = None,
+    stack_name: typing.Optional[builtins.str] = None,
+    synthesizer: typing.Optional[_aws_cdk_ceddda9d.IStackSynthesizer] = None,
+    tags: typing.Optional[typing.Mapping[builtins.str, builtins.str]] = None,
+    termination_protection: typing.Optional[builtins.bool] = None,
+) -> None:
+    """Type checking stubs"""
+    pass
