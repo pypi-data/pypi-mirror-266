051b31401f90cc2c0c624951cddda257b88a47e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 03:04:38 +0000

    Add package falconz/2.1.0/falconz-2.1.0-py3-none-any.whl

---
+++ packages/falconz/falconz-2.1.0-py3-none-any.whl/falconz/resources.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: resources
+   :platform: Unix, Windows
+   :synopsis: A module for storing urls and filenames of the binaries required for the falconz.
+
+.. moduleauthor:: Lalith Kumar Shiyam Sundar <lalith.shiyamsundar@meduniwien.ac.at>
+
+This module contains the urls and filenames of the binaries that are required for the falconz.
+
+Usage:
+    The variables in this module can be imported and used in other modules within the falconz to download the necessary
+    binaries for the falconz.
+"""
+import psutil
+
+FALCON_BINARIES = {
+    "falcon-windows-x86_64": {
+        "url": "https://ucd-emic-muv.s3.us-west-2.amazonaws.com/falcon/falcon-windows-x86_64.zip",
+        "filename": "falcon-windows-x86_64.zip",
+        "directory": "falcon-windows-x86_64",
+    },
+    "falcon-linux-x86_64": {
+        "url": "https://ucd-emic-muv.s3.us-west-2.amazonaws.com/falcon/falcon-linux-x86_64.zip",
+        "filename": "falcon-linux-x86_64.zip",
+        "directory": "falcon-linux-x86_64",
+    },
+    "falcon-mac-x86_64": {
+        "url": "https://ucd-emic-muv.s3.us-west-2.amazonaws.com/falcon/falcon-mac-x86_64.zip",
+        "filename": "falcon-mac-x86_64.zip",
+        "directory": "falcon-mac-x86_64",
+    },
+    "falcon-mac-arm64": {
+        "url": "https://ucd-emic-muv.s3.us-west-2.amazonaws.com/falcon/falcon-mac-arm64.zip",
+        "filename": "falcon-mac-arm64.zip",
+        "directory": "falcon-mac-arm64",
+    },
+}
+
+
+# Create a function to get CPU and Memory usage
+def get_system_stats():
+    cpu_percent = psutil.cpu_percent(interval=None)
+    memory_info = psutil.virtual_memory()
+    memory_percent = memory_info.percent
+    return cpu_percent, memory_percent
+
