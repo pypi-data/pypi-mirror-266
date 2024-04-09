41fa8eb7f461f29f3ea589fd52bfc1a4207f1aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 03:04:15 +0000

    Add package wikiteam3/4.2.5/wikiteam3-4.2.5.tar.gz

---
+++ packages/wikiteam3/wikiteam3-4.2.5.tar.gz/wikiteam3-4.2.5/wikiteam3/dumpgenerator/exceptions.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+
+class PageMissingError(Exception):
+    def __init__(self, title, xml):
+        self.title = title
+        self.xml = xml
+
+    def __str__(self):
+        return "page '%s' not found" % self.title
+
+
+class ExportAbortedError(Exception):
+    def __init__(self, index):
+        self.index = index
+
+    def __str__(self):
+        return "Export from '%s' did not return anything." % self.index
+
+
+class FileSizeError(Exception):
+    def __init__(self, file: str, got_size: int, excpected_size: int, online_url: Optional[str] = None):
+        self.file = file
+        self.got_size = got_size
+        self.excpected_size = excpected_size
+        self.online_url = online_url
+
+    def __str__(self):
+        return f"File '{self.file}' size {self.got_size} is not match '{self.excpected_size}'." \
+            + (f"(url: {self.online_url})" if self.online_url else "")
+
+
+class FileSha1Error(Exception):
+    def __init__(self, file, excpected_sha1):
+        self.file = file
+        self.excpected_sha1 = excpected_sha1
+
+    def __str__(self):
+        return f"File '{self.file}' sha1 is not match '{self.excpected_sha1}'."
---
+++ packages/wikiteam3/wikiteam3-4.2.5.tar.gz/wikiteam3-4.2.5/wikiteam3/dumpgenerator/version.py
@@ -0,0 +1,5 @@
+__VERSION__ = "4.2.5"
+
+
+def getVersion():
+    return __VERSION__
