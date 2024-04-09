9a5001ee95baf51e039b44550991face605fe0e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 05:05:03 +0000

    Add package thirdai/0.7.38/thirdai-0.7.38-cp39-cp39-win_amd64.whl

---
+++ packages/thirdai/thirdai-0.7.38-cp39-cp39-win_amd64.whl/thirdai/neural_db_v2/documents/pdf.py
@@ -0,0 +1,63 @@
+from typing import Iterable
+
+import pandas as pd
+
+import thirdai_python_package.neural_db.parsing_utils.sliding_pdf_parse as pdf_parse
+
+from ..core.documents import Document
+from ..core.types import NewChunkBatch
+from .utils import join_metadata, series_from_value
+
+
+class PDF(Document):
+    def __init__(
+        self,
+        path,
+        chunk_size=100,
+        stride=40,
+        emphasize_first_words=0,
+        ignore_header_footer=True,
+        ignore_nonstandard_orientation=True,
+        table_parsing=False,
+        doc_metadata=None,
+    ):
+        super().__init__()
+
+        self.path = path
+        self.chunk_size = chunk_size
+        self.stride = stride
+        self.emphasize_first_words = emphasize_first_words
+        self.ignore_header_footer = ignore_header_footer
+        self.ignore_nonstandard_orientation = ignore_nonstandard_orientation
+        self.table_parsing = table_parsing
+        self.doc_metadata = doc_metadata
+
+    def chunks(self) -> Iterable[NewChunkBatch]:
+        parsed_chunks = pdf_parse.make_df(
+            filename=self.path,
+            chunk_words=self.chunk_size,
+            stride_words=self.stride,
+            emphasize_first_n_words=self.emphasize_first_words,
+            ignore_header_footer=self.ignore_header_footer,
+            ignore_nonstandard_orientation=self.ignore_nonstandard_orientation,
+            table_parsing=self.table_parsing,
+        )
+
+        text = parsed_chunks["para"]
+        keywords = parsed_chunks["emphasis"]
+
+        metadata = join_metadata(
+            n_rows=len(text),
+            chunk_metadata=parsed_chunks[["chunk_boxes", "page"]],
+            doc_metadata=self.doc_metadata,
+        )
+
+        return [
+            NewChunkBatch(
+                custom_id=None,
+                text=text,
+                keywords=keywords,
+                metadata=metadata,
+                document=series_from_value(self.path, len(text)),
+            )
+        ]
