a44d29a84c112778008ffebe028e7e101eb7051e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 16:05:45 +0000

    Add package otc-sphinx-directives/0.3.0/otc_sphinx_directives-0.3.0.tar.gz

---
+++ packages/otc-sphinx-directives/otc_sphinx_directives-0.3.0.tar.gz/otc_sphinx_directives-0.3.0/doc/source/conf.py
@@ -0,0 +1,14 @@
+project = 'otc_sphinx_directives'
+copyright = '2023, Ecosystem Squad'
+author = 'Open Telekom Cloud Ecosystem Squad'
+
+extensions = [
+    'otcdocstheme',
+    'otc_sphinx_directives'
+]
+
+exclude_patterns = ['_build']
+source_suffix = '.rst'
+master_doc = 'index'
+html_theme = 'otcdocs'
+html_static_path = ['_static']
---
+++ packages/otc-sphinx-directives/otc_sphinx_directives-0.3.0.tar.gz/otc_sphinx_directives-0.3.0/doc/source/examples/index.rst
@@ -0,0 +1,11 @@
+Documentation Examples
+======================
+
+.. toctree::
+
+   directives_obs
+   directives_obs_clean
+   directives_ecs
+   directives_ecs_internal
+   directives_docsportal
+   container_item
