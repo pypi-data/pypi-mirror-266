3a90d699bbda99f5fd20b3f71684a3c6808b3f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 15:04:10 +0000

    Add package liulianmao/1.2.0/liulianmao-1.2.0-py3-none-any.whl

---
+++ packages/liulianmao/liulianmao-1.2.0-py3-none-any.whl/liulianmao/client/core.py
@@ -0,0 +1,234 @@
+import json
+import os
+import sys
+from typing import List
+
+import requests
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(current_dir, ".."))
+
+from module.authentication import API_KEY, API_URL
+from module.log import logger
+from module.model import select_model
+from module.storage import PROJECT_FOLDER, get_user_folder, init
+
+conversation = []
+
+def load_conf():
+    config_file_path = os.path.join(
+        get_user_folder(), PROJECT_FOLDER, "assets", "config.json"
+    )
+    with open(config_file_path, "r") as file:
+        config = json.load(file)
+
+    logger.trace("[Config]\n" + f"{config}")
+    return config
+
+
+def models():
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json",
+    }
+    logger.trace("[Headers]\n" + f"{headers}")
+
+    response = requests.get(
+        API_URL + "/v1/models", headers=headers
+    )
+
+    def extract_ids(data):
+        collected_ids = []
+
+        for item in data:
+            for key, value in item.items():
+                if key == 'id':
+                    if value[0:3].lower() == "gpt":
+                        collected_ids.append(value)
+
+        return collected_ids
+
+    if response.status_code == 200:
+        logger.trace("[Debug] response.status_code == 200")
+        # judge mime
+        try:
+            logger.trace("[Response]\n" + str(response.json()))
+        except Exception as e:
+            logger.trace(e)
+            logger.critical("RESPONSE NOT JSON")
+        extracted_ids=extract_ids(response.json()["data"])
+        logger.debug(extracted_ids)
+        return extracted_ids
+    else:
+        logger.trace("[Debug] response.status_code != 200")
+        logger.error(
+            f"Error: {response.status_code} {response.content.decode('utf-8')}"
+        )
+        return {}
+
+
+def speech(msg):
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json",
+    }
+
+    data = {"model": "tts-1", "input": msg, "voice": "alloy"}
+    response = requests.post(
+        API_URL + "/v1/audio/speech", json=data, headers=headers
+    )
+
+    if response.status_code == 200:
+        speech_file_path = os.path.join(
+            get_user_folder(), PROJECT_FOLDER, "audios", "speech.mp3"
+        )
+        with open(speech_file_path, "wb") as f:
+            f.write(response.content)
+        logger.success("音频文件保存成功。")
+    else:
+        logger.error(
+            "生成语音失败。状态码：",
+            response.status_code,
+            "\n",
+            response.content.decode("utf-8"),
+        )
+
+def completion(question,available_models:List[str]=[]):
+    config = load_conf()
+    model_type = config["model_type"]
+    system_content = config["system_message"]["content"]
+    temperature = float(config["settings"]["temperature"])
+
+
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json",
+    }
+
+    payload = {
+        "model": select_model(model_type,available_models,direct_debug=True),
+        "messages": (
+            [{"role": "system", "content": system_content}]
+            + conversation
+            + [{"role": "user", "content": question}]
+        ),
+        "temperature": temperature,
+        "max_tokens": 2048,
+        "top_p": 1.0,
+        "frequency_penalty": 0.0,
+        "presence_penalty": 0.0,
+        "stop": None,
+        # "plugins": [
+        #     {
+        #         "name": "plugin_name_here",
+        #         "parameters": {"param1": "value1", "param2": "value2"},
+        #     }
+        # ],
+    }
+
+    logger.trace("[Headers]\n" + f"{headers}")
+    logger.trace("[Payload]\n" + f"{payload}")
+
+    flag_echo_input = False
+    if flag_echo_input:
+        logger.debug("[Question]\n" + f"{question}")
+    else:
+        logger.trace("[Question]\n" + f"{question}")
+
+    response = requests.post(
+        API_URL + "/v1/chat/completions", headers=headers, json=payload
+    )
+
+    if response.status_code == 200:
+        logger.trace("[Debug] response.status_code == 200")
+        # judge mime
+        try:
+            logger.trace("[Response]\n" + str(response.json()))
+        except Exception as e:
+            logger.trace(e)
+            logger.critical("RESPONSE NOT JSON")
+        # judge schema
+        try:
+            conversation.append({"role": "user", "content": question})
+            conversation.append(
+                {
+                    "role": "system",
+                    "content": response.json()["choices"][0]["message"]["content"],
+                }
+            )
+        except Exception as e:
+            logger.trace(e)
+            logger.critical("WRONG RESPONSE SCHEMA")
+        logger.trace("[History]\n" + str(conversation))
+        return response.json()
+    else:
+        logger.trace("[Debug] response.status_code != 200")
+        logger.error(
+            f"Error: {response.status_code} {response.content.decode('utf-8')}"
+        )
+        return {}
+
+
+def ask(msg: str,available_models:List[str]):
+    response = completion(msg,available_models)
+    content = response["choices"][0]["message"]["content"]
+    logger.success("[Answer]\n" + content)
+    return content
+
+
+def chat():
+    available_models=models()
+
+    with open(
+        os.path.join(
+            get_user_folder(), PROJECT_FOLDER, "terminal", "question.txt"
+        ),
+        "r",
+        encoding="utf-8",
+    ) as file:
+        msg = file.read()
+
+    flag_continue = True
+    response = ask(msg,available_models)
+
+    if not flag_continue:
+        with open(
+            os.path.join(
+                get_user_folder(), PROJECT_FOLDER, "terminal", "answer.txt"
+            ),
+            "w",
+            encoding="utf-8",
+        ) as file:
+            file.write(response)
+    else:
+        flag_end = False
+        while not flag_end:
+            import time
+
+            # 部分控制台输入是异步的，给足够的时间以保证不会打断输出
+            time.sleep(0.05)
+            logger.info("[Interaction] 请输入追问")
+            append_question = input()
+            append_question_judge = append_question.replace("\n", "").replace(
+                " ", ""
+            )
+            if append_question_judge != "END" and append_question_judge != "":
+                response = ask(append_question,available_models)
+            else:
+                flag_end = True
+                break
+
+
+def talk():
+    with open("terminal/question.txt", "r", encoding="utf-8") as file:
+        msg = file.read()
+    speech(msg)
+
+
+def main():
+    logger.critical("THIS PROGRAM NOT INTENT TO RUN SUBMODULE".upper())
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/liulianmao/liulianmao-1.2.0-py3-none-any.whl/liulianmao/module/authentication.py
@@ -0,0 +1,73 @@
+import os
+from os.path import abspath, dirname, isfile, join
+from typing import Optional
+
+from .log import logger
+from .storage import PROJECT_FOLDER, get_user_folder
+
+
+def get_env(var_name: str, default: str) -> str:
+    """
+    尝试从环境变量获取值，如果失败，尝试从用户目录下和同目录下的文件读取，最后使用默认值。
+    """
+
+    def get_valid_value(value: str) -> Optional[str]:
+        """判断字符串是否有效，如果有效则返回该字符串，否则返回None。"""
+        value = value.strip()
+        if value and not value.startswith("#"):
+            return value
+        return None
+
+    def get_from_env(var_name: str) -> Optional[str]:
+        """尝试从环境变量获取值。"""
+        return os.environ.get(var_name)
+
+    def get_from_user_folder(var_name: str) -> Optional[str]:
+        """尝试从用户目录的文件夹读取。"""
+        var_file_path = join(get_user_folder(), PROJECT_FOLDER, var_name)
+        if isfile(var_file_path):
+            try:
+                with open(var_file_path) as f:
+                    values = f.read().split("\n")
+                    for value in values:
+                        if get_valid_value(value):
+                            return value
+            except Exception as e:
+                logger.error(f"Error reading {var_name} from user folder file: {e}")
+        return None
+
+    def get_from_current_dir(var_name: str) -> Optional[str]:
+        """尝试从当前文件所在目录读取。"""
+        var_file_path = join(dirname(abspath(__file__)), var_name)
+        if isfile(var_file_path):
+            try:
+                with open(var_file_path) as f:
+                    values = f.read().split("\n")
+                    for value in values:
+                        if get_valid_value(value):
+                            return value
+            except Exception as e:
+                logger.error(f"Error reading {var_name} from current directory file: {e}")
+        return None
+
+
+    # 定义尝试顺序
+    attempts = [get_from_user_folder, get_from_current_dir, get_from_env]
+
+    for attempt in attempts:
+        result = attempt(var_name)
+        if result is not None:
+            logger.trace("[Authentication]\n" + f"{var_name} = {result}")
+            return result
+
+    # 如果所有尝试都失败了，记录错误并返回默认值
+    logger.error(f"{var_name} not found in environment variables, user folder, or current directory file, using default.")
+    return default
+
+
+# 获取API URL和API KEY
+API_URL = get_env("OPENAI_BASE_URL", "https://api.openai.com")
+API_KEY = get_env(
+    "OPENAI_API_KEY",
+    "You may need to check your environment variables' configure.",
+)
