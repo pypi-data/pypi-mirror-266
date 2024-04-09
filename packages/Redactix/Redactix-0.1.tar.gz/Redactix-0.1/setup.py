625efef362b983096f5e82502f080ab356249d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 05:04:59 +0000

    Add package Redactix/0.1/Redactix-0.1.tar.gz

---
+++ packages/Redactix/Redactix-0.1.tar.gz/Redactix-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Redactix",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "transformers",
+        "torch",
+        "gradio",
+        "pandas",
+    ],
+    package_data={"Redactix": ["data/logo.png"]},
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'launch_redactpii=Redactix.gradio_app:main',
+        ],
+    },
+)
