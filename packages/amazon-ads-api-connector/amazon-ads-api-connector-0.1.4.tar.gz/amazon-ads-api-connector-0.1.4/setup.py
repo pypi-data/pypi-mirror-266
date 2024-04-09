c3ea441fbd5722d077de58078a93baa4b19af84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 03:04:35 +0000

    Add package amazon-ads-api-connector/0.1.4/amazon-ads-api-connector-0.1.4.tar.gz

---
+++ packages/amazon-ads-api-connector/amazon-ads-api-connector-0.1.4.tar.gz/amazon-ads-api-connector-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="amazon-ads-api-connector",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "types-requests",
+    ],
+    author="Markus U. Wahl",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    description="API wrapper for the Amazon Ads API",
+)
