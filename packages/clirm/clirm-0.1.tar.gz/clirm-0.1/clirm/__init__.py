a3a1565d1d47ef8bec4f988bbab6512dfa6efae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 04:06:17 +0000

    Add package clirm/0.1/clirm-0.1.tar.gz

---
+++ packages/clirm/clirm-0.1.tar.gz/clirm-0.1/clirm/__init__.py
@@ -0,0 +1,6 @@
+from .base import Clirm as Clirm
+from .base import DoesNotExist as DoesNotExist
+from .base import Field as Field
+from .base import Func as Func
+from .base import Model as Model
+from .base import Query as Query
