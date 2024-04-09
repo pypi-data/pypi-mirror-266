f4101073841a4325e6b62107133aa71c66092142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 05:05:37 +0000

    Add package batchalign/0.7.0b3/batchalign-0.7.0b3.tar.gz

---
+++ packages/batchalign/batchalign-0.7.0b3.tar.gz/batchalign-0.7.0b3/batchalign/pipelines/__init__.py
@@ -0,0 +1,13 @@
+from .pipeline import BatchalignPipeline
+from .base import BatchalignEngine
+from .asr import WhisperEngine, RevEngine, WhisperXEngine
+
+from .morphosyntax import StanzaEngine
+from .cleanup import NgramRetraceEngine, DisfluencyReplacementEngine
+from .speaker import NemoSpeakerEngine
+
+from .fa import WhisperFAEngine
+from .utr import WhisperUTREngine, RevUTREngine
+
+from .analysis import EvaluationEngine
+
