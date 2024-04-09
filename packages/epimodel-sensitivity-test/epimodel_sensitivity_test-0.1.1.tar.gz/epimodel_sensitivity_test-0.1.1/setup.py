de51eb74f767d6d11818294f911dd6decfb9b1af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 05:04:47 +0000

    Add package epimodel-sensitivity-test/0.1.1/epimodel_sensitivity_test-0.1.1.tar.gz

---
+++ packages/epimodel-sensitivity-test/epimodel_sensitivity_test-0.1.1.tar.gz/epimodel_sensitivity_test-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='epimodel_sensitivity_test',
+    version='0.1.1',
+    author='Kolos Kovács',
+    author_email='kovkol21@gmail.com',
+    description='Efficient sensitivity analysis and evaluation of epidemiological models',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/KKol21/epimodel_sensitivity',
+    packages=find_packages(),
+    package_data={
+        '': ['*.json', '*.xls']},
+    install_requires=[
+        "smt~=1.3.0",
+        "tqdm==4.51.0",
+        "xlrd==1.2.0",
+        "torch~=2.0.0",
+        "torchode~=0.1.8",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
