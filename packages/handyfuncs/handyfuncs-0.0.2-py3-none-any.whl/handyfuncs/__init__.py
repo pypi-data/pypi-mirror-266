fedd9bffb11d23008ad4e055d83b33c24b82ef40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 17:04:33 +0000

    Add package handyfuncs/0.0.2/handyfuncs-0.0.2-py3-none-any.whl

---
+++ packages/handyfuncs/handyfuncs-0.0.2-py3-none-any.whl/handyfuncs/__init__.py
@@ -0,0 +1,20 @@
+from handyfuncs.automation import (
+    write_to_log
+    , exec_fn
+    , create_empty_log_file
+    , run_subprocess
+    , create_empty_text_file
+    , append_to_text_file
+    , write_to_new_text_file
+    , current_method_name
+    )
+
+from handyfuncs.manipulation import (
+    number_of_words_in_string
+    , all_same_character
+    , get_initials
+    , literal_return
+    , explode_frame_list_column
+    , convert_dict_to_list
+    , merge_dictionaries
+    )
