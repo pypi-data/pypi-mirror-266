2ef9207270806460b3c4182f2cf0c3efc373f50d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 15:04:48 +0000

    Add package python-bridge/0.0.17/python-bridge-0.0.17.tar.gz

---
+++ packages/python-bridge/python-bridge-0.0.17.tar.gz/python-bridge-0.0.17/bridge/console.py
@@ -0,0 +1,34 @@
+from contextlib import contextmanager
+from datetime import datetime
+
+from rich.console import Console
+
+
+console = Console()
+
+
+@contextmanager
+def log_task(start_message, end_message):
+    with console.status(
+        f"      {start_message}", spinner="aesthetic", spinner_style="blue"
+    ):
+        try:
+            # Before entering the block
+            yield
+        finally:
+            # After exiting the block
+
+            # Format the current time to include leading zeros (HH:MM:SS)
+            timestamp_str = datetime.now().strftime("[%H:%M:%S]")
+            console.print(
+                f"{timestamp_str} [bright_green]✓[/bright_green] {end_message}",
+                highlight=False,
+            )
+
+
+def log_error(message):
+    console.print(f"[bright_red]✗ Bridge Error[/bright_red]: {message}")
+
+
+def log_warning(message):
+    console.print(f"[yellow]Bridge Warning[/yellow]: {message}")
