2b028735df3d4921a420eac20e5ee51912767aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 04:06:01 +0000

    Add package vnpy_mydata/0.0.1/vnpy_mydata-0.0.1-py3-none-any.whl

---
+++ packages/vnpy_mydata/vnpy_mydata-0.0.1-py3-none-any.whl/vnpy_mydata/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2024-present liuyanzhi08 <702368372@qq.com>
+#
+# SPDX-License-Identifier: MIT
+
+from vnpy.trader.datafeed import BaseDatafeed
+
+class Datafeed(BaseDatafeed):
+    pass
