3c272099c4e8ff84a21bf54826bbd0c071ca0140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 03:04:19 +0000

    Add package ontolutils/0.2.17/ontolutils-0.2.17.tar.gz

---
+++ packages/ontolutils/ontolutils-0.2.17.tar.gz/ontolutils-0.2.17/ontolutils/namespacelib/__init__.py
@@ -0,0 +1,10 @@
+"""Auto-generated file. Do not edit!"""
+from .m4i import M4I
+from .obo import OBO
+from .pivmeta import PIVMETA
+from .codemeta import CODEMETA
+from .qudt_unit import QUDT_UNIT
+from .qudt_kind import QUDT_KIND
+from .schema import SCHEMA
+from .ssno import SSNO
+from ._iana_utils import IANA
