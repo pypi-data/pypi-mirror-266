5a9df22c32a8a7dcdbb2228673e117f7ed29beeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 15:04:20 +0000

    Add package lqbox/2.4.0/lqbox-2.4.0-py3-none-any.whl

---
+++ packages/lqbox/lqbox-2.4.0-py3-none-any.whl/lqbox/bidp/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Project : lqbox 
+@File    : __init__.py
+@Date    : 2023/8/25 10:51:45
+@Author  : zhchen
+@Desc    : 
+"""
+from lqbox.base import BaseBox
+
+
+class BidpBox(BaseBox):
+    base_url = 'aHR0cHM6Ly9iaWRwLmxlcWVlLmNvbQ=='
