0008c124ef887affc0b22311c484c52b0a1ca2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 17:04:33 +0000

    Add package cogflow/1.7.0/cogflow-1.7.0.tar.gz

---
+++ packages/cogflow/cogflow-1.7.0.tar.gz/cogflow-1.7.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cogflow",
+    version="1.7.0",
+    author="Sai_kireeti",
+    author_email="sai.kireeti@hiro-microdatacenters.nl",
+    description="cog modules",
+    packages=find_packages(),
+    install_requires=[
+        "mlflow==2.10.2",
+        "kfp==1.8.22",
+        "boto3",
+        "tenacity",
+        "pandas",
+        "numpy",
+        "kubernetes",
+        "minio",
+        "scikit-learn==1.4.0",
+        "awscli",
+        "s3fs",
+        "setuptools",
+        "kserve",
+        "tensorflow",
+        "ray==2.9.3",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
