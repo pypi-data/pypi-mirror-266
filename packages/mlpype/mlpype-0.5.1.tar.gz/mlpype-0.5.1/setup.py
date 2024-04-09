a451877a99986200911b83d3244b44a797929716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 05:05:15 +0000

    Add package mlpype/0.5.1/mlpype-0.5.1.tar.gz

---
+++ packages/mlpype/mlpype-0.5.1.tar.gz/mlpype-0.5.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_namespace_packages, setup
+
+setup(
+    name="mlpype",
+    packages=find_namespace_packages(include=["mlpype.*"]),
+    python_requires=">=3.8",
+    version="0.5.1",
+    license="MIT",
+    author="Jeroen van den Hoven",
+    url="https://github.com/jeroenvdhoven/mlpype",
+)
