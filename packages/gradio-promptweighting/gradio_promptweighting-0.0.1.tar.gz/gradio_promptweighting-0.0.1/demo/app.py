1a9c8d21956d7fc551e41b817ae5a70a6f2af6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 14:04:07 +0000

    Add package gradio-promptweighting/0.0.1/gradio_promptweighting-0.0.1.tar.gz

---
+++ packages/gradio-promptweighting/gradio_promptweighting-0.0.1.tar.gz/gradio_promptweighting-0.0.1/demo/app.py
@@ -0,0 +1,40 @@
+import gradio as gr
+from gradio_promptweighting import PromptWeighting
+
+
+example = PromptWeighting().example_value()
+
+
+def predict(input):
+    return (input, input)
+
+
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            prompt = PromptWeighting(
+                value=[{"prompt": "a cat", "scale": 1.5}], step=0.001
+            )
+            btn = gr.Button("Update Prompt")
+        with gr.Column():
+            text = gr.Textbox(
+                label="Prompt",
+                placeholder="",
+            )
+            prompt2 = PromptWeighting(min=0, max=10, step=0.001)
+    inputs = [prompt]
+    outputs = [text, prompt2]
+
+    btn.click(fn=predict, inputs=inputs, outputs=outputs, show_progress=False)
+    prompt.change(
+        fn=predict,
+        inputs=inputs,
+        outputs=outputs,
+        queue=False,
+        trigger_mode="always_last",
+        show_progress=False,
+    )
+
+
+if __name__ == "__main__":
+    demo.launch()
---
+++ packages/gradio-promptweighting/gradio_promptweighting-0.0.1.tar.gz/gradio_promptweighting-0.0.1/frontend/Index.svelte
@@ -0,0 +1,96 @@
+<svelte:options accessors={true} />
+
+<script lang="ts">
+  import type { PromptType, SelectData } from "types";
+  import { nanoid } from "nanoid";
+  import type { Gradio } from "@gradio/utils";
+  import { BlockTitle } from "@gradio/atoms";
+  import { Block } from "@gradio/atoms";
+  import { StatusTracker } from "@gradio/statustracker";
+  import type { LoadingStatus } from "@gradio/statustracker";
+  import PrompstList from "./lib/PrompstList.svelte";
+
+  import { tick } from "svelte";
+
+  export let gradio: Gradio<{
+    change: PromptType[];
+    submit: never;
+    blur: never;
+    select: SelectData;
+    input: never;
+    focus: never;
+  }>;
+  export let label = "Prompt Weighting";
+  export let elem_id = "";
+  export let elem_classes: string[] = [];
+  export let visible = true;
+  export let value: PromptType[] = [];
+  export let placeholder = "";
+  export let show_label: boolean;
+  export let scale: number | null = null;
+  export let min_width: number | undefined = undefined;
+  export let loading_status: LoadingStatus | undefined = undefined;
+  export let value_is_output = false;
+  export let interactive: boolean;
+  export let rtl = false;
+  export let min = 0;
+  export let max = 2;
+  export let step = 0.01;
+  let promptsList: PromptType[] = [];
+
+  let el: HTMLTextAreaElement | HTMLInputElement;
+  const container = false;
+
+  function handle_change(): void {
+    gradio.dispatch("change", value);
+    if (!value_is_output) {
+      gradio.dispatch("input");
+      gradio.dispatch("submit");
+    }
+  }
+
+  async function handle_keypress(e: KeyboardEvent): Promise<void> {
+    await tick();
+    if (e.key === "Enter") {
+      e.preventDefault();
+      gradio.dispatch("submit");
+    }
+  }
+
+  $: if (value === null) value = [];
+
+  $: value, handle_change();
+</script>
+
+<Block
+  {visible}
+  {elem_id}
+  {elem_classes}
+  {scale}
+  {min_width}
+  allow_overflow={false}
+  padding={true}
+>
+  {#if loading_status}
+    <StatusTracker
+      autoscroll={gradio.autoscroll}
+      i18n={gradio.i18n}
+      {...loading_status}
+    />
+  {/if}
+
+  <!-- svelte-ignore a11y-label-has-associated-control -->
+  <label class:container class="relative">
+    <BlockTitle {show_label} info={undefined}>{label}</BlockTitle>
+    <PrompstList
+      {min}
+      {max}
+      {step}
+      classNames={"py-5"}
+      bind:promptsList={value}
+    />
+  </label>
+</Block>
+
+<style>
+</style>
