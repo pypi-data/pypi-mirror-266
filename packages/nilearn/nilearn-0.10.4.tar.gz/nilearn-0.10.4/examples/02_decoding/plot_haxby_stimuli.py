9d216fe1a61e561daf13fdf18520dfc3129b7c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 16:06:08 +0000

    Add package nilearn/0.10.4/nilearn-0.10.4.tar.gz

---
+++ packages/nilearn/nilearn-0.10.4.tar.gz/nilearn-0.10.4/examples/02_decoding/plot_haxby_stimuli.py
@@ -0,0 +1,46 @@
+"""
+Show stimuli of Haxby et al. dataset
+====================================
+
+In this script we plot an overview of the stimuli used
+in :footcite:t:`Haxby2001`.
+"""
+
+try:
+    import matplotlib.pyplot as plt
+except ImportError:
+    raise RuntimeError("This script needs the matplotlib library")
+
+# %%
+from nilearn import datasets
+from nilearn.plotting import show
+
+haxby_dataset = datasets.fetch_haxby(subjects=[], fetch_stimuli=True)
+stimulus_information = haxby_dataset.stimuli
+
+# %%
+
+for stim_type in stimulus_information:
+    # skip control images, there are too many
+    if stim_type != "controls":
+        file_names = stimulus_information[stim_type]
+
+        fig, axes = plt.subplots(6, 8)
+        fig.suptitle(stim_type)
+
+        for img_path, ax in zip(file_names, axes.ravel()):
+            ax.imshow(plt.imread(img_path), cmap=plt.cm.gray)
+
+        for ax in axes.ravel():
+            ax.axis("off")
+
+show()
+
+# %%
+# References
+# ----------
+#
+#  .. footbibliography::
+
+
+# sphinx_gallery_dummy_images=7
