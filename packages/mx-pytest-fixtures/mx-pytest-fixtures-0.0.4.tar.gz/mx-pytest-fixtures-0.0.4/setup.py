0cd1a6d9a44f457a222d18b19f49e628c02ba32a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 05:05:05 +0000

    Add package mx-pytest-fixtures/0.0.4/mx-pytest-fixtures-0.0.4.tar.gz

---
+++ packages/mx-pytest-fixtures/mx-pytest-fixtures-0.0.4.tar.gz/mx-pytest-fixtures-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Test fixtures for pytest testing framework.'
+LONG_DESCRIPTION = 'A package which provides fixtures for the pytest testing framework.'
+
+# Setting up
+setup(
+    name="mx-pytest-fixtures",
+    version=VERSION,
+    author="Ismail Chbiki",
+    author_email="is.chbiki@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'pytest', 'fixtures', 'testing'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
