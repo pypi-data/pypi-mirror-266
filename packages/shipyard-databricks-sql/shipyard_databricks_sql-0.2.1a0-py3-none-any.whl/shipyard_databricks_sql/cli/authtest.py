77e58d05f38f1ca1dbdb669a9b757e66a1e2e803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 06:04:16 +0000

    Add package shipyard-databricks-sql/0.2.1a0/shipyard_databricks_sql-0.2.1a0-py3-none-any.whl

---
+++ packages/shipyard-databricks-sql/shipyard_databricks_sql-0.2.1a0-py3-none-any.whl/shipyard_databricks_sql/cli/authtest.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from shipyard_databricks_sql import DatabricksSqlClient
+from shipyard_templates import ShipyardLogger 
+
+logger = ShipyardLogger.get_logger()
+
+
+def main():
+    try:
+        conn = DatabricksSqlClient(
+            access_token=os.getenv("DATABRICKS_SQL_ACCESS_TOKEN"),
+            server_host=os.getenv("DATABRICKS_SERVER_HOST"),
+            http_path=os.getenv("DATABRICKS_HTTP_PATH"),
+        ).connect()
+    except Exception as e:
+        logger.authtest(f"Error in connecting to Databricks with provided credentials. Message: {e}")
+        sys.exit(1)
+
+    else:
+        conn.close()
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/shipyard-databricks-sql/shipyard_databricks_sql-0.2.1a0-py3-none-any.whl/shipyard_databricks_sql/cli/execute.py
@@ -0,0 +1,51 @@
+import argparse
+import sys
+
+from shipyard_templates import ExitCodeException, ShipyardLogger
+from shipyard_databricks_sql import DatabricksSqlClient
+
+logger = ShipyardLogger.get_logger()
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--access-token", dest="access_token", required=True)
+    parser.add_argument("--server-host", dest="server_host", required=True)
+    parser.add_argument("--http-path", dest="http_path", required=True)
+    parser.add_argument("--catalog", dest="catalog", required=False, default="")
+    parser.add_argument("--schema", dest="schema", required=False, default="")
+    parser.add_argument("--query", dest="query", required=True)
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    catalog = args.catalog if args.catalog != "" else None
+    schema = args.schema if args.schema != "" else None
+
+    try:
+        client = DatabricksSqlClient(
+            server_host=args.server_host,
+            http_path=args.http_path,
+            access_token=args.access_token,
+            catalog=catalog,
+            schema=schema,
+        )
+        client.connect()
+        client.execute_query(args.query)
+    except ExitCodeException as ec:
+        logger.error(f"ExitCodeException: Error in executing query {ec.message}")
+        sys.exit(ec.exit_code)
+    except Exception as e:
+        logger.error(f"Error in attempting to execute query in Databricks:{str(e)}")
+        sys.exit(client.EXIT_CODE_INVALID_QUERY)
+    else:
+        logger.info("Query executed successfully")
+
+    finally:
+        client.close()
+
+
+if __name__ == "__main__":
+    main()
