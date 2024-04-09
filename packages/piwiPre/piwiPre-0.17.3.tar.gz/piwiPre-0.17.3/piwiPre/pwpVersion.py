9f77924a13efb867a6a30736724d4718da378eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 16:06:04 +0000

    Add package piwiPre/0.17.3/piwiPre-0.17.3.tar.gz

---
+++ packages/piwiPre/piwiPre-0.17.3.tar.gz/piwiPre-0.17.3/piwiPre/pwpVersion.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------------------------------------------
+# piwiPre project
+# This program and library is licenced under the European Union Public Licence v1.2 (see LICENCE)
+# developed by fabien.battini(at)gmail.com
+# ---------------------------------------------------------------------------------------------------------------
+
+
+class PwpVersion:
+    help = '0.17.3 at 04/09/2024 08:56:21'
+    spec = '0.17.3'
+    release = 0
+    version = 17
+    patch = 3
+    date = '04/09/2024 08:56:21'
+    url = 'https://gitlab.com/api/v4/projects/48915444/packages/generic/piwiPre/0.17.3/piwiPre-0.17.3.exe'
+    url = 'https://gitlab.com/api/v4/projects/48915444/packages/generic/piwiPre/0.17/piwiPreInstaller-0.17.exe'
+
+    def __init__(self):
+        pass
