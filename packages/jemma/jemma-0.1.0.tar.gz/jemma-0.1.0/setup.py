7143393943baa5a8362811d707444f98b7893623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 15:04:38 +0000

    Add package jemma/0.1.0/jemma-0.1.0.tar.gz

---
+++ packages/jemma/jemma-0.1.0.tar.gz/jemma-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jemma',
+    version='0.1.0',
+    description='convert ideas into code',
+    author='tolitius',
+    packages=find_packages(),
+    install_requires=[
+        'python-dotenv==1.0.1',
+        'anthropic==0.21.3',
+        'openai==1.14.3',
+        'ollama==0.1.8',
+        'replicate==0.25.1',
+    ],
+)
