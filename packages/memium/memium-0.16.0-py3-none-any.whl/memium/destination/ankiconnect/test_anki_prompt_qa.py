8a10661f0aa9576c10c2831eabf958e37a2fa794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 16:06:14 +0000

    Add package memium/0.16.0/memium-0.16.0-py3-none-any.whl

---
+++ packages/memium/memium-0.16.0-py3-none-any.whl/memium/destination/ankiconnect/test_anki_prompt_qa.py
@@ -0,0 +1,46 @@
+from collections.abc import Sequence
+from dataclasses import dataclass, field
+
+from .anki_prompt_cloze import AnkiCloze
+from .anki_prompt_qa import AnkiQA
+
+
+def fake_tag_factory() -> Sequence[str]:
+    return ["FakeTag"]
+
+
+@dataclass(frozen=True)
+class FakeAnkiCloze(AnkiCloze):
+    text: str = "FakeText"
+    base_deck: str = "FakeBaseDeck"
+    tags: Sequence[str] = field(default_factory=fake_tag_factory)
+    css: str = "FakeCSS"
+    uuid: int = 0
+
+
+@dataclass(frozen=True)
+class FakeAnkiQA(AnkiQA):
+    base_deck: str = "FakeBaseDeck"
+    tags: Sequence[str] = field(default_factory=fake_tag_factory)
+    question: str = "FakeQuestion"
+    answer: str = "FakeAnswer"
+    css: str = "FakeCSS"
+    uuid: int = 0
+
+
+def test_ankiqa_deck_inference():
+    card = FakeAnkiQA(tags=["anki/deck/Subdeck"])
+    assert card.deck == "FakeBaseDeck::Subdeck"
+
+
+def test_ankiqa_deck_inference_with_wikilinks():
+    card = FakeAnkiQA(
+        tags=["anki/deck/Subdeck"], question="What are [[Wikilinks]] on [[Wikipedia]]?"
+    )
+    assert card.deck == "FakeBaseDeck::Subdeck::Wikilinks-Wikipedia"
+
+
+def test_formatting():
+    card = FakeAnkiQA(question="Q. This is a _question_?")
+    note = card.to_genanki_note()
+    assert note.fields[0] == "<p>Q. This is a <em>question</em>?</p>"  # type: ignore
