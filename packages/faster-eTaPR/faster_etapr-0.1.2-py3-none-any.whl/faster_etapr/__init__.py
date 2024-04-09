32a35abdaf546ad7175955df6e87ab223c3bc67b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 05:05:11 +0000

    Add package faster-eTaPR/0.1.2/faster_etapr-0.1.2-py3-none-any.whl

---
+++ packages/faster-eTaPR/faster_etapr-0.1.2-py3-none-any.whl/faster_etapr/__init__.py
@@ -0,0 +1,17 @@
+__version__ = '0.1.2'
+
+__title__ = 'faster-eTaPR'
+__description__ = 'Faster implementation of github.com/saurf4ng/eTaPR.'
+
+__author__ = 'Gorden Platz'
+__email__ = '36087062+GPla@users.noreply.github.com'
+
+__copyright__ = 'Copyright (c) 2024 Gorden Platz'
+
+
+from .etapr import evaluate_from_preds, evaluate_from_ranges
+
+__all__ = [
+    'evaluate_from_preds',
+    'evaluate_from_ranges',
+]
