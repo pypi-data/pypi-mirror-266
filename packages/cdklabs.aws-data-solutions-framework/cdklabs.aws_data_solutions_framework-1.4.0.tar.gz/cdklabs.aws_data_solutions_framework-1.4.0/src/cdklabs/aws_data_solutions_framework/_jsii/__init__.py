c783ea72e0e84e29e1f5b917156fc7daa45905b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 05:05:15 +0000

    Add package cdklabs.aws-data-solutions-framework/1.4.0/cdklabs.aws_data_solutions_framework-1.4.0.tar.gz

---
+++ packages/cdklabs.aws-data-solutions-framework/cdklabs.aws_data_solutions_framework-1.4.0.tar.gz/cdklabs.aws_data_solutions_framework-1.4.0/src/cdklabs/aws_data_solutions_framework/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import aws_cdk.lambda_layer_kubectl_v27._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cdklabs/aws-data-solutions-framework",
+    "1.4.0",
+    __name__[0:-6],
+    "aws-data-solutions-framework@1.4.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
