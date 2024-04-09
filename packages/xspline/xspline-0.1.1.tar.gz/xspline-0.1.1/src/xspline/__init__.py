7750f29f45daaa65d2b0744709f7bac25b4886cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 17:04:34 +0000

    Add package xspline/0.1.1/xspline-0.1.1.tar.gz

---
+++ packages/xspline/xspline-0.1.1.tar.gz/xspline-0.1.1/src/xspline/__init__.py
@@ -0,0 +1,15 @@
+from .bspl import Bspl
+from .indi import Indi
+from .poly import Poly
+from .xfunction import BasisXFunction, BundleXFunction, XFunction
+from .xspl import XSpline
+
+__all__ = [
+    "Bspl",
+    "Indi",
+    "Poly",
+    "XSpline",
+    "BasisXFunction",
+    "BundleXFunction",
+    "XFunction",
+]
