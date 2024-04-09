f820543a777227876e59ec5ea3d3b36b41108ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 15:04:33 +0000

    Add package mdpython/0.0.6/mdpython-0.0.6.tar.gz

---
+++ packages/mdpython/mdpython-0.0.6.tar.gz/mdpython-0.0.6/hello_world.py
@@ -0,0 +1,5 @@
+from mdpython.fileutils import compare
+
+compare.compare_files(r"C:\Users\Dell\OneDrive\Desktop\spark\b.py",
+                      r"C:\Users\Dell\OneDrive\Desktop\spark\c.py",
+                      r"C:\Users\Dell\OneDrive\Desktop\spark\out.html")
