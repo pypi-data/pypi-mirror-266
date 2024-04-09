247737e6405f636503adc0cae32bde6e185386c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 14:04:13 +0000

    Add package galileo-protect/0.2.0/galileo_protect-0.2.0.tar.gz

---
+++ packages/galileo-protect/galileo_protect-0.2.0.tar.gz/galileo_protect-0.2.0/src/galileo_protect/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa: F401
+# ruff: noqa: F401
+from galileo_protect.invoke import invoke
+from galileo_protect.project import create_project
+from galileo_protect.stage import create_stage
+
+__version__ = "0.2.0"
