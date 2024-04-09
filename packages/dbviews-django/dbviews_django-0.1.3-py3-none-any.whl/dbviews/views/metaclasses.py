e0b0438e14982fde4e805413aefbf191d88937e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 05:05:15 +0000

    Add package dbviews-django/0.1.3/dbviews_django-0.1.3-py3-none-any.whl

---
+++ packages/dbviews-django/dbviews_django-0.1.3-py3-none-any.whl/dbviews/views/metaclasses.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from dbviews.views.fields import QueryField
+
+
+class ViewModelMeta(models.base.ModelBase):
+    """
+    This metaclass is used to validate whether the view and
+    materialized has `view_query` field
+    """
+
+    def __new__(cls, name, bases, attrs, **kwargs):
+        new_class = super().__new__(cls, name, bases, attrs, **kwargs)
+
+        if attrs.pop("_skip_meta_validations", False):
+            return new_class
+        if "view_query" not in attrs:
+            raise FileNotFoundError(
+                "`view_query` field value is required to create a view and materialized view"
+            )
+
+        if not isinstance(attrs["view_query"], QueryField):
+            raise TypeError("`view_query` must be instance of `QueryField`")
+
+        if not isinstance(attrs["view_query"].query, str):
+            raise TypeError("Provide sql query as query value in `view_query`")
+
+        return new_class
