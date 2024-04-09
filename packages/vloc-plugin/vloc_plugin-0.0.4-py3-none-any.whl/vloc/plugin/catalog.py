b7d003d83ad206c414d963b7e396150b2382f88a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 16:06:11 +0000

    Add package vloc-plugin/0.0.4/vloc_plugin-0.0.4-py3-none-any.whl

---
+++ packages/vloc-plugin/vloc_plugin-0.0.4-py3-none-any.whl/vloc/plugin/catalog.py
@@ -0,0 +1,18 @@
+from vloc.plugin.__info__ import DetectInfo
+
+
+class action:
+    ...
+    
+def slide_up(start: float = 0.8, end: float = 0.1, duration=0):
+    ...
+
+
+def slide_down(start: float = 0.1, end: float = 0.8, duration=0):
+    ...
+
+
+def slide_e2e(start: DetectInfo, end: DetectInfo, duration=1000):
+    ...
+
+
