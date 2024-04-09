50ef31cd99dcc5bfe8adb34a3f399fbdb848720f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 04:05:42 +0000

    Add package upp/0.2.1/upp-0.2.1.tar.gz

---
+++ packages/upp/upp-0.2.1.tar.gz/upp-0.2.1/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='upp',
+    version='0.2.1',
+    author='Samir Ibradžić',
+    description='Uplift Power Play',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/sibradzic/upp',
+    package_dir={'': 'src'},
+    packages=['upp', 'upp/atom_gen'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7, <4',
+    install_requires=[
+        'click',
+        'setuptools'
+    ],
+    entry_points={
+        'console_scripts': [
+            'upp=upp.upp:main',
+        ],
+    },
+)
