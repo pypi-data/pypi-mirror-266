ece5ece7cd8575a9d50bd7be0cf9f7245548758c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 04:05:39 +0000

    Add package nextconsole/0.0.7/nextconsole-0.0.7-py3-none-any.whl

---
+++ packages/nextconsole/nextconsole-0.0.7-py3-none-any.whl/nextconsole/command.py
@@ -0,0 +1,72 @@
+import os
+import json
+
+# import requests
+# response = requests.post("http://192.168.6.182:8228/api/nc", data=json.dumps({"chat":"部署nginx"}), headers={"Content-Type": "application/json"}, stream=True)
+
+
+# strs = []
+# last_lines = 0
+# for c in response.iter_content(chunk_size=10):
+#     c = c.decode()
+#     for _ in range(last_lines):
+#         print(u'\u001b[1A', end='')
+#     if c is not None:
+#         strs.append(c)
+#     md = Markdown("".join(strs))
+#     console.print(md, end='')
+#     if len(md.parsed) > 0:
+#         last_lines = md.parsed[0].map[1] + 1
+
+        
+        
+        
+def cmd():
+    import requests
+    import json
+    from rich.console import Console
+    from rich.markdown import Markdown
+    from rich.prompt import Prompt
+    import re
+    import sys
+    if len(sys.argv) < 2:
+        print("NextConsole 未识别出需求呢!")
+        exit()
+    prompt = sys.argv[1]
+    console = Console(soft_wrap=True)
+
+    content = ""
+    strs = []
+    last_lines = 0
+    response = requests.post("http://nc.turing.fun:8228/api/nc", data=json.dumps({"chat": prompt}), headers={"Content-Type": "application/json"}, stream=True)
+    console.print("命令如下\n")
+    for i, c in enumerate(response.iter_content(chunk_size=10)):
+        if i <= 3:
+            print(u'\u001b[1A', end='')
+        c = c.decode()
+
+        if c is not None:
+            strs.append(c)
+        content = "".join(strs)
+        md = Markdown(content)
+        console.print(md, end='')
+        if len(md.parsed) > 0:
+            last_lines = md.parsed[0].map[1] + 1
+        for _ in range(last_lines):
+            print(u'\u001b[1A', end='')
+
+
+    # content = chat(prompt)
+    cmd = "\n".join(re.findall("```shell\n([\s\S]*?)```", 
+                             content))
+
+
+    console = Console()
+    md = Markdown(content)
+    console.print(md)
+    sure = Prompt.ask("Exec these commands?", choices=["Yes(Default)", "No", "N", "n"], default="Yes")
+    if sure == 'Yes':
+        os.system(cmd)
+
+if __name__ == "__main__":
+    cmd()
