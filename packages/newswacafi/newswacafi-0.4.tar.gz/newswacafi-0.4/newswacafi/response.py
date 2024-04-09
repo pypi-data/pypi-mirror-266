1c9eb767cd3ee390aebd32e33596657f767d70a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 16:06:05 +0000

    Add package newswacafi/0.4/newswacafi-0.4.tar.gz

---
+++ packages/newswacafi/newswacafi-0.4.tar.gz/newswacafi-0.4/newswacafi/response.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+@dataclass
+class NewswacafiNews:
+    Title: str
+    title: str
+    date: str
+    country: str
+    countrysort: str
+    description: str
+    content: str
+    category: str
+    comments: str
+    ncomments: str
+    link: str
+    username: str
+    name: str
+    userid: str
+    source: str
+    urlimage: str
+    mentions: str
+
+@dataclass
+class NewsWacafiResponse:
+    status: str
+    data: list[NewswacafiNews]
+
+
+
