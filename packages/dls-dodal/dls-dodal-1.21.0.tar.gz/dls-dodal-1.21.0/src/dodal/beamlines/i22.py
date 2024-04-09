4f813d3a4c7c943dcc43d8afb4c2f2c92fe39a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 17:04:42 +0000

    Add package dls-dodal/1.21.0/dls-dodal-1.21.0.tar.gz

---
+++ packages/dls-dodal/dls-dodal-1.21.0.tar.gz/dls-dodal-1.21.0/src/dodal/beamlines/i22.py
@@ -0,0 +1,37 @@
+from dodal.beamlines.beamline_utils import device_instantiation, get_directory_provider
+from dodal.beamlines.beamline_utils import set_beamline as set_utils_beamline
+from dodal.devices.tetramm import TetrammDetector
+from dodal.log import set_beamline as set_log_beamline
+from dodal.utils import get_beamline_name
+
+BL = get_beamline_name("i22")
+set_log_beamline(BL)
+set_utils_beamline(BL)
+
+
+def i0(
+    wait_for_connection: bool = True,
+    fake_with_ophyd_sim: bool = False,
+) -> TetrammDetector:
+    return device_instantiation(
+        TetrammDetector,
+        "i0",
+        "-EA-XBPM-02",
+        wait_for_connection,
+        fake_with_ophyd_sim,
+        directory_provider=get_directory_provider(),
+    )
+
+
+def it(
+    wait_for_connection: bool = True,
+    fake_with_ophyd_sim: bool = False,
+) -> TetrammDetector:
+    return device_instantiation(
+        TetrammDetector,
+        "it",
+        "-EA-TTRM-02",
+        wait_for_connection,
+        fake_with_ophyd_sim,
+        directory_provider=get_directory_provider(),
+    )
