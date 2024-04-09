1d9d97bff9b8d1cac680a0469e4d1381f3b7e5ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 16:05:53 +0000

    Add package MeMeST/2024.4.9.1/MeMeST-2024.4.9.1.tar.gz

---
+++ packages/MeMeST/MeMeST-2024.4.9.1.tar.gz/MeMeST-2024.4.9.1/setup.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import datetime
+from setuptools import setup
+from setuptools import find_packages
+
+
+def get_today_date():
+    today = datetime.date.today()
+    return today.strftime("%Y.%m.%d")
+
+
+VERSION = get_today_date().replace(".0", ".") + '.1'
+
+if sys.argv[1] == "publish":
+    os.system("rm -rf dist/")
+    os.system("python3 setup.py sdist")
+    os.system("python3 setup.py bdist_wheel")
+    os.system("twine upload dist/*")
+else:
+    setup(
+        name='MeMeST',
+        version=VERSION,
+        author='Xin-Xin Ma',
+        description="Multi-Repository Sync Tool",
+        description_content_type="text/markdown",
+        long_description_content_type="text/markdown",
+        long_description=open("./Readme.md", 'r').read(),
+        packages=find_packages(),
+        data_files=[("", ["LICENSE"])],
+        license="GPL",
+        # project_urls={
+        # 'Source': 'https://github.com/xxmawhu/LinuxRecycle',
+        # },
+        entry_points={'console_scripts': ['memest=mest.main:main']},
+        install_requires=['termcolor', 'loguru', "psutil"]
+    )
