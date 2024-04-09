7741129d9b1ac788dc0422e47006969100d768a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 15:04:16 +0000

    Add package anacondas/0.0.2/anacondas-0.0.2.tar.gz

---
+++ packages/anacondas/anacondas-0.0.2.tar.gz/anacondas-0.0.2/src/anacondas/FileSystemModel/_dir.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+import os
+import pywintypes
+import win32security
+
+
+class Dir:
+
+    def __init__(self, path: str):
+        try:
+            file_security = win32security.GetFileSecurity(path, win32security.OWNER_SECURITY_INFORMATION)
+            security_descriptor_owner = file_security.GetSecurityDescriptorOwner()
+            account, domain, _ = win32security.LookupAccountSid(None, security_descriptor_owner)
+            self.owner: str = domain + '\\' + account
+        except pywintypes.error:
+            self.owner: str = '...'
+        self.name: str = os.path.basename(path)
+        self.ctime: int = int(os.path.getctime(path))
+        self.mtime: int = int(os.path.getmtime(path))
+        self.readable: bool = os.access(path, os.R_OK)
+        self.writable: bool = os.access(path, os.W_OK)
+        self.executable: bool = os.access(path, os.X_OK)
+
+    def __str__(self):
+        return (f'Dir('
+                f'name={self.name!r}, '
+                f'ctime={self.ctime!r}, '
+                f'mtime={self.mtime!r}, '
+                f'owner={self.owner!r}, '
+                f'readable={self.readable!r}, '
+                f'writable={self.writable!r}, '
+                f'executable={self.executable!r})')
