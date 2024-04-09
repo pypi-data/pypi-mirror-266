37fa208047e95d5a356e8788cfaec7ec85a0e94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 14:04:16 +0000

    Add package hebill/1.1.1/hebill-1.1.1-py3-none-any.whl

---
+++ packages/hebill/hebill-1.1.1-py3-none-any.whl/hebill/__init__.py
@@ -0,0 +1,19 @@
+from . import dir
+from . import file
+from . import html
+from . import image
+from . import mysql
+from . import pdf
+from . import pypi
+from . import sys
+from . import url
+from .file.core import File
+from .dir.core import Dir
+
+app_version = '1.1.1'
+app_name = 'hebill'
+app_description = 'Python Hebill'
+app_url = 'http://www.hebill.net/'
+app_author = 'He Bill'
+app_author_email = 'hebill@hebill.net'
+app_author_url = 'http://www.hebill.net/'
