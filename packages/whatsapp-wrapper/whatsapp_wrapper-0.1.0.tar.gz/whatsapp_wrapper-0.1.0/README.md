66c63d5bb6ca9c8f0a9cea31f522a64159d27961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 05:04:51 +0000

    Add package whatsapp-wrapper/0.1.0/whatsapp_wrapper-0.1.0.tar.gz

---
+++ packages/whatsapp-wrapper/whatsapp_wrapper-0.1.0.tar.gz/whatsapp_wrapper-0.1.0/README.md
@@ -0,0 +1,14 @@
+# whatsapp-wrapper
+
+Integration layer for WhatsApp Cloud API with Firestore for easy message storage and management.
+
+## WhatsApp Documentation
+
+- [API Endpoint References](https://developers.facebook.com/docs/whatsapp/cloud-api/reference)
+    - [Messages](https://developers.facebook.com/docs/whatsapp/cloud-api/reference/messages)
+- [API Error Codes](https://developers.facebook.com/docs/whatsapp/cloud-api/support/error-codes)
+- [Override Callback URL](https://developers.facebook.com/docs/whatsapp/embedded-signup/webhooks/#overriding-the-callback-url)
+
+## Graph API Documentation
+
+- [WhatsApp Business Account](https://developers.facebook.com/docs/graph-api/reference/whats-app-business-account/)
---
+++ packages/whatsapp-wrapper/whatsapp_wrapper-0.1.0.tar.gz/whatsapp_wrapper-0.1.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='whatsapp_wrapper',
+    version='0.1.0',
+    author='Antonio Ventilii',
+    author_email='antonioventilii@gmail.com',
+    license='MIT',
+    description='Integration layer for WhatsApp Cloud API with Firestore for easy message storage and management.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/AntonioVentilii/whatsapp-wrapper',
+    project_urls={
+        'Source Code': 'https://github.com/AntonioVentilii/whatsapp-wrapper',
+        'Issue Tracker': 'https://github.com/AntonioVentilii/whatsapp-wrapper/issues',
+    },
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    keywords='WhatsApp, Firestore, API, integration, messaging',
+    install_requires=[
+        'requests',
+        'firestore-wrapper'
+    ],
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
