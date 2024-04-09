f67a02c47b7f773cdf4108cf426f20e69771db24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 05:05:02 +0000

    Add package MorS-package/0.1.0/MorS_package-0.1.0.tar.gz

---
+++ packages/MorS-package/MorS_package-0.1.0.tar.gz/MorS_package-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='MorS_package',
+version='0.1.0',
+author='MorS',
+author_email='derictor228@gmail.com',
+description='Shit for tea.xyz',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
