6e154e996b9409f2d436bf6d2a7fde94f09d74d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 05:04:48 +0000

    Add package mtok/4.1.4/mtok-4.1.4.tar.gz

---
+++ packages/mtok/mtok-4.1.4.tar.gz/mtok-4.1.4/src/mtok/helpers.py
@@ -0,0 +1,46 @@
+import json
+from os import environ
+from pathlib import Path
+
+import requests
+
+class Const :
+    # local GitHub token absolute filepath $HOME/.gt.json, I assume it is in the home directory
+    lg = Path(environ['HOME']) / '.gt.json'
+    # GitHub username
+    gu = 'imahdimir'
+
+c = Const()
+
+def get_gt() :
+    with open(c.lg , 'r') as fi :
+        gt = json.load(fi)['gt']
+    return gt
+
+def get_all_tokens_fr_tokens_repo() -> dict :
+    """ Gets all tokens from the private tokens repo """
+    gt = get_gt()
+
+    trg_repo = 'tokens'
+    br = 'main'
+    fn = 'main.json'
+    url = ret_github_url_for_private_access_to_file(gt , trg_repo , br , fn)
+
+    _hdr = {
+            'Accept' : 'application/vnd.github.v3+json'
+            }
+    print(url)
+    r = requests.get(url , headers = _hdr)
+    print(r.status_code)
+    j = r.json()
+
+    return j
+
+def ret_github_url_for_private_access_to_file(tok , trg_repo , brnch , fn) :
+    return f'https://{c.gu}:{tok}@raw.githubusercontent.com/{c.gu}/{trg_repo}/{brnch}/{fn}'
+
+##
+j = json.load(open(c.lg , 'r'))
+
+##
+j['gt']
