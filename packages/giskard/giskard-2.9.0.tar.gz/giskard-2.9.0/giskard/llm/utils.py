e171cfa06240ce88e4a951134e6dee94b6944aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 04:05:30 +0000

    Add package giskard/2.9.0/giskard-2.9.0.tar.gz

---
+++ packages/giskard/giskard-2.9.0.tar.gz/giskard-2.9.0/giskard/llm/utils.py
@@ -0,0 +1,47 @@
+from typing import Optional
+
+from ..models.base.model import BaseModel
+from .generators.base import BaseDataGenerator
+
+
+def generate_test_dataset(
+    model: BaseModel,
+    num_samples: int = 10,
+    prompt: Optional[str] = None,
+    temperature=0.5,
+    column_types=None,
+    rng_seed: int = 1729,
+):
+    """Generates a synthetic test dataset using an LLM.
+
+    Parameters
+    ----------
+    model : BaseModel
+        The model to generate a test dataset for.
+    num_samples : int
+        The number of samples to generate, by default 10.
+    prompt : Optional[str]
+        The prompt to use for the generation, if not specified a default will be used.
+    temperature : Optional[float]
+        The temperature to use for the generation, by default 0.5.
+    column_types :
+        (Default value = None)
+    rng_seed :
+        (Default value = 1729), seed used to generate completion
+
+    Returns
+    -------
+    Dataset
+        The generated dataset.
+
+    Raises
+    ------
+    LLMGenerationError
+        If the generation fails.
+
+    See also
+    --------
+    :class:`giskard.llm.generators.BaseDataGenerator`
+    """
+    generator = BaseDataGenerator(prompt=prompt, llm_temperature=temperature, rng_seed=rng_seed)
+    return generator.generate_dataset(model, num_samples, column_types)
