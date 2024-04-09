f743ae7329328c4c1291e60f5f97d41cf4c2ce63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 17:04:38 +0000

    Add package mlcvzoo-base/5.7.2/mlcvzoo_base-5.7.2-py3-none-any.whl

---
+++ packages/mlcvzoo-base/mlcvzoo_base-5.7.2-py3-none-any.whl/mlcvzoo_base/__init__.py
@@ -0,0 +1,8 @@
+# Copyright Open Logistics Foundation
+#
+# Licensed under the Open Logistics Foundation License 1.3.
+# For details on the licensing terms, see the LICENSE file.
+# SPDX-License-Identifier: OLFL-1.3
+
+__version__ = "5.7.2"
+__license__ = "OLFL-1.3"
