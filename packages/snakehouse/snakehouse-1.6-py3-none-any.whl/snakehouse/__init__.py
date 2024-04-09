adb7135e96ea9b0ff524b1b27fdf57a0ea2d7c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 16:06:26 +0000

    Add package snakehouse/1.6/snakehouse-1.6-py3-none-any.whl

---
+++ packages/snakehouse/snakehouse-1.6-py3-none-any.whl/snakehouse/__init__.py
@@ -0,0 +1,10 @@
+import pkg_resources
+from .build import build
+from .multibuild import Multibuild, find_all
+from .faster_builds import monkey_patch_parallel_compilation
+from .requirements import read_requirements_txt, find_c, find_pyx_and_c, find_pyx
+
+try:
+    __version__ = pkg_resources.require('snakehouse')[0].version
+except pkg_resources.DistributionNotFound:
+    __version__ = '1.6'
