32d1bbb9c699bcf43ff1fc28ed4fc1388a181175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 03:04:05 +0000

    Add package ign8/4.8.28/ign8-4.8.28.tar.gz

---
+++ packages/ign8/ign8-4.8.28.tar.gz/ign8-4.8.28/src/ign8/main.py
@@ -0,0 +1,85 @@
+from .common import prettylog
+import os
+import subprocess
+import time
+import sys
+import redis
+
+def getenv(key, default):
+    if key in os.environ:
+        return os.environ[key]
+    else:
+        return default
+
+def main():
+    prettylog("INFO", )
+    return True
+
+def in_venv():
+    return sys.prefix != sys.base_prefix
+
+def init_service():
+    prettylog("INFO", "Init service")
+
+    servicefile = "\
+[Unit]\
+Description=Ign8 Service\
+After=network.target\
+\
+[Service]\
+Type=simple\
+User=ign8\
+ExecStart=/usr/local/bin/ign8 serve\
+Restart=always\
+\
+[Install]\
+WantedBy=multi-user.target\
+"
+    servicefilename = "/etc/systemd/system/ign8.service"
+    write_string_to_file(servicefilename, servicefile )
+    subprocess.run(["systemctl", "daemon-reload"])
+    subprocess.run(["systemctl", "enable", "ign8"])
+
+def check_redis():
+    prettylog("INFO", "Check redis")
+    try:
+        r = redis.Redis(host='localhost', port=6379, db=0)
+        r.ping()
+    except redis.ConnectionError:
+        prettylog("ERROR", "Redis is not running")
+        prettylog("INFO", "Starting Redis")
+        subprocess.run(["systemctl", "start", "redis"])
+    else:
+        prettylog("INFO", "Redis is running")
+
+    
+    
+
+
+
+def write_string_to_file(filename, content):
+    with open(filename, 'w') as file:
+        file.write(content)
+    
+    # Create and overwrite the service file"
+
+
+
+
+
+
+def start_service():
+    prettylog("INFO", "start service")
+    subprocess.run(["systemctl", "start", "ign8"])
+
+
+def stop_service():
+    prettylog("INFO", "stop service")
+    subprocess.run(["systemctl", "stop", "ign8"])
+
+def serve():
+    init_service()
+    while True:
+        prettylog("info" , "Start loop")
+        time.sleep(1)
+        check_redis()
