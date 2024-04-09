07f79b6d341f1a0cd4dd99f00a6780dfb980edca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 14:04:07 +0000

    Add package sagemath-standard/10.4b2/sagemath-standard-10.4b2.tar.gz

---
+++ packages/sagemath-standard/sagemath-standard-10.4b2.tar.gz/sagemath-standard-10.4b2/sage/schemes/hyperelliptic_curves/hyperelliptic_rational_field.py
@@ -0,0 +1,55 @@
+"""
+Hyperelliptic curves over the rationals
+"""
+#*****************************************************************************
+#  Copyright (C) 2006 David Kohel <kohel@maths.usyd.edu>
+#  Distributed under the terms of the GNU General Public License (GPL)
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
+import sage.rings.abc
+
+from sage.rings.padics.factory import Qp as pAdicField
+
+from sage.schemes.curves.projective_curve import ProjectivePlaneCurve_field
+
+from . import hyperelliptic_generic
+
+
+class HyperellipticCurve_rational_field(hyperelliptic_generic.HyperellipticCurve_generic,
+                                        ProjectivePlaneCurve_field):
+
+    def matrix_of_frobenius(self, p, prec=20):
+
+        # BUG: should get this method from HyperellipticCurve_generic
+        def my_chage_ring(self, R):
+            from .constructor import HyperellipticCurve
+            f, h = self._hyperelliptic_polynomials
+            y = self._printing_ring.gen()
+            x = self._printing_ring.base_ring().gen()
+            return HyperellipticCurve(f.change_ring(R), h, "%s,%s" % (x,y))
+
+        import sage.schemes.hyperelliptic_curves.monsky_washnitzer as monsky_washnitzer
+        if isinstance(p, (sage.rings.abc.pAdicField, sage.rings.abc.pAdicRing)):
+            K = p
+        else:
+            K = pAdicField(p, prec)
+        frob_p, forms = monsky_washnitzer.matrix_of_frobenius_hyperelliptic(my_chage_ring(self, K))
+        return frob_p
+
+    def lseries(self, prec=53):
+        """
+        Return the L-series of this hyperelliptic curve of genus 2.
+
+        EXAMPLES::
+
+            sage: x = polygen(QQ, 'x')
+            sage: C = HyperellipticCurve(x^2+x, x^3+x^2+1)
+            sage: C.lseries()
+            PARI L-function associated to Hyperelliptic Curve
+            over Rational Field defined by y^2 + (x^3 + x^2 + 1)*y = x^2 + x
+        """
+        from sage.lfunctions.pari import LFunction, lfun_genus2
+        L = LFunction(lfun_genus2(self), prec=prec)
+        L.rename('PARI L-function associated to %s' % self)
+        return L
