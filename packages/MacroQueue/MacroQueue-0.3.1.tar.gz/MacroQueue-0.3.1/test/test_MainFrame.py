007cc85deec4289f69c5395e677a70a6b8c359df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 04:05:47 +0000

    Add package MacroQueue/0.3.1/MacroQueue-0.3.1.tar.gz

---
+++ packages/MacroQueue/MacroQueue-0.3.1.tar.gz/MacroQueue-0.3.1/test/test_MainFrame.py
@@ -0,0 +1,23 @@
+import wx
+import unittest
+import multiprocessing as mp
+import sys
+import os
+application_path = os.path.dirname(__file__)
+sys.path.append(os.path.realpath(application_path+'\\..\\MacroQueue\\'))
+from MacroQueue import MacroQueue
+
+class MainFrame_test(unittest.TestCase):
+    def test_GUI(self):
+        app = wx.App() 
+        MyMainFrame = MacroQueue(test=True)
+        MyMainFrame.CheckQueue(event=None)
+        MyMainFrame.IdleLoop(event=None)
+        MyMainFrame.Close()
+
+        
+        pass
+    
+
+if __name__ == '__main__':
+    unittest.main()
