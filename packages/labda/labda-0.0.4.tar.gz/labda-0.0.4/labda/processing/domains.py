6a1b01a6c3ca03e3593cda5331942b420e459187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 03:04:32 +0000

    Add package labda/0.0.4/labda-0.0.4.tar.gz

---
+++ packages/labda/labda-0.0.4.tar.gz/labda-0.0.4/labda/processing/domains.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+from ..utils import columns_not_exists
+
+# TODO: Possibility to specify if point should be INSIDE or OUTSIDE the domain. Maybe?
+# TODO: Get domains
+
+
+def detect_domains(
+    df: pd.DataFrame,
+    domains: pd.DataFrame,
+    *,
+    overwrite: bool = False,
+) -> pd.DataFrame:
+    df = df.copy()
+    unique_domains = domains.groupby("domain")
+
+    for domain in unique_domains:
+        name = f"domain_{domain[0]}"
+
+        columns_not_exists(df, [name], overwrite=overwrite)
+
+        domain_df = domain[1]
+        df[name] = False
+
+        for interval in domain_df.itertuples():
+            df.loc[
+                (df.index >= interval.start) & (df.index <= interval.end),
+                name,
+            ] = True
+
+    return df
