426c6e70c0139b47954e6fcd3877a34205475e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 05:05:08 +0000

    Add package netbox-healthcheck-plugin/0.1.4/netbox-healthcheck-plugin-0.1.4.tar.gz

---
+++ packages/netbox-healthcheck-plugin/netbox-healthcheck-plugin-0.1.4.tar.gz/netbox-healthcheck-plugin-0.1.4/netbox_healthcheck_plugin/navigation.py
@@ -0,0 +1,11 @@
+from extras.plugins import PluginMenuButton, PluginMenuItem
+from utilities.choices import ButtonColorChoices
+
+
+menu_items = (
+    PluginMenuItem(
+        link='plugins:netbox_healthcheck_plugin:healthcheck_list',
+        link_text='HealthCheck',
+        buttons=None
+    ),
+)
