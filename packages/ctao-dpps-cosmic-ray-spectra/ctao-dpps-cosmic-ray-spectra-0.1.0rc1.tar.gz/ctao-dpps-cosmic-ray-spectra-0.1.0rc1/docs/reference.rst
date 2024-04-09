4f5967753e14edfc42610abefd59399b2808f86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 03:04:03 +0000

    Add package ctao-dpps-cosmic-ray-spectra/0.1.0rc1/ctao-dpps-cosmic-ray-spectra-0.1.0rc1.tar.gz

---
+++ packages/ctao-dpps-cosmic-ray-spectra/ctao-dpps-cosmic-ray-spectra-0.1.0rc1.tar.gz/ctao-dpps-cosmic-ray-spectra-0.1.0rc1/docs/reference.rst
@@ -0,0 +1,7 @@
+API Reference
+=============
+
+.. currentmodule:: ctao_cr_spectra
+
+.. automodule:: ctao_cr_spectra
+   :members:
---
+++ packages/ctao-dpps-cosmic-ray-spectra/ctao-dpps-cosmic-ray-spectra-0.1.0rc1.tar.gz/ctao-dpps-cosmic-ray-spectra-0.1.0rc1/src/ctao_cr_spectra/__init__.py
@@ -0,0 +1,6 @@
+"""Reference Cosmic Ray Spectra definitions."""
+from .version import __version__
+
+__all__ = [
+    "__version__",
+]
