b37efc4c08ace379e8aa93857416f17456a55ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 05:06:22 +0000

    Add package scematk/0.0.2rc7.post1/scematk-0.0.2rc7.post1.tar.gz

---
+++ packages/scematk/scematk-0.0.2rc7.post1.tar.gz/scematk-0.0.2rc7.post1/src/scematk/process/dask/_rechunk.py
@@ -0,0 +1,10 @@
+from .._process import Process
+from ...image._image import Image
+
+class Rechunk(Process):
+    def __init__(self, chunks: tuple):
+        super().__init__(f'Rechunk image to {str(chunks)}')
+        self.chunks = chunks
+
+    def run(self, image: Image) -> Image:
+        return image.rechunk(self.chunks)
