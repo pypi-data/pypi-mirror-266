216e3431c4ca517c33939fdeb1af0dc22ce9460f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 06:04:23 +0000

    Add package selenium-network-intercept/1.0.0/selenium-network-intercept-1.0.0.tar.gz

---
+++ packages/selenium-network-intercept/selenium-network-intercept-1.0.0.tar.gz/selenium-network-intercept-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+file = open('README.md', 'r',encoding='utf-8')
+readme = file.read()
+
+
+setup(
+    name='selenium-network-intercept',
+    version='1.0.0',
+    license='MIT License',
+    author='Alexandre Mariano',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author_email='alexandre_mariano@hotmail.com.br',
+    keywords=['selenium', 'network', 'intercept','http','requests','selenium network intercept','selenium intercept'],
+    description='Interceptador de requisições http não oficial do selenium 4',
+    packages=['selenium_network_intercept'],
+    install_requires=[]
+)
+
+file.close()
