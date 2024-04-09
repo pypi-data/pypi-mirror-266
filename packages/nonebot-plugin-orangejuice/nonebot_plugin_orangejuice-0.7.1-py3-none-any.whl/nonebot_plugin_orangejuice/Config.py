538a17bbf609a3c2ab072dfc8b43a1c20e37a660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 15:04:18 +0000

    Add package nonebot-plugin-orangejuice/0.7.1/nonebot_plugin_orangejuice-0.7.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-orangejuice/nonebot_plugin_orangejuice-0.7.1-py3-none-any.whl/nonebot_plugin_orangejuice/Config.py
@@ -0,0 +1,22 @@
+from typing import Optional, List, Literal
+from pydantic import BaseModel
+
+try:
+    # pydantic v2
+    from nonebot import get_plugin_config
+except ImportError:
+    # pydantic v1
+    from nonebot import get_driver
+
+class Config(BaseModel):
+    oj_data_path: Optional[str] = 'data/100oj'
+    match_socre: Optional[int] = 75
+
+plugin_config = None
+
+try:
+    # pydantic v2
+    plugin_config = get_plugin_config(Config)
+except:
+    # pydantic v1
+    plugin_config = Config.parse_obj(get_driver().config)
