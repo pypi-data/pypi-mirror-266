3e3993fd9b853487880805fbfadfab3402e68c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 03:04:49 +0000

    Add package donetools/0.0.2/donetools-0.0.2-py3-none-any.whl

---
+++ packages/donetools/donetools-0.0.2-py3-none-any.whl/donetools/path.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+
+from donetools import info
+
+def norm(path: str) -> str:
+    return os.path.normcase(os.path.normpath(path))
+
+def join(*paths: str) -> str:
+    return norm(os.path.join(*map(norm, paths)))
+
+def dirname(path: str) -> str:
+    return os.path.dirname(norm(path))
+
+def basename(path: str) -> str:
+    return os.path.splitext(os.path.basename(norm(path)))[0]
+
+def isdir(path: str) -> bool:
+    return os.path.isdir(norm(path))
+
+def isfile(path: str) -> bool:
+    return os.path.isfile(norm(path))
+
+def collide(path: str) -> bool:
+    path = norm(path)
+    return isfile(path) or (isdir(path) and len(os.listdir(path)) > 0)
+
+def remove(path: str) -> None:
+    path = norm(path)
+    shutil.rmtree(path) if os.path.isdir(path) else os.unlink(path)
+
+def removeall(*paths: str) -> None:
+    for path in paths:
+        remove(path)
+
+def reconcile(*paths: str, override: bool = False) -> None:
+    conflicts = list(filter(collide, paths))
+    if len(conflicts) > 0:
+        prompt = f"Agree to {info.warn('remove')} conflicts?" + 2*os.linesep
+        if override or info.dilemma(prompt + info.indent(os.linesep.join(conflicts))):
+            removeall(*conflicts)
+        else: exit()
+
+def secure(*paths: str, override: bool = False) -> None:
+    reconcile(*paths, override=override)
+    for path in paths:
+        os.makedirs(path if isdir(path) else dirname(path), exist_ok=True)
