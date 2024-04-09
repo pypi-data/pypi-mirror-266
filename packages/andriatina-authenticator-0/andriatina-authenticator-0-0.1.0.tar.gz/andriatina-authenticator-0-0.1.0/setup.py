2b6f321170d1ae3527492f72c69255a11d2ed25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 03:04:10 +0000

    Add package andriatina-authenticator-0/0.1.0/andriatina-authenticator-0-0.1.0.tar.gz

---
+++ packages/andriatina-authenticator-0/andriatina-authenticator-0-0.1.0.tar.gz/andriatina-authenticator-0-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='andriatina-authenticator-0',
+    version='0.1.0',
+    packages=['andriatina_auth'],
+    install_requires=[
+        'pyotp',  # Exemple de dépendance
+    ],
+    author='andriatina',
+    author_email='andriatina@aims.ac.za',
+    description='authentificator',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    #url='https://lien_vers_votre_bibliotheque',
+    classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ],
+    license='Universite of Antananarivo',
+)
