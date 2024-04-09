e0f0b5216e959cebc5718e681786eee336a9c35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 03:04:35 +0000

    Add package amazon-ads-api-connector/0.1.4/amazon_ads_api_connector-0.1.4-py3-none-any.whl

---
+++ packages/amazon-ads-api-connector/amazon_ads_api_connector-0.1.4-py3-none-any.whl/amazon_ads_api_connector/__init__.py
@@ -0,0 +1,10 @@
+from .report_types import (
+    Report,
+    CampaignsReport,
+    TargetingReport,
+    SearchTermReport,
+    AdvertisedProductReports,
+    PurchasedProductReport,
+)
+
+from .amazon_ads_api_connector import AmazonAdsAPIConnector
