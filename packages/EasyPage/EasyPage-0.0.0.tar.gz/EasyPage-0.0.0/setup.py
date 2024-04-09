349fceed000701e02737af04e95fe4cf2bd10bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 16:05:50 +0000

    Add package EasyPage/0.0.0/EasyPage-0.0.0.tar.gz

---
+++ packages/EasyPage/EasyPage-0.0.0.tar.gz/EasyPage-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EasyPage',
+    version='0.0.0',
+    author='leviathangk',
+    author_email='1015295213@qq.com',
+    description='基于 CDP 协议，简洁快速的浏览器控制 API',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
