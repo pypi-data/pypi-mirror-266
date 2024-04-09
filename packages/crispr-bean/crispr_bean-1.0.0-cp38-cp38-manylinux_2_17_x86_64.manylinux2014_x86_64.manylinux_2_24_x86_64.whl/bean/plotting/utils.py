297f9ba4be5725908d5185560022b16c1fc5c2f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 06:04:21 +0000

    Add package crispr-bean/1.0.0/crispr_bean-1.0.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_24_x86_64.whl

---
+++ packages/crispr-bean/crispr_bean-1.0.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_24_x86_64.whl/bean/plotting/utils.py
@@ -0,0 +1,56 @@
+import argparse
+
+
+def parse_args(parser=None):
+    if parser is None:
+        parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "bdata_path", help="Path to the ReporterScreen object to run QC on", type=str
+    )
+    parser.add_argument(
+        "-o",
+        "--output-prefix",
+        help="Output prefix of editing pattern report (prefix.html, prefix.ipynb). If not provided, base name of `bdata_path` is used.",
+        type=str,
+    )
+    parser.add_argument(
+        "--replicate-col",
+        help="Column name in `bdata.samples` that describes replicate ID.",
+        type=str,
+        default="replicate",
+    )
+    parser.add_argument(
+        "--condition-col",
+        help="Column name in `bdata.samples` that describes experimental condition. (sorting bin, time, etc.)",
+        type=str,
+        default="bin",
+    )
+    parser.add_argument(
+        "--pam-col",
+        help="Column name describing PAM of each gRNA in `bdata.guides`.",
+        type=str,
+        default=None,
+    )
+    parser.add_argument(
+        "--control-condition",
+        help="Control condition where editing preference would be profiled at. Pre-filters data where `bdata.samples[condition_col] == control_condition`.",
+        type=str,
+        default="bulk",
+    )
+    parser.add_argument(
+        "-w",
+        "--window-length",
+        help="Window length of editing window of maximal editing efficiency to be identified. This window is used to quantify context specificity within the window.",
+        type=int,
+        default=6,
+    )
+
+    return parser
+
+
+def check_args(args):
+    if args.window_length < 1:
+        raise ValueError(f"window_length {args.window_length} is too small.")
+    if args.window_length > 20:
+        raise ValueError(f"window_length {args.window_length} is too large.")
+    return args
