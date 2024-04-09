bd0715c0e25f680d5dfa9b505e0a1f3028f96a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 04:05:42 +0000

    Add package hellopkgismail/0.0.2/hellopkgismail-0.0.2.tar.gz

---
+++ packages/hellopkgismail/hellopkgismail-0.0.2.tar.gz/hellopkgismail-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'A basic hello package'
+LONG_DESCRIPTION = 'A basic hello package with a function that prints hello!'
+
+# Setting up
+setup(
+    name="hellopkgismail",
+    version=VERSION,
+    author="Ismail Chbiki",
+    author_email="ismailchbiki.nl@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'hello', 'hello package'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
