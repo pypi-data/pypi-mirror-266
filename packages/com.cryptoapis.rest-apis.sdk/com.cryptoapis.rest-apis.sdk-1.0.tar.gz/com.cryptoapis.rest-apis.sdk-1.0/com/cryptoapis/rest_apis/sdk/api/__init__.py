806a59967d3038210316b1c8c3667ffbba2aa56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 16:05:56 +0000

    Add package com.cryptoapis.rest-apis.sdk/1.0/com.cryptoapis.rest-apis.sdk-1.0.tar.gz

---
+++ packages/com.cryptoapis.rest-apis.sdk/com.cryptoapis.rest-apis.sdk-1.0.tar.gz/com.cryptoapis.rest-apis.sdk-1.0/com/cryptoapis/rest_apis/sdk/api/__init__.py
@@ -0,0 +1,22 @@
+# flake8: noqa
+
+# import apis into api package
+from com.cryptoapis.rest_apis.sdk.api.assets_api import AssetsApi
+from com.cryptoapis.rest_apis.sdk.api.automatic_coins_forwarding_api import AutomaticCoinsForwardingApi
+from com.cryptoapis.rest_apis.sdk.api.automatic_tokens_forwarding_api import AutomaticTokensForwardingApi
+from com.cryptoapis.rest_apis.sdk.api.callback_data_api import CallbackDataApi
+from com.cryptoapis.rest_apis.sdk.api.create_subscriptions_for_api import CreateSubscriptionsForApi
+from com.cryptoapis.rest_apis.sdk.api.create_subscriptions_for_api import CreateSubscriptionsForApi
+from com.cryptoapis.rest_apis.sdk.api.exchange_rates_api import ExchangeRatesApi
+from com.cryptoapis.rest_apis.sdk.api.features_api import FeaturesApi
+from com.cryptoapis.rest_apis.sdk.api.generating_api import GeneratingApi
+from com.cryptoapis.rest_apis.sdk.api.hd_wallets_api import HDWalletsApi
+from com.cryptoapis.rest_apis.sdk.api.informative_api import InformativeApi
+from com.cryptoapis.rest_apis.sdk.api.manage_subscriptions_api import ManageSubscriptionsApi
+from com.cryptoapis.rest_apis.sdk.api.metadata_api import MetadataApi
+from com.cryptoapis.rest_apis.sdk.api.tokens_api import TokensApi
+from com.cryptoapis.rest_apis.sdk.api.transactions_api import TransactionsApi
+from com.cryptoapis.rest_apis.sdk.api.unified_endpoints_api import UnifiedEndpointsApi
+from com.cryptoapis.rest_apis.sdk.api.xrp_ripple_api import XRPRippleApi
+from com.cryptoapis.rest_apis.sdk.api.internal_api import InternalApi
+
