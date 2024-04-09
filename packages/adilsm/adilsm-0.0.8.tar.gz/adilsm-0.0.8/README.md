b3a2195d9bbc1f60c8e99f51f7ce6090aa43cb52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 17:04:37 +0000

    Add package adilsm/0.0.8/adilsm-0.0.8.tar.gz

---
+++ packages/adilsm/adilsm-0.0.8.tar.gz/adilsm-0.0.8/README.md
@@ -0,0 +1,17 @@
+# Install
+
+```bash
+pip install adilsm
+```
+
+# Description
+
+ILSM is Integrated Longitudinal Multi Source Model.
+
+# Usage
+
+```python
+import adilsm.adilsm as ilsm
+```
+
+
---
+++ packages/adilsm/adilsm-0.0.8.tar.gz/adilsm-0.0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+requirements = """
+adnmtf==0.1.164
+"""
+
+description = "Integrated Longitudinal Multi Source Model"
+
+setup(
+    name="adilsm",
+    version='0.0.8',
+    author="Mazars",
+    author_email="victor.chemla@mazars.fr",
+    description=f"{description}",
+    packages=find_packages(),
+    install_requires=requirements,
+)
