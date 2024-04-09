dc210701260f6dc889cd1893e090c57319d6ae35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 16:05:53 +0000

    Add package textual-query-sandbox/0.5.1/textual-query-sandbox-0.5.1.tar.gz

---
+++ packages/textual-query-sandbox/textual-query-sandbox-0.5.1.tar.gz/textual-query-sandbox-0.5.1/textual_query_sandbox/__init__.py
@@ -0,0 +1,13 @@
+"""A simple tool for experimenting with Textual DOM queries.."""
+
+######################################################################
+# Main app information.
+__author__ = "Dave Pearson"
+__copyright__ = "Copyright 2023, Dave Pearson"
+__credits__ = ["Dave Pearson"]
+__maintainer__ = "Dave Pearson"
+__email__ = "davep@davep.org"
+__version__ = "0.5.1"
+__licence__ = "GPLv3+"
+
+### __init__.py ends here
