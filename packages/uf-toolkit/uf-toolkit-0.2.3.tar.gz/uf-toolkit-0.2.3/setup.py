432859ceb1ced87f01709733d7af8c2947a8bee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 14:03:54 +0000

    Add package uf-toolkit/0.2.3/uf-toolkit-0.2.3.tar.gz

---
+++ packages/uf-toolkit/uf-toolkit-0.2.3.tar.gz/uf-toolkit-0.2.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+    name="uf-toolkit",
+    version="0.2.3",
+    packages=find_packages(),
+    description="A simple Union Find Class for Python.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Chas Huggins",
+    author_email="hugginsc10@gmail.com",
+    url="https://github.com/hugginsc10/uf-toolkit",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+        ],
+)
