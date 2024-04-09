52d05b95dfea839a47d6fb24aa1bc37f0d4f0f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 15:04:31 +0000

    Add package langflow-base/0.0.24/langflow_base-0.0.24.tar.gz

---
+++ packages/langflow-base/langflow_base-0.0.24.tar.gz/langflow_base-0.0.24/langflow/components/model_specs/__init__.py
@@ -0,0 +1,34 @@
+from .AmazonBedrockSpecs import AmazonBedrockComponent
+from .AnthropicLLMSpecs import ChatAntropicSpecsComponent
+
+from .AzureChatOpenAISpecs import AzureChatOpenAISpecsComponent
+from .BaiduQianfanChatEndpointsSpecs import QianfanChatEndpointComponent
+from .BaiduQianfanLLMEndpointsSpecs import QianfanLLMEndpointComponent
+from .ChatAnthropicSpecs import ChatAnthropicComponent
+from .ChatLiteLLMSpecs import ChatLiteLLMComponent
+from .ChatOllamaEndpointSpecs import ChatOllamaComponent
+from .ChatOpenAISpecs import ChatOpenAIComponent
+from .ChatVertexAISpecs import ChatVertexAIComponent
+from .CohereSpecs import CohereComponent
+from .GoogleGenerativeAISpecs import GoogleGenerativeAIComponent
+from .HuggingFaceEndpointsSpecs import HuggingFaceEndpointsComponent
+from .OllamaLLMSpecs import OllamaLLM
+from .VertexAISpecs import VertexAIComponent
+
+__all__ = [
+    "AmazonBedrockComponent",
+    "ChatAntropicSpecsComponent",
+    "AzureChatOpenAISpecsComponent",
+    "QianfanChatEndpointComponent",
+    "QianfanLLMEndpointComponent",
+    "ChatAnthropicComponent",
+    "ChatLiteLLMComponent",
+    "ChatOllamaComponent",
+    "ChatOpenAIComponent",
+    "ChatVertexAIComponent",
+    "CohereComponent",
+    "GoogleGenerativeAIComponent",
+    "HuggingFaceEndpointsComponent",
+    "OllamaLLM",
+    "VertexAIComponent",
+]
