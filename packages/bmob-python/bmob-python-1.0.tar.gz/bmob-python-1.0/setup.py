0785ef1d69ba1d226902dfab8f3cf8858c119ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 04:05:49 +0000

    Add package bmob-python/1.0/bmob-python-1.0.tar.gz

---
+++ packages/bmob-python/bmob-python-1.0.tar.gz/bmob-python-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bmob-python',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        # 任何依赖项都在这里列出
+    ],
+    author='Bmob',
+    author_email='admin@bmobapp.com',
+    description='Bmob Python SDK',
+    license='MIT',
+    keywords='Bmob Python SDK',
+    url='https://gitee.com/bmober/bmob-python'
+)
