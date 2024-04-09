5046250e91afd3edba56e324cf9e7219d21038ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-09 04:05:44 +0000

    Add package sgr-lib/0.0.2/sgr-lib-0.0.2.tar.gz

---
+++ packages/sgr-lib/sgr-lib-0.0.2.tar.gz/sgr-lib-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='sgr-lib',
+    version='0.0.2',
+    packages=['sgr_library', 'sgr_library.generated', 'sgr_library.generated.generic',
+              'sgr_library.generated.product', 'sgr_library.generated.communicator',
+              'sgr_library.generated.functional_profile', 'sgr_library.api', 'sgr_library.validators',
+              'sgr_library.converters'],
+    url='https://github.com/SGrPython',
+    license='',
+    author='daniel',
+    author_email='',
+    description=''
+)
