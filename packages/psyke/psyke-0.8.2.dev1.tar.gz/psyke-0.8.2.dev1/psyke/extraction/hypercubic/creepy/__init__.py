7e09740b67f287a240450d39df9ee61a203f4a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 14:04:30 +0000

    Add package psyke/0.8.2.dev1/psyke-0.8.2.dev1.tar.gz

---
+++ packages/psyke/psyke-0.8.2.dev1.tar.gz/psyke-0.8.2.dev1/psyke/extraction/hypercubic/creepy/__init__.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from collections import Iterable
+import numpy as np
+import pandas as pd
+from sklearn.base import ClassifierMixin
+from tuprolog.theory import Theory
+from psyke import Clustering
+from psyke.clustering import HyperCubeClustering
+from psyke.extraction.hypercubic import HyperCubeExtractor
+from psyke.utils import Target, get_default_random_seed
+
+
+class CReEPy(HyperCubeExtractor):
+    """
+    Explanator implementing CReEPy algorithm.
+    """
+
+    def __init__(self, predictor, clustering=Clustering.exact, depth: int = 3, error_threshold: float = 0.1,
+                 output: Target = Target.CONSTANT, gauss_components: int = 5, ranks: list[(str, float)] = [],
+                 ignore_threshold: float = 0.0, discretization=None, normalization=None,
+                 seed: int = get_default_random_seed()):
+        super().__init__(predictor, Target.CLASSIFICATION if isinstance(predictor, ClassifierMixin) else output,
+                         discretization, normalization)
+        self.clustering = clustering(depth, error_threshold, self._output, gauss_components, discretization,
+                                     normalization, seed)
+        self.ranks = ranks
+        self.ignore_threshold = ignore_threshold
+        self._default_surrounding_cube = True
+
+    def _extract(self, dataframe: pd.DataFrame) -> Theory:
+        if not isinstance(self.clustering, HyperCubeClustering):
+            raise TypeError("clustering must be a HyperCubeClustering")
+
+        self.clustering.fit(dataframe)
+        self._hypercubes = self.clustering.get_hypercubes()
+        for cube in self._hypercubes:
+            for dimension, relevance in self.ranks:
+                if relevance < self.ignore_threshold:
+                    cube[dimension] = [-np.inf, np.inf]
+        return self._create_theory(dataframe)
