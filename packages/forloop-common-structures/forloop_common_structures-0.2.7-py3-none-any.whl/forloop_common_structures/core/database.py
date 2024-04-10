98a2aaf89906f0094f381d729c8e9f1a2bb05e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 04:06:36 +0000

    Add package forloop-common-structures/0.2.7/forloop_common_structures-0.2.7-py3-none-any.whl

---
+++ packages/forloop-common-structures/forloop_common_structures-0.2.7-py3-none-any.whl/forloop_common_structures/core/database.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Database:
+    database_name: str
+    server: str
+    port: int
+    database: str
+    username: str
+    password: str
+    dialect: str
+    project_uid: str  # = "0"
+    uid: Optional[str] = None
