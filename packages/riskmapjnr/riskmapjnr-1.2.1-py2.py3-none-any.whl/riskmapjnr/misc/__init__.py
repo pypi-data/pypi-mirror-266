caed9f7fc594c75f84e1578c6f22eabfb97055b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 14:04:40 +0000

    Add package riskmapjnr/1.2.1/riskmapjnr-1.2.1-py2.py3-none-any.whl

---
+++ packages/riskmapjnr/riskmapjnr-1.2.1-py2.py3-none-any.whl/riskmapjnr/misc/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ===================================================================
+# author          :Ghislain Vieilledent
+# email           :ghislain.vieilledent@cirad.fr, ghislainv@gmail.com
+# web             :https://ecology.ghislainv.fr
+# python_version  :>=3
+# license         :GPLv3
+# ===================================================================
+
+from .miscellaneous import invlogit, make_dir, tree
+from .miscellaneous import makeblock, progress_bar
+from .miscellaneous import make_square, rescale
+from .countpix import countpix
+
+# EOF
