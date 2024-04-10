34a7429b1c99c2050f9565e14006d601f8292188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 14:04:37 +0000

    Add package linkedin-burak/1.0.0/linkedin-burak-1.0.0.tar.gz

---
+++ packages/linkedin-burak/linkedin-burak-1.0.0.tar.gz/linkedin-burak-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='linkedin-burak',
+    version='1.0.0',
+    packages=find_packages(),
+    description='linkedin-burak',
+    author='Burak Atak',
+    author_email='your.email@example.com',
+    install_requires=[
+    ],
+)
