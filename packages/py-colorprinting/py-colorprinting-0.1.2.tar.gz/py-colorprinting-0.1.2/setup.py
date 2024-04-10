b1a0cca88f65a4acaf25a7e49d530880746ab957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 06:05:31 +0000

    Add package py-colorprinting/0.1.2/py-colorprinting-0.1.2.tar.gz

---
+++ packages/py-colorprinting/py-colorprinting-0.1.2.tar.gz/py-colorprinting-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-colorprinting",
+    version="0.1.2",
+    packages=find_packages(),
+    license="MIT",
+    description="Print colored text in python!",
+    long_description=open('README.md').read(),
+    author="Arpaia",
+    author_email="raf@arpaiacorp.com",
+    url="",
+    install_requires=[
+        "requests>=2.25.0,<3.0.0"
+    ],
+)
