9478d8c42d4aceacf1665b6bd570bc6700ef8045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 04:06:16 +0000

    Add package fair-trees/2.3.5/fair_trees-2.3.5.tar.gz

---
+++ packages/fair-trees/fair_trees-2.3.5.tar.gz/fair_trees-2.3.5/fair_trees/__init__.py
@@ -0,0 +1,3 @@
+from fair_trees import sdp_score as sdp_score 
+from fair_trees import FairDecisionTreeClassifier as FairDecisionTreeClassifier
+from fair_trees import FairRandomForestClassifier as FairRandomForestClassifier
