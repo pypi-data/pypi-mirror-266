c2f4deddbc83e8d3d802b27824580a345dbd688d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 04:05:48 +0000

    Add package fast-dp/1.7/fast_dp-1.7.tar.gz

---
+++ packages/fast-dp/fast_dp-1.7.tar.gz/fast_dp-1.7/fast_dp/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+__author__ = """Diamond Light Source"""
+__email__ = "scientificsoftware@diamond.ac.uk"
+__version__ = "1.7"
---
+++ packages/fast-dp/fast_dp-1.7.tar.gz/fast_dp-1.7/test/test_resource_access.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+import fast_dp.output
+
+
+def test_ispyb_xml_template_is_accessible():
+    xml = fast_dp.output.get_ispyb_template()
+    assert "<AutoProcContainer>" in xml
