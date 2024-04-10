9f21849191a0fa96730d605733fa31d826e60529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 03:05:51 +0000

    Add package aiorubika/0.0.2/aiorubika-0.0.2-py3-none-any.whl

---
+++ packages/aiorubika/aiorubika-0.0.2-py3-none-any.whl/aiorubika/__init__.py
@@ -0,0 +1,7 @@
+from .sessions import SQLiteSession, StringSession
+from .client import Client
+from .rubino import Rubino
+from . import types, utils, filters, exceptions, enums
+
+__author__ = 'Shayan Heidari' # Forked by amirali irvany
+__version__ = '0.0.2'
