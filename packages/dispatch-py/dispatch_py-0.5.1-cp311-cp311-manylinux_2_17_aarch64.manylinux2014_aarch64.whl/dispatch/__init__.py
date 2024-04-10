ee3c4c9f323d0548da2914ee0e9576aa97b4672f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 14:04:33 +0000

    Add package dispatch-py/0.5.1/dispatch_py-0.5.1-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/dispatch-py/dispatch_py-0.5.1-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/dispatch/__init__.py
@@ -0,0 +1,28 @@
+"""The Dispatch SDK for Python."""
+
+from __future__ import annotations
+
+import dispatch.integrations
+from dispatch.coroutine import all, any, call, gather, race
+from dispatch.function import DEFAULT_API_URL, Client, Registry, Reset
+from dispatch.id import DispatchID
+from dispatch.proto import Call, Error, Input, Output
+from dispatch.status import Status
+
+__all__ = [
+    "Client",
+    "DispatchID",
+    "DEFAULT_API_URL",
+    "Input",
+    "Output",
+    "Call",
+    "Error",
+    "Reset",
+    "Status",
+    "call",
+    "gather",
+    "all",
+    "any",
+    "race",
+    "Registry",
+]
