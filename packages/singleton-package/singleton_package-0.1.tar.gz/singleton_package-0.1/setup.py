5e84ae21fcb55da7c0b63f939d277f5641b73a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 04:05:57 +0000

    Add package singleton-package/0.1/singleton_package-0.1.tar.gz

---
+++ packages/singleton-package/singleton_package-0.1.tar.gz/singleton_package-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="singleton_package",
+    version="0.1",
+    packages=find_packages(),
+    description="A simple Python package to create singleton objects",
+    author="Mahdi Kiani",
+    author_email="mahdikiany@gmail.com",
+    url="https://github.com/mahdikiani/singleton_package",
+)
