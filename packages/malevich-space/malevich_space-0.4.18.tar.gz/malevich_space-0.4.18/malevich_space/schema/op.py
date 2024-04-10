934ce7cfa80a45ec07acc7fca8663d6542bf5bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 03:06:18 +0000

    Add package malevich-space/0.4.18/malevich_space-0.4.18.tar.gz

---
+++ packages/malevich-space/malevich_space-0.4.18.tar.gz/malevich_space-0.4.18/malevich_space/schema/op.py
@@ -0,0 +1,41 @@
+from typing import Sequence, Optional
+
+from pydantic import BaseModel
+
+from .dep import DepSchema, LoadedDepSchema
+from .schema import LoadedSchemaSchema
+
+
+class OpArg(BaseModel):
+    arg_name: Optional[str]
+    arg_type: Optional[str]
+    arg_order: Optional[int]
+
+
+class OpSchema(BaseModel):
+    core_id: str
+    type: str
+
+    requires: Sequence[DepSchema] = []
+
+
+class LoadedOpSchema(OpSchema):
+    uid: str
+    name: Optional[str]
+    doc: Optional[str]
+    finish_msg: Optional[str]
+
+    tl: Optional[int]
+    query: Optional[str]
+    mode: Optional[str]
+
+    collections_names: Optional[list[str]]
+    extra_collections_names: Optional[list[str]]
+
+    collection_out_names: Optional[list[str]]
+
+    args: Optional[list[OpArg]]
+
+    input_schema: Sequence[LoadedSchemaSchema] = []
+    output_schema: Sequence[LoadedSchemaSchema] = []
+    loaded_requires: Sequence[LoadedDepSchema] = []
