ffbb18b6e685c9818c8967ed5314b52baade8cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 03:06:39 +0000

    Add package kitconcept.solr/1.0.0a6/kitconcept.solr-1.0.0a6-py3-none-any.whl

---
+++ packages/kitconcept.solr/kitconcept.solr-1.0.0a6-py3-none-any.whl/kitconcept/solr/__init__.py
@@ -0,0 +1,13 @@
+"""Init and utils."""
+
+from zope.i18nmessageid import MessageFactory
+
+import logging
+
+
+PACKAGE_NAME = "kitconcept.solr"
+_ = MessageFactory(PACKAGE_NAME)
+
+logger = logging.getLogger(PACKAGE_NAME)
+
+config = {}
