3f93cbc1f558ed56b5486f4fd59aed7ab53142c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 03:06:11 +0000

    Add package anjali/0.1/anjali-0.1.tar.gz

---
+++ packages/anjali/anjali-0.1.tar.gz/anjali-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="anjali",
+    version="0.1",
+    description="Python package to display content of a text file",
+    author="Your Name",
+    packages=["anjali"],
+    install_requires=["requests"],
+)
