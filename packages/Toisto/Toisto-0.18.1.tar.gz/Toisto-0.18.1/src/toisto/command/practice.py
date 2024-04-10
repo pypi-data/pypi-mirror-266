9034babd3244c2b47d9c5567a8e0b2543f25667c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 06:05:10 +0000

    Add package Toisto/0.18.1/Toisto-0.18.1.tar.gz

---
+++ packages/Toisto/Toisto-0.18.1.tar.gz/Toisto-0.18.1/src/toisto/command/practice.py
@@ -0,0 +1,69 @@
+"""Practice command."""
+
+from collections.abc import Callable
+from configparser import ConfigParser
+from typing import get_args
+
+from toisto.model.language.iana_language_subtag_registry import ALL_LANGUAGES
+from toisto.model.language.label import Label
+from toisto.model.quiz.progress import Progress
+from toisto.model.quiz.quiz import ListenQuizType, Quiz
+from toisto.persistence.progress import save_progress
+from toisto.ui.dictionary import linkify
+from toisto.ui.speech import say
+from toisto.ui.text import (
+    DONE,
+    TRY_AGAIN,
+    TRY_AGAIN_IN_ANSWER_LANGUAGE,
+    feedback_correct,
+    feedback_incorrect,
+    instruction,
+)
+
+
+def do_quiz_attempt(quiz: Quiz, config: ConfigParser, attempt: int = 1) -> Label:
+    """Present the question and get the answer from the user."""
+    while True:
+        say(quiz.question_language, quiz.question.pronounceable, config, slow=attempt > 1)
+        if answer := Label(quiz.answer_language, input("> ").strip()):
+            break
+        print("\033[F", end="")  # noqa: T201  # Move cursor one line up
+    return answer
+
+
+def evaluate_answer(quiz: Quiz, progress: Progress, answer: Label, attempt: int = 1) -> str:
+    """Evaluate the answer and return the user feedback."""
+    if quiz.is_correct(answer):
+        progress.mark_correct_answer(quiz)
+        return feedback_correct(answer, quiz)
+    if answer != Label(quiz.answer_language, "?") and attempt == 1:
+        if quiz.is_question(answer) and not quiz.is_grammatical:
+            return TRY_AGAIN_IN_ANSWER_LANGUAGE % dict(language=ALL_LANGUAGES[quiz.answer_language])
+        return TRY_AGAIN
+    progress.mark_incorrect_answer(quiz)
+    return feedback_incorrect(answer, quiz)
+
+
+def do_quiz(write_output: Callable[..., None], quiz: Quiz, progress: Progress, config: ConfigParser) -> None:
+    """Do one quiz and update the progress."""
+    write_output(instruction(quiz))
+    if quiz.quiz_types[0] not in get_args(ListenQuizType):
+        write_output(linkify(quiz.question))
+    for attempt in range(1, 3):
+        answer = do_quiz_attempt(quiz, config, attempt)
+        feedback = evaluate_answer(quiz, progress, answer, attempt)
+        write_output(feedback)
+        if quiz.is_correct(answer) or answer == Label(quiz.answer_language, "?"):
+            break
+
+
+def practice(write_output: Callable[..., None], progress: Progress, config: ConfigParser) -> None:
+    """Practice a language."""
+    try:
+        while quiz := progress.next_quiz():
+            do_quiz(write_output, quiz, progress, config)
+        write_output(DONE)
+    except (KeyboardInterrupt, EOFError):
+        write_output()  # Make sure the shell prompt is displayed on a new line
+    finally:
+        save_progress(progress)
---
+++ packages/Toisto/Toisto-0.18.1.tar.gz/Toisto-0.18.1/src/toisto/ui/text.py
@@ -0,0 +1,152 @@
+"""Output for the user."""
+
+import sys
+from collections.abc import Callable, Sequence
+from configparser import ConfigParser
+from typing import Final
+
+from rich.console import Console
+from rich.panel import Panel
+
+from ..metadata import CHANGELOG_URL, NAME, README_URL, VERSION
+from ..model.language.iana_language_subtag_registry import ALL_LANGUAGES
+from ..model.language.label import END_OF_SENTENCE_PUNCTUATION, Label
+from ..model.quiz.quiz import Quiz
+from .dictionary import DICTIONARY_URL, linkify_and_enumerate
+from .diff import colored_diff
+from .style import QUIZ, SECONDARY
+
+console = Console()
+
+LINK_KEY: Final = "⌘ (the command key)" if sys.platform == "darwin" else "Ctrl (the control key)"
+
+WELCOME: Final = f"""👋 Welcome to [underline]{NAME} [white not bold]v{VERSION}[/white not bold][/underline]!
+
+Practice as many words and phrases as you like, for as long as you like.
+
+[{SECONDARY}]{NAME} quizzes you on words and phrases repeatedly. Each time you answer
+a quiz correctly, {NAME} will wait longer before repeating it. If you
+answer incorrectly, you get one additional attempt to give the correct
+answer. If the second attempt is not correct either, {NAME} will reset
+the quiz interval.
+
+How does it work?
+● To answer a quiz: type the answer, followed by Enter.
+● To repeat the spoken text: type Enter without answer.
+● To skip to the answer immediately: type ?, followed by Enter.
+● To read more about an [link={DICTIONARY_URL}/underlined]underlined[/link] word: keep {LINK_KEY} pressed
+  while clicking the word. Not all terminals may support this.
+● To quit: type Ctrl-C or Ctrl-D.
+[/{SECONDARY}]"""
+
+NEWS: Final = (
+    f"🎉 {NAME} [white not bold]{{0}}[/white not bold] is [link={CHANGELOG_URL}]available[/link]. "
+    f"Upgrade with [code]pipx upgrade {NAME}[/code]."
+)
+
+CONFIG_LANGUAGE_TIP = (
+    "️️👉 You may want to use a configuration file to store your language preferences.\n"
+    f"See {README_URL.replace('#toisto', '#how-to-configure-toisto')}."
+)
+
+DONE: Final = f"""👍 Good job. You're done for now. Please come back later or try a different concept.
+[{SECONDARY}]Type `{NAME.lower()} -h` for more information.[/{SECONDARY}]
+"""
+
+TRY_AGAIN: Final = "⚠️  Incorrect. Please try again."
+
+TRY_AGAIN_IN_ANSWER_LANGUAGE: Final = "⚠️  Incorrect. Please try again, in [yellow][bold]%(language)s[/bold][/yellow]."
+
+CORRECT: Final = "✅ Correct.\n"
+
+INCORRECT: Final = "❌ Incorrect. "
+
+
+def feedback_correct(guess: Label, quiz: Quiz) -> str:
+    """Return the feedback about a correct result."""
+    return CORRECT + colloquial(quiz) + meaning(quiz) + other_answers(guess, quiz) + answer_notes(quiz) + examples(quiz)
+
+
+def feedback_incorrect(guess: Label, quiz: Quiz) -> str:
+    """Return the feedback about an incorrect result."""
+    if guess == Label(quiz.answer_language, "?"):
+        answers = quiz.non_generated_answers
+        label = "The correct answer is" if len(answers) == 1 else "The correct answers are"
+        feedback = f"{label} {linkify_and_enumerate(*answers)}.\n" + colloquial(quiz) + meaning(quiz)
+    else:
+        label = f'{INCORRECT}The correct answer is "{colored_diff(guess, quiz.answer)}".\n'
+        feedback = label + colloquial(quiz) + meaning(quiz) + other_answers(quiz.answer, quiz)
+    return feedback + answer_notes(quiz)
+
+
+def colloquial(quiz: Quiz) -> str:
+    """Return the feedback about colloquial label, if any."""
+    if not quiz.question.is_colloquial:
+        return ""
+    feedback = f'The colloquial {ALL_LANGUAGES[quiz.question_language]} spoken was "{quiz.question.strip("*")}".'
+    return wrap(feedback, SECONDARY)
+
+
+def meaning(quiz: Quiz) -> str:
+    """Return the quiz's meaning, if any."""
+    if quiz.question_meanings and quiz.answer_meanings:
+        question_meanings = linkify_and_enumerate(*quiz.question_meanings)
+        answer_meanings = linkify_and_enumerate(*quiz.answer_meanings)
+        meanings = f"{question_meanings}, respectively {answer_meanings}"
+    else:
+        meanings = linkify_and_enumerate(*(quiz.question_meanings + quiz.answer_meanings))
+    return wrap(f"Meaning {meanings}.", SECONDARY) if meanings else ""
+
+
+def other_answers(guess: Label, quiz: Quiz) -> str:
+    """Return the quiz's other answers, if any."""
+    if answers := quiz.other_answers(guess):
+        label = "Another correct answer is" if len(answers) == 1 else "Other correct answers are"
+        return wrap(f"{label} {linkify_and_enumerate(*answers)}.", SECONDARY)
+    return ""
+
+
+def examples(quiz: Quiz) -> str:
+    """Return the quiz's examples, if any."""
+    examples: list[Label] = []
+    language = quiz.answer_language if "write" in quiz.quiz_types else quiz.question_language
+    for example in quiz.concept.get_related_concepts("example"):
+        labels = [label.non_generated_spelling_alternatives[0] for label in example.labels(language)]
+        examples.extend(labels)
+    return bulleted_list("Example", examples)
+
+
+def answer_notes(quiz: Quiz) -> str:
+    """Return the answer notes, if any."""
+    return bulleted_list("Note", quiz.answer_notes)
+
+
+def instruction(quiz: Quiz) -> str:
+    """Return the instruction for the quiz."""
+    return wrap(f"{quiz.instruction}:", QUIZ, postfix="")
+
+
+def show_welcome(write_output: Callable[..., None], latest_version: str | None, config: ConfigParser) -> None:
+    """Show the welcome message."""
+    write_output(WELCOME)
+    if latest_version and latest_version.strip("v") > VERSION:
+        write_output(Panel(NEWS.format(latest_version), expand=False))
+        write_output()
+    elif not config.has_section("languages"):
+        write_output(Panel(CONFIG_LANGUAGE_TIP, expand=False))
+        write_output()
+
+
+def bulleted_list(label: str, items: Sequence[str], style: str = SECONDARY, bullet: str = "-") -> str:
+    """Create a bulleted list of the items."""
+    items = [item if item[-1] in END_OF_SENTENCE_PUNCTUATION else f"{item}." for item in items]
+    if len(items) == 0:
+        return ""
+    if len(items) == 1:
+        return wrap(f"{label}: {items[0]}", style)
+    return wrap(f"{label}s:\n" + "\n".join([f"{bullet} {item}" for item in items]), style)
+
+
+def wrap(text: str, style: str, postfix: str = "\n") -> str:
+    """Wrap the text with the style."""
+    return f"[{style}]{text}[/{style}]{postfix}"
