3729855729a4673ec528f3c0df4cda1218a4db88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 03:05:25 +0000

    Add package pyiron-gpl/0.0.5/pyiron-gpl-0.0.5.tar.gz

---
+++ packages/pyiron-gpl/pyiron-gpl-0.0.5.tar.gz/pyiron-gpl-0.0.5/setup.py
@@ -0,0 +1,43 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    name='pyiron-gpl',
+    version=versioneer.get_version(),
+    description='pyiron - an integrated development environment (IDE) for computational materials science.',
+    long_description='http://pyiron.org',
+
+    url='https://github.com/pyiron/pyiron_gpl',
+    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
+    author_email='janssen@mpie.de',
+    license='GPLv3',
+
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'License :: OSI Approved :: BSD License',
+                 'Intended Audience :: Science/Research',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 3.9',
+                 'Programming Language :: Python :: 3.10',
+                 'Programming Language :: Python :: 3.11',
+    ],
+
+    keywords='pyiron',
+    packages=find_packages(exclude=["*tests*", "*docs*", "*binder*", "*conda*", "*notebooks*", "*.ci_support*"]),
+    install_requires=[
+        'molmod>=1.4.8',
+        'numpy>=1.26.4',
+        'atomistics>=0.1.24',
+        'pyiron_atomistics>=0.5.0',
+        'spglib>=2.3.1',
+        'scipy>=1.12.0',
+        'quickff>=2.2.4',
+        'tamkin>=1.2.6',
+        'yaff>=1.6.0'
+    ],
+    cmdclass=versioneer.get_cmdclass(),
+
+    )
