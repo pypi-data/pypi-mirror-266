03f8a7e365f7638b985dffcc709b7d7b1fc6e14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 06:05:07 +0000

    Add package ProjectManagerSdk/107.0.3368/projectmanagersdk-107.0.3368-py3-none-any.whl

---
+++ packages/ProjectManagerSdk/projectmanagersdk-107.0.3368-py3-none-any.whl/ProjectManagerSdk/models/projecttemplatecategorydto.py
@@ -0,0 +1,44 @@
+#
+# ProjectManager API for Python
+#
+# (c) 2023-2024 ProjectManager.com, Inc.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# @author     ProjectManager.com <support@projectmanager.com>
+# @copyright  2023-2024 ProjectManager.com, Inc.
+# @link       https://github.com/projectmgr/projectmanager-sdk-python
+#
+
+
+from ProjectManagerSdk.models.projecttemplatedto import ProjectTemplateDto
+import dataclasses
+
+@dataclasses.dataclass
+class ProjectTemplateCategoryDto:
+    """
+    Information about a Category of ProjectTemplates.
+    """
+
+    id: str | None = None
+    """
+    The unique identifier of this ProjectTemplate Category.
+    """
+
+    name: str | None = None
+    """
+    A friendly name for this ProjectTemplate Category.
+    """
+
+    order: int | None = None
+    """
+    The overall order of this ProjectTemplate Category
+    """
+
+    templates: list[ProjectTemplateDto] | None = None
+    """
+    A list of templates in this category
+    """
+
+
