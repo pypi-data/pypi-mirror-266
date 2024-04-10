4d6d9fe9c4f1d5283aff4e68d88b84716db89315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 03:05:55 +0000

    Add package dynamics-utils/0.0.1/dynamics_utils-0.0.1.tar.gz

---
+++ packages/dynamics-utils/dynamics_utils-0.0.1.tar.gz/dynamics_utils-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dynamics_utils',
+    author='chrisdkolloff',
+    author_email='chrisdkolloff@gmail.com',
+    description='Utils for dealing with dynamic data (MSM, NMR, etc.)',
+    url='https://github.com/chrisdkolloff/dynamics_utils.git',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy', 'torch', 'deeptime', 'h5py'
+    ],
+    py_modules=['dynamics_utils'],
+)
