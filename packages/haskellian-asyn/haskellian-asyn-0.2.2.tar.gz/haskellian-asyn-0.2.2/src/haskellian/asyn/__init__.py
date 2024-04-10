a51c0c8aed77ac72e00a5f7b81bbc2d4eab2438c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 05:06:38 +0000

    Add package haskellian-asyn/0.2.2/haskellian-asyn-0.2.2.tar.gz

---
+++ packages/haskellian-asyn/haskellian-asyn-0.2.2.tar.gz/haskellian-asyn-0.2.2/src/haskellian/asyn/__init__.py
@@ -0,0 +1,2 @@
+from .awaitables import all, bind, of, then, ManagedAwaitable, Promise
+from .iterables import asyncify, batch, enumerate, flatten, map, concurrent_map, ManagedIterable, skip, split, syncify, take
