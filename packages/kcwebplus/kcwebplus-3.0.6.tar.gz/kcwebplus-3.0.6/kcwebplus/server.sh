babd5bd0dcc7a4106491196aef51bebc191d1918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 04:06:16 +0000

    Add package kcwebplus/3.0.6/kcwebplus-3.0.6.tar.gz

---
+++ packages/kcwebplus/kcwebplus-3.0.6.tar.gz/kcwebplus-3.0.6/kcwebplus/server.sh
@@ -0,0 +1,6 @@
+chmod -R 777 /kcwebplus
+./server stop
+nohup ./server --h 0.0.0.0 --p 39001 --w 2 start > server.log 2>&1 &
+nohup ./server --h 0.0.0.0 --p 39002 --w 2 start > server.log 2>&1 &
+nohup ./server --h 0.0.0.0 --p 39003 --w 2 start > server.log 2>&1 &
+nohup python3.8kcw_plus server.py intapp/index/pub/clistartplan --cli > server.log 2>&1 &
