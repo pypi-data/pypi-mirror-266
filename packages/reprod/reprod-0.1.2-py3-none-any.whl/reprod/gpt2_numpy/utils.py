f016129b2bf6c2cccd4428698cacbe6d8c6dfc0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 06:05:08 +0000

    Add package reprod/0.1.2/reprod-0.1.2-py3-none-any.whl

---
+++ packages/reprod/reprod-0.1.2-py3-none-any.whl/reprod/gpt2_numpy/utils.py
@@ -0,0 +1,25 @@
+from functools import cache
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+
+@cache
+def load_tokenizer():
+    tokenizer_hf = GPT2Tokenizer.from_pretrained(
+        "openai-community/gpt2", cache_dir="./.hf_cache"
+    )
+
+    return tokenizer_hf
+
+
+@cache
+def load_hf_model():
+    model_hf = GPT2LMHeadModel.from_pretrained(
+        "openai-community/gpt2", cache_dir="./.hf_cache"
+    )
+    return model_hf
+
+
+@cache
+def load_model_params():
+    model_hf = load_hf_model()
+    return {k: v.numpy() for k, v in model_hf.state_dict().items()}
