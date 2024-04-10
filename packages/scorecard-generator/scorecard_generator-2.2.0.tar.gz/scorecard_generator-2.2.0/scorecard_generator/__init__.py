c3a509d037c66e685d8340e0b2b75de339800911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 14:04:44 +0000

    Add package scorecard-generator/2.2.0/scorecard_generator-2.2.0.tar.gz

---
+++ packages/scorecard-generator/scorecard_generator-2.2.0.tar.gz/scorecard_generator-2.2.0/scorecard_generator/__init__.py
@@ -0,0 +1,6 @@
+# Import the main class from the miner module
+from .scorecard import generate_scorecard as scorecard
+
+# Optionally define an __all__ for explicitness on what is exported
+__all__ = ['scorecard']
+
