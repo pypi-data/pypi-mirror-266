2450ec4f51c8e50531ccf2a9088003558b7566f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 04:05:29 +0000

    Add package NeoNMM/2.5/NeoNMM-2.5.tar.gz

---
+++ packages/NeoNMM/NeoNMM-2.5.tar.gz/NeoNMM-2.5/setup.py
@@ -0,0 +1,18 @@
+__author__ = "Yochum Maxime"
+__copyright__ = "No Copyright"
+__credits__ = ["Yochum Maxime"]
+__email__ = "maxime.yochum@univ-rennes1.fr"
+__status__ = "Prototype"
+
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+import setuptools
+
+setup(name='NeoNMM',
+      version='2.5',
+      description='NeoNMM',
+      author='Maxime Yochum',
+      packages=setuptools.find_packages(),
+     )
