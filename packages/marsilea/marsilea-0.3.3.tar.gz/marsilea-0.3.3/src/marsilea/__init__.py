38358f28024a9396881b93e8f0fc1928022f3bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 03:05:20 +0000

    Add package marsilea/0.3.3/marsilea-0.3.3.tar.gz

---
+++ packages/marsilea/marsilea-0.3.3.tar.gz/marsilea-0.3.3/src/marsilea/__init__.py
@@ -0,0 +1,13 @@
+"""Create x-layout visualization"""
+
+__version__ = "0.3.3"
+
+import marsilea.plotter as plotter
+from ._deform import Deformation
+from .base import WhiteBoard, ClusterBoard, ZeroWidth, ZeroHeight
+from .dataset import load_data
+from .dendrogram import Dendrogram, GroupDendrogram
+from .heatmap import Heatmap, SizedHeatmap, CatHeatmap
+from .layers import Piece, Layers
+from .layout import CrossLayout, CompositeCrossLayout
+from .upset import UpsetData, Upset
