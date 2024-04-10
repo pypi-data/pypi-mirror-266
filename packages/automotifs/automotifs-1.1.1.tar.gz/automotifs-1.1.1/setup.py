3b0fae4f1cd074c7a4af8e0fc6f21103f972b7b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 04:05:13 +0000

    Add package automotifs/1.1.1/automotifs-1.1.1.tar.gz

---
+++ packages/automotifs/automotifs-1.1.1.tar.gz/automotifs-1.1.1/setup.py
@@ -0,0 +1,21 @@
+"""This module sets up the package for distribution."""
+from setuptools import setup, find_packages
+setup(
+    name='automotifs',
+    version='1.1.1',
+    packages=find_packages(),
+    description='A wrapper for automatic Motif Detection',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Giorgio Micaletto',
+    author_email='giorgio.micaletto@studbocconi.it',
+    url='https://github.com/GiorgioMB/auto_dotmotif/',
+    install_requires=[
+        'pandas>=1.1.5',
+        'pylint>=2.6.0',
+        'numpy>=1.23',
+        'dotmotif>=0.14.0',
+        'networkx>=3.2.1'
+    ],
+    python_requires='>=3.6',
+)
