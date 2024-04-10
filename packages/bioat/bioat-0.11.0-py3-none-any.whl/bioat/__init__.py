d81ec3c835d1996fc9194526b7b231e80bbe3f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 05:05:51 +0000

    Add package bioat/0.11.0/bioat-0.11.0-py3-none-any.whl

---
+++ packages/bioat/bioat-0.11.0-py3-none-any.whl/bioat/__init__.py
@@ -0,0 +1,19 @@
+"""Doc of bioat.
+
+"""
+from bioat.exceptions import *  # must be the first
+from bioat.logger import get_logger  # must be the second
+from bioat.about import about
+from bioat.bamtools import BamTools
+from bioat.bedtools import BedTools
+from bioat.crisprtools import CrisprTools
+from bioat.fastxtools import FastxTools
+from bioat.hictools import HiCTools
+from bioat.metatools import MetaTools
+from bioat.searchtools import SearchTools
+from bioat.systemtools import SystemTools
+from bioat.tabletools import TableTools
+from bioat.target_seq import TargetSeq
+from bioat.version import (__version__, __author__, __doc_format__)
+
+__name__ = "bioat"
