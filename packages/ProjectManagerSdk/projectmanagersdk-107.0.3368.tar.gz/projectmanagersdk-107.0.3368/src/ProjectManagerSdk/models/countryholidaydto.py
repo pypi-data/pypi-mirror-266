fb1da50c261991c2c5071fb263adc503b5c37cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 06:05:07 +0000

    Add package ProjectManagerSdk/107.0.3368/projectmanagersdk-107.0.3368.tar.gz

---
+++ packages/ProjectManagerSdk/projectmanagersdk-107.0.3368.tar.gz/projectmanagersdk-107.0.3368/src/ProjectManagerSdk/models/countryholidaydto.py
@@ -0,0 +1,38 @@
+#
+# ProjectManager API for Python
+#
+# (c) 2023-2024 ProjectManager.com, Inc.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# @author     ProjectManager.com <support@projectmanager.com>
+# @copyright  2023-2024 ProjectManager.com, Inc.
+# @link       https://github.com/projectmgr/projectmanager-sdk-python
+#
+
+
+import dataclasses
+
+@dataclasses.dataclass
+class CountryHolidayDto:
+    """
+    Country holiday entry
+    """
+
+    id: str | None = None
+    """
+    Holiday id
+    """
+
+    date: str | None = None
+    """
+    Date of holiday
+    """
+
+    countryId: int | None = None
+    """
+    Country id holiday associated to
+    """
+
+
---
+++ packages/ProjectManagerSdk/projectmanagersdk-107.0.3368.tar.gz/projectmanagersdk-107.0.3368/src/ProjectManagerSdk/models/discussionemoji.py
@@ -0,0 +1,33 @@
+#
+# ProjectManager API for Python
+#
+# (c) 2023-2024 ProjectManager.com, Inc.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# @author     ProjectManager.com <support@projectmanager.com>
+# @copyright  2023-2024 ProjectManager.com, Inc.
+# @link       https://github.com/projectmgr/projectmanager-sdk-python
+#
+
+
+import dataclasses
+
+@dataclasses.dataclass
+class DiscussionEmoji:
+    """
+    A reaction to a specific comment within a discussion thread.
+    """
+
+    name: str | None = None
+    """
+    The name of the emoji
+    """
+
+    userIds: list[str] | None = None
+    """
+    The list of user IDs of the users who tagged these emoji
+    """
+
+
