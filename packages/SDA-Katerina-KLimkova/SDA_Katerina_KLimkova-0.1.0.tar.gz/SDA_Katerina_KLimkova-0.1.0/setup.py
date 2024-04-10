e91360326d9658bd80b5da730f1372de4a3f69cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 05:08:32 +0000

    Add package SDA-Katerina-KLimkova/0.1.0/SDA_Katerina_KLimkova-0.1.0.tar.gz

---
+++ packages/SDA-Katerina-KLimkova/SDA_Katerina_KLimkova-0.1.0.tar.gz/SDA_Katerina_KLimkova-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="SDA_Katerina_KLimkova",
+    version="0.1.0",
+    description="This is my first library",
+    author="Kateřina Klimková",
+    author_email="katerina.klimkova@email.cz"
+)
+
