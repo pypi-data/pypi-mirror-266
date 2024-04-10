235ecdcc1fd457490d3001aaf3b2bd7ef9c9b7e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 03:05:48 +0000

    Add package sanic-prometheus-mon/0.3.9/sanic-prometheus-mon-0.3.9.tar.gz

---
+++ packages/sanic-prometheus-mon/sanic-prometheus-mon-0.3.9.tar.gz/sanic-prometheus-mon-0.3.9/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+
+setup(
+    name='sanic-prometheus-mon',
+    version='0.3.9',
+    description='Exposes Prometheus monitoring metrics of Sanic apps.',
+    url='https://github.com/valerylisay/sanic-prometheus-mon',
+    author='Dan Kruchinin',
+    author_email='dan.kruchinin@gmail.com',
+    maintainer='Valeriy Lisay',
+    maintainer_email='valery@lisay.ru',
+    license='MIT',
+    packages=['sanic_prometheus'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'sanic>=18.12',
+        'prometheus-client~=0.11.0',
+        'psutil~=5.8.0',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Topic :: System :: Monitoring',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='python sanic prometheus monitoring metrics'
+)
