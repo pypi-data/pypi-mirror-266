e59526994927062cb241b986452552c887345f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 06:05:15 +0000

    Add package quacc/0.7.3/quacc-0.7.3.tar.gz

---
+++ packages/quacc/quacc-0.7.3.tar.gz/quacc-0.7.3/src/quacc/wflow_tools/db.py
@@ -0,0 +1,38 @@
+"""Utility functions for interfacing with databases."""
+
+from __future__ import annotations
+
+import uuid
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any
+
+    from maggma.core import Store
+
+
+def results_to_db(store: Store, results: dict[str, Any] | list[dict]) -> None:
+    """
+    Store the results of a quacc recipe in a user-specified Maggma Store. A UUID will be
+    generated for each entry.
+
+    Parameters
+    ----------
+    store
+        The Maggma Store object to store the results in
+    results
+        The output summary dictionary or list of dictionaries from a quacc
+        recipe
+
+    Returns
+    -------
+    None
+    """
+    if not isinstance(results, list):
+        results = [results]
+
+    for result in results:
+        result["uuid"] = str(uuid.uuid4())
+
+    with store:
+        store.update(results, key="uuid")
