adb325f64aae7066619a3e37c7e2c7d961edc2f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 03:05:35 +0000

    Add package memium/0.16.1/memium-0.16.1.tar.gz

---
+++ packages/memium/memium-0.16.1.tar.gz/memium-0.16.1/memium/source/prompts/prompt.py
@@ -0,0 +1,27 @@
+from collections.abc import Sequence
+from typing import Protocol, runtime_checkable
+
+
+@runtime_checkable
+class BasePrompt(Protocol):
+    @property
+    def scheduling_uid(self) -> int:
+        """UID used when scheduling the prompt. If this UID changes, the old prompt is deleted and a new prompt is created."""
+        ...
+
+    @property
+    def update_uid(self) -> int:
+        """The UID used to determine whether the prompt state has changed. If this UID changes, the prompt is updated on the remote."""
+        ...
+
+    @property
+    def tags(self) -> Sequence[str]: ...
+
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class DestinationPrompt:
+    prompt: BasePrompt
+    destination_id: str
