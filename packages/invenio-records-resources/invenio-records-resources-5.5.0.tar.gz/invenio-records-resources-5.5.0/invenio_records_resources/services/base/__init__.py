bd639dd2869f10345031dae5b5cca2dfefc2f7e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 03:05:33 +0000

    Add package invenio-records-resources/5.5.0/invenio-records-resources-5.5.0.tar.gz

---
+++ packages/invenio-records-resources/invenio-records-resources-5.5.0.tar.gz/invenio-records-resources-5.5.0/invenio_records_resources/services/base/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2023 CERN.
+# Copyright (C) 2020 Northwestern University.
+#
+# Invenio-Records-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Base Service API."""
+
+from .config import ServiceConfig
+from .links import ConditionalLink, Link, LinksTemplate
+from .results import ServiceItemResult, ServiceListResult
+from .service import Service
+
+__all__ = (
+    "ConditionalLink",
+    "Link",
+    "LinksTemplate",
+    "Service",
+    "ServiceConfig",
+    "ServiceItemResult",
+    "ServiceListResult",
+)
